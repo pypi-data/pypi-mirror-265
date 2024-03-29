7182584b8610303689b3e7db57a731b2340741e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-29 15:03:13 +0000

    Add package commits2pdf/0.0.2/commits2pdf-0.0.2.tar.gz

---
+++ packages/commits2pdf/commits2pdf-0.0.2.tar.gz/commits2pdf-0.0.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open("README.md") as f:
+    LONG_DESCRIPTION = f.read()
+
+setup(
+    name="commits2pdf",
+    version="0.0.2",
+    author="Tomas Vana",
+    url="https://github.com/tomasvana/commits2pdf",
+    description="View commit history through a PDF",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    license="MIT",
+    install_requires=[
+        "GitPython"
+    ],
+    classifiers=[
+        "Topic :: Multimedia"
+    ],
+    platforms="any",
+    entry_points={
+        "console_scripts": [
+            "commits2pdf = commits2pdf.cli:main"
+        ],
+    }
+)
