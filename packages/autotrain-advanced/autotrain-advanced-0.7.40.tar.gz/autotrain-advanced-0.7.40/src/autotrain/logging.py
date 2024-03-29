bc33dd7fa42ef2906031e86461f80d3468b1253d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-29 15:03:16 +0000

    Add package autotrain-advanced/0.7.40/autotrain-advanced-0.7.40.tar.gz

---
+++ packages/autotrain-advanced/autotrain-advanced-0.7.40.tar.gz/autotrain-advanced-0.7.40/src/autotrain/logging.py
@@ -0,0 +1,43 @@
+import sys
+
+from accelerate.state import PartialState
+from loguru import logger
+
+
+log_file_path = "/tmp/app.log"
+
+emojis = {
+    "TRACE": "🔍",
+    "DEBUG": "🐛",
+    "INFO": "🚀",
+    "SUCCESS": "✅",
+    "WARNING": "⚠️",
+    "ERROR": "❌",
+    "CRITICAL": "🚨",
+}
+
+
+def should_log(record):
+    return PartialState().is_main_process
+
+
+def emoji_filter(record):
+    level = record["level"].name
+    record["level_emoji"] = emojis.get(level, "") + " " + level
+    return True
+
+
+log_format = (
+    "<level>{level_emoji: <8}</level> | "
+    "<green>{time:YYYY-MM-DD HH:mm:ss}</green> | "
+    "<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - "
+    "<level>{message}</level>"
+)
+
+logger.remove()
+if not hasattr(logger, "_is_customized") or not logger._is_customized:
+    logger.add(sys.stderr, format=log_format, filter=lambda x: should_log(x) and emoji_filter(x))
+    logger.add(log_file_path, format=log_format, filter=lambda x: should_log(x) and emoji_filter(x), rotation="10 MB")
+    logger._is_customized = True
+
+custom_logger = logger
