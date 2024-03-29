d96dc322b7257ba62d3c6ac0c368fbd9b6b94d87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-29 14:02:49 +0000

    Add package lyrebird-bugit/1.16.1/lyrebird-bugit-1.16.1.tar.gz

---
+++ packages/lyrebird-bugit/lyrebird-bugit-1.16.1.tar.gz/lyrebird-bugit-1.16.1/setup.py
@@ -0,0 +1,36 @@
+import os
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='lyrebird-bugit',
+    version='1.16.1',
+    packages=['lyrebird_bugit'],
+    url='https://github.com/Meituan-Dianping/lyrebird-bugit',
+    author='HBQA',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=(
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: MacOS",
+    ),
+    entry_points={
+        'lyrebird_plugin': [
+            'bugit = lyrebird_bugit.manifest'
+        ]
+    },
+    install_requires=[
+        "lyrebird>=2.15.2"
+    ])
