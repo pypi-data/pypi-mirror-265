21bc0fd8f4fc14d57003ddb6163938b0c4c6b44f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-29 14:03:33 +0000

    Add package vietthan/0.1.2/vietthan-0.1.2.tar.gz

---
+++ packages/vietthan/vietthan-0.1.2.tar.gz/vietthan-0.1.2/README.md
@@ -0,0 +1,1 @@
+# VietThan PyPi library
---
+++ packages/vietthan/vietthan-0.1.2.tar.gz/vietthan-0.1.2/src/vietthan/__init__.py
@@ -0,0 +1,13 @@
+"""a test pypi module by VietThan"""
+
+
+def name() -> str:
+    """
+    Returns "vietthan"
+
+    Returns
+    -------
+    str
+        "vietthan"
+    """
+    return "vietthan"
