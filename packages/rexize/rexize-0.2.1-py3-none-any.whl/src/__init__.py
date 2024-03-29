561360d6d79d871bf6cce427130c9b6d1c374e24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-29 14:03:11 +0000

    Add package rexize/0.2.1/rexize-0.2.1-py3-none-any.whl

---
+++ packages/rexize/rexize-0.2.1-py3-none-any.whl/src/__init__.py
@@ -0,0 +1,5 @@
+# flake8: noqa
+# from cli import CLI
+# from file_iterator import FileIterator
+# from image_manipulator import ImageFormat, ImageManipulator, ImageSizeUnit
+# from main import main
