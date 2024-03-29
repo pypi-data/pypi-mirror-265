586322d8b63c427036adbacd6e764527a2e13407
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-29 14:02:51 +0000

    Add package crypto-checker/1.0.2/crypto_checker-1.0.2.tar.gz

---
+++ packages/crypto-checker/crypto_checker-1.0.2.tar.gz/crypto_checker-1.0.2/README.md
@@ -0,0 +1,15 @@
+# crypto_checker
+
+A simple crypto price checker using CoinGecko API
+
+
+# Example
+
+
+>>> from crypto_checker.checker import CryptoChecker
+>>>
+>>> cc = CryptoChecker()
+>>>
+>>> cc.get_price('bitcoin')
+>>>
+>>
---
+++ packages/crypto-checker/crypto_checker-1.0.2.tar.gz/crypto_checker-1.0.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='crypto_checker',
+    version='1.0.2',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+    ],
+    author='0xbichain',
+    author_email='author@mahaka.net',
+    description='A simple crypto price checker using CoinGecko API',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/0xbichain/crypto_checker',
+    project_urls={
+        'Source': 'https://github.com/0xbichain/crypto_checker',
+    },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    python_requires='>=3.6',
+)
