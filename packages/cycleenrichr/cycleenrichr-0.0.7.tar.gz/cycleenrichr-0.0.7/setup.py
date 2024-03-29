5a620d0d75f02f7342e7245d16473314930c346d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-29 14:03:04 +0000

    Add package cycleenrichr/0.0.7/cycleenrichr-0.0.7.tar.gz

---
+++ packages/cycleenrichr/cycleenrichr-0.0.7.tar.gz/cycleenrichr-0.0.7/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cycleenrichr",
+    version="0.0.7",
+    author="Alexander Lachmann",
+    author_email="alexander.lachmann@mssm.edu",
+    description="Cycleenrichr uses PrismEXP predictions to calculate enrichment of gene sets that do not have gene annotations.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/maayanlab/cycleenrichr",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+            'h5py>=3.7.0',
+            'numpy>=1.23.3',
+            'pandas>=1.5.0',
+            'scipy>=1.12.0',
+            'setuptools>=67.8.0',
+            'setuptools>=58.0.4',
+            'statsmodels>=0.14.0',
+            'tqdm>=4.64.1'
+        ],
+    python_requires='>=3.7',
+    package_data={
+        "cycleenrichr": ["data/*"]
+    },
+    include_package_data=True,
+)
