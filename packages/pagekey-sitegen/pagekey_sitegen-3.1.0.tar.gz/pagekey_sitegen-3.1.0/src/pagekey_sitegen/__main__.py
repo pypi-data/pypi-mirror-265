b775243dc22f3d38c7fc149ef8403bc0505de6b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-29 14:02:52 +0000

    Add package pagekey-sitegen/3.1.0/pagekey_sitegen-3.1.0.tar.gz

---
+++ packages/pagekey-sitegen/pagekey_sitegen-3.1.0.tar.gz/pagekey_sitegen-3.1.0/src/pagekey_sitegen/__main__.py
@@ -0,0 +1,5 @@
+from pagekey_sitegen.cli import main
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/pagekey-sitegen/pagekey_sitegen-3.1.0.tar.gz/pagekey_sitegen-3.1.0/src/pagekey_sitegen/templates/next/src/lib/Layout.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Layout({ children }: { children: JSX.Element }) {
+    return (
+        <>
+            {children}
+        </>
+    )
+}
