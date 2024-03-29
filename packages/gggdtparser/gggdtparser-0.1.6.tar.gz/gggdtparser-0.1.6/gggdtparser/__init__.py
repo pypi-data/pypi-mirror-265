4c6047e54ca494ceaa30584bfb13794f1e4a54ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-29 14:03:07 +0000

    Add package gggdtparser/0.1.6/gggdtparser-0.1.6.tar.gz

---
+++ packages/gggdtparser/gggdtparser-0.1.6.tar.gz/gggdtparser-0.1.6/gggdtparser/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+# author: kusen
+# email: 1194542196@qq.com
+# date: 2023/3/27
+
+
+from .dtparser import parse
+from .dtparser import check
+from .dtparser import parse_by_format
+from .dtparser import parse_by_regex
+from .dtframe import parse as parse_frame
