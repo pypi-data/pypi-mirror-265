3e070c25ff8519eb04832632e3ac5299b9868e2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-29 15:02:58 +0000

    Add package wtu-mlflow-triton-plugin/0.0.2/wtu-mlflow-triton-plugin-0.0.2.tar.gz

---
+++ packages/wtu-mlflow-triton-plugin/wtu-mlflow-triton-plugin-0.0.2.tar.gz/wtu-mlflow-triton-plugin-0.0.2/wtu_mlflow_triton_plugin/model_config.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass, field
+from typing import List
+
+
+@dataclass
+class InputOutput:
+    name: str
+    datatype: str
+    shape: List[int]
+
+
+@dataclass
+class ModelConfig:
+    name: str
+    versions: List[str]
+    platform: str
+    inputs: List[InputOutput] = field(default_factory=list)
+    outputs: List[InputOutput] = field(default_factory=list)
