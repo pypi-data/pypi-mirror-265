c080dee153693756dd6b43c662c2cf9db46297fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-29 15:03:27 +0000

    Add package pakka/0.0.0/pakka-0.0.0.tar.gz

---
+++ packages/pakka/pakka-0.0.0.tar.gz/pakka-0.0.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+# with open("./README.md", "r") as f:
+#    long_desc = f.read()
+
+setup(
+    name="pakka",
+    version="0.0.0",
+    description="",
+    packages=[],
+    long_description="",
+    long_description_content_type="text/markdown",
+    url="https://github.com/timothyckl/iota",
+    author="timothyckl",
+    author_email="timothy.ckl@outlook.com",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 1 - Planning",
+    ],
+    install_requires=[],
+    extras_require={},
+    python_requires=">=3.6",
+)
