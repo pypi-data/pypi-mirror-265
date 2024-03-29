6aad18ea89dc00b8b1bbc077b5a4a0974d1fd83a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-29 14:02:50 +0000

    Add package regdiffusion/0.0.5/regdiffusion-0.0.5.tar.gz

---
+++ packages/regdiffusion/regdiffusion-0.0.5.tar.gz/regdiffusion-0.0.5/README.md
@@ -0,0 +1,23 @@
+# RegDiffusion: Probabilistic Diffusion-Based Neural Inference of Gene Regulatory Networks
+
+Diffusion model has been widely used in generative AI, especially in the vision domain. In our paper, we proposed RegDiffusion, a diffusion based model for GRN inference. Compared with previous model, RegDiffusion completes inference within a fraction of time and yield better benchmarking results.
+
+```
+From Noise to Knowledge: Probabilistic Diffusion-Based Neural Inference of Gene Regulatory Networks
+Hao Zhu, Donna K. Slonim
+bioRxiv 2023.11.05.565675; doi: https://doi.org/10.1101/2023.11.05.565675
+```
+
+
+## Installation
+
+RegDiffusion is on pypi.
+
+```
+pip install regdiffusion
+```
+
+
+
+Check out the [this tutorial](https://tuftsbcb.github.io/RegDiffusion/quick_tour.html) for a quick tour of how to use RegDiffusion for your research!
+
---
+++ packages/regdiffusion/regdiffusion-0.0.5.tar.gz/regdiffusion-0.0.5/docs/conf.py
@@ -0,0 +1,48 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import sys, os
+sys.path.insert(0, os.path.abspath('..'))
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'regdiffusion'
+copyright = '2024, Hao Zhu, Donna Slonim'
+author = 'Hao Zhu, Donna Slonim'
+release = '0.0.5'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',
+    'sphinx_copybutton',
+    # "sphinx_panels",
+    'myst_parser'
+]
+html_extra_path = ['supplements']
+
+copybutton_prompt_text = ">>> "
+
+autosummary_generate = True
+numpydoc_show_class_members = False
+
+source_suffix = ['.rst', '.md']
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '.ipynb_checkpoints', '__pycache__/']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_book_theme'
+html_static_path = ['_static']
+html_logo = "_static/regdiffusion.png"
