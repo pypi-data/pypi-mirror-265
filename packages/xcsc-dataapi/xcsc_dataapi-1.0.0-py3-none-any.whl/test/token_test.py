65b990682ba5fcdee83ed64231366fb588a47d75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-29 15:03:21 +0000

    Add package xcsc-dataapi/1.0.0/xcsc_dataapi-1.0.0-py3-none-any.whl

---
+++ packages/xcsc-dataapi/xcsc_dataapi-1.0.0-py3-none-any.whl/test/token_test.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+"""
+Created on 2024/3/28
+@author: pei jian
+"""
+from xcsc_dataapi import get_token
+
+if __name__ == "__main__":
+    token = get_token('06799', 'bm#^@d%6h$$z', 'e6jr3v93rma9q80h', 'bqzn8cermye8s566')
