066607e2266c105684fec076f75aa65e958e8e75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-29 15:03:45 +0000

    Add package ubo-gui/0.10.4/ubo_gui-0.10.4-py3-none-any.whl

---
+++ packages/ubo-gui/ubo_gui-0.10.4-py3-none-any.whl/ubo_gui/notification/__init__.py
@@ -0,0 +1,84 @@
+"""Notification widget."""
+
+from __future__ import annotations
+
+import pathlib
+import warnings
+from typing import TYPE_CHECKING, Callable
+
+from immutable import Immutable
+from kivy.lang.builder import Builder
+from kivy.metrics import dp
+from kivy.properties import ColorProperty, ObjectProperty, StringProperty
+
+from ubo_gui.constants import DANGER_COLOR
+from ubo_gui.menu.types import ActionItem
+from ubo_gui.page import PAGE_MAX_ITEMS, PageWidget
+
+if TYPE_CHECKING:
+    from ubo_gui.menu.types import Item
+
+
+class NotificationAction(Immutable):
+    """A notification action."""
+
+    title: str
+    callback: Callable
+
+
+class NotificationWidget(PageWidget):
+    """renders a notification."""
+
+    __events__ = ('on_dismiss',)
+
+    notification = ObjectProperty()
+    notification_title: str = StringProperty()
+    content: str = StringProperty()
+    icon: str = StringProperty()
+    color = ColorProperty()
+
+    def __init__(
+        self: NotificationWidget,
+        *args: object,
+        **kwargs: object,
+    ) -> None:
+        """Create a new `NotificationWidget` object."""
+        super().__init__(
+            *args,
+            items=[
+                ActionItem(
+                    icon='󰆴',
+                    action=lambda: self.dispatch('on_dismiss') and None,
+                    label='',
+                    is_short=True,
+                    background_color=DANGER_COLOR,
+                ),
+            ],
+            **kwargs,
+        )
+
+    def go_down(self: NotificationWidget) -> None:
+        """Scroll down the notification list."""
+        self.ids.slider.animated_value -= dp(50)
+
+    def go_up(self: NotificationWidget) -> None:
+        """Scroll up the notification list."""
+        self.ids.slider.animated_value += dp(50)
+
+    def get_item(self: NotificationWidget, index: int) -> Item | None:
+        """Get the page item at the given index."""
+        if index != PAGE_MAX_ITEMS - 1:
+            warnings.warn('index must be 2', ResourceWarning, stacklevel=1)
+            return None
+        return self.items[index - 2]
+
+    def on_dismiss(self: PageWidget) -> None:
+        """Signal when the notification is dismissed."""
+
+
+Builder.load_file(
+    pathlib.Path(__file__)
+    .parent.joinpath('notification_widget.kv')
+    .resolve()
+    .as_posix(),
+)
