a32fcb8a05b1e62bea2461db7d9e3612c9869dbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-29 15:03:52 +0000

    Add package odoo14-addon-mrp-bom-attribute-match-duplicate-kit/14.0.1.1.2.dev1/odoo14_addon_mrp_bom_attribute_match_duplicate_kit-14.0.1.1.2.dev1-py3-none-any.whl

---
+++ packages/odoo14-addon-mrp-bom-attribute-match-duplicate-kit/odoo14_addon_mrp_bom_attribute_match_duplicate_kit-14.0.1.1.2.dev1-py3-none-any.whl/odoo/addons/mrp_bom_attribute_match_duplicate_kit/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright (C) 2023 Cetmix OÜ
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "BOM Attribute Match Duplicate Kit",
+    "version": "14.0.1.1.1",
+    "category": "Manufacturing",
+    "author": "Cetmix, Ooops, Odoo Community Association (OCA)",
+    "summary": "BOM Attribute Match Duplicate Kit",
+    "maintainers": ["aleuffre"],
+    "depends": ["mrp_bom_attribute_match"],
+    "license": "AGPL-3",
+    "website": "https://github.com/OCA/manufacture",
+    "data": [
+        "security/security.xml",
+        "security/ir.model.access.csv",
+        "views/product_template_views.xml",
+        "wizard/product_template_kit_wizard.xml",
+    ],
+    "demo": [
+        "demo/res_users_demo.xml",
+    ],
+}
