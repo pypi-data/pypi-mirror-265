497b3b52db497345576bb636299a64179ffab8d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-29 14:03:22 +0000

    Add package jsonsql/1.0.1/jsonsql-1.0.1.tar.gz

---
+++ packages/jsonsql/jsonsql-1.0.1.tar.gz/jsonsql-1.0.1/tests/test.py
@@ -0,0 +1,49 @@
+from jsonsql import JsonSQL
+
+if __name__ == "__main__":
+    request = {
+        "query": "SELECT",
+        "items": ["*"],
+        "table": "images",
+        "connection": "WHERE",
+        "logic": {
+            "AND": [
+                {"creature": {"=": "owlbear"}},
+                {"OR": [
+                    {"userID": {"<=": 555}},
+                    {"userID": {"=": 111}},
+                    {"AND": [
+                        {"userID": {"=": 1111}},
+                        {"imageID": {"=": "imageIDString"}}
+                    ]},
+                    {"userID": {"BETWEEN": [1000, 2000]}}
+                ]},
+            ]}
+    }
+
+    allowed_queries = [
+        "SELECT"
+    ]
+
+    allowed_items = [
+        "*"
+    ]
+
+    allowed_connections = [
+        "WHERE"
+    ]
+
+    allowed_tables = [
+        "images"
+    ]
+
+    allowed_columns = {
+        "creature": str,
+        "userID": int,
+        "imageID":str
+    }
+
+    jsonsql_ = JsonSQL(allowed_queries, allowed_items,
+                       allowed_tables, allowed_connections, allowed_columns)
+
+    print(jsonsql_.sql_parse(request))
