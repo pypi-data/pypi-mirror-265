e4b69bed0b0ad61564b8205b128a01353cd44fa2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-29 14:03:21 +0000

    Add package peth/1.0.4/peth-1.0.4.tar.gz

---
+++ packages/peth/peth-1.0.4.tar.gz/peth-1.0.4/peth/core/config.py
@@ -0,0 +1,39 @@
+import json
+import os
+
+BASE_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+
+if os.path.exists("config.json"):
+    # If we found config file, set current dir as workspace.
+    CHAIN_CONFIG_PATH = "config.json"
+    SIG_DB_PATH = "4byte.json"
+    OUTPUT_PATH = "output"
+else:
+    # Default path.
+    CHAIN_CONFIG_PATH = os.path.join(BASE_PATH, "config.json")
+    SIG_DB_PATH = os.path.join(BASE_PATH, "4byte.json")
+    OUTPUT_PATH = os.path.expanduser("~/.peth")
+
+SIG_DB_URL = "https://raw.githubusercontent.com/ethereum/go-ethereum/master/signer/fourbyte/4byte.json"
+
+CACHE_PATH = os.path.join(OUTPUT_PATH, "contract_info_cache")
+DIFF_PATH = os.path.join(OUTPUT_PATH, "diff")
+DIFF_TMP_FILE = os.path.join(OUTPUT_PATH, "diff", "tmp.sol")
+REPORT_PATH = os.path.join(OUTPUT_PATH, "report")
+SOURCE_PATH = os.path.join(OUTPUT_PATH, "sources")
+
+SCAN_API_INTERVAL = 6
+
+user_config = json.load(open(CHAIN_CONFIG_PATH))
+chain_config = user_config["chains"]
+contracts_config = user_config["contracts"]
+
+DIFF_MIN_SIMILARITY = 0.5
+
+ENABLE_SLITHER = False
+
+
+def print_config():
+    for key, value in globals().items():
+        if key.isupper():
+            print("%s = %s" % (key, repr(value)))
---
+++ packages/peth/peth-1.0.4.tar.gz/peth-1.0.4/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+requires = open("requirements.txt").read().splitlines()
+requires = filter(lambda x: not x.startswith("#"), requires)
+
+setup(
+    name="peth",
+    description="An all-in-one Ethereum SDK and command-line tool, written in Python.",
+    url="https://github.com/lmy375/peth",
+    author="Moon",
+    version="1.0.4",
+    packages=find_packages(exclude=["tests", "peth/4byte.json"]),
+    python_requires=">=3.8",
+    install_requires=requires,
+    license="AGPL-3.0",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    entry_points={
+        "console_scripts": [
+            "peth = peth.__main__:main",
+            "tx-expl-server = peth.tools.txexpl.server.server:main",
+        ]
+    },
+)
