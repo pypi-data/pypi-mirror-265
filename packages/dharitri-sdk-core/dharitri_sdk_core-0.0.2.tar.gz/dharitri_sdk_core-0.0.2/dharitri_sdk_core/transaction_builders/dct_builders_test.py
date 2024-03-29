5be6d01317da8ee566f938afc9b0221a048d4a38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-29 15:03:10 +0000

    Add package dharitri-sdk-core/0.0.2/dharitri_sdk_core-0.0.2.tar.gz

---
+++ packages/dharitri-sdk-core/dharitri_sdk_core-0.0.2.tar.gz/dharitri_sdk_core-0.0.2/dharitri_sdk_core/transaction_builders/dct_builders_test.py
@@ -0,0 +1,34 @@
+
+from dharitri_sdk_core.address import Address
+from dharitri_sdk_core.transaction_builders.default_configuration import \
+    DefaultTransactionBuildersConfiguration
+from dharitri_sdk_core.transaction_builders.dct_builders import \
+    DCTIssueBuilder
+
+dummyConfig = DefaultTransactionBuildersConfiguration(chain_id="D")
+
+
+def test_dct_issue_builder():
+    issuer = Address.from_bech32("moa1qyu5wthldzr8wx5c9ucg8kjagg0jfs53s8nr3zpz3hypefsdd8ssfq94h8")
+
+    builder = DCTIssueBuilder(
+        config=dummyConfig,
+        issuer=issuer,
+        token_name="FOO",
+        token_ticker="FOO",
+        initial_supply=1000000000000,
+        num_decimals=8,
+        can_freeze=True,
+        can_mint=True,
+        can_upgrade=True
+    )
+
+    payload = builder.build_payload()
+    tx = builder.build()
+
+    assert payload.data == b"issue@464f4f@464f4f@e8d4a51000@08@63616e467265657a65@74727565@63616e4d696e74@74727565@63616e55706772616465@74727565"
+    assert tx.chainID == "D"
+    assert tx.sender == issuer
+    assert tx.receiver.bech32() == "moa1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqzllls29jpxv"
+    assert tx.gas_limit == 50000 + payload.length() * 1500 + 60000000
+    assert tx.data.encoded() == payload.encoded()
