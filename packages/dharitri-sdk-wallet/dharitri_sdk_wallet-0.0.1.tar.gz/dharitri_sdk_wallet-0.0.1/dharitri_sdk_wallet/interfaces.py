c5cd7a2bee1bd8733a2b5e89b44cfe1e700ea4b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-29 15:03:15 +0000

    Add package dharitri-sdk-wallet/0.0.1/dharitri_sdk_wallet-0.0.1.tar.gz

---
+++ packages/dharitri-sdk-wallet/dharitri_sdk_wallet-0.0.1.tar.gz/dharitri_sdk_wallet-0.0.1/dharitri_sdk_wallet/interfaces.py
@@ -0,0 +1,24 @@
+
+from typing import Protocol
+
+ISignature = bytes
+
+
+class ISignable(Protocol):
+    def serialize_for_signing(self) -> bytes: ...
+
+
+class IVerifiable(Protocol):
+    signature: ISignature
+
+    def serialize_for_signing(self) -> bytes: ...
+
+
+class IRandomness(Protocol):
+    salt: bytes
+    iv: bytes
+    id: str
+
+
+class IAddress(Protocol):
+    def bech32(self) -> str: ...
