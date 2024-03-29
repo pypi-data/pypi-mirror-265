3101b5c657689b5587a2417af1f594674b03b39f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-29 15:03:14 +0000

    Add package wfp-veda/0.1.0/wfp-veda-0.1.0.tar.gz

---
+++ packages/wfp-veda/wfp-veda-0.1.0.tar.gz/wfp-veda-0.1.0/setup.py
@@ -0,0 +1,12 @@
+import setuptools 
+import pathlib
+
+setuptools.setup(
+    name='wfp-veda',
+    version='0.1.0',
+    packages=setuptools.find_packages(),
+    description = " Test lib",
+    install_requires=[
+        # list your dependencies here
+    ],
+)
