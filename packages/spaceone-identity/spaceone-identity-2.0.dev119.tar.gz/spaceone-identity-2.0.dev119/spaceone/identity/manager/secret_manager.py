eabd3b062791e04105a0e847ea2a9963b00e9e3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-29 15:03:16 +0000

    Add package spaceone-identity/2.0.dev119/spaceone-identity-2.0.dev119.tar.gz

---
+++ packages/spaceone-identity/spaceone-identity-2.0.dev119.tar.gz/spaceone-identity-2.0.dev119/spaceone/identity/manager/secret_manager.py
@@ -0,0 +1,91 @@
+import logging
+
+from spaceone.core import config
+from spaceone.core.manager import BaseManager
+from spaceone.core.connector.space_connector import SpaceConnector
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class SecretManager(BaseManager):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.secret_conn: SpaceConnector = self.locator.get_connector(
+            "SpaceConnector", service="secret"
+        )
+
+    def get_secret_data(self, secret_id: str, domain_id: str) -> dict:
+        system_token = config.get_global("TOKEN")
+
+        response = self.secret_conn.dispatch(
+            "Secret.get_data",
+            {"secret_id": secret_id, "domain_id": domain_id},
+            token=system_token,
+        )
+        return response["data"]
+
+    def get_trusted_secret_data(self, trusted_secret_id: str, domain_id: str) -> dict:
+        system_token = config.get_global("TOKEN")
+
+        response = self.secret_conn.dispatch(
+            "TrustedSecret.get_data",
+            {"trusted_secret_id": trusted_secret_id, "domain_id": domain_id},
+            token=system_token,
+        )
+        return response["data"]
+
+    def create_trusted_secret(self, params: dict) -> dict:
+        return self.secret_conn.dispatch("TrustedSecret.create", params)
+
+    def update_trusted_secret_data(
+            self, trusted_secret_id: str, schema_id: str, data: dict
+    ) -> None:
+        self.secret_conn.dispatch(
+            "TrustedSecret.update_data",
+            {
+                "trusted_secret_id": trusted_secret_id,
+                "schema_id": schema_id,
+                "data": data,
+            },
+        )
+
+    def delete_related_trusted_secrets(self, trusted_account_id: str):
+        response = self.list_trusted_secrets({"trusted_account_id": trusted_account_id})
+
+        for trusted_secret_info in response.get("results", []):
+            trusted_secret_id = trusted_secret_info["trusted_secret_id"]
+            self.secret_conn.dispatch(
+                "TrustedSecret.delete",
+                {"trusted_secret_id": trusted_secret_id},
+            )
+
+    def list_trusted_secrets(self, params: dict) -> dict:
+        return self.secret_conn.dispatch("TrustedSecret.list", params)
+
+    def create_secret(self, params: dict) -> dict:
+        return self.secret_conn.dispatch("Secret.create", params)
+
+    def update_secret(self, params: dict) -> dict:
+        return self.secret_conn.dispatch("Secret.update", params)
+
+    def delete_secret(self, secret_id: str) -> None:
+        self.secret_conn.dispatch("Secret.delete", {"secret_id": secret_id})
+
+    def update_secret_data(self, secret_id: str, schema_id: str, data: dict) -> None:
+        self.secret_conn.dispatch(
+            "Secret.update_data",
+            {"secret_id": secret_id, "schema_id": schema_id, "data": data},
+        )
+
+    def delete_related_secrets(self, service_account_id: str):
+        response = self.list_secrets({"service_account_id": service_account_id})
+
+        for secret_info in response.get("results", []):
+            secret_id = secret_info["secret_id"]
+            self.secret_conn.dispatch(
+                "Secret.delete",
+                {"secret_id": secret_id},
+            )
+
+    def list_secrets(self, params: dict) -> dict:
+        return self.secret_conn.dispatch("Secret.list", params)
