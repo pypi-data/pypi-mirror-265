89a5b58d1bb2abf3c0c0d7fb04cce7f6404572f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-29 14:03:23 +0000

    Add package cycleenrichr/0.0.3/cycleenrichr-0.0.3.tar.gz

---
+++ packages/cycleenrichr/cycleenrichr-0.0.3.tar.gz/cycleenrichr-0.0.3/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+# New code to read from requirements.txt
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setuptools.setup(
+    name="cycleenrichr",
+    version="0.0.3",
+    author="Alexander Lachmann",
+    author_email="alexander.lachmann@mssm.edu",
+    description="Cycleenrichr uses PrismEXP predictions to calculate enrichment of gene sets that do not have gene annotations.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/maayanlab/cycleenrichr",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=requirements,
+    python_requires='>=3.7',
+    package_data={
+        "cycleenrichr": ["data/*", "requirements.txt"]
+    },
+    include_package_data=True,
+)
