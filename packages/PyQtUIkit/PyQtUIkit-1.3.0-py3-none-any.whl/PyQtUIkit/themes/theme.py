0a1c25c5f6146ff659779f8a0f095bf7a898cdb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-30 03:03:06 +0000

    Add package PyQtUIkit/1.3.0/PyQtUIkit-1.3.0-py3-none-any.whl

---
+++ packages/PyQtUIkit/PyQtUIkit-1.3.0-py3-none-any.whl/PyQtUIkit/themes/theme.py
@@ -0,0 +1,32 @@
+class KitTheme:
+    def __init__(self,
+                 theme_data: dict,
+                 inherit: 'KitTheme' = None,
+                 is_dark=False):
+        self.is_dark = is_dark
+        self._data = theme_data
+        self._inherit = inherit
+
+    def get(self, key: str | tuple):
+        if isinstance(key, str):
+            key = (key,)
+        for el in key:
+            if el in self._data:
+                return self._data[el]
+        if not self._inherit:
+            raise KeyError(f"Key {key} not found")
+        return self._inherit.get(key)
+
+    def __getitem__(self, item):
+        return self.get(item)
+
+
+class KitPalette:
+    def __init__(self, main, hover=None, selected=None, text=None):
+        self.main = main
+        self.hover = hover or main
+        self.selected = selected or hover or main
+        self.text = text or selected or hover or main
+
+    def __str__(self):
+        return f"KitPalette({self.main}, {self.hover}, {self.selected}, {self.text})"
