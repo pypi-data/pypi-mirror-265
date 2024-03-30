b06b5500016f243eda3dab640b449896d41a73cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-30 03:03:41 +0000

    Add package neo4py/1.1.2b0/neo4py-1.1.2b0.tar.gz

---
+++ packages/neo4py/neo4py-1.1.2b0.tar.gz/neo4py-1.1.2b0/tests/test_neo.py
@@ -0,0 +1,8 @@
+from src.neo4py.main import Graph
+class TestNeo():
+    def test_match(self):
+        graph = Graph("bolt://localhost:7687",("neo4j","12345678"))
+        try:
+            assert  graph.run("MATCH (n) RETURN n") == {}
+        except Exception as e:
+            print(e)
