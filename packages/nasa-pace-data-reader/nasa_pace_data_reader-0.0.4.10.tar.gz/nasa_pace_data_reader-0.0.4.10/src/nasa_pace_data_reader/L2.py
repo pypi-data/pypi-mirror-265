e49e74bdbe8d67ea4617d1361849db27fdbc79d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-30 03:03:31 +0000

    Add package nasa-pace-data-reader/0.0.4.10/nasa_pace_data_reader-0.0.4.10.tar.gz

---
+++ packages/nasa-pace-data-reader/nasa_pace_data_reader-0.0.4.10.tar.gz/nasa_pace_data_reader-0.0.4.10/src/nasa_pace_data_reader/L2.py
@@ -0,0 +1,9 @@
+class L2:
+    """Class for reading
+        NASA PACE Level 1C data files.
+        
+    """
+
+    def __init__(self):
+        """Initializes the class."""
+        self.instrument = 'HARP2'   # Default instrument
