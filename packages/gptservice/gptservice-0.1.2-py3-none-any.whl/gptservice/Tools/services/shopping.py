364f08594bd15adfe534e0e034b9f688ec0242e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-30 15:04:00 +0000

    Add package gptservice/0.1.2/gptservice-0.1.2-py3-none-any.whl

---
+++ packages/gptservice/gptservice-0.1.2-py3-none-any.whl/gptservice/Tools/services/shopping.py
@@ -0,0 +1,37 @@
+from .features import Service
+
+
+class shopping(Service):
+
+    @property
+    def service(self):
+        return "GoogleShopping"
+    
+    @property
+    def content(self):
+        return "You are a shopping assistant. Provide me the top 3 shopping results. Only get the title, price and link for each item in natural language."
+    
+    @property
+    def tools(self):
+        return   {
+      "name": "extractShoppingDetail",
+      "description": "Extract shopping detail from the user prompt",
+      "parameters": {
+        "type": "object",
+        "properties": {
+          "item": {
+            "type": "string",
+            "description": "The item name"
+          },
+          "brand": {
+            "type": "string",
+            "description": "The brand of the item"
+          },
+          "price": {
+            "type": "number",
+            "description": "The price of the item"
+          }
+        },
+        "required": ["query"]
+      }
+    }
