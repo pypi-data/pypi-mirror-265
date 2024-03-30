5c7e98caaf4bbc441f3f63af26fbb1e5ab2246b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-30 04:05:19 +0000

    Add package pytest-relaxed/2.0.2/pytest_relaxed-2.0.2-py3-none-any.whl

---
+++ packages/pytest-relaxed/pytest_relaxed-2.0.2-py3-none-any.whl/pytest_relaxed/classes.py
@@ -0,0 +1,155 @@
+import inspect
+import logging
+import types
+
+from pytest import Class, Module
+
+# NOTE: don't see any other way to get access to pytest innards besides using
+# the underscored name :(
+from _pytest.python import PyCollector
+
+
+log = logging.getLogger("relaxed")
+
+
+# NOTE: these are defined here for reuse by both pytest's own machinery and our
+# internal bits.
+def istestclass(name):
+    return not name.startswith("_")
+
+
+# NOTE: this is defined at top level due to a couple spots of reuse outside of
+# the mixin class itself.
+def istestfunction(obj, name):
+    is_hidden_name = name.startswith("_") or name in (
+        "setup",
+        "setup_method",
+        "teardown",
+        "teardown_method",
+    )
+    # TODO: is this reliable? how about __pytest_wrapped__?
+    is_fixture = hasattr(obj, "_pytestfixturefunction")
+    return not (is_hidden_name or is_fixture)
+
+
+# All other classes in here currently inherit from PyCollector, and it is what
+# defines the default istestfunction/istestclass, so makes sense to inherit
+# from it for our mixin. (PyobjMixin, another commonly found class, offers
+# nothing of interest to us however.)
+class RelaxedMixin(PyCollector):
+    """
+    A mixin applying collection rules to both modules and inner/nested classes.
+    """
+
+    # TODO:
+    # - worth calling super() in these? Difficult to know what to do with it;
+    # it would say "no" to lots of stuff we want to say "yes" to.
+    # - are there other tests to apply to 'obj' in a vacuum? so far only thing
+    # we test 'obj' for is its membership in a module, which must happen inside
+    # SpecModule's override.
+
+    def istestclass(self, obj, name):
+        return istestclass(name)
+
+    def istestfunction(self, obj, name):
+        return istestfunction(obj, name)
+
+
+class SpecModule(RelaxedMixin, Module):
+    def _is_test_obj(self, test_func, obj, name):
+        # First run our super() test, which should be RelaxedMixin's.
+        good_name = getattr(super(), test_func)(obj, name)
+        # If RelaxedMixin said no, we can't really say yes, as the name itself
+        # was bad - private, other non test name like setup(), etc
+        if not good_name:
+            return False
+        # Here, we dig further based on our own wrapped module obj, by
+        # rejecting anything not defined locally.
+        if inspect.getmodule(obj) is not self.obj:
+            return False
+        # No other complaints -> it's probably good
+        return True
+
+    def istestfunction(self, obj, name):
+        return self._is_test_obj("istestfunction", obj, name)
+
+    def istestclass(self, obj, name):
+        return self._is_test_obj("istestclass", obj, name)
+
+    def collect(self):
+        # Given we've overridden naming constraints etc above, just use
+        # superclass' collection logic for the rest of the necessary behavior.
+        items = super().collect()
+        collected = []
+        for item in items:
+            # Replace Class objects with recursive SpecClasses
+            # NOTE: we could explicitly skip unittest objects here (we'd want
+            # them to be handled by pytest's own unittest support) but since
+            # those are almost always in test_prefixed_filenames anyways...meh
+            if isinstance(item, Class):
+                item = SpecClass.from_parent(item.parent, name=item.name)
+            collected.append(item)
+        return collected
+
+
+class SpecClass(RelaxedMixin, Class):
+    def _getobj(self):
+        # Regular object-making first
+        obj = super()._getobj()
+        # Short circuit if this obj isn't a nested class (aka child):
+        # - no parent attr: implies module-level obj definition
+        # - parent attr, but isn't a class: implies method
+        if not hasattr(self, "parent") or not isinstance(
+            self.parent, SpecClass
+        ):
+            return obj
+        # Then decorate it with our parent's extra attributes, allowing nested
+        # test classes to appear as an aggregate of parents' "scopes".
+        parent_obj = self.parent.obj
+        # Obtain parent attributes, etc not found on our obj (serves as both a
+        # useful identifier of "stuff added to an outer class" and a way of
+        # ensuring that we can override such attrs), and set them on obj
+        delta = set(dir(parent_obj)).difference(set(dir(obj)))
+        for name in delta:
+            value = getattr(parent_obj, name)
+            # Pytest's pytestmark attributes always get skipped, we don't want
+            # to spread that around where it's not wanted. (Besides, it can
+            # cause a lot of collection level warnings.)
+            if name == "pytestmark":
+                continue
+            # Classes get skipped; they'd always just be other 'inner' classes
+            # that we don't want to copy elsewhere.
+            if isinstance(value, type):
+                continue
+            # Functions (methods) may get skipped, or not, depending:
+            # NOTE: as of pytest 7, for some reason the value appears as a
+            # function and not a method (???) so covering both bases...
+            if isinstance(value, (types.MethodType, types.FunctionType)):
+                # If they look like tests, they get skipped; don't want to copy
+                # tests around!
+                if istestfunction(obj, name):
+                    continue
+                # Non-test == they're probably lifecycle methods
+                # (setup/teardown) or helpers (_do_thing). Rebind them to the
+                # target instance, otherwise the 'self' in the setup/helper is
+                # not the same 'self' as that in the actual test method it runs
+                # around or within!
+                setattr(obj, name, value)
+            # Anything else should be some data-type attribute, which is copied
+            # verbatim / by-value.
+            else:
+                setattr(obj, name, value)
+        return obj
+
+    def collect(self):
+        ret = []
+        for item in super().collect():
+            # More pytestmark skipping.
+            if item.name == "pytestmark":
+                continue
+            if isinstance(item, Class):
+                item = SpecClass.from_parent(
+                    parent=item.parent, name=item.name, obj=item.obj
+                )
+            ret.append(item)
+        return ret
---
+++ packages/pytest-relaxed/pytest_relaxed-2.0.2-py3-none-any.whl/pytest_relaxed/trap.py
@@ -0,0 +1,80 @@
+"""
+Test decorator for capturing stdout/stderr/both.
+
+Based on original code from Fabric 1.x, specifically:
+
+* fabric/tests/utils.py
+* as of Git SHA 62abc4e17aab0124bf41f9c5f9c4bc86cc7d9412
+
+Though modifications have been made since.
+"""
+
+import io
+import sys
+from functools import wraps
+
+
+class CarbonCopy(io.BytesIO):
+    """
+    An IO wrapper capable of multiplexing its writes to other buffer objects.
+    """
+
+    def __init__(self, buffer=b"", cc=None):
+        """
+        If ``cc`` is given and is a file-like object or an iterable of same,
+        it/they will be written to whenever this instance is written to.
+        """
+        super().__init__(buffer)
+        if cc is None:
+            cc = []
+        elif hasattr(cc, "write"):
+            cc = [cc]
+        self.cc = cc
+
+    def write(self, s):
+        # Ensure we always write bytes.
+        if isinstance(s, str):
+            s = s.encode("utf-8")
+        # Write out to our capturing object & any CC's
+        super().write(s)
+        for writer in self.cc:
+            writer.write(s)
+
+    # Real sys.std(out|err) requires writing to a buffer attribute obj in some
+    # situations.
+    @property
+    def buffer(self):
+        return self
+
+    # Make sure we always hand back strings
+    def getvalue(self):
+        ret = super().getvalue()
+        if isinstance(ret, bytes):
+            ret = ret.decode("utf-8")
+        return ret
+
+
+def trap(func):
+    """
+    Replace sys.std(out|err) with a wrapper during execution, restored after.
+
+    In addition, a new combined-streams output (another wrapper) will appear at
+    ``sys.stdall``. This stream will resemble what a user sees at a terminal,
+    i.e. both out/err streams intermingled.
+    """
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        # Use another CarbonCopy even though we're not cc'ing; for our "write
+        # bytes, return strings" behavior. Meh.
+        sys.stdall = CarbonCopy()
+        my_stdout, sys.stdout = sys.stdout, CarbonCopy(cc=sys.stdall)
+        my_stderr, sys.stderr = sys.stderr, CarbonCopy(cc=sys.stdall)
+        try:
+            return func(*args, **kwargs)
+        finally:
+            sys.stdout = my_stdout
+            sys.stderr = my_stderr
+            del sys.stdall
+
+    return wrapper
