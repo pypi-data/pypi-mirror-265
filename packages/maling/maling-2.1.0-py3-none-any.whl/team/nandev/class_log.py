f48cfe9eb1886260ea02c8afe23232b25ccd706b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-30 03:03:41 +0000

    Add package maling/2.1.0/maling-2.1.0-py3-none-any.whl

---
+++ packages/maling/maling-2.1.0-py3-none-any.whl/team/nandev/class_log.py
@@ -0,0 +1,39 @@
+################################################################
+"""
+ Mix-Userbot Open Source . Maintained ? Yes Oh No Oh Yes Ngentot
+ 
+ @ CREDIT : NAN-DEV
+"""
+################################################################
+
+import logging
+import os
+import sys
+
+
+class ConnectionHandler(logging.Handler):
+    def emit(self, record):
+        for ah in ["OSError", "socket"]:
+            if ah in record.getMessage():
+                os.execl(sys.executable, sys.executable, "-m", "Mix")
+
+
+logger = logging.getLogger()
+logger.setLevel(logging.ERROR)
+
+formatter = logging.Formatter("[%(levelname)s] - %(name)s - %(message)s", "%d-%b %H:%M")
+stream_handler = logging.StreamHandler()
+
+stream_handler.setFormatter(formatter)
+connection_handler = ConnectionHandler()
+
+logger.addHandler(stream_handler)
+logger.addHandler(connection_handler)
+logging.getLogger("pyrogram").setLevel(logging.ERROR)
+
+LOGS = logging.getLogger("Mix-Userbot")
+
+def LOGG(name: str) -> logging.Logger:
+    return logging.getLogger(name)
+    
+LOGGER = LOGG("Mix-Userbot")
