60de2798a6dd00c7070064cb0851ca81b25381cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-30 04:03:13 +0000

    Add package doy/1.8/doy-1.8.tar.gz

---
+++ packages/doy/doy-1.8.tar.gz/doy-1.8/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['doy']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['filelock>=3.12.2,<4.0.0',
+ 'matplotlib>=3.6.2',
+ 'numpy',
+ 'rich>=13.4.1',
+ 'tqdm>=4.65.0',
+ 'wandb>=0.15.5']
+
+setup_kwargs = {
+    'name': 'doy',
+    'version': '1.8',
+    'description': '',
+    'long_description': '# Doy\n\nSimple utility package\n',
+    'author': 'Dominik Schmidt',
+    'author_email': 'schmidtdominik30@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8',
+}
+
+
+setup(**setup_kwargs)
