8b88c186d3cdd45c301bc71d30bad7e7914c7cbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-30 15:03:32 +0000

    Add package hexdoc/1!0.1.0a11/hexdoc-1!0.1.0a11-py3-none-any.whl

---
+++ packages/hexdoc/hexdoc-1!0.1.0a11-py3-none-any.whl/hexdoc/patchouli/page/__init__.py
@@ -0,0 +1,33 @@
+__all__ = [
+    "CraftingPage",
+    "EmptyPage",
+    "EntityPage",
+    "ImagePage",
+    "LinkPage",
+    "Multiblock",
+    "MultiblockPage",
+    "Page",
+    "PageWithText",
+    "PageWithTitle",
+    "QuestPage",
+    "RelationsPage",
+    "SmeltingPage",
+    "SpotlightPage",
+    "TextPage",
+]
+
+from .abstract_pages import Page, PageWithText, PageWithTitle
+from .pages import (
+    CraftingPage,
+    EmptyPage,
+    EntityPage,
+    ImagePage,
+    LinkPage,
+    Multiblock,
+    MultiblockPage,
+    QuestPage,
+    RelationsPage,
+    SmeltingPage,
+    SpotlightPage,
+    TextPage,
+)
