3f011facbeeaa844e486fd90dd7b74ed160f836e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-30 04:03:51 +0000

    Add package outsetapy/0.1.3/outsetapy-0.1.3.tar.gz

---
+++ packages/outsetapy/outsetapy-0.1.3.tar.gz/outsetapy-0.1.3/src/outsetapy/models/crm/person.py
@@ -0,0 +1,31 @@
+from outsetapy.models.shared.address import Address
+from outsetapy.models.crm.account import Account
+
+
+class Person:
+    def __init__(self):
+        self.Email = ""
+        self.FirstName = ""
+        self.LastName = ""
+        self.ProfileImageS3Url = ""
+        self.MailingAddress = Address()
+        self.PasswordMustChange = False
+        self.PhoneMobile = ""
+        self.PhoneWork = ""
+        self.Title = ""
+        self.Timezone = None
+        self.Language = ""
+        self.IPAddress = ""
+        self.Referer = ""
+        self.UserAgent = ""
+        self.LastLoginDateTime = None
+        self.OAuthGoogleProfileId = None
+        self.PersonAccount = []
+        self.DealPeople = []
+        self.Account = Account()
+        self.FullName = ""
+        self.OAuthIntegrationStatus = 0
+        self.UserAgentPlatformBrowser = ""
+        self.Uid = ""
+        self.Created = None
+        self.Updated = None
