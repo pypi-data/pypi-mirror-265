e4506e84ef8ff75392ddbf788f4c15991f4193c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-30 05:03:12 +0000

    Add package cpphospitalmanagementproject/0.0.1/cpphospitalmanagementproject-0.0.1.tar.gz

---
+++ packages/cpphospitalmanagementproject/cpphospitalmanagementproject-0.0.1.tar.gz/cpphospitalmanagementproject-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cpphospitalmanagementproject',
+    version='0.0.1',
+    packages=find_packages(),
+    description='Hospital Management',
+    install_requires=['asgiref','Django','parse','Pillow','sqlparse','tzdata'],  # Add dependencies if any
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+)
+
