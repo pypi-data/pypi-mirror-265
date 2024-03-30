dac3944b8ad43805dd577c98e9333d79e999f8e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-30 05:03:19 +0000

    Add package htagweb/0.24.0/htagweb-0.24.0-py3-none-any.whl

---
+++ packages/htagweb/htagweb-0.24.0-py3-none-any.whl/htagweb/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# #############################################################################
+# Copyright (C) 2023 manatlan manatlan[at]gmail(dot)com
+#
+# MIT licence
+#
+# https://github.com/manatlan/htagweb
+# #############################################################################
+
+__version__ = "0.24.0" # auto updated
+
+from .appserver import AppServer
+from .simpleserver import SimpleServer  # a completly different beast, but compatible with ^^
+from .htagserver import HtagServer  # a completly different beast.
+
+__all__= ["AppServer","SimpleServer"]
+
