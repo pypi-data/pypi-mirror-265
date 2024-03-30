bf51b6513aded5dbb8a2eeb805e29d2625404233
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-30 15:04:05 +0000

    Add package time-granularity/1.1/time-granularity-1.1.tar.gz

---
+++ packages/time-granularity/time-granularity-1.1.tar.gz/time-granularity-1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='time-granularity',
+    version='1.1',
+    packages=find_packages(),
+    install_requires=[],  # Add dependencies if any
+    author='Prateek Dutta',
+    author_email='parteekdutta2001@email.com',
+    description='A Python package for managing time granularity with options to re-execute at different intervals.',
+    url='https://github.com/PrateekDutta2001/time-granularity',
+)
