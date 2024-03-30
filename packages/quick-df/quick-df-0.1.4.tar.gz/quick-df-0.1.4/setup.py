ace2eba43d12046abe45d07e666a8fc9ab71ca82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-30 03:04:05 +0000

    Add package quick-df/0.1.4/quick-df-0.1.4.tar.gz

---
+++ packages/quick-df/quick-df-0.1.4.tar.gz/quick-df-0.1.4/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+import random
+import numpy
+import datetime
+import pandas
+import string
+import warnings
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+VERSION = '0.1.4'
+DESCRIPTION = 'Library to create custom dataframe quickly'
+LONG_DESCRIPTION = 'This library will help to create our own custom dataframes quickly. You can populate an int,category and dates column easily'
+
+
+setup(
+    name="quick-df",
+    version=VERSION,
+    author="Marcel Tino",
+    author_email="<marceltino92@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    install_requires=['pandas','random','datetime','numpy'],
+    keywords=['dataframe','quick'])
