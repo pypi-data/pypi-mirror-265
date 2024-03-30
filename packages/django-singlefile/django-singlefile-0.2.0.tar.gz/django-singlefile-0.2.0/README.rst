0353e4d957599b323f0cca42bf787a206e9d0726
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-30 04:03:46 +0000

    Add package django-singlefile/0.2.0/django-singlefile-0.2.0.tar.gz

---
+++ packages/django-singlefile/django-singlefile-0.2.0.tar.gz/django-singlefile-0.2.0/README.rst
@@ -0,0 +1,56 @@
+django-singlefile
+=================
+
+This is a small library that makes it easier to write single-file Django
+applications in a similar way to how you'd write Flask applications.
+
+It's still alpha, but hey, it works for small projects.
+
+
+Example App
+-----------
+
+::
+
+    from django.http import HttpResponse
+    from django.singlefile import SingleFileApp
+
+    app = SingleFileApp()
+
+
+    @app.path("")
+    def index(request):
+        name = request.GET.get("name", "World")
+        return HttpResponse(f"Hello, {name}!")
+
+
+    if __name__ == "__main__":
+        app.main()
+
+
+To run the app, you can just call it from the command line::
+
+    python app.py runserver
+
+Or you can pass the `app` object inside it to a WSGI server as normal!
+
+If you'd like to see a more in-depth example app, take a look at the project
+I initially wrote this for: `andrewgodwin/emf-equipment <https://github.com/andrewgodwin/emf-equipment/>`_.
+
+
+Supported Features
+------------------
+
+* **Templates**: Put them in a ``templates/`` directory.
+* **Static Files**: Put them in a ``static/`` directory.
+* **Forms**: Use them as normal.
+
+Unsupported Features
+--------------------
+
+If you want to use these, just go make a normal Django project; trying to fit
+them into a single file is a bad idea due to their innate complexity.
+
+* Models/Migrations (maybe in future, if I can figure out how nicely)
+* Admin
+* Authentication
---
+++ packages/django-singlefile/django-singlefile-0.2.0.tar.gz/django-singlefile-0.2.0/django/singlefile/__init__.py
@@ -0,0 +1,91 @@
+import os
+import sys
+import inspect
+
+from django.conf import settings
+from django.core.wsgi import get_wsgi_application
+from django.urls import path
+from django.utils.crypto import get_random_string
+from django.core.management import execute_from_command_line
+from django.singlefile.environment import EnvironmentSettings
+
+
+class SingleFileApp:
+    """
+    Class that aids in making a single-file Django app.
+
+    Inspired by Flask, but not trying to be API compatible in the slightest.
+    """
+
+    urlpatterns: list
+
+    def __init__(self, template_directory="templates", static_directory="static"):
+        # Figure out project root directory via some... shenanigans
+        previous_filename = inspect.getframeinfo(inspect.currentframe().f_back)[0]
+        self.root_directory = os.path.dirname(previous_filename)
+        # Do initial settings configuration
+        envsettings = EnvironmentSettings()
+        settings.configure(
+            DEBUG=envsettings.get_bool("DJANGO_DEBUG"),
+            ALLOWED_HOSTS=["*"],  # Disable host header validation
+            ROOT_URLCONF=__name__,  # Could be fancier with middleware?
+            SECRET_KEY=envsettings.get("DJANGO_SECRET_KEY", get_random_string(50)),
+            TEMPLATES=[
+                {
+                    "BACKEND": "django.template.backends.django.DjangoTemplates",
+                    "DIRS": [os.path.join(self.root_directory, template_directory)],
+                },
+            ],
+            STATIC_URL="static/",
+            STATIC_ROOT=os.path.join(self.root_directory, static_directory),
+            MIDDLEWARE=[
+                "django.middleware.security.SecurityMiddleware",
+                "whitenoise.middleware.WhiteNoiseMiddleware",
+                "django.contrib.sessions.middleware.SessionMiddleware",
+                "django.middleware.common.CommonMiddleware",
+                "django.middleware.csrf.CsrfViewMiddleware",
+                "django.contrib.messages.middleware.MessageMiddleware",
+                "django.middleware.clickjacking.XFrameOptionsMiddleware",
+            ],
+        )
+        self.urlpatterns = []
+        self.app = get_wsgi_application()
+
+    def path(self, route: str):
+        """
+        Decorator that adds a url pattern
+        """
+
+        def inner(view):
+            # Ensure we dispatch class-based views correctly
+            if hasattr(view, "as_view"):
+                self.add_urlpattern(path(route, view.as_view()))
+            else:
+                self.add_urlpattern(path(route, view))
+            return view
+
+        return inner
+
+    def __call__(self, environ, start_response):
+        """
+        WSGI entrypoint
+        """
+        return self.app(environ, start_response)
+
+    def add_urlpattern(self, pattern):
+        """
+        Adds a urlpattern and ensures the global variable matches
+        """
+        global urlpatterns
+        self.urlpatterns.append(pattern)
+        urlpatterns = self.urlpatterns
+
+    def main(self):
+        """
+        Dispatch point - has to be called for now.
+        """
+        execute_from_command_line(sys.argv)
+
+
+# This gets set by the app __init__
+urlpatterns = []
