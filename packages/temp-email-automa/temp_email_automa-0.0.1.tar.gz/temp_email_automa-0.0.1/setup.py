1ddd88fd09a02ad794031d8b3cc0a41fc5b2289f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-30 04:03:08 +0000

    Add package temp-email-automa/0.0.1/temp_email_automa-0.0.1.tar.gz

---
+++ packages/temp-email-automa/temp_email_automa-0.0.1.tar.gz/temp_email_automa-0.0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="temp_email_automa",
+    version="0.0.1",
+    description="A Python Package to Provide Temp Emails for AUTOMATION!",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/temp_email_automa",
+    author="Your Name",
+    author_email="your.email@example.com",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: OS Independent",
+    ],
+    keywords="temp-email automation email",
+    packages=find_packages(),
+    install_requires=["requests"],
+)
