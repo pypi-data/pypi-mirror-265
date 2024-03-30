da191b0aa38e69cbed7c361d0f7987675e1d6c12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-30 05:03:14 +0000

    Add package kayalab/0.7.27/kayalab-0.7.27.tar.gz

---
+++ packages/kayalab/kayalab-0.7.27.tar.gz/kayalab-0.7.27/ezinfra/cloudinit.py
@@ -0,0 +1,62 @@
+import bcrypt
+
+
+def cloudinit_userdata(
+    hostname: str,
+    fqdn: str,
+    ssh_username: str,
+    ssh_password: str,
+    ssh_pubkey: str,
+    swap_disk: str,
+):
+    return f"""
+#cloud-config
+hostname: {hostname}
+manage_etc_hosts: true
+fqdn: {fqdn}
+user: {ssh_username}
+password: {ssh_password}
+ssh_authorized_keys:
+  - {ssh_pubkey}
+chpasswd:
+  list: |
+    root:{ssh_password}
+    {ssh_username}: {ssh_password}
+  expire: False
+ssh_pwauth: true
+disable_root: false
+
+# users:
+#   - default
+
+fs_setup:
+  - device: {swap_disk}
+    filesystem: swap
+mounts:
+  - ["{swap_disk}", "none", "swap", "sw,nofail,x-systemd.requires=cloud-init.service", "0", "0"]
+
+"""
+
+
+def cloudinit_metadata(hostname: str):
+    return f"""
+instance-id: {hostname}
+local-hostname: {hostname}"""
+
+
+def cloudinit_networkconfig(
+    ip_address: str, network_bits: int, gateway: str, nameserver: str, searchdomain: str
+):
+    return f"""version: 1
+config:
+  - type: physical
+    name: eth0
+    subnets:
+      - type: static
+        address: {ip_address}/{network_bits}
+        gateway: {gateway}
+        dns_nameservers:
+          - {nameserver}
+        dns_search:
+         - {searchdomain}
+"""
