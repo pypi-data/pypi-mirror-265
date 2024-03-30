8c681ea00ea5a1ebba30bb22670888f6ef0430ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-30 04:03:07 +0000

    Add package CrunchyrollAPI/0.1.5/CrunchyrollAPI-0.1.5-py3-none-any.whl

---
+++ packages/CrunchyrollAPI/CrunchyrollAPI-0.1.5-py3-none-any.whl/CrunchyrollAPI/__init__.py
@@ -0,0 +1,19 @@
+# Dummy file to make this directory a package.
+# import utils
+from CrunchyrollAPI.crunchyrollentity import *
+from CrunchyrollAPI.crunchyrollapi import *
+from CrunchyrollAPI.utils import *
+
+__all__ = [
+        'SettingsData',
+        'AccountData',
+        'LoginError',
+        'SeasonData',
+        'SeriesData',
+        'EpisodeData',
+        'ListableItem',
+        'CrunchyrollError',
+        'CrunchyrollAPI',
+        'CrunchyrollSettings',
+        'CrunchyrollSettingsArgs',
+        'CrunchyrollSettingsClass']
