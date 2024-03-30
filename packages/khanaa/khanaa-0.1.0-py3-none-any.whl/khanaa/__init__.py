1ea637d74db448826d5e49010609d298e0548a70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-30 15:03:38 +0000

    Add package khanaa/0.1.0/khanaa-0.1.0-py3-none-any.whl

---
+++ packages/khanaa/khanaa-0.1.0-py3-none-any.whl/khanaa/__init__.py
@@ -0,0 +1,5 @@
+from .decomposition import spelling_decompose
+from .main import *
+from .thai_spelling import *
+from .utils import (find_vowel_pair, find_vowel_length, find_letter_list,
+    check_checked, find_tone)
