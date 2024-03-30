c3fbef102e8e4a61dc4709e07bdea4f3ae66f6dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-30 03:04:41 +0000

    Add package pysat-abel/0.0.3/pysat-abel-0.0.3.tar.gz

---
+++ packages/pysat-abel/pysat-abel-0.0.3.tar.gz/pysat-abel-0.0.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert_file('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
+
+setup(
+    name='pysat-abel',
+    version = "0.0.3",
+    description='Python implementation for Spline-based Abel Transform',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://gitlab.coria-cfd.fr/littinm/pysat',
+    author='Mijail Littin, Alexandre Poux, Guillaume Lefevre, Marek Mazur, Felipe Escudero, Andrés Fuentes, Jérôme Yon',
+    author_email='littinm@coria.fr',
+    packages=['pysat'],
+    dependencies = [ "scipy", "numpy", 'abel', 'joblib'],
+
+    classifiers=[
+        'Intended Audience :: Science/Research',
+    ],
+)
