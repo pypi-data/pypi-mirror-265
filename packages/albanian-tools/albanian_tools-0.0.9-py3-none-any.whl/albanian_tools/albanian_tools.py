ee463a6e717568029d61279583955b3b131456a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-30 15:03:58 +0000

    Add package albanian-tools/0.0.9/albanian_tools-0.0.9-py3-none-any.whl

---
+++ packages/albanian-tools/albanian_tools-0.0.9-py3-none-any.whl/albanian_tools/albanian_tools.py
@@ -0,0 +1,14 @@
+from .utils.functions import integer_to_albanian_text_converter, replace_numbers_with_albanian_text, remove_leading_numbers
+
+class AlbanianTools:
+    def random_albanian_name(self) -> str:
+        return f"Filan Fisteku"
+    
+    def remove_leading_numbers(self, text: str) -> str:
+        return remove_leading_numbers(text)
+    
+    def integer_to_albanian_text_converter(self, decimal_integer: int) -> str:
+        return integer_to_albanian_text_converter(decimal_integer)
+    
+    def replace_numbers_with_albanian_text(self, text: str) -> str:
+        return replace_numbers_with_albanian_text(text)
