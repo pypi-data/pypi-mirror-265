8bbf8cac743d5cbc58b52ca43c927e9fbe3c574e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-30 04:03:47 +0000

    Add package wagtail-editorjs/1.5.6/wagtail_editorjs-1.5.6.tar.gz

---
+++ packages/wagtail-editorjs/wagtail_editorjs-1.5.6.tar.gz/wagtail_editorjs-1.5.6/wagtail_editorjs/features/__init__.py
@@ -0,0 +1,31 @@
+from .blocks import (
+    CodeFeature,
+    DelimiterFeature,
+    HeaderFeature,
+    HTMLFeature,
+    WarningFeature,
+    TableFeature,
+    BlockQuoteFeature,
+)
+from .lists import (
+    NestedListFeature,
+    CheckListFeature,
+)
+from .documents import (
+    AttachesFeature,
+)
+from .images import (
+    ImageFeature,
+    ImageRowFeature,
+)
+from .inlines import (
+    LinkFeature,
+    LinkAutoCompleteFeature,
+    DocumentFeature,
+)
+from .tunes import (
+    AlignmentBlockTune,
+    TextVariantTune,
+    ColorTune,
+    BackgroundColorTune,
+)
