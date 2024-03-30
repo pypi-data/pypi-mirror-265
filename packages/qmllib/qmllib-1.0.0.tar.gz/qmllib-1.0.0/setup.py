3d1496f78a6883dc7c4eba589817fbfee79b5999
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-30 05:03:11 +0000

    Add package qmllib/1.0.0/qmllib-1.0.0.tar.gz

---
+++ packages/qmllib/qmllib-1.0.0.tar.gz/qmllib-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+try:
+    import _compile
+except ImportError:
+    import sys
+    from pathlib import Path
+    sys.path.append(str(Path(__file__).resolve().parent))
+    import _compile
+
+if __name__ == "__main__":
+    _compile.main()
+    setup()
