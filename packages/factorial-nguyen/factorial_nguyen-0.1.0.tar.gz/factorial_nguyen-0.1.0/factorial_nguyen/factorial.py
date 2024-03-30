afd35816edd24967c5e06ae53733dd2b1e31e1db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-30 15:03:58 +0000

    Add package factorial-nguyen/0.1.0/factorial_nguyen-0.1.0.tar.gz

---
+++ packages/factorial-nguyen/factorial_nguyen-0.1.0.tar.gz/factorial_nguyen-0.1.0/factorial_nguyen/factorial.py
@@ -0,0 +1,14 @@
+def fact(n) :
+    """
+    Tính giai thừa của một số nguy ên dương n.
+    Tham số:
+    - n: Số nguy ên dương.
+    Trả về:
+    - Giai thừa của n.
+    """
+    if n < 0:
+        raise ValueError (" Factorial is not defined for negative numbers ")
+    elif n == 0 or n == 1:
+        return 1
+    else :
+        return n * fact (n -1)
