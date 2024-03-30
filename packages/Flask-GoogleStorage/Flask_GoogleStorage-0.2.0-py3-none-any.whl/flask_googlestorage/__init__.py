5e9a57a00b028385843894be19dc56b31e58dcba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-30 04:03:01 +0000

    Add package Flask-GoogleStorage/0.2.0/Flask_GoogleStorage-0.2.0-py3-none-any.whl

---
+++ packages/Flask-GoogleStorage/Flask_GoogleStorage-0.2.0-py3-none-any.whl/flask_googlestorage/__init__.py
@@ -0,0 +1,11 @@
+# flake8: noqa
+
+from .google_storage import GoogleStorage
+from .buckets import Bucket, LocalBucket, CloudBucket
+from .exceptions import (
+    NotAllowedUploadError,
+    NotFoundBucketError,
+    NotFoundDestinationError,
+)
+
+__version__ = "0.2.0"
