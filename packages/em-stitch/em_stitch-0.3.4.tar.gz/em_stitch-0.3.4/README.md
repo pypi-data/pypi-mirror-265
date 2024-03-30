f930f1e45ff1ee9503360ae8872b095b68783968
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-30 14:04:06 +0000

    Add package em-stitch/0.3.4/em_stitch-0.3.4.tar.gz

---
+++ packages/em-stitch/em_stitch-0.3.4.tar.gz/em_stitch-0.3.4/README.md
@@ -0,0 +1,33 @@
+[![Build Status](https://travis-ci.org/AllenInstitute/em_stitch.svg?branch=master)](https://travis-ci.org/AllenInstitute/em_stitch)
+[![codecov](https://codecov.io/gh/AllenInstitute/em_stitch/branch/master/graph/badge.svg)](https://codecov.io/gh/AllenInstitute/em_stitch)
+
+
+# em_stitch
+
+Lens correction solver and stitching utilities for EM images.
+
+This repo has overlap with
+https://github.com/AllenInstitute/asap-modules
+
+It is meant to be independent of asap-modules and a running render server.
+
+## Installation
+
+This package is pip-installable with
+```pip install em-stitch```
+
+installing from this repo can be accomplished with 
+```python setup.py install```
+
+
+## Optional dependencies
+The performance of em-stitch can benefit from the installation of `pandas` and will use it when available.  Bigfeta will use `petsc4py` to implement its solver if available, which can improve memory utilization and performance.
+
+
+# support
+
+We are planning on occasional updating this tool with no fixed schedule. Community involvement is encouraged through both issues and pull requests. Please make pull requests against the develop branch, as we will test changes there before merging into master.
+
+# Acknowledgement of Government Sponsorship
+
+Supported by the Intelligence Advanced Research Projects Activity (IARPA) via Department of Interior / Interior Business Center (DoI/IBC) contract number D16PC00004. The U.S. Government is authorized to reproduce and distribute reprints for Governmental purposes notwithstanding any copyright annotation thereon. Disclaimer: The views and conclusions contained herein are those of the authors and should not be interpreted as necessarily representing the official policies or endorsements, either expressed or implied, of IARPA, DoI/IBC, or the U.S. Government.
---
+++ packages/em-stitch/em_stitch-0.3.4.tar.gz/em_stitch-0.3.4/em_stitch/viz/update_urls.py
@@ -0,0 +1,83 @@
+import logging
+import os
+import pathlib
+import shutil
+
+from six.moves import urllib
+
+from argschema import ArgSchemaParser
+from bigfeta import jsongz
+import renderapi
+
+from .schemas import UpdateUrlSchema
+
+logger = logging.getLogger(__name__)
+
+example = {
+        "backup_copy": True,
+        "client_mount_or_map": "/data/em-131fs3",  # could also be "Q:" e.g
+        "fdir": 'lctest/T6.2019.04.18.110-120/000116/0/',
+        "resolved_file": "resolvedtiles_AffineModel_0.json.gz",
+        "server_mount": "/data/em-131fs3",  # leave as posix
+        "image_directory": None,
+        "log_level": "INFO"
+        }
+
+
+def backup(f):
+    for ext in ['.json', '.json.gz']:
+        if f.endswith(ext):
+            b = f.split(ext)[0]
+            break
+    i = 0
+    newf = f
+    while os.path.isfile(newf):
+        # were having some permission errors
+        # from multiple users writing this backup
+        # file.
+        newf = b + '_backup_%d' % i + ext
+        i += 1
+    shutil.copy(f, newf)
+    logger.info("copied %s to %s" % (f, newf))
+
+
+class UpdateUrls(ArgSchemaParser):
+    default_schema = UpdateUrlSchema
+
+    def run(self):
+        logger.setLevel(self.args['log_level'])
+
+        resolved_path = os.path.join(
+                self.args['client_mount_or_map'],
+                self.args['fdir'],
+                self.args['resolved_file'])
+
+        if self.args['backup_copy']:
+            backup(resolved_path)
+
+        resolved = renderapi.resolvedtiles.ResolvedTiles(
+                json=jsongz.load(resolved_path))
+
+        if self.args['image_directory'] is None:
+            self.args['image_directory'] = pathlib.PurePosixPath(
+                    self.args['server_mount'],
+                    self.args['fdir'])
+
+        for t in resolved.tilespecs:
+            for k in ['imageUrl', 'maskUrl']:
+                s = t.ip['0'][k]
+                if s:
+                    orig = urllib.parse.unquote(urllib.parse.urlparse(s).path)
+                    t.ip['0'][k] = pathlib.PurePosixPath(
+                            self.args['image_directory'],
+                            os.path.basename(orig)).as_uri()
+
+        self.args['resolved_file'] = jsongz.dump(
+                resolved.to_dict(), resolved_path, compress=None)
+
+        logger.info("updated tilespec urls in %s" % resolved_path)
+
+
+if __name__ == '__main__':
+    u = UpdateUrls(input_data=example)
+    u.run()
