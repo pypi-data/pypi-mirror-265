32c89fdeaee113ce1fdd92e92e5f87629469fbac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-30 03:04:12 +0000

    Add package invokelint/0.9.2/invokelint-0.9.2-py3-none-any.whl

---
+++ packages/invokelint/invokelint-0.9.2-py3-none-any.whl/invokelint/ruff.py
@@ -0,0 +1,30 @@
+"""To unify executing ruff from style and from lint."""
+
+from typing import TYPE_CHECKING
+
+from invokelint.path import EXISTING_TEST_PACKAGES, PYTHON_DIRS, PYTHON_DIRS_EXCLUDING_TEST
+from invokelint.run import run_in_pty
+
+if TYPE_CHECKING:
+    from invoke import Context, Result
+
+
+def chk(context: "Context", *, fix: bool = False, show_fixes: bool = False) -> "Result":
+    """Lints code with Ruff."""
+    list_options = []
+    if fix:
+        list_options.append("--fix")
+    if show_fixes:
+        list_options.append("--show-fixes")
+    options = " " + " ".join(list_options) if list_options else ""
+    run_in_pty(context, "ruff check{} {}".format(options, " ".join(PYTHON_DIRS_EXCLUDING_TEST)))
+    return run_in_pty(context, "ruff check{} --ignore S101 {}".format(options, " ".join(EXISTING_TEST_PACKAGES)))
+
+
+def fmt(context: "Context", *, diff: bool = False) -> "Result":
+    """Lints code with Ruff."""
+    list_options = []
+    if diff:
+        list_options.append("--diff")
+    options = " " + " ".join(list_options) if list_options else ""
+    return run_in_pty(context, "ruff format{} {}".format(options, " ".join(PYTHON_DIRS)))
