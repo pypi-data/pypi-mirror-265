0d310c41cf83397a403c248ee86db510959b7385
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-30 15:03:43 +0000

    Add package esa-2scm/0.1.8/esa_2scm-0.1.8-py3-none-any.whl

---
+++ packages/esa-2scm/esa_2scm-0.1.8-py3-none-any.whl/esa_2scm/__init__.py
@@ -0,0 +1,12 @@
+"""
+The esa-2scm package is an implementation of the ESA-2SCM algorithm (Sanghoon Lee, 2024)
+For documentation and algorithm/methodology details, please refer to my original article: http://www.snbperi.org/article/227
+
+Should you use this package, I kindly request you to cite my article:
+Lee, Sanghoon (2024). ESA-2SCM for Causal Discovery: Causal Modeling with Elastic Segmentation-based Synthetic Instrumental Variable, SnB Political and Economic Research Institute, 1, 16. <snbperi.org/article/227>
+"""
+
+
+__version__ = "0.1.8"
+
+from .models import Esa2Scm
