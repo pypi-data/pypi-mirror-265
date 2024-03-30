2dc025431be42b51f7f218de3fc1e02e8866870d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-30 04:04:26 +0000

    Add package philter-lite/0.5.0/philter_lite-0.5.0-py3-none-any.whl

---
+++ packages/philter-lite/philter_lite-0.5.0-py3-none-any.whl/philter_lite/__init__.py
@@ -0,0 +1,15 @@
+from importlib import metadata
+
+from philter_lite.coordinate_map import CoordinateMap
+from philter_lite.filters import Filter, filter_from_dict, load_filters
+from philter_lite.philter import detect_phi
+
+# Writers
+from .asterisk import transform_text_asterisk
+from .i2b2 import transform_text_i2b2
+
+_DISTRIBUTION_METADATA = metadata.metadata("philter_lite")
+
+__author__ = _DISTRIBUTION_METADATA["Author"]
+__email__ = _DISTRIBUTION_METADATA["Author-email"]
+__version__ = _DISTRIBUTION_METADATA["Version"]
