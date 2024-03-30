29e2e521ce63725e3c790dc96f67018a7bfec9c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-30 05:03:39 +0000

    Add package Files.com/1.4.28/Files.com-1.4.28.tar.gz

---
+++ packages/Files.com/Files.com-1.4.28.tar.gz/Files.com-1.4.28/files_sdk/models/remote_server_configuration_file.py
@@ -0,0 +1,50 @@
+import builtins  # noqa: F401
+from files_sdk.api import Api  # noqa: F401
+from files_sdk.error import (  # noqa: F401
+    InvalidParameterError,
+    MissingParameterError,
+    NotImplementedError,
+)
+
+
+class RemoteServerConfigurationFile:
+    default_attributes = {
+        "id": None,  # int64 - Agent ID
+        "permission_set": None,  # string -
+        "private_key": None,  # string - private key
+        "subdomain": None,  # string
+        "root": None,  # string - Agent local root path
+        "api_token": None,  # string - Files Agent API Token
+        "port": None,  # int64 - Incoming port for files agent connections
+        "hostname": None,  # string
+        "public_key": None,  # string - public key
+        "status": None,  # string - either running or shutdown
+        "server_host_key": None,  # string
+        "config_version": None,  # string - agent config version
+    }
+
+    def __init__(self, attributes=None, options=None):
+        if not isinstance(attributes, dict):
+            attributes = {}
+        if not isinstance(options, dict):
+            options = {}
+        self.set_attributes(attributes)
+        self.options = options
+
+    def set_attributes(self, attributes):
+        for (
+            attribute,
+            default_value,
+        ) in RemoteServerConfigurationFile.default_attributes.items():
+            setattr(self, attribute, attributes.get(attribute, default_value))
+
+    def get_attributes(self):
+        return {
+            k: getattr(self, k, None)
+            for k in RemoteServerConfigurationFile.default_attributes
+            if getattr(self, k, None) is not None
+        }
+
+
+def new(*args, **kwargs):
+    return RemoteServerConfigurationFile(*args, **kwargs)
