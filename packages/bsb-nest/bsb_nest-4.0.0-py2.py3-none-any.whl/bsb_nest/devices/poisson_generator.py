1dd8e2bdf3895f883d0410a3ff08e27ee417e8af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-30 04:03:43 +0000

    Add package bsb-nest/4.0.0/bsb_nest-4.0.0-py2.py3-none-any.whl

---
+++ packages/bsb-nest/bsb_nest-4.0.0-py2.py3-none-any.whl/bsb_nest/devices/poisson_generator.py
@@ -0,0 +1,32 @@
+import nest
+from bsb import config
+from neo import SpikeTrain
+
+from ..device import NestDevice
+
+
+@config.node
+class PoissonGenerator(NestDevice, classmap_entry="poisson_generator"):
+    rate = config.attr(type=float, required=True)
+
+    def implement(self, adapter, simulation, simdata):
+        nodes = self.get_target_nodes(adapter, simulation, simdata)
+        device = self.register_device(
+            simdata, nest.Create("poisson_generator", params={"rate": self.rate})
+        )
+        sr = nest.Create("spike_recorder")
+        nest.Connect(device, sr)
+        self.connect_to_nodes(device, nodes)
+
+        def recorder(segment):
+            segment.spiketrains.append(
+                SpikeTrain(
+                    sr.events["times"],
+                    units="ms",
+                    senders=sr.events["senders"],
+                    t_stop=simulation.duration,
+                    device=self.name,
+                )
+            )
+
+        simdata.result.create_recorder(recorder)
