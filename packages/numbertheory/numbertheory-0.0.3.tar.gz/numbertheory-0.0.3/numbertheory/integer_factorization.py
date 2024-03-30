e102c5fd163961885f489b296167e147d2f1c87d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-30 05:02:56 +0000

    Add package numbertheory/0.0.3/numbertheory-0.0.3.tar.gz

---
+++ packages/numbertheory/numbertheory-0.0.3.tar.gz/numbertheory-0.0.3/numbertheory/integer_factorization.py
@@ -0,0 +1,34 @@
+from common_functions import gcd, mod_exp
+
+
+def brute_force_factorization(n: int) -> list[int]:
+    factors = []
+    divisor = 2
+
+    while n > 1:
+        while n % divisor == 0:
+            factors.append(divisor)
+            n //= divisor
+        divisor += 1
+    
+    return factors
+
+
+def fermat_factorization(n: int) -> tuple[int, int]:
+    a = int(n ** 0.5) + 1
+    b2 = a * a - n
+    while not int(b2 ** 0.5) ** 2 == b2:
+        a += 1
+        b2 = a * a - n
+    b = int(b2 ** 0.5)
+    p = a + b
+    q = a - b
+    return p, q
+
+
+def pollards_p_minus_one_factorization(n: int, b: int) -> tuple[int, int]:
+    a = 2
+    for j in range(2, b + 1):
+        a = mod_exp(a, j, n)
+    p = gcd(a - 1, n)
+    return p, n // p
---
+++ packages/numbertheory/numbertheory-0.0.3.tar.gz/numbertheory-0.0.3/numbertheory/modulus_operations.py
@@ -0,0 +1,19 @@
+from common_functions import extended_euclidean
+
+
+def mod_inverse(a: int, m: int) -> int:
+    x, _, gcd = extended_euclidean(a, m)
+    if gcd != 1:
+        raise ValueError("The modular inverse does not exist.")
+    return x % m if x >= 0 else x + m
+
+
+def chinese_remainder_theorem(moduli: list[int], remainders: list[int]) -> list[int]:
+    sum = 0
+    prod = 1
+    for n_i in moduli:
+        prod *= n_i
+    for n_i, a_i in zip(moduli, remainders):
+        p = prod // n_i
+        sum += a_i * mod_inverse(p, n_i) * p
+    return sum % prod
