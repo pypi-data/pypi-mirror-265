11b37a6749dcd7f706ae69e739cfcc66e5d2c3b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-30 03:03:40 +0000

    Add package winzig/0.2.19/winzig-0.2.19-py3-none-any.whl

---
+++ packages/winzig/winzig-0.2.19-py3-none-any.whl/winzig/management.py
@@ -0,0 +1,82 @@
+import csv
+import json
+from sqlalchemy import delete, select
+from sqlalchemy.ext.asyncio import AsyncSession
+from winzig.models import Feed
+from winzig.console import console
+
+
+async def remove_empty_feeds(session: AsyncSession) -> None:
+    try:
+        with console.status("Searching for empty feeds...", spinner="earth"):
+            statement = delete(Feed).where(~Feed.posts.any())
+            results = await session.execute(statement)
+            if results.rowcount:
+                console.log(
+                    f"[green bold]SUCCESS[/green bold]: {results.rowcount} empty feeds removed"
+                )
+            else:
+                console.log("[yellow bold]WARNING[/yellow bold]: No feeds removed")
+
+            await session.commit()
+    except Exception as e:
+        console.log(f"[red bold]ERROR[/red bold]: Failed to remove empty feeds: {e}")
+
+
+async def export_feeds_to_txt(session: AsyncSession, output: str) -> None:
+    try:
+        with console.status("Exporting feeds to plain text...", spinner="earth"):
+            results = await session.execute(select(Feed))
+            feeds = results.scalars().all()
+
+            with open(output, "w", encoding="utf-8") as f:
+                for feed in feeds:
+                    f.write(feed.url + "\n")
+
+        console.log(f"[green bold]SUCCESS[/green bold]: Feeds exported to {output}")
+    except Exception as e:
+        console.log(f"[red bold]ERROR[/red bold]: Failed to export feeds: {e}")
+
+
+async def export_feeds_to_csv(session: AsyncSession, output: str) -> None:
+    try:
+        with console.status("Exporting feeds to CSV...", spinner="earth"):
+            results = await session.execute(select(Feed))
+            feeds = results.scalars().all()
+
+            with open(output, "w", newline="", encoding="utf-8") as f:
+                writer = csv.writer(f)
+                writer.writerow(["id", "title", "url"])
+                for feed in feeds:
+                    writer.writerow([feed.id, f"{feed.title}", feed.url])
+
+        console.log(f"[green bold]SUCCESS[/green bold]: Feeds exported to {output}")
+    except Exception as e:
+        console.log(f"[red bold]ERROR[/red bold]: Failed to export feeds: {e}")
+
+
+def get_feeds_from_csv(file) -> list[str]:
+    with file as csvfile:
+        reader = csv.reader(csvfile)
+        next(reader)
+
+        feeds = [row[2] for row in reader]
+        return feeds
+
+
+async def export_feeds_to_json(session: AsyncSession, output: str):
+    try:
+        with console.status("Exporting feeds to JSON...", spinner="earth"):
+            results = await session.execute(select(Feed))
+            feeds = results.scalars().all()
+
+            feed_data = [
+                {"id": feed.id, "title": feed.title, "url": feed.url} for feed in feeds
+            ]
+
+            with open(output, "w", encoding="utf-8") as file:
+                json.dump(feed_data, file, ensure_ascii=False, indent=4)
+
+        console.log(f"[green bold]SUCCESS[/green bold]: Feeds exported to {output}")
+    except Exception as e:
+        console.log(f"[red bold]ERROR[/red bold]: Failed to export feeds: {e}")
---
+++ packages/winzig/winzig-0.2.19-py3-none-any.whl/winzig/utils.py
@@ -0,0 +1,31 @@
+import string
+import emoji
+
+emojis = set(emoji.EMOJI_DATA.keys())
+
+translation_table = str.maketrans(
+    {char: " " for char in string.punctuation + "“”’‘¶■▌▲▼└│─√©" + "".join(emojis)}
+)
+
+
+def update_url_scores(old: dict[str, float], new: dict[str, float]) -> dict[str, float]:
+    for url, score in new.items():
+        if url in old:
+            old[url] += score
+        else:
+            old[url] = score
+
+    return old
+
+
+def normalize_text(text: str) -> str:
+    normalized = text.translate(translation_table).lower()
+    normalized = " ".join(normalized.split())
+    return normalized
+
+
+def get_top_urls(scores_dict: dict, n: int):
+    sorted_urls = sorted(scores_dict.items(), key=lambda x: x[1], reverse=True)
+    top_n_urls = sorted_urls[:n]
+    top_n_dict = dict(top_n_urls)
+    return top_n_dict
