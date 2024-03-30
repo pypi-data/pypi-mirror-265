6bab00d95aff27169df29332efda8faad24ca30a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-30 04:03:59 +0000

    Add package ape-infura/0.7.2/ape_infura-0.7.2-py3-none-any.whl

---
+++ packages/ape-infura/ape_infura-0.7.2-py3-none-any.whl/ape_infura/__init__.py
@@ -0,0 +1,36 @@
+from ape import plugins
+
+from .provider import Infura
+
+NETWORKS = {
+    "ethereum": [
+        "mainnet",
+        "goerli",
+        "sepolia",
+    ],
+    "arbitrum": [
+        "mainnet",
+        "sepolia",
+    ],
+    "optimism": [
+        "mainnet",
+        "sepolia",
+    ],
+    "polygon": [
+        "mainnet",
+        "mumbai",
+        "amoy",
+    ],
+    # TODO: Comment out after ape-linea supports 0.7
+    # "linea": [
+    #     "mainnet",
+    #     "goerli",
+    # ],
+}
+
+
+@plugins.register(plugins.ProviderPlugin)
+def providers():
+    for ecosystem_name in NETWORKS:
+        for network_name in NETWORKS[ecosystem_name]:
+            yield ecosystem_name, network_name, Infura
