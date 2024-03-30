58836050127c99f4f28cd87c7691ced645be0463
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-30 15:04:04 +0000

    Add package hmeg/0.1.0/hmeg-0.1.0.tar.gz

---
+++ packages/hmeg/hmeg-0.1.0.tar.gz/hmeg-0.1.0/hmeg/inflections.py
@@ -0,0 +1,7 @@
+# dictionary with fixes for conjugation of the mlconjug3
+verbs_singular_3rd = {
+    'borrow': 'borrows',
+    'hurt': 'hurts',
+    'look for': 'looks for',
+    'name': 'names',
+}
