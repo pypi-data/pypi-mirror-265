e189fb5a2264dc68ff309b5253b813f3d3a836df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-30 04:05:31 +0000

    Add package esa-2scm/0.1.7/esa-2scm-0.1.7.tar.gz

---
+++ packages/esa-2scm/esa-2scm-0.1.7.tar.gz/esa-2scm-0.1.7/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    README = fh.read()
+
+setup(
+    author="Sanghoon Lee (DSsoli)",
+    author_email="solisoli3197@gmail.com",
+    name="esa-2scm",
+    version="0.1.7",
+    description="ESA-2SCM Python Package for Causal Discovery",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    install_requires=["numpy", "pandas", "scipy", "scikit-learn"],
+    url="https://github.com/DSsoli/esa-2scm.git",
+    packages=find_packages(include=['esa_2scm', 'esa_2scm.*']),
+    package_data={"esa_2scm": ['LICENSE', 'examples/*']},
+    include_package_data=True
+)
