bc78af848fae42b60de18e4f5c5c0150d5c5f035
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-30 14:03:59 +0000

    Add package vesselasid/1.0.10/vesselasid-1.0.10.tar.gz

---
+++ packages/vesselasid/vesselasid-1.0.10.tar.gz/vesselasid-1.0.10/vesselasid/baserender.py
@@ -0,0 +1,25 @@
+import logging
+from vesselasid.asid import Asid
+
+
+class VesselAsidRenderer:
+    def __init__(self, asid):
+        self.asid = asid
+
+    def start(self):
+        return
+
+    def stop(self):
+        return
+
+    def note_on(self, msg):
+        return
+
+    def note_off(self, msg):
+        return
+
+    def control_change(self, msg):
+        return
+
+    def pitchwheel(self, msg):
+        return
