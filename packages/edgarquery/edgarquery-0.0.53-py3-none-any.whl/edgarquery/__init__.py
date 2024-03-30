bdc38e39501209fb97fb25f4ae4fe2504b466534
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-30 05:03:12 +0000

    Add package edgarquery/0.0.53/edgarquery-0.0.53-py3-none-any.whl

---
+++ packages/edgarquery/edgarquery-0.0.53-py3-none-any.whl/edgarquery/__init__.py
@@ -0,0 +1,17 @@
+# SPDX-FileCopyrightText: 2023-present Don Caldwell <dfwcnj@gmail.com>
+#
+# SPDX-License-Identifier: MIT
+# https://www.sec.gov/edgar/sec-api-documentation
+
+import edgarquery.common
+# import edgarquery.companyconcepttocsv
+# import edgarquery.companyfactstocsv
+# import edgarquery.companyfactsziptocsv
+# import edgarquery.latest10K
+# import edgarquery.latestsubmissions
+# import edgarquery.doquery
+# import edgarquery.submissions
+# import edgarquery.submissionsziptocsv
+# import edgarquery.tickerstocsv
+# import edgarquery.xbrlframestocsv
+# import edgarquery.cikperson
