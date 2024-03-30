48535985c440acfef482c809557635f0c3db05e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-30 04:04:25 +0000

    Add package WFMPlan/0.1.0/WFMPlan-0.1.0.tar.gz

---
+++ packages/WFMPlan/WFMPlan-0.1.0.tar.gz/WFMPlan-0.1.0/setup.py
@@ -0,0 +1,12 @@
+import setuptools 
+import pathlib
+
+setuptools.setup(
+    name='WFMPlan',
+    version='0.1.0',
+    packages=setuptools.find_packages(),
+    description = " Test lib",
+    install_requires=[
+        # list your dependencies here
+    ],
+)
