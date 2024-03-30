7ba2bd3f550e938f471034c85408b670fa50f566
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-30 04:03:55 +0000

    Add package allphins-sdk/1.0.6/allphins_sdk-1.0.6-py3-none-any.whl

---
+++ packages/allphins-sdk/allphins_sdk-1.0.6-py3-none-any.whl/allphins/const.py
@@ -0,0 +1,11 @@
+"""Constants for the project."""
+import os
+
+ALLPHINS_API_URL = f"{os.environ.get('ALLPHINS_URL', 'https://app.allphins.com')}/api/v1"
+ACCESS_TOKEN_URL = f'{ALLPHINS_API_URL}/token/'
+REFRESH_TOKEN_URL = f'{ALLPHINS_API_URL}/token/refresh/'
+
+GET = 'GET'
+POST = 'POST'
+
+ISO_8601 = '%Y-%m-%d'
