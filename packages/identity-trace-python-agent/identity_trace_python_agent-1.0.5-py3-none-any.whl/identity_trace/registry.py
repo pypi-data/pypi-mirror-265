e74948096dfe1f3091d82d4b3896a17944db063c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-30 05:03:23 +0000

    Add package identity-trace-python-agent/1.0.5/identity_trace_python_agent-1.0.5-py3-none-any.whl

---
+++ packages/identity-trace-python-agent/identity_trace_python_agent-1.0.5-py3-none-any.whl/identity_trace/registry.py
@@ -0,0 +1,29 @@
+
+
+_function_registry = {}
+__function_registry = {}
+
+
+def register_frame(frame, execution_context):
+    _function_registry[_get_frame_id(frame)] = execution_context
+
+
+def is_frame_registered(frame):
+    return _function_registry.get(_get_frame_id(frame), None)
+
+
+def remove_frame(frame):
+    if _function_registry.get(frame, None):
+        del _function_registry[frame]
+
+
+def _get_frame_id(frame):
+    return id(frame)
+
+
+def register_function(function_name, func):
+    __function_registry[function_name] = func
+
+
+def get_function_by_name(function_name):
+    return __function_registry.get(function_name, None)
