4d958a2f3ec2a0c6ab7cdadfda0253a9d02e44f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-30 14:03:50 +0000

    Add package pix2text/1.0.2.3/pix2text-1.0.2.3-py3-none-any.whl

---
+++ packages/pix2text/pix2text-1.0.2.3-py3-none-any.whl/scripts/try_service.py
@@ -0,0 +1,33 @@
+# coding: utf-8
+
+import requests
+
+
+def main():
+    url = 'http://0.0.0.0:8503/pix2text'
+
+    image_fp = 'docs/examples/mixed.jpg'
+    # image_fp = 'docs/examples/math-formula-42.png'
+    # image_fp = 'docs/examples/english.jpg'
+    data = {
+        "image_type": "mixed",
+        "resized_shape": 768,
+        "embed_sep": " $,$ ",
+        "isolated_sep": "$$\n, \n$$"
+    }
+    files = {
+        "image": (image_fp, open(image_fp, 'rb'), 'image/jpeg')
+    }
+
+    r = requests.post(url, data=data, files=files)
+
+    outs = r.json()['results']
+    if isinstance(outs, str):
+        only_text = outs
+    else:
+        only_text = '\n'.join([out['text'] for out in outs])
+    print(f'{only_text=}')
+
+
+if __name__ == '__main__':
+    main()
