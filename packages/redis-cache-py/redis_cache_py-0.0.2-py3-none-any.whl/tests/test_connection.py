fb7f10290e030dd62b7775268457417706bb1aeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-30 04:04:41 +0000

    Add package redis-cache-py/0.0.2/redis_cache_py-0.0.2-py3-none-any.whl

---
+++ packages/redis-cache-py/redis_cache_py-0.0.2-py3-none-any.whl/tests/test_connection.py
@@ -0,0 +1,50 @@
+# Test redis cache connection from environment variable or argument passing to
+# RedisCache
+import os
+import unittest
+from redis_cache_py import RedisCache, Singleton
+from redis.exceptions import ConnectionError
+
+
+class TestRedisCacheConnection(unittest.TestCase):
+
+    def test_connect_via_host_port(self):
+        # Disable Singleton for testing
+        Singleton.disable = True
+        redis_cache = RedisCache(host="127.0.0.1", port=6379)
+        assert redis_cache.client.ping() is True
+
+    def test_connect_via_host_port_wrong(self):
+        # Redis does not listening on port 6380
+        Singleton.disable = True
+        redis_cache = RedisCache(
+            host="127.0.0.1",
+            port=99999,
+        )
+        self.assertRaises(
+            ConnectionError,
+            redis_cache.client.ping
+        )
+
+    def test_connect_via_url(self):
+        Singleton.disable = True
+        redis_cache = RedisCache(redis_url="redis://127.0.0.1:6379")
+        assert redis_cache.client.ping() is True
+
+    def test_connect_via_env(self):
+        Singleton.disable = True
+        os.environ["REDIS_URL"] = "redis://127.0.0.1:6379"
+        # Provide incorrect host, port to demostrate passing redis_url via Env possible
+        redis_cache = RedisCache(host="fakehost", port=1)
+        os.environ["REDIS_URL"] = "" # Reset
+        assert redis_cache.client.ping() is True
+
+        os.environ["REDIS_CACHE_URL"] = "redis://127.0.0.1:6379"
+        # Provide incorrect host, port to demostrate passing redis_url via Env possible
+        redis_cache = RedisCache(host="fakehost", port=1)
+        os.environ["REDIS_CACHE_URL"] = "" # Reset
+        assert redis_cache.client.ping() is True
+
+
+if __name__ == '__main__':
+    unittest.main()
