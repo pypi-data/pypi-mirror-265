bbc2d79de319cacf2d87e38e2567cdef81b503e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-30 04:03:27 +0000

    Add package flaskws3/3.0.2/flaskws3-3.0.2.tar.gz

---
+++ packages/flaskws3/flaskws3-3.0.2.tar.gz/flaskws3-3.0.2/flaskws/defs.py
@@ -0,0 +1,163 @@
+#-*- coding: utf-8 -*-
+
+import time
+import threading
+from io import StringIO
+
+
+crlf = '\r\n'
+ws_uid = '258EAFA5-E914-47DA-95CA-C5AB0DC85B11'
+
+MAX_FRAME_SIZE = 1024 * 8
+
+OP_CONTINUATION = 0
+OP_TEXT = 1
+OP_BINARY = 2
+OP_CLOSE = 8
+OP_PING = 9
+OP_PONG = 0xA
+
+
+class WsError(Exception):
+    
+    def __init__(self, *args):
+        self.msg = args[0] if args else None
+
+    def __str__(self):
+        if self.msg:
+            if isinstance(self.msg, unicode):
+                return self.msg.encode('utf-8')
+        return self.msg or ''
+
+
+class WsTimeout(WsError):
+    
+    def __init__(self, *args):
+        self.msg = args[0] if args else None
+
+
+class WsIOError(WsError): pass
+
+
+class WsClosedByRemote(Warning): pass
+
+
+class HTTPError(Exception):
+
+    def __init__(self, code, msg):
+        self.code = code
+        if msg and isinstance(msg, unicode):
+            msg = msg.encode('utf-8')
+        self.msg = msg or ''
+
+    def __str__(self):
+        return '%d: %s' % (self.code, self.msg)
+
+
+class EOF(Exception): pass
+
+
+class Headers(dict):
+
+    def __init__(self, *args, **kargs):
+        super(Headers, self).__init__(*args, **kargs)
+
+    def get_lower_list(self, key):
+        if key in self:
+            v = self[key]
+            if isinstance(v, (list, tuple)):
+                return [v1.lower() for v1 in v]
+            else:
+                return [v.lower()]
+        return []
+
+class StrReader(object):
+
+    def __init__(self, d):
+        self.d = d or ''
+        self.cur = 0
+
+    def read(self, size):
+        cur = self.cur + size
+        if cur > len(self.d):
+            raise EOF()
+        v = self.d[self.cur:self.cur + size]
+        self.cur += size
+        return v
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args, **kargs):
+        pass
+
+
+class PromisedReader(object):
+
+    def __init__(self, f):
+        self.f = f
+        self.buff = StringIO()
+        self.evt_close = threading.Event()
+        self.read_lock = threading.Lock()
+
+    def read(self, size, timeout=60.0):
+        if not size:
+            return ''
+        try:
+            self.read_lock.acquire(True)
+            self.buff.seek(0)
+            self.buff.truncate()
+            # print '---- read(%d) ----' % size
+            while not self.evt_close.is_set():
+                _size = size - self.buff.tell()
+                # print 'raw_read(%d): ' % _size
+                data = self.f.read(_size)
+                # print '%d bytes.' % (len(data) if data else 0)
+                if data:
+                    self.buff.write(data)
+                    # print 'buff.tell(): %d' % self.buff.tell()
+                    if self.buff.tell() >= size:
+                        return self.buff.getvalue()
+        finally:
+            self.read_lock.release()
+
+    def close(self):
+        self.f = None
+        self.buff = None
+        if self.evt_close:
+            if not self.evt_close.is_set():
+                self.evt_close.set()
+                self.evt_close = None
+            
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args, **kargs):
+        self.close()
+
+
+class BlockedReader(object):
+    '''convert a tornado stream to a blocked reader'''
+
+    def __init__(self, f):
+        self.f = f
+        self.data = ''
+        self.evt_ready = threading.Event()
+
+    def _on_ready(self, data):
+        if data:
+            self.data += data
+        self.evt_ready.set()
+
+    def read(self, size, timeout=5.0):
+        self.data = ''
+        while True:
+            self.evt_ready.clear()
+            try:
+                self.f.read_bytes(size, self._on_ready)
+            except:
+                raise WsIOError()
+            self.evt_ready.wait()
+            if len(self.data) >= size:
+                break
+        return self.data
---
+++ packages/flaskws3/flaskws3-3.0.2.tar.gz/flaskws3-3.0.2/flaskws/ws.py
@@ -0,0 +1,188 @@
+#-*- coding: utf-8 -*-
+
+import functools
+import json
+import logging
+import traceback
+from werkzeug.routing import Map, Rule
+# from werkzeug.wrappers import Response as BaseResponse
+# from werkzeug.datastructures import Headers
+import threading
+from .defs import *
+from .wssock import WsSocket, _BaseWsSock
+from .wsuwsgi import UwsgiWsSock, has_uwsgi
+from .wstornado import TornadoWebSocketAdapter
+
+    
+class WsDeliver(Exception):
+
+    def __init__(self, response):
+        self.response = response
+
+    
+class WsMiddleware(object):
+
+    def __init__(self, wsgi_app, *args, **kargs):
+        self.wsgi_app = wsgi_app
+        self.app = args[0] if args else None
+        self.wss = args[1] if len(args) > 1 else None
+        self.use_tornado = kargs.get('use_tornado', False)
+
+    def __call__(self, *args):
+        try:
+            return self._call(*args)
+        finally:
+            pass
+
+    def _call(self, environ, start_response):
+        logging.debug('WsMiddleware.__call__')
+        # join dead threads
+        curr_th = threading.current_thread()
+        for th in threading.enumerate():
+            if not th or th == curr_th:
+                continue
+            if th.is_alive():
+                continue
+            th.join()
+            logging.debug('thread %s joinned.' % th.name)
+        try:
+            return self.wsgi_app(environ, start_response)
+        except WsDeliver as e:
+            if has_uwsgi:
+                key = environ.get('HTTP_SEC_WEBSOCKET_KEY', '')
+                origin = environ.get('HTTP_ORIGIN', '')
+                origin = ''
+                # print key
+                # print origin
+                uwsgi.websocket_handshake(key, origin)
+            resp = e.response
+            return resp.run(environ, start_response, self.use_tornado)
+
+
+class WsServer(object):
+
+    def __init__(self, app):
+        self.map = Map()
+        self.app = app
+        app.wsgi_app = WsMiddleware(app.wsgi_app, app, self)
+
+    def add_rule(self, rule):
+        self.map.add(rule)
+
+    def route(self, pattern):
+        def wrapper_maker(view):
+            self.add_rule(Rule(pattern, endpoint=view))
+            @functools.wraps(view)
+            def wrapper(*args, **kargs):
+                return view(*args, **kargs)
+            return wrapper
+        return wrapper_maker
+
+
+class WsResponse(object):
+
+    def __init__(self, handler, **kargs):
+        self.handler = handler
+        self.values = kargs
+
+    def run(self, environ, start_response, use_tornado=False):
+        if use_tornado:
+            _t_app = environ['x.tornado.app']
+            _t_request = environ['x.tornado.request']
+            sock = TornadoWebSocketAdapter(environ, _t_app, _t_request)
+            if sock.handshake():
+                return sock.handle(self.handler, self.values)
+            else:
+                return sock.html()
+        if has_uwsgi:
+            sock = UwsgiWsSock(environ)
+            return sock.serve_handler(self.handler, self.values)
+        sock = WsSocket(environ, self.handler, self.values)
+        if sock.handshake(environ, start_response):
+            return sock()
+        else:
+            return sock.html(environ, start_response)
+
+    def __call__(self, environ, start_response):
+        raise WsDeliver(self)
+
+
+class WsResponseForServer(object):
+
+    def __init__(self, server_cls, **kargs):
+        self.server_cls = server_cls
+        self.values = kargs
+
+    def run(self, environ, start_response, use_tornado):
+        if use_tornado:
+            logging.debug('WsResponseForServer.run(), use_tornado.')
+            _t_app = environ['x.tornado.app']
+            _t_request = environ['x.tornado.request']
+            sock = TornadoWebSocketAdapter(environ, _t_app, _t_request)
+            if sock.handshake():
+                logging.debug('WsResponseForServer.run(), handshake ok.')
+                return sock.server(self.server_cls(sock, **self.values))
+            else:
+                logging.debug('WsResponseForServer.run(), handshake fail.')
+                return sock.html()
+        if has_uwsgi:
+            # key = environ.get('HTTP_SEC_WEBSOCKET_KEY', '')
+            # origin = environ.get('HTTP_ORIGIN', '')
+            # uwsgi.websocket_handshake(key, origin)
+            sock = UwsgiWsSock(environ)
+            return sock.server(self.server_cls(sock, **self.values))
+        sock = WsSocket(environ, None, None)
+        if sock.handshake(environ, start_response):
+            return sock.server(self.server_cls(sock, **self.values))
+        else:
+            return sock.html(environ, start_response)
+
+    def __call__(self, environ, start_response):
+        logging.debug('WsResponseForServer.__call__: raise deliver')
+        raise WsDeliver(self)
+
+
+def ws_server_view(handler):
+    @functools.wraps(handler)
+    def wrapper(**kargs):
+        return WsResponse(handler, **kargs)
+    return wrapper
+
+
+def ws_server(server_cls):
+    @functools.wraps(server_cls)
+    def wrapper(**kargs):
+        return WsResponseForServer(server_cls, **kargs)
+    return wrapper
+
+
+# class WsAppDirect(object):
+#     u'''用于直接使用socket实现http服务器io的情形
+#     例如from werkzeug.serving import run_simple'''
+
+#     def __init__(self, server_cls, **kargs):
+#         self.server_cls = server_cls
+#         self.kargs = kargs
+
+#     def __call__(self, environ, start_response):
+#         sock = WsSocket(environ, None, None)
+#         resp = None
+#         if not sock.handshake(environ, start_response):
+#             resp = sock.html(environ, start_response)
+#         else:
+#             print 'handshake ok.'
+#             resp = sock.server(self.server_cls(sock, **self.kargs))
+#         print type(resp)
+#         print resp
+#         for i in resp:
+#             print i
+#             yield i
+#         print '--- done. ---'
+
+# def ws_server_direct(server_cls):
+#     u'''用于直接使用socket实现http服务器io的情形
+#     例如from werkzeug.serving import run_simple'''
+#     @functools.wraps(server_cls)
+#     def wrapper(**kargs):
+#         return WsAppDirect(server_cls, **kargs)
+#     return wrapper
