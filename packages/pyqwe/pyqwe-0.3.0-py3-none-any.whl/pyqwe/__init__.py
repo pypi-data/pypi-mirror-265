fba73a2fbf227cd5c022ff3dd23732d0616fe30b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-30 05:03:23 +0000

    Add package pyqwe/0.3.0/pyqwe-0.3.0-py3-none-any.whl

---
+++ packages/pyqwe/pyqwe-0.3.0-py3-none-any.whl/pyqwe/__init__.py
@@ -0,0 +1,45 @@
+import tomllib
+from pathlib import Path
+
+from .helpers import *
+from .parser import ArgumentParser
+
+__version__ = "0.3.0"
+
+# sr = start of runner
+# er = end of runner
+# runner = sr:er
+# flask = app:run
+# will invoke the run function in the app module
+
+_cwd = Path().cwd()
+_pyproject_file = _cwd / "pyproject.toml"
+
+if not _pyproject_file.exists():
+    raise FileNotFoundError("pyproject.toml not found")
+
+_pyproject = tomllib.loads(_pyproject_file.read_text())
+_qwe = _pyproject.get("tool", {}).get("pyqwe")
+
+if not _qwe or not isinstance(_qwe, dict):
+    raise ValueError("tool.qwe not found in pyproject.toml")
+
+
+def main():
+    pars = ArgumentParser(prog="pyqwe", add_help=False)
+    pars.add_argument("--version", "-v", action="version", version=f"qwe {__version__}")
+    pars.add_argument("--help", "-h", action="help")
+    subp = pars.add_subparsers()
+
+    for entry, runner in _qwe.items():
+        pars.options.append((entry, runner))
+        _ = subp.add_parser(entry)
+        _.set_defaults(entry=entry, runner=runner)
+
+    args = pars.parse_args()
+
+    try:
+        _run(*_split_runner(args.runner), _cwd=_cwd)
+    except AttributeError as e:
+        _ = e
+        pars.print_help()
