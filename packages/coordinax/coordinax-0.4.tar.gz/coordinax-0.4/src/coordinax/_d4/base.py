9b6d459cd08116398d369b9bf02afbbf7606a7d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-30 15:03:38 +0000

    Add package coordinax/0.4/coordinax-0.4.tar.gz

---
+++ packages/coordinax/coordinax-0.4.tar.gz/coordinax-0.4/src/coordinax/_d4/base.py
@@ -0,0 +1,30 @@
+"""Representation of coordinates in different systems."""
+
+__all__ = ["Abstract4DVector"]
+
+
+from abc import abstractmethod
+from typing import TYPE_CHECKING
+
+from coordinax._base import AbstractVectorBase
+from coordinax._base_vec import AbstractVector
+from coordinax._utils import classproperty
+
+if TYPE_CHECKING:
+    from typing_extensions import Never
+
+
+class Abstract4DVector(AbstractVector):
+    """Abstract representation of 4D coordinates in different systems."""
+
+    @classproperty
+    @classmethod
+    def _cartesian_cls(cls) -> type[AbstractVectorBase]:
+        raise NotImplementedError
+
+    @classproperty
+    @classmethod
+    @abstractmethod
+    def differential_cls(cls) -> "Never":  # type: ignore[override]
+        msg = "Not yet implemented"
+        raise NotImplementedError(msg)
