aa3d5d66c3f61d5bdebb272b24711f285564a207
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-30 04:04:38 +0000

    Add package redis-cache-py/0.0.1/redis_cache_py-0.0.1-py3-none-any.whl

---
+++ packages/redis-cache-py/redis_cache_py-0.0.1-py3-none-any.whl/tests/test_singleton.py
@@ -0,0 +1,29 @@
+import os
+import unittest
+from py_redis_cache import RedisCache, Singleton
+
+
+class TestRedisCacheConnection(unittest.TestCase):
+
+    def test_two_calls(self):
+        # Enable Singleton for testing this case
+        Singleton.disable = False
+
+        redis_cache = RedisCache()
+        redis_cache2 = RedisCache()
+
+        assert redis_cache is redis_cache2  # Sample object
+
+    def test_current_redis_cache(self):
+        # Enable Singleton for testing this case
+        Singleton.disable = False
+
+        redis_cache = RedisCache()
+        
+        from py_redis_cache import current_redis_cache
+
+        assert redis_cache is current_redis_cache  # Sample object
+
+
+if __name__ == '__main__':
+    unittest.main()
