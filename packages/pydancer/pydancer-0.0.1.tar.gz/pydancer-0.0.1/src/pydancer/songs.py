7d0007deeb03a1069eb33e29804330d1d5665fb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-30 05:02:55 +0000

    Add package pydancer/0.0.1/pydancer-0.0.1.tar.gz

---
+++ packages/pydancer/pydancer-0.0.1.tar.gz/pydancer-0.0.1/src/pydancer/songs.py
@@ -0,0 +1,8 @@
+# prob want this to just read the titles of song files instead of hard coded?
+def listSongs(genre):
+    if genre == 'rock':
+        return "Rock Songs:\nBlood And Steel"
+    elif genre == 'house':
+        return "House Songs:\nTwo In The Rain"
+    else:
+        return "Rock Songs:\nBlood And Steel \nHouse Songs:\nTwo In The Rain"
