92b6f73c95528f45e8a28a7752cf3591f0953524
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-30 15:03:43 +0000

    Add package albanian-tools/0.0.4/albanian_tools-0.0.4.tar.gz

---
+++ packages/albanian-tools/albanian_tools-0.0.4.tar.gz/albanian_tools-0.0.4/src/albanian_tools/example.py
@@ -0,0 +1,11 @@
+from albanian_tools import AlbanianTools
+
+albanian_tools = AlbanianTools()
+
+if __name__ == '__main__':
+    print(albanian_tools.integer_to_albanian_text_converter(217))
+    print(albanian_tools.integer_to_albanian_text_converter(451))
+    print(albanian_tools.integer_to_albanian_text_converter(19))
+    print(albanian_tools.integer_to_albanian_text_converter(717))
+    print(albanian_tools.integer_to_albanian_text_converter(888))
+
