efe59e19a9693266477cef079a65e55449eea399
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-30 15:04:05 +0000

    Add package comic-parser/0.0.1/comic_parser-0.0.1-py3-none-any.whl

---
+++ packages/comic-parser/comic_parser-0.0.1-py3-none-any.whl/comic-parser/comic-parser.py
@@ -0,0 +1,17 @@
+import os
+import img2pdf
+from PIL import Image
+
+for pathRoot, subdirsRoot, filesRoot in os.walk('./'):
+    for dirNames in subdirsRoot:
+        imagenes = []
+
+        for path, subdirs, files in os.walk(dirNames):
+            for name in files:
+                if ".py" not in name and ".pdf" not in name and ".db" not in name:
+                    imagenes.append(os.path.join(path, name))
+
+        imagenes.sort()
+
+        with open(os.path.basename(os.getcwd() + dirNames) + ".pdf", "wb") as manga:
+            manga.write(img2pdf.convert(imagenes))
