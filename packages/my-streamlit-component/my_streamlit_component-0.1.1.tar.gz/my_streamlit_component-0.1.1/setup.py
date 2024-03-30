156a4eaaf4e0b98ebe39dbeea2df0af449ddfad1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-30 03:04:04 +0000

    Add package my-streamlit-component/0.1.1/my_streamlit_component-0.1.1.tar.gz

---
+++ packages/my-streamlit-component/my_streamlit_component-0.1.1.tar.gz/my_streamlit_component-0.1.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="my_streamlit_component",
+    version="0.1.1",
+    author="Charly Wargnier",
+    author_email="cwar05@gnail.com",
+    description="A custom Streamlit component for Chart.js",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "streamlit",
+        # Any other dependencies your component needs
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
