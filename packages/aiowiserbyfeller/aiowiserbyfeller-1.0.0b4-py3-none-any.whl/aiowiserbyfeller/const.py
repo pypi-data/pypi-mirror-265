a4b9b9b2b0e012425369cc81ab05c42150ddd943
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-30 03:03:26 +0000

    Add package aiowiserbyfeller/1.0.0b4/aiowiserbyfeller-1.0.0b4-py3-none-any.whl

---
+++ packages/aiowiserbyfeller/aiowiserbyfeller-1.0.0b4-py3-none-any.whl/aiowiserbyfeller/const.py
@@ -0,0 +1,11 @@
+"""Various value constants."""
+
+# Types for light loads
+TYPE_LIGHT = 0
+TYPE_SWITCH = 1
+
+# Types for motor loads
+TYPE_MOTOR = 0
+TYPE_VENETIAN_BLINDs = 1
+TYPE_ROLLER_SHUTTER = 2
+TYPE_AWNING = 3
