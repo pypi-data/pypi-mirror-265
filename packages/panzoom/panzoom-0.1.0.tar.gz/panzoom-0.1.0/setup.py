5ff0afdf805ac5691cecaf03de246ae8538869a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-30 04:04:00 +0000

    Add package panzoom/0.1.0/panzoom-0.1.0.tar.gz

---
+++ packages/panzoom/panzoom-0.1.0.tar.gz/panzoom-0.1.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="panzoom",
+    version="0.1.0",
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'panzoom=panzoom.cli:main',
+        ],
+    },
+    install_requires=[
+        'numpy',
+        'opencv-python',
+        'Pillow',
+        'requests',
+    ],
+    author="Nazim Zeeshan",
+    author_email="nazim.zeeshan@gmail.com",
+    description="A CLI tool for creating pan and zoom videos from images.",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/nzee/panzoom",
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
