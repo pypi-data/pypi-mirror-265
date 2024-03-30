f0eec0ddd8c64828cc5739589d109f26ab30ca4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-30 03:04:11 +0000

    Add package django-easy-validation/1.1/django-easy-validation-1.1.tar.gz

---
+++ packages/django-easy-validation/django-easy-validation-1.1.tar.gz/django-easy-validation-1.1/setup.py
@@ -0,0 +1,13 @@
+# setup.py
+from setuptools import setup
+setup(
+ name='django-easy-validation',
+ version='1.1',
+ description='Easy Validation for Django based projects especially for Ajax based request.',
+ readme="README.md",
+ url='https://github.com/sandysh/django-easy-validation',
+ author='Sandesh Satyal',
+ author_email='sandeshsatyal@gmail.com',
+ license='MIT',
+ zip_safe=False,
+)
