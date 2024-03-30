a0c40ee8e38636f9eb3a82027e2a9ea2c446fc32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-30 04:04:06 +0000

    Add package drex/0.0.24/drex-0.0.24.tar.gz

---
+++ packages/drex/drex-0.0.24.tar.gz/drex-0.0.24/Simulation/src/main.py
@@ -0,0 +1,8 @@
+# D-rex simulation start by reading the set of input data that needs to be replicated
+# then call an algorithm to chunk and distribute the data on the storage nodes.
+# Lastly data about time, space and saturation are written in an output file.
+
+import sys
+from algorithm import
+from read_input import
+from write_output import
