babd0f136bd3c44ca7e55d1ca8f3f6086db32dd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-30 03:03:20 +0000

    Add package mteb/1.3.2/mteb-1.3.2-py3-none-any.whl

---
+++ packages/mteb/mteb-1.3.2-py3-none-any.whl/mteb/tasks/Classification/multilingual/NordicLangClassification.py
@@ -0,0 +1,59 @@
+from __future__ import annotations
+
+import datasets
+
+from mteb.abstasks import AbsTaskClassification
+from mteb.abstasks.TaskMetadata import TaskMetadata
+
+
+class NordicLangClassification(AbsTaskClassification):
+    metadata = TaskMetadata(
+        name="NordicLangClassification",
+        description="A dataset for Nordic language identification.",
+        reference="https://aclanthology.org/2021.vardial-1.8/",
+        hf_hub_name="strombergnlp/nordic_langid",
+        type="Classification",
+        category="s2s",
+        eval_splits=["test"],
+        eval_langs=["no", "nn"],
+        main_score="accuracy",
+        revision="e254179d18ab0165fdb6dbef91178266222bee2a",
+        date=None,
+        form=None,
+        domains=None,
+        task_subtypes=None,
+        license=None,
+        socioeconomic_status=None,
+        annotations_creators=None,
+        dialect=None,
+        text_creation=None,
+        bibtex_citation=None,
+        n_samples={"test": 3000},
+        avg_character_length={"test": 78.2},
+    )
+
+    @property
+    def metadata_dict(self) -> dict[str, str]:
+        metadata_dict = dict(self.metadata)
+        metadata_dict["n_experiments"] = 10
+        metadata_dict["samples_per_label"] = 32
+        return metadata_dict
+
+    def load_data(self, **kwargs):
+        """
+        Load dataset from HuggingFace hub
+        """
+        if self.data_loaded:
+            return
+
+        self.dataset = datasets.load_dataset(
+            self.metadata_dict["hf_hub_name"],
+            "10k",
+            revision=self.metadata_dict.get("revision"),  # select relevant subset
+        )
+        self.dataset_transform()
+        self.data_loaded = True
+
+    def dataset_transform(self):
+        self.dataset = self.dataset.rename_column("sentence", "text")
+        self.dataset = self.dataset.rename_column("language", "label")
---
+++ packages/mteb/mteb-1.3.2-py3-none-any.whl/mteb/tasks/Clustering/es/FloresClusteringS2S.py
@@ -0,0 +1,32 @@
+from __future__ import annotations
+
+from mteb.abstasks.TaskMetadata import TaskMetadata
+
+from ....abstasks.AbsTaskClustering import AbsTaskClustering
+
+
+class FloresClusteringS2S(AbsTaskClustering):
+    metadata = TaskMetadata(
+        name="FloresClusteringS2S",
+        description="Clustering of sentences from various web articles, 32 topics in total.",
+        reference="https://huggingface.co/datasets/facebook/flores",
+        hf_hub_name="mteb/flores",
+        type="Clustering",
+        category="s2s",
+        eval_splits=["test"],
+        eval_langs=["es"],
+        main_score="v_measure",
+        revision="480b580487f53a46f881354a8348335d4edbb2de",
+        date=None,
+        form=None,
+        domains=None,
+        task_subtypes=None,
+        license=None,
+        socioeconomic_status=None,
+        annotations_creators=None,
+        dialect=None,
+        text_creation=None,
+        bibtex_citation=None,
+        n_samples=None,
+        avg_character_length=None,
+    )
