004970ec067422a8ba3ed5938b55aee16366a9cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-30 14:04:02 +0000

    Add package pybhspc/0.1.0/pybhspc-0.1.0-cp310-cp310-win_amd64.whl

---
+++ packages/pybhspc/pybhspc-0.1.0-cp310-cp310-win_amd64.whl/bh_spc/__init__.py
@@ -0,0 +1,94 @@
+# This file is part of pybhspc
+# Copyright 2024 Board of Regents of the University of Wisconsin System
+# SPDX-License-Identifier: MIT
+
+# ruff: noqa: E402    # Module level import not at top of file
+# ruff: noqa: TRY003  # Avoid specifying long messages outside exception class
+
+"""
+The root package of pybhspc.
+
+For low-level control of the SPCM DLL, see the `spcm` module. Some utility
+functions are provided here in the root package.
+"""
+
+__all__ = [
+    "dump_module_state",
+    "dump_state",
+    "ini_file",
+    "minimal_spcm_ini",
+    "spcm",
+    "spcm_dll_version",
+]
+
+import functools
+import os
+import os.path
+import platform
+import winreg
+
+from ._file_version import dll_file_version as _dll_file_version
+from ._version import __version__  # noqa: F401
+
+if platform.machine() != "AMD64":
+    raise RuntimeError("Only supported on Windows x64")
+
+
+@functools.cache
+def _spcm_dll_dir() -> str:
+    candidates = []
+    try:
+        with winreg.OpenKey(
+            winreg.HKEY_CURRENT_USER, "SOFTWARE\\BH\\SPCM"
+        ) as spcm_key:
+            exe_path, _ = winreg.QueryValueEx(spcm_key, "FilePath")
+        candidates.append(os.path.join(os.path.dirname(exe_path), "DLL"))
+    except OSError:
+        pass
+
+    # Fallback in case registry key missing for some reason.
+    fixed_dir = "C:/Program Files (x86)/BH/SPCM/DLL"
+    if fixed_dir not in candidates:
+        candidates.append(fixed_dir)
+
+    for dll_dir in candidates:
+        if os.path.isdir(dll_dir) and os.path.exists(
+            os.path.join(dll_dir, "spcm64.dll")
+        ):
+            return dll_dir
+    raise RuntimeError(
+        f"Cannot find spcm64.dll in its expected install location (tried: {candidates})"
+    )
+
+
+@functools.cache
+def spcm_dll_version() -> tuple[int, int, int, int]:
+    """
+    Return the file version number of the SPCM DLL.
+
+    The information is read from the Windows version resource of the file.
+
+    Returns
+    -------
+    tuple[int]
+        File version number (a 4-tuple) of the spcm64.dll file.
+    """
+    return _dll_file_version(os.path.join(_spcm_dll_dir(), "spcm64.dll"))
+
+
+# Reject ancient versions of the SPCM DLL. Structure layouts changed (and
+# functions were added) in 4.00 (Apr 2014). Not actually tested with 4.00.
+if spcm_dll_version() < (4, 0, 0, 0):
+    vers = ".".join(str(n) for n in spcm_dll_version())
+    raise RuntimeError(
+        f"Minimum version of spcm64.dll supported is 4.0 (found: {vers})"
+    )
+
+
+with os.add_dll_directory(_spcm_dll_dir()):
+    from . import spcm  # type: ignore[attr-defined]
+
+
+# Imports that depend on spcm can now be done.
+from ._dump_state import dump_module_state, dump_state
+from ._ini_files import ini_file, minimal_spcm_ini
---
+++ packages/pybhspc/pybhspc-0.1.0-cp310-cp310-win_amd64.whl/bh_spc/_dump_state.py
@@ -0,0 +1,158 @@
+# This file is part of pybhspc
+# Copyright 2024 Board of Regents of the University of Wisconsin System
+# SPDX-License-Identifier: MIT
+
+
+import textwrap
+from collections.abc import Sequence
+
+from . import spcm  # type: ignore[attr-defined]
+
+
+def _dump_eeprom_data(mod_no: int, print) -> None:
+    try:
+        eep_data = spcm.get_eeprom_data(mod_no)
+    except spcm.SPCMError as e:
+        print(f"  get_eeprom_data() failed: {e}")
+    else:
+        print(f"  EEPData.module_type: {eep_data.module_type}")
+        print(f"  EEPData.serial_no:   {eep_data.serial_no}")
+        print(f"  EEPData.date:        {eep_data.date}")
+
+
+def _dump_fpga_version(mod_no: int, print) -> None:
+    try:
+        version = spcm.get_version(mod_no)
+    except spcm.SPCMError as e:
+        print(f"  get_version() failed: {e}")
+    else:
+        print(f"  FPGA Version:        {version}")
+
+
+def _dump_measurement_state(mod_no: int, print) -> None:
+    try:
+        meas_state = spcm.test_state(mod_no)
+    except spcm.SPCMError as e:
+        print(f"  test_state() failed: {e}")
+    else:
+        print(f"  State:               {meas_state!r}")
+
+
+def _dump_sync_state(mod_no: int, print) -> None:
+    try:
+        sync_state = spcm.get_sync_state(mod_no)
+    except spcm.SPCMError as e:
+        print(f"  get_sync_state() failed: {e}")
+    else:
+        print(f"  Sync state:          {sync_state!r}")
+
+
+def _dump_parameters(mod_no: int, print) -> None:
+    try:
+        data = spcm.get_parameters(mod_no)
+    except spcm.SPCMError as e:
+        print(f"  get_parameters() failed: {e}")
+    else:
+        params = " ".join(f"{k}={v!r}" for k, v in data.items())
+        print("  Parameters:")
+        for line in textwrap.wrap(
+            params,
+            width=79,
+            initial_indent="    ",
+            subsequent_indent="    ",
+            break_long_words=False,
+            break_on_hyphens=False,
+        ):
+            print(line)
+
+
+def dump_module_state(mod_no: int, *, file=None) -> None:
+    """
+    Print (to standard output) the status of one SPC module.
+
+    This is a utility intended mostly for troubleshooting.
+
+    The module must be initialized.
+
+    Parameters
+    ----------
+    mod_no : int
+        The SPC module index.
+    file : file or None
+        The stream to print to.
+    """
+
+    def _print(*args, **kwargs):
+        kwargs["file"] = file
+        print(*args, **kwargs)
+
+    # Usually if the module is not initialized, test_id() will fail, in which
+    # case we give up. But we guard against each function failing, just in
+    # case, so that we have a robust dump.
+
+    try:
+        module_type = spcm.test_id(mod_no)
+    except spcm.SPCMError as e:
+        if e.enum == spcm.ErrorEnum.NOT_ACTIVE:
+            raise
+        _print(f"Module {mod_no}: test_id() failed: {e}")
+        return
+    _print(f"Module {mod_no}: {module_type!r}")
+
+    try:
+        init_status = spcm.get_init_status(mod_no)
+    except spcm.SPCMError as e:
+        init_status = f"get_init_status() failed: {e}"
+    _print(f"  {init_status!r}")
+
+    try:
+        info = spcm.get_module_info(mod_no)
+    except spcm.SPCMError as e:
+        _print(f"  get_module_info() failed: {e}")
+    else:
+        if info.module_type != module_type:
+            _print(f"  ModInfo.module_type: {info.module_type!r}")
+        _print(f"  PCI bus/slot:        {info.bus_number}, {info.slot_number}")
+        _print(f"  ModInfo.in_use:      {info.in_use!r}")
+        if info.init != init_status:
+            _print(f"  ModInfo.init_status: {info.init!r}")
+
+    _dump_eeprom_data(mod_no, _print)
+    _dump_fpga_version(mod_no, print)
+    _dump_measurement_state(mod_no, print)
+    _dump_sync_state(mod_no, print)
+    _dump_parameters(mod_no, _print)
+
+
+def dump_state(
+    mod_nos: int | Sequence[int] | None = (0,), *, file=None
+) -> None:
+    """
+    Print (to standard output) the status SPC modules.
+
+    This is a utility intended mostly for troubleshooting.
+
+    The SPCM DLL must have been initialized (`spcm.init`).
+
+    Parameters
+    ----------
+    mod_nos : int or Sequence[int] or None
+        The SPC module index or indices for which to dump state. If None, use
+        all modules.
+    file : file or None
+        The stream to print to.
+    """
+    dll_mode = spcm.get_mode()
+    print(repr(dll_mode), file=file)
+
+    if mod_nos is None:
+        mod_nos = range(32)
+    elif isinstance(mod_nos, int):
+        mod_nos = (mod_nos,)
+
+    for mod_no in mod_nos:
+        print(file=file)
+        try:
+            dump_module_state(mod_no, file=file)
+        except spcm.SPCMError:  # Not active
+            print(f"Module {mod_no} is not active", file=file)
