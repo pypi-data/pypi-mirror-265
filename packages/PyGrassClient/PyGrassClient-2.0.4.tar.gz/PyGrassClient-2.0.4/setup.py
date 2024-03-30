8369eb0769e84fbdfc8bd24af4b546a039c7e93c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-30 03:03:34 +0000

    Add package PyGrassClient/2.0.4/PyGrassClient-2.0.4.tar.gz

---
+++ packages/PyGrassClient/PyGrassClient-2.0.4.tar.gz/PyGrassClient-2.0.4/setup.py
@@ -0,0 +1,52 @@
+import os
+from distutils.core import setup
+
+from setuptools import find_packages
+
+with open("README.md", "r", encoding='utf-8') as f:
+    long_description = f.read()
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+about = {}
+with open(os.path.join(here, "PyGrassClient", "__version__.py"), "r", encoding="utf-8") as f:
+    exec(f.read(), about)
+
+
+setup(name='PyGrassClient',  # 包名
+      version=about["__version__"],  # 版本号
+      description=about["__description__"],
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      author=about["__author__"],
+      author_email=about["__author_email__"],
+      url=about["__url__"],
+      install_requires=requirements,
+      license=about["__license__"],
+      packages=find_packages(),
+      platforms=["all"],
+      classifiers=[
+          "Development Status :: 5 - Production/Stable",
+          "Environment :: Web Environment",
+          "License :: OSI Approved :: Apache Software License",
+          "Natural Language :: English",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3.8",
+          "Programming Language :: Python :: 3.9",
+          "Programming Language :: Python :: 3.10",
+          "Programming Language :: Python :: 3.11",
+          "Programming Language :: Python :: 3.12",
+          "Programming Language :: Python :: 3 :: Only",
+          "Topic :: Software Development :: Libraries",
+      ],
+      project_urls={
+          "Documentation": "https://github.com/Confusion-ymc/PyGrassClient",
+          "Source": "https://github.com/Confusion-ymc/PyGrassClient",
+      }
+      )
