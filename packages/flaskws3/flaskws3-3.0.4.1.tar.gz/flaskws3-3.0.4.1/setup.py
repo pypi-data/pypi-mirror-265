fc06b3bfa43fef01a084c1195695722f66a853ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-30 04:05:30 +0000

    Add package flaskws3/3.0.4.1/flaskws3-3.0.4.1.tar.gz

---
+++ packages/flaskws3/flaskws3-3.0.4.1.tar.gz/flaskws3-3.0.4.1/setup.py
@@ -0,0 +1,21 @@
+#-*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+
+packages = (
+	"flaskws",
+)
+
+setup(
+	name="flaskws3",
+	version="3.0.4.1",
+	packages=packages,
+	install_requires=["flask", "tornado"],
+	description="WebSockets for Flask.",
+	author="RixTheTyrunt",
+	author_email="rixthetyrunt@gmail.com",
+	url="https://github.com/RixInGithub/flaskws3",
+	long_description=open("README.md", "r").read(),
+	long_description_content_type="text/markdown"
+)
