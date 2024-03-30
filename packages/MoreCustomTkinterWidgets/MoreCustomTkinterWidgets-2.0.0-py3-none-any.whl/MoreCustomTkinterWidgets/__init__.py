9384712284bdbed8bb36147276c416a470e275c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-30 04:04:39 +0000

    Add package MoreCustomTkinterWidgets/2.0.0/MoreCustomTkinterWidgets-2.0.0-py3-none-any.whl

---
+++ packages/MoreCustomTkinterWidgets/MoreCustomTkinterWidgets-2.0.0-py3-none-any.whl/MoreCustomTkinterWidgets/__init__.py
@@ -0,0 +1,10 @@
+from .AskValue import AskValue, askstring, askinteger, askfloat
+from .AskDialog import AskDialog, askyesno
+from .Message import Message, showinfo, showwarning, showerror
+from .FileExplorer import FileExplorer, Filedialog, askfile, askdir
+from .Selector import Selector
+from .SmoothFrame import SmoothFrame, get_coordinates_from_grid
+from .BetterCTkImage import BetterCTkImage
+
+
+_version = "2.0.0"
