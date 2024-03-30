4af0fdbfd49089dbf2318aea9af13f37fd08576b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-30 15:03:38 +0000

    Add package ask-llm/0.1.13/ask-llm-0.1.13.tar.gz

---
+++ packages/ask-llm/ask-llm-0.1.13.tar.gz/ask-llm-0.1.13/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ask-llm",
+    version="0.1.13",
+    packages=find_packages(),
+    url="https://github.com/FlorianMgs/ask-llm",
+    license="MIT",
+    author="Florian Magisson",
+    author_email="madgic.shrooms@gmail.com",
+    description="The easiest way to supercharge your apps with LLM!",
+    install_requires=[
+        "Jinja2",
+        "pillow",
+        "langchain>=0.1.10",
+        "langchain-community>=0.0.25",
+        "langchain-core>=0.1.28",
+        "langchain-openai>=0.0.8",
+        "langchain-text-splitters>=0.0.1",
+        "python-dotenv>=1.0.1",
+    ],
+)
