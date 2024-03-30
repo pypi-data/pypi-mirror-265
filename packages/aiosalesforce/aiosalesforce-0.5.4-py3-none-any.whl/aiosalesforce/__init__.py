d80ebad6d11ed8f35993aa728f8826d10497abd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-30 05:03:29 +0000

    Add package aiosalesforce/0.5.4/aiosalesforce-0.5.4-py3-none-any.whl

---
+++ packages/aiosalesforce/aiosalesforce-0.5.4-py3-none-any.whl/aiosalesforce/__init__.py
@@ -0,0 +1,32 @@
+__version__ = "0.5.4"
+
+__all__ = [
+    "ClientCredentialsFlow",
+    "SoapLogin",
+    "Salesforce",
+    "BulkApiBatchConsumptionEvent",
+    "RequestEvent",
+    "ResponseEvent",
+    "RestApiCallConsumptionEvent",
+    "RetryEvent",
+    "ExceptionRule",
+    "ResponseRule",
+    "RetryPolicy",
+    "format_soql",
+]
+
+from .auth import ClientCredentialsFlow, SoapLogin
+from .client import Salesforce
+from .events import (
+    BulkApiBatchConsumptionEvent,
+    RequestEvent,
+    ResponseEvent,
+    RestApiCallConsumptionEvent,
+    RetryEvent,
+)
+from .retries import (
+    ExceptionRule,
+    ResponseRule,
+    RetryPolicy,
+)
+from .utils import format_soql
