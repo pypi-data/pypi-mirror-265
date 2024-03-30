53e1627e91245df907d3f9da9be5b7dceea086cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-30 14:04:21 +0000

    Add package pydancer/0.0.3/pydancer-0.0.3-py3-none-any.whl

---
+++ packages/pydancer/pydancer-0.0.3-py3-none-any.whl/pydancer/howto.py
@@ -0,0 +1,5 @@
+def printHowTo(long):
+    if long:
+        return('Pydancer is a python version of dance arcade games. To play, first enter the play command and its options listed in the README. Once the game screen pops up, press the corresponding arrow key when they enter the purple zone. You have until the end of the song to collect points. Try different difficulties to challenge yourself!')       
+    else:
+        return('Press the corresponding arrow key when they enter the purple zone.')
