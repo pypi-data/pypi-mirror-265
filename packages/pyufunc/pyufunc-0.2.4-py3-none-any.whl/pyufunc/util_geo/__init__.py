08cf51e98e5dd68f18891e8c7ea12ac2f17d8308
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-30 15:03:32 +0000

    Add package pyufunc/0.2.4/pyufunc-0.2.4-py3-none-any.whl

---
+++ packages/pyufunc/pyufunc-0.2.4-py3-none-any.whl/pyufunc/util_geo/__init__.py
@@ -0,0 +1,25 @@
+# -*- coding:utf-8 -*-
+##############################################################
+# Created Date: Sunday, July 9th 2023
+# Contact Info: luoxiangyong01@gmail.com
+# Author/Copyright: Mr. Xiangyong Luo
+##############################################################
+
+
+from pyufunc.util_geo._geo_circle import create_circle_at_point_with_radius
+from pyufunc.util_geo._geo_distance import (proj_point_to_line,
+                                            calc_distance_on_unit_sphere,
+                                            find_closest_point,
+                                            get_coordinates_from_geom,
+                                            find_k_nearest_points,
+                                            )
+
+import pyufunc.util_geo._gmns as gmns_geo
+
+__all__ = ['create_circle_at_point_with_radius',
+           'proj_point_to_line',
+           'calc_distance_on_unit_sphere',
+           'find_closest_point',
+           'get_coordinates_from_geom',
+           'find_k_nearest_points',
+           "gmns_geo"]
