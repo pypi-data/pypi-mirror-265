897ebe13081b033d0ebc73d68d723145092e6ad2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-30 04:03:31 +0000

    Add package launchflow/0.3.9/launchflow-0.3.9.tar.gz

---
+++ packages/launchflow/launchflow-0.3.9.tar.gz/launchflow-0.3.9/launchflow/__init__.py
@@ -0,0 +1,15 @@
+# ruff: noqa
+import asyncio
+from contextlib import asynccontextmanager, contextmanager
+
+from launchflow.resource import Resource
+
+from . import aws, fastapi, gcp
+from .flows.resource_flows import clean, create
+
+# TODO: Add generic resource imports, like Postgres, StorageBucket, etc.
+# This should probably live directly under launchflow, i.e. launchflow/postgres.py
+
+
+async def connect_all(*resources: Resource):
+    asyncio.gather(*(resource.connect_async() for resource in resources))
---
+++ packages/launchflow/launchflow-0.3.9.tar.gz/launchflow-0.3.9/launchflow/clients/environments_client.py
@@ -0,0 +1,75 @@
+import enum
+
+import httpx
+from launchflow.clients.response_schemas import (
+    EnvironmentResponse,
+    EnvironmentType,
+    OperationResponse,
+)
+from launchflow.config import config
+from launchflow.exceptions import LaunchFlowRequestFailure
+
+
+class _CloudProvider(enum.Enum):
+    AWS = "aws"
+    GCP = "gcp"
+
+
+class EnvironmentsAsyncClient:
+    def __init__(self, http_client: httpx.AsyncClient):
+        self.http_client = http_client
+
+    def base_url(self, project_name: str) -> str:
+        return f"{config.settings.launch_service_address}/projects/{project_name}/environments"
+
+    async def _create(
+        self,
+        project_name: str,
+        env_name: str,
+        env_type: EnvironmentType,
+        cloud_provider: _CloudProvider,
+    ) -> OperationResponse:
+        body = {
+            "name": env_name,
+            "environment_type": env_type.value,
+        }
+        response = await self.http_client.post(
+            f"{self.base_url(project_name)}/{cloud_provider.value}",
+            json=body,
+            headers={"Authorization": f"Bearer {config.get_access_token()}"},
+        )
+        if response.status_code not in [200, 201, 202]:
+            raise LaunchFlowRequestFailure(response)
+        return OperationResponse.model_validate(response.json())
+
+    async def create_aws(
+        self, project_name: str, env_name: str, env_type: EnvironmentType
+    ) -> OperationResponse:
+        return await self._create(project_name, env_name, env_type, _CloudProvider.AWS)
+
+    async def create_gcp(
+        self, project_name: str, env_name: str, env_type: EnvironmentType
+    ) -> OperationResponse:
+        return await self._create(project_name, env_name, env_type, _CloudProvider.GCP)
+
+    async def get(self, project_name: str, env_name: str):
+        url = f"{self.base_url(project_name)}/{env_name}"
+        response = await self.http_client.get(
+            url,
+            headers={"Authorization": f"Bearer {config.get_access_token()}"},
+        )
+        if response.status_code != 200:
+            raise LaunchFlowRequestFailure(response)
+        return EnvironmentResponse.model_validate(response.json())
+
+    async def list(self, project_name):
+        response = await self.http_client.get(
+            self.base_url(project_name),
+            headers={"Authorization": f"Bearer {config.get_access_token()}"},
+        )
+        if response.status_code != 200:
+            raise LaunchFlowRequestFailure(response)
+        return [
+            EnvironmentResponse.model_validate(env)
+            for env in response.json()["environments"]
+        ]
