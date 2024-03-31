97a60500650efcdda6e5b9dd16b1dbc2445e5d9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-31 04:03:29 +0000

    Add package jupyter-notepad/0.0.1.dev2/jupyter_notepad-0.0.1.dev2-py3-none-any.whl

---
+++ packages/jupyter-notepad/jupyter_notepad-0.0.1.dev2-py3-none-any.whl/jupyter_notepad/__init__.py
@@ -0,0 +1,5 @@
+from jupyter_notepad.hooks import (
+    _jupyter_labextension_paths,  # noqa: F401
+    _jupyter_nbextension_paths,  # noqa: F401
+)
+from jupyter_notepad.repo import Repo  # noqa: F401
