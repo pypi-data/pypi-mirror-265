f80df54a175e469ce05d7cf53e00810913d6d040
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-31 04:05:18 +0000

    Add package data4co/0.0.1a17/data4co-0.0.1a17.tar.gz

---
+++ packages/data4co/data4co-0.0.1a17.tar.gz/data4co-0.0.1a17/data4co/__init__.py
@@ -0,0 +1,22 @@
+import importlib.util
+
+# base 
+from .data import TSPLIBOriginDataset, TSPUniformDataset
+from .data import SATLIBData, SATLIBDataset
+from .evaluate import TSPEvaluator, TSPLIBOriginEvaluator, TSPUniformEvaluator
+from .evaluate import SATLIBEvaluator
+from .generator import TSPDataGenerator, MISDataGenerator
+from .solver import TSPSolver, TSPLKHSolver, TSPConcordeSolver
+from .solver import MISSolver, KaMISSolver, MISGurobi
+from .utils import download, compress_folder, extract_archive, _get_md5
+
+# expand
+found_matplotlib = importlib.util.find_spec("matplotlib")
+if found_matplotlib is not None:
+    from .draw.tsp import draw_tsp_problem, draw_tsp_solution
+else:
+    print("matplotlib not installed")
+
+
+__version__ = '0.0.1a17'
+__author__ = 'ThinkLab at SJTU'
