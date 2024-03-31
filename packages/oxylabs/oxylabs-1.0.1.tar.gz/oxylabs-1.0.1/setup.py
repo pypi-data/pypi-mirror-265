0169b3e5136c0833b77274e51ffd249b3d7eeb5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-31 14:07:05 +0000

    Add package oxylabs/1.0.1/oxylabs-1.0.1.tar.gz

---
+++ packages/oxylabs/oxylabs-1.0.1.tar.gz/oxylabs-1.0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+from oxylabs.version import SDK_VERSION
+
+long_description = """
+The official Python library for the Oxylabs Scraper APIs.
+
+Collect public data at scale with industry-leading web scraping solutions and the world's largest ethical proxy network.
+Documentation can be found at https://github.com/mslmio/oxylabs-sdk-python.
+"""
+
+setup(
+    name="oxylabs",
+    version=SDK_VERSION,
+    description="Official Python library for Oxylabs Scraper APIs",
+    long_description=long_description,
+    url="https://oxylabs.io/",
+    author="Oxylabs",
+    author_email="support@oxylabs.io",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=["aiohttp", "requests"],
+)
