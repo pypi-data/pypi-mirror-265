3b894446bb6f684d09e6ba027f63d6663bb400b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-31 03:03:46 +0000

    Add package adaptix/3.0.0b4/adaptix-3.0.0b4.tar.gz

---
+++ packages/adaptix/adaptix-3.0.0b4.tar.gz/adaptix-3.0.0b4/tests/test_doc.py
@@ -0,0 +1,41 @@
+import importlib
+from pathlib import Path
+
+import pytest
+
+from adaptix._internal.feature_requirement import HAS_PY_311
+
+REPO_ROOT = Path(__file__).parent.parent
+DOCS_EXAMPLES_ROOT = REPO_ROOT / "docs" / "examples"
+EXCLUDE = ["__init__.py"]
+GLOB = "*.py"
+
+
+def pytest_generate_tests(metafunc):
+    if "import_path" not in metafunc.fixturenames:
+        return
+
+    paths_to_test = [
+        sub_path
+        for sub_path in DOCS_EXAMPLES_ROOT.rglob(GLOB)
+        if sub_path.name not in EXCLUDE and not sub_path.is_dir()
+    ]
+    metafunc.parametrize(
+        ["import_path", "case_id"],
+        [
+            pytest.param(
+                ".".join((path_to_test.parent / path_to_test.stem).relative_to(REPO_ROOT).parts),
+                str((path_to_test.parent / path_to_test.stem).relative_to(DOCS_EXAMPLES_ROOT).as_posix()),
+                id=str((path_to_test.parent / path_to_test.stem).relative_to(DOCS_EXAMPLES_ROOT).as_posix()),
+            )
+            for path_to_test in paths_to_test
+        ],
+    )
+
+
+def test_example(import_path: str, case_id: str):
+    if case_id == "loading-and-dumping/tutorial/unexpected_error" and not HAS_PY_311:
+        pytest.skip("Need Python >= 3.11")
+
+    pytest.register_assert_rewrite(import_path)
+    importlib.import_module(import_path)
