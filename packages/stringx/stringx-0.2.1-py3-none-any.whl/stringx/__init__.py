25ff9ae62ff6023ae23398ff32c716becd609245
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-31 14:07:24 +0000

    Add package stringx/0.2.1/stringx-0.2.1-py3-none-any.whl

---
+++ packages/stringx/stringx-0.2.1-py3-none-any.whl/stringx/__init__.py
@@ -0,0 +1,23 @@
+"""
+STRING API client using httpx.
+"""
+
+__version__ = "0.2.1"
+
+from typing import List
+
+from .client import Client
+
+__all__ = ["Client"]
+
+DEFAULT_CALLER_IDENTITY = f"{__name__} {__version__}"
+
+
+def map_identifiers(identifiers: List[str], species: int):
+    with Client() as client:
+        return client.map_identifiers(identifiers=identifiers, species=species)
+
+
+def interaction_partners(identifiers: List[str], species: int):
+    with Client() as client:
+        return client.interaction_partners(identifiers=identifiers, species=species)
