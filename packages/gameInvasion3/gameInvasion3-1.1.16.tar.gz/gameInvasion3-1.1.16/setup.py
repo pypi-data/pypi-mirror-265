6e8a47d28e846d28ff00563884e755201584063f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-31 14:07:34 +0000

    Add package gameInvasion3/1.1.16/gameInvasion3-1.1.16.tar.gz

---
+++ packages/gameInvasion3/gameInvasion3-1.1.16.tar.gz/gameInvasion3-1.1.16/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt',encoding='utf-8') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='gameInvasion3',
+    version='1.1.16',
+    url='https://github.com/MaoJiayang/gameInvasion3',
+    author='Jiayang Mao',
+    author_email='Nucleon_17th@njust.edu.cn',
+    description='a python game engine for my 2D game development',
+    packages=find_packages(),
+    package_data={
+        'invasion_game_demo': ['resources/**/*'],
+    },  
+    install_requires=requirements,
+    entry_points={
+        'console_scripts': [
+            'gameInvasion3=invasion_game_demo.main:main', 
+        ],
+    },
+
+)
