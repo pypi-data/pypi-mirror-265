1a0f2f6e8151f3f8c8e435f9ebeb72243dd4660f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-31 14:07:06 +0000

    Add package pyufunc/0.2.5/pyufunc-0.2.5.tar.gz

---
+++ packages/pyufunc/pyufunc-0.2.5.tar.gz/pyufunc-0.2.5/pyufunc/util_img/__init__.py
@@ -0,0 +1,43 @@
+# -*- coding:utf-8 -*-
+##############################################################
+# Created Date: Thursday, February 15th 2024
+# Contact Info: luoxiangyong01@gmail.com
+# Author/Copyright: Mr. Xiangyong Luo
+##############################################################
+
+from ._img_cvt import (
+    cvt_img_to_bytes,
+    cvt_PIL_img_to_bytes,
+    cvt_img_bytes_to_PIL_img,
+)
+
+from ._img_operate import (
+    is_PIL_img,
+    is_CV_img,
+    cvt_img_PIL_to_CV,
+    cvt_img_CV_to_PIL,
+    img_translate,
+    img_rotate,
+    img_rotate_bound,
+    img_resize,
+    img_show
+)
+
+__all__ = [
+    # _img_cvt
+    "cvt_img_to_bytes",
+    "cvt_PIL_img_to_bytes",
+    "cvt_img_bytes_to_PIL_img",
+
+    # _img_rotate
+    "is_PIL_img",
+    "is_CV_img",
+    "cvt_img_PIL_to_CV",
+    "cvt_img_CV_to_PIL",
+    "img_translate",
+    "img_rotate",
+    "img_rotate_bound",
+    "img_resize",
+    "img_show"
+
+]
