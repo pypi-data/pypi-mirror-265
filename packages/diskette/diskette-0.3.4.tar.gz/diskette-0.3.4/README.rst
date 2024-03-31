807d878d13dcbd7832e58009dab26186082448df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-31 03:03:08 +0000

    Add package diskette/0.3.4/diskette-0.3.4.tar.gz

---
+++ packages/diskette/diskette-0.3.4.tar.gz/diskette-0.3.4/README.rst
@@ -0,0 +1,42 @@
+.. _Python: https://www.python.org/
+.. _Django: https://www.djangoproject.com/
+.. _django-sendfile2: https://github.com/moggers87/django-sendfile2
+.. _datalookup: https://datalookup.readthedocs.io/
+
+========
+Diskette
+========
+
+Export and import Django application datas and medias.
+
+
+Features
+********
+
+* Based on *Django apps* to know about applications and their models;
+* Application datas are dumped with Django ``dumpdata`` command as JSON fixtures, dumps
+  can be naturally loaded in any database type using Django command  ``loaddata``;
+* Define application to be dumped with multiple options;
+* Advanced data drainage for undefined applications;
+* Media archiving is done through *Storages* (not *Django storages*) that can be
+  whatever directory you need to backup;
+* Many excluding rules for datas and storages to avoid useless content in archive;
+* Build a complete archive that can be automatically loaded with Diskette or manually;
+* Support models made with ``django-polymorphic``;
+
+
+Dependancies
+************
+
+* `Python`_>=3.9;
+* `Django`_>=4.0,<5.0;
+* `django-sendfile2`_>=0.7.0;
+* `datalookup`_>=1.0.0;
+
+
+Links
+*****
+
+* Read the documentation on `Read the docs <https://diskette.readthedocs.io/>`_;
+* Download its `PyPi package <https://pypi.python.org/pypi/diskette>`_;
+* Clone it on its `Github repository <https://github.com/emencia/diskette>`_;
---
+++ packages/diskette/diskette-0.3.4.tar.gz/diskette-0.3.4/diskette/management/commands/diskette_load.py
@@ -0,0 +1,77 @@
+from pathlib import Path
+
+from django.core.management.base import BaseCommand
+
+from ...core.handlers import LoadCommandHandler
+from ...utils.loggers import DjangoCommandOutput
+
+
+class Command(BaseCommand, LoadCommandHandler):
+    """
+    Diskette load.
+    """
+    help = (
+        "Restore application datas and storage files from a Diskette archive file "
+        "(TAR GZ)."
+    )
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "archive",
+            default=None,
+            help=(
+                "Archive file path to restore its content."
+            )
+        )
+        parser.add_argument(
+            "--storages-basepath",
+            type=Path,
+            default=None,
+            help="Directory path where to restore storage contents.",
+        )
+        parser.add_argument(
+            "--no-data",
+            action="store_true",
+            help="Disable application data restoration.",
+        )
+        parser.add_argument(
+            "--no-storages",
+            action="store_true",
+            help="Disable storages restoration.",
+        )
+        parser.add_argument(
+            "--download-destination",
+            type=Path,
+            default=None,
+            help=(
+                "Directory path where to write download archive. This option is "
+                "ignored for local archive file."
+            ),
+        )
+        parser.add_argument(
+            "--keep",
+            action="store_true",
+            help="Don't automatically remove archive when finished.",
+        )
+        parser.add_argument(
+            "--checksum",
+            default=None,
+            help=(
+                "Checksum string to compare to the archive checksum, if checksum "
+                "comparison fails operation is aborted. Give value 'no' to disable "
+                "checksum creation from archive."
+            ),
+        )
+
+    def handle(self, *args, **options):
+        self.logger = DjangoCommandOutput(command=self, verbosity=options["verbosity"])
+
+        self.load(
+            options["archive"],
+            storages_basepath=options["storages_basepath"],
+            no_data=options["no_data"],
+            no_storages=options["no_storages"],
+            download_destination=options["download_destination"],
+            keep=options["keep"],
+            checksum=options["checksum"],
+        )
