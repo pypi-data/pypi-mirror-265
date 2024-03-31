9e7956ae2354c24b259bcf54e0c1d9eea3a7641e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-31 14:07:07 +0000

    Add package one-tap-sign-in/0.1/one_tap_sign_in-0.1.tar.gz

---
+++ packages/one-tap-sign-in/one_tap_sign_in-0.1.tar.gz/one_tap_sign_in-0.1/one_tap_sign_in/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class OneTapSignInConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'django_one_tap_sign_in'
+    label = 'one_tap_sign_in'
