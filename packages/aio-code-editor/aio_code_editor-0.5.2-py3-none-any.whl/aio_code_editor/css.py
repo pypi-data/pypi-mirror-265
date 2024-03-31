0ff384e6100a109707ae40ebb5449dec3a04a893
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-31 03:03:06 +0000

    Add package aio-code-editor/0.5.2/aio_code_editor-0.5.2-py3-none-any.whl

---
+++ packages/aio-code-editor/aio_code_editor-0.5.2-py3-none-any.whl/aio_code_editor/css.py
@@ -0,0 +1,73 @@
+custom_btns = [{
+                "name": "Copy",
+                "feather": "Copy",
+                "alwaysOn": True,
+                "commands": ["copyAll"],
+                "style": {"top": "0.46rem", "right": "0.4rem"}
+                },
+               
+               {
+                "name": "Run",
+                "feather": "Play",
+                "primary": True,
+                "showWithIcon": True,
+                "alwaysOn": True,
+                "commands": ["submit"],
+                "style": {"bottom": "0.44rem", "right": "0.4rem"}
+                },]
+
+# css to inject related to info bar
+css_string = '''
+background-color: #bee1e5;
+
+body > #root .ace-streamlit-dark~& {
+   background-color: #262830;
+}
+
+.ace-streamlit-dark~& span {
+   color: #fff;
+   opacity: 0.6;
+}
+
+span {
+   color: #000;
+   opacity: 0.5;
+}
+
+.code_editor-info.message {
+   width: inherit;
+   margin-right: 75px;
+   order: 2;
+   text-align: center;
+   opacity: 0;
+   transition: opacity 0.7s ease-out;
+}
+
+.code_editor-info.message.show {
+   opacity: 0.6;
+}
+
+.ace-streamlit-dark~& .code_editor-info.message.show {
+   opacity: 0.5;
+}
+'''
+# create info bar dictionary
+info_bar = {
+  "name": "language info",
+  "css": css_string,
+  "style": {
+            "order": "1",
+            "display": "flex",
+            "flexDirection": "row",
+            "alignItems": "center",
+            "width": "100%",
+            "height": "2.5rem",
+            "padding": "0rem 0.75rem",
+            "borderRadius": "8px 8px 0px 0px",
+            "zIndex": "9993"
+           },
+  "info": [{
+            "name": "python",
+            "style": {"width": "100px"}
+           }]
+}
