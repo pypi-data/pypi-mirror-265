0d80b27c931c4555985ac2a8be46231d7bab48f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-31 03:03:05 +0000

    Add package gurulearn/1.0.4/gurulearn-1.0.4.tar.gz

---
+++ packages/gurulearn/gurulearn-1.0.4.tar.gz/gurulearn-1.0.4/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+setup(
+    name='gurulearn',
+    version='1.0.4',
+    description='library for linear_regression and gvgg16 model generation(fixed bugs)',
+    author='Guru Dharsan T',
+    author_email='gurudharsan123@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'opencv-python',
+        'scipy',
+        'matplotlib',
+        'tensorflow==2.13.1',
+        'Keras',
+        'pandas',
+        'numpy',
+        'plotly',
+        'scikit-learn',
+    ],
+)
