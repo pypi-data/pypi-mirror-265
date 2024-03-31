3ff7d1a4255827de49a1583a81060ebb6188905f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-31 04:04:08 +0000

    Add package dcicsnovault/11.13.0.1b14/dcicsnovault-11.13.0.1b14.tar.gz

---
+++ packages/dcicsnovault/dcicsnovault-11.13.0.1b14.tar.gz/dcicsnovault-11.13.0.1b14/snovault/project/ingestion.py
@@ -0,0 +1,10 @@
+# Ingestion related functions which may be overriden by an implementing app,
+# e.g. Foursight or CGAP portal, using the dcicutils project_utils mechanism.
+
+class SnovaultProjectIngestion:
+
+    def note_ingestion_enqueue_uuids_for_request(self, ingestion_type, request, uuids):
+        pass
+
+    def note_submit_for_ingestion(self, submission_uuid, request):
+        pass
