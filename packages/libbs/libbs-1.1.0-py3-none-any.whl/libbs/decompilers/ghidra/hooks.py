f2136e78af653707e0536f037dc149a67e183379
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-31 04:03:57 +0000

    Add package libbs/1.1.0/libbs-1.1.0-py3-none-any.whl

---
+++ packages/libbs/libbs-1.1.0-py3-none-any.whl/libbs/decompilers/ghidra/hooks.py
@@ -0,0 +1,22 @@
+import typing
+import threading
+
+if typing.TYPE_CHECKING:
+    from libbs.decompilers.ghidra.compat.ghidra_api import GhidraAPIWrapper
+
+
+def create_context_action(ghidra: "GhidraAPIWrapper", name, action_string, callback_func, category=None):
+    ProgramLocationContextAction = ghidra.import_module_object("ghidra.app.context", "ProgramLocationContextAction")
+    MenuData = ghidra.import_module_object("docking.action", "MenuData")
+
+    # XXX: you can't ever use super().__init__() due to some remote import issues
+    class GenericDecompilerCtxAction(ProgramLocationContextAction):
+        def actionPerformed(self, ctx):
+            threading.Thread(target=callback_func, daemon=True).start()
+
+    action = GenericDecompilerCtxAction(name, category)
+    category_list = category.split("/") if category else []
+    category_start = category_list[0] if category_list else category
+    action.setPopupMenuData(MenuData(category_list + [action_string], None, category_start))
+
+    return action
