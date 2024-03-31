3d0148cd3f338dac36ac3a446f4f8c426f19bee8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-31 03:03:45 +0000

    Add package kp-hello/0.2/kp_hello-0.2.tar.gz

---
+++ packages/kp-hello/kp_hello-0.2.tar.gz/kp_hello-0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='kp_hello',
+    version='0.2',
+    packages=find_packages(),
+    install_requires=[
+        # Add dependencies here.
+    ],
+    entry_points={
+        "console_scripts" : [
+            "kp-hello = kp_hello:hello",
+        ],
+    },
+)
