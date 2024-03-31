fae9511d1c1da844aa3a478bed1199df74035ed9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-31 03:03:09 +0000

    Add package gameInvasion3/1.0.4/gameInvasion3-1.0.4.tar.gz

---
+++ packages/gameInvasion3/gameInvasion3-1.0.4.tar.gz/gameInvasion3-1.0.4/custom_components/gyroscope.py
@@ -0,0 +1,48 @@
+from invasionEngine.components import PIDController,ComponentBase
+from .constants import CustomConstants as Constants
+class Gyroscope(ComponentBase):
+    #陀螺仪组件，用于控制施加于飞船上的力矩(或者任意一维量的控制)
+    def __init__(self,stop_threshold: float = 0.3,
+                 max_torque: float = 5000,
+                 kp:float = 1000,
+                 ki:float = 0.5,
+                 kd:float = 100000
+                 ) -> None:
+
+        super().__init__()
+        self.stop_threshold=stop_threshold, 
+
+        if type(self.stop_threshold) != float:#如果停止阈值是一个有一个元素的元组，则取出元组的第一个元素。否则报错
+            if len(self.stop_threshold) == 1:
+                self.stop_threshold = self.stop_threshold[0]
+            else:#这是个莫名其妙的错误，不知道为什么会出现。明明输入的是一个元组，但是却不是一个元组
+                raise TypeError("stop_threshold must be a tuple with one element or a float,get:",self.stop_threshold)
+            
+        self.max_torque=max_torque * Constants.PIXELS_PER_METER
+        self.PIDController: PIDController = PIDController(kp, ki, kd, 0)
+
+
+    def level_limit(self, torque: float) -> tuple[float, float]:
+        #将力矩限制在最大和最小
+        if abs(torque) < self.stop_threshold:
+            torque = 0
+        else:
+            torque = max(min(torque, self.max_torque), -self.max_torque)
+        return torque
+    
+    def upgrade(self) -> None:
+        self.max_torque += 1000
+
+    def downgrade(self) -> None:
+        if self.max_torque > 1000:
+            self.max_torque -= 1000
+
+    def update(self,current:float = 0,target:float = 0) -> float:
+        '''
+        根据当前角度和目标角度，输出力矩
+        '''
+        torque = self.PIDController.update(current, target)
+        return self.level_limit(torque)
+    
+    def destroy(self):
+        pass
---
+++ packages/gameInvasion3/gameInvasion3-1.0.4.tar.gz/gameInvasion3-1.0.4/custom_gos/enemy.py
@@ -0,0 +1,119 @@
+from custom_components.npc_behavior import ChaseAndAimingBehavior,Behavior
+from invasionEngine.game_objects import PhysicalGO
+from invasionEngine.components import Camera, PIDController,ResourceManager,EventManager,KeyboardController
+from invasionEngine.events import CreateEvent
+from custom_components.thruster import Thruster
+from custom_components.gun import Autocannon,Railgun,Gatling
+from custom_components.gyroscope import Gyroscope
+from custom_components.constants import CustomConstants as Constants
+from custom_components.custom_events import HitEvent
+from invasionEngine.events import Event
+from custom_components.health import HealthSystem
+import pygame
+from pygame import Surface
+import pymunk
+from pymunk import Vec2d
+from typing import Tuple, List, Literal
+import random
+import math
+
+class Enemy(PhysicalGO):
+    '''
+    敌人类
+    '''
+    def __init__(self, 
+                position: Tuple[int, int], 
+                space: pymunk.space, 
+                screen: Surface,
+                angle: float = 0,
+                mass: float = 5,#5
+                shape_type: Literal['box', 'circle', 'poly'] = 'poly',
+                elasticity: float = 1,
+                friction: float= 0.2,
+                gyroscope: Gyroscope = Gyroscope(max_torque=5000000000000000),#0.01, 0.00001, 10
+                thruster: Thruster = Thruster(maxForce=4000),
+                gunPower: float = 50,
+                bulletTTL: int = 1000,
+                assets: ResourceManager = None,
+                scaling: float = 0.125,
+                **kwargs
+                 ):
+        super().__init__(position,space,screen,angle,mass,shape_type,elasticity,friction,assets,scaling=scaling,**kwargs)
+        self.targetX = position[0]
+        self.targetY = position[1]
+        self.gyroscope = gyroscope
+        self.thruster = thruster
+
+        self.gun  = Gatling(self,fire_rate=30)
+        # self.gun = Railgun(self,max_charge_time=3000)
+        # self.gun = Autocannon(self,fire_rate=4)
+        self.mouse_left_down = False
+        self.physics_part.shape.collision_type = Constants.SHIP
+        self.behavior : Behavior = ChaseAndAimingBehavior(self,min_distance=5,max_distance=15)
+        self.health_system = HealthSystem(max_health=120000,health_bar_size=(400,25))
+        self.font = pygame.font.SysFont('arial', 20)#用于显示测试文字
+    @property
+    def center(self):
+        return self.physics_part.center 
+
+    def angle_difference(self,x, y):
+        """计算两个角度之间的最小差值，结果在 -180 到 180 之间"""
+        diff = (x - y) % 360
+        if diff >= 180:
+            diff -= 360
+        return diff
+
+    def angular_update(self,target_angle:float):
+        # 使用 PID 控制器计算出需要施加的力矩
+        diff = self.angle_difference(self.physics_part.body.angle,target_angle)
+
+        torque = self.gyroscope.update(diff, 0)
+
+        return torque
+
+    def physical_update(self) -> None:
+            target_position, target_angle = self.behavior.update()  # 使用行为来更新
+            #print(target_position, target_angle)
+            self.targetX = target_position[0]
+            self.targetY = target_position[1]
+            rotation_torque = self.angular_update(target_angle)  # 更新角度
+            self.physics_part.update(
+                force=self.thruster.update(self.center, target_position), 
+                rotation_torque=rotation_torque
+            )
+            if self.health_system.health_status == 0:
+                self.destroy()
+            
+    
+    def render(self, camera: Camera) -> None:
+        '''
+        这里为了方便，直接调用了父类的render方法，故使用了传入的camera，而不是自己的camera
+        '''
+        super().render(camera)
+        #在目标位置渲染一个点
+        pygame.draw.circle(self.screen, (255,100,0), camera.apply((self.targetX, self.targetY)), 4)
+        # 绘制血条
+        health_bar_surface = self.health_system.health_bar
+        health_bar_position = (self.screen.get_width() / 2 - health_bar_surface.get_width() / 2, 20)
+        self.screen.blit(health_bar_surface, health_bar_position)
+
+        text_surface = self.font.render('Enemy Health', True, (255, 0, 0))  # 白色文字
+        # 计算文字的位置
+        text_position = (health_bar_position[0] - text_surface.get_width() - 10, health_bar_position[1])
+        # 将文字绘制到屏幕上
+        self.screen.blit(text_surface, text_position)
+
+    def handle_event(self, event: Event) -> None:
+        super().handle_event(event)
+        if event.event_type == Constants.HIT_EVENT:
+            event: HitEvent
+            if event.target == self:
+                self.health_system.take_damage(event.damage - self.physics_part.body.kinetic_energy/(Constants.PIXELS_PER_METER**2))
+    
+    def destroy(self):
+        print('敌人被摧毁,剩余生命百分比：',self.health_system.health_status)
+        self.gun.destroy()
+        self.behavior.destroy()
+        self.health_system.destroy()
+        super().destroy()
+
