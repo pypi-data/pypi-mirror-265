cb87de6a2f6f70e1c5d1d119e88db93b65f126ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-31 04:04:08 +0000

    Add package yacv-server/0.8.6/yacv_server-0.8.6-cp311-cp311-manylinux_2_35_x86_64.whl

---
+++ packages/yacv-server/yacv_server-0.8.6-cp311-cp311-manylinux_2_35_x86_64.whl/yacv_server/frontend/___vite-browser-external_commonjs-proxy-pPgBnUXj.js
@@ -0,0 +1,2 @@
+import{bL as e}from"./index-CEwq6DBV.js";const t={},r=Object.freeze(Object.defineProperty({__proto__:null,default:t},Symbol.toStringTag,{value:"Module"})),a=e(r);export{a as r};
+//# sourceMappingURL=___vite-browser-external_commonjs-proxy-pPgBnUXj.js.map
