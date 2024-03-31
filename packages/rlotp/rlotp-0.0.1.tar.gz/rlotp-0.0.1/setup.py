4db87833d6e5eef1d868eae4f78bc7e66f8e40d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-31 14:07:34 +0000

    Add package rlotp/0.0.1/rlotp-0.0.1.tar.gz

---
+++ packages/rlotp/rlotp-0.0.1.tar.gz/rlotp-0.0.1/setup.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+import os
+import sys
+from codecs import open
+from setuptools import setup, find_packages
+
+app_name = 'rlotp'
+
+sys.path[0:0] = [app_name]
+
+from version import __version__
+
+def get_readme(filename):
+    content = ""
+    try:
+        with open(os.path.join(os.path.dirname(__file__), filename), 'r', encoding='utf-8') as readme:
+            content = readme.read()
+    except Exception as e:
+        pass
+    return content
+
+setup(name=app_name,
+      version=__version__,
+      author="Md. Jahidul Hamid",
+      author_email="jahidulhamid@yahoo.com",
+      description="Random length OTP",
+      license="BSD",
+      keywords="OTP, random, length, authenticator, madmin",
+      url="https://github.com/neurobin/rlotp",
+      packages=find_packages(include=('rlotp*',)),
+      long_description=get_readme("README.md"),
+      long_description_content_type="text/markdown",
+      classifiers=[
+        # See: https://pypi.python.org/pypi?:action=list_classifiers
+        #'Development Status :: 5 - Production/Stable',
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+      ],
+      python_requires='>=3.7.0',
+      install_requires=['pyotp'],
+      # scripts=['morm/morm_admin', 'morm/init_fap',],
+      # test_suite="morm.tests.test"
+)
