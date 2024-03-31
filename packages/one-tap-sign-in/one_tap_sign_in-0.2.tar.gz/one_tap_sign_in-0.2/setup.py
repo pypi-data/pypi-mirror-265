e2b0451c62cff92d2569a2d46fcbeee6f085f88b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-31 14:07:07 +0000

    Add package one-tap-sign-in/0.2/one_tap_sign_in-0.2.tar.gz

---
+++ packages/one-tap-sign-in/one_tap_sign_in-0.2.tar.gz/one_tap_sign_in-0.2/setup.py
@@ -0,0 +1,11 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='one_tap_sign_in',
+    version='0.2',
+    packages=find_packages(),
+    install_requires=[
+    ],
+)
