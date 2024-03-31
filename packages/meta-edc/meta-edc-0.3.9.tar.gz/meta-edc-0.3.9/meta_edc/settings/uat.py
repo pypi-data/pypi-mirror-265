df7e7f80557361cc495dd5f2ba81e03561f853d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-31 14:07:35 +0000

    Add package meta-edc/0.3.9/meta-edc-0.3.9.tar.gz

---
+++ packages/meta-edc/meta-edc-0.3.9.tar.gz/meta-edc-0.3.9/meta_edc/settings/uat.py
@@ -0,0 +1,13 @@
+from multisite import SiteID
+
+from .defaults import *  # noqa
+
+print(f"Settings file {__file__}")
+
+SITE_ID = SiteID(default=1)
+EDC_SITES_UAT_DOMAIN = True
+EDC_MODEL_ADMIN_CSS_THEME = "edc_purple"
+LIVE_SYSTEM = False
+EDC_CONSENT_BYPASS_CONSENT_DATETIME_VALIDATION = True
+EDC_SITES_DOMAIN_SUFFIX = "meta4.clinicedc.org"
+MULTISITE_SYNC_ALIAS_MANUALLY = True
