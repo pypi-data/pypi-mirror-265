be33728059ac2de5495650223f735e16ec12c133
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-31 04:03:34 +0000

    Add package gpgi/1.0.0/gpgi-1.0.0-cp311-cp311-macosx_10_9_x86_64.whl

---
+++ packages/gpgi/gpgi-1.0.0-cp311-cp311-macosx_10_9_x86_64.whl/gpgi/_typing.py
@@ -0,0 +1,8 @@
+from typing import TypeVar
+
+import numpy as np
+import numpy.typing as npt
+
+Real = TypeVar("Real", np.float32, np.float64)
+RealArray = npt.NDArray[Real]
+HCIArray = npt.NDArray[np.uint16]
