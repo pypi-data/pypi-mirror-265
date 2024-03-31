ed6b23349a76fdcbb148e530209dc3530ddbaabb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-31 04:04:36 +0000

    Add package powercli/0.1.1/powercli-0.1.1-py3-none-any.whl

---
+++ packages/powercli/powercli-0.1.1-py3-none-any.whl/cli/commands/__init__.py
@@ -0,0 +1,14 @@
+from .install import install
+from .open_ import open_urls
+from .run import (
+    capture_output,
+    capture_output_lines,
+    capture_return_code,
+    completes_successfully,
+    launch,
+    pipe_output_and_capture,
+    run,
+    run_commands,
+    run_commands_in_shell,
+    run_in_console,
+)
