5c20c9e3bb958142b8eed7c21ff0e995c90cf86c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-31 04:04:33 +0000

    Add package PathMePy-Gustoon/0.0.1/PathMePy-Gustoon-0.0.1.tar.gz

---
+++ packages/PathMePy-Gustoon/PathMePy-Gustoon-0.0.1.tar.gz/PathMePy-Gustoon-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+name='PathMePy-Gustoon',
+version='0.0.1',
+author='Gustoon',
+author_email='',
+description='A tool to add scripts to Path',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.6',
+)
