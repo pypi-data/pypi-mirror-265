72718334a8f449928d5ec7fed7f0b8ed58bcadb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-31 14:07:16 +0000

    Add package bitbrowser-api/0.3/bitbrowser_api-0.3.tar.gz

---
+++ packages/bitbrowser-api/bitbrowser_api-0.3.tar.gz/bitbrowser_api-0.3/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+project_urls = {
+  'Source': 'https://github.com/embzheng/bitbrowser_api'
+}
+setuptools.setup(
+    name="bitbrowser_api",
+    version="0.3",
+    author="embzheng",
+    author_email="embzheng@qq.com",
+    description="This is a bitbrowser operation tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/embzheng/bitbrowser_api",
+    packages=setuptools.find_packages(),
+    install_requires=['playwright>=1.42.0','requests','wallet_tool','my_logtool'],    
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    project_urls = project_urls
+)
