21317d38725fc7988184088376322fcd7ae5447a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-31 15:03:58 +0000

    Add package haedream/0.0.5/haedream-0.0.5.tar.gz

---
+++ packages/haedream/haedream-0.0.5.tar.gz/haedream-0.0.5/haedream/saveLog.py
@@ -0,0 +1,43 @@
+import requests
+import json
+import zlib
+
+class ModelRunner:
+
+    server_url = "http://localhost:8088/save_data"
+
+    def run_model(self, modelName, projectName, inputData, apiKey, outputData):
+        
+        # 결과를 JSON 형식으로 변환
+        json_output = self._format_output(modelName, projectName, inputData, outputData, apiKey)
+        
+        # 데이터를 압축
+        compressed_data = zlib.compress(json_output.encode('utf-8'))
+
+        # 결과를 서버로 전송하여 저장
+        self._save_to_server(compressed_data)
+        
+        # 결과 반환
+        return json_output
+
+    def _format_output(self, modelName, projectName, inputData, outputData, apiKey):
+        # 결과를 JSON 형식으로 포맷팅합니다.
+        result = {
+            "modelName": modelName,
+            "projectName": projectName,
+            "inputData": inputData,
+            "outputData": outputData,
+            "apiKey": apiKey
+        }
+        return json.dumps(result)
+
+    def _save_to_server(self, data):
+        # 서버로 데이터를 전송하여 저장합니다.
+        
+        data2 = {"js" : data}
+        response = requests.post(self.server_url, params=data2)
+        if (response.status_code == 200 or response.status_code == 201):
+            print("Data successfully saved to server.")
+        else:
+            print("Failed to save data to server.")
+            print(response.status_code)
