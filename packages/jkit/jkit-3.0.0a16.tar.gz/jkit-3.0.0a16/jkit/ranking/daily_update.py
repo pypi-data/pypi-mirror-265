c42ba023ba5df83f8bada29a21a7a325094d43c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-31 03:03:03 +0000

    Add package jkit/3.0.0a16/jkit-3.0.0a16.tar.gz

---
+++ packages/jkit/jkit-3.0.0a16.tar.gz/jkit-3.0.0a16/jkit/ranking/daily_update.py
@@ -0,0 +1,45 @@
+from typing import TYPE_CHECKING, AsyncGenerator
+
+from jkit._base import DATA_OBJECT_CONFIG, DataObject, ResourceObject
+from jkit._network_request import get_json
+from jkit.config import CONFIG
+from jkit.msgspec_constraints import PositiveInt, UserName, UserSlug, UserUploadedUrl
+
+if TYPE_CHECKING:
+    from jkit.user import User
+
+
+class UserInfoField(DataObject, **DATA_OBJECT_CONFIG):
+    slug: UserSlug
+    name: UserName
+    avatar_url: UserUploadedUrl
+
+    def to_user_obj(self) -> "User":
+        from jkit.user import User
+
+        return User.from_slug(self.slug)._as_checked()
+
+
+class DailyUpdateRankingRecord(DataObject, **DATA_OBJECT_CONFIG):
+    ranking: PositiveInt
+    days: PositiveInt
+    user_info: UserInfoField
+
+
+class DailyUpdateRanking(ResourceObject):
+    async def __aiter__(self) -> AsyncGenerator[DailyUpdateRankingRecord, None]:
+        data = await get_json(
+            endpoint=CONFIG.endpoints.jianshu,
+            path="/asimov/daily_activity_participants/rank",
+        )
+
+        for item in data["daps"]:
+            yield DailyUpdateRankingRecord(
+                ranking=item["rank"],
+                days=item["checkin_count"],
+                user_info=UserInfoField(
+                    slug=item["slug"],
+                    name=item["nickname"],
+                    avatar_url=item["avatar"],
+                ),
+            )._validate()
