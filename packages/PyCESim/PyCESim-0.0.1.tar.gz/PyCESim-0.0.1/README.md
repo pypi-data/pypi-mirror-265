9a363f719d37c9643a1ab7e320c31188730ceaef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-31 04:04:10 +0000

    Add package PyCESim/0.0.1/PyCESim-0.0.1.tar.gz

---
+++ packages/PyCESim/PyCESim-0.0.1.tar.gz/PyCESim-0.0.1/README.md
@@ -0,0 +1,2 @@
+# PyCESim
+Python package for the classical simulation of Coulomb explosion
---
+++ packages/PyCESim/PyCESim-0.0.1.tar.gz/PyCESim-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1' 
+DESCRIPTION = 'PyCESim - classical simulation of Coulomb explosion'
+
+setup(
+        name="PyCESim", 
+        version=VERSION,
+        author="Felix Allum",
+        author_email="<fallum@stanford.edu",
+        description=DESCRIPTION,
+        packages=find_packages(),
+        install_requires=['cclib', 'numpy', 'matplotlib', 'scipy', 'pandas'],
+        url='https://github.com/f-allum/PyCESim/archive/refs/tags/v0.0.1.tar.gz',
+        keywords=['Coulomb explosion']
+)
