b1863df92693b582dcf67e614103b38d0b4347e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-31 04:04:51 +0000

    Add package ShellLM/0.0.1/ShellLM-0.0.1.tar.gz

---
+++ packages/ShellLM/ShellLM-0.0.1.tar.gz/ShellLM-0.0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ShellLM',
+    version='0.0.1',
+    packages=find_packages(),
+    description='A placeholder package for ShellLM',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/ShellLM',
+    author='Your Name',
+    author_email='your.email@example.com',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
