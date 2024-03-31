fa5dd9fa07c610d2a8887853c3f21d449f398e26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-31 04:04:09 +0000

    Add package QRComponent41C/0.0.7/QRComponent41C-0.0.7.tar.gz

---
+++ packages/QRComponent41C/QRComponent41C-0.0.7.tar.gz/QRComponent41C-0.0.7/QRComponent41C/main.py
@@ -0,0 +1,51 @@
+import qrcode
+from qrcode.main import QRCode
+from qrcode.image.styledpil import StyledPilImage
+from io import BytesIO
+from qrcode.image.styles.moduledrawers.pil import (
+    SquareModuleDrawer,
+    RoundedModuleDrawer,
+    GappedSquareModuleDrawer,
+    CircleModuleDrawer,
+    VerticalBarsDrawer,
+    HorizontalBarsDrawer,
+    StyledPilQRModuleDrawer
+)
+
+
+def image_as_bytes(image):
+    with BytesIO() as output:
+        image.save(output, format="PNG")
+        return output.getvalue()
+
+
+class QR41C:
+    module_drawer: StyledPilQRModuleDrawer
+    module_corrector: int
+
+    module_drawer_mapping = {
+        "Стандартный": SquareModuleDrawer,
+        "Закругленный": RoundedModuleDrawer,
+        "Разорванный": GappedSquareModuleDrawer,
+        "Круглый": CircleModuleDrawer,
+        "Вертикальный": VerticalBarsDrawer,
+        "Горизонтальный": HorizontalBarsDrawer,
+    }
+
+    module_corrector_mapping = {
+        "Маленький": qrcode.constants.ERROR_CORRECT_L,
+        "Средний": qrcode.constants.ERROR_CORRECT_M,
+        "Высокий": qrcode.constants.ERROR_CORRECT_Q,
+        "Очень высокий": qrcode.constants.ERROR_CORRECT_H,
+    }
+
+    def make(self, *args, **kwargs):
+        self.module_drawer = self.module_drawer_mapping[kwargs.get("module_drawer", "Стандартный")]()
+        self.module_corrector = self.module_corrector_mapping[kwargs.get("module_corrector", "Маленький")]
+        data = args[0]
+        qr = QRCode(
+            error_correction=self.module_corrector
+        )
+        qr.add_data(data)
+        image = qr.make_image(image_factory=StyledPilImage, module_drawer=self.module_drawer)
+        return image_as_bytes(image)
