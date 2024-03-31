e3360014ca0e0270a18875c13b3b3c185ec67a85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-31 14:07:39 +0000

    Add package GANDLF/0.0.20.dev20240331/GANDLF-0.0.20.dev20240331.tar.gz

---
+++ packages/GANDLF/GANDLF-0.0.20.dev20240331.tar.gz/GANDLF-0.0.20.dev20240331/GANDLF/compute/step.py
@@ -0,0 +1,97 @@
+from typing import Optional, Tuple
+import torch
+import psutil
+from .loss_and_metric import get_loss_and_metrics
+
+
+def step(
+    model: torch.nn.Module,
+    image: torch.Tensor,
+    label: torch.Tensor,
+    params: dict,
+    train: Optional[bool] = True,
+) -> Tuple[float, dict, torch.Tensor, torch.Tensor]:
+    """
+    This function performs a single step of training or validation.
+
+    Args:
+        model (torch.nn.Module): The model to process the input image with, it should support appropriate dimensions.
+        image (torch.Tensor): The input image stack according to requirements.
+        label (torch.Tensor): The input label for the corresponding image tensor.
+        params (dict): The parameters dictionary.
+        train (Optional[bool], optional): Whether the step is for training or validation. Defaults to True.
+
+    Returns:
+        Tuple[float, dict, torch.Tensor, torch.Tensor]: The loss, metrics, output, and attention map.
+    """
+    if params["verbose"]:
+        if torch.cuda.is_available():
+            print(torch.cuda.memory_summary())
+        print(
+            "|===========================================================================|"
+        )
+        print(
+            "|                              CPU Utilization                              |"
+        )
+        print("Load_Percent   :", psutil.cpu_percent(interval=None))
+        print("MemUtil_Percent:", psutil.virtual_memory()[2])
+        print(
+            "|===========================================================================|"
+        )
+
+    # for the weird cases where mask is read as an RGB image, ensure only the first channel is used
+    if label is not None:
+        if params["problem_type"] == "segmentation":
+            if label.shape[1] == 3:
+                label = label[:, 0, ...].unsqueeze(1)
+                # this warning should only come up once
+                if params["print_rgb_label_warning"]:
+                    print(
+                        "WARNING: The label image is an RGB image, only the first channel will be used.",
+                        flush=True,
+                    )
+                    params["print_rgb_label_warning"] = False
+
+            if params["model"]["dimension"] == 2:
+                label = torch.squeeze(label, -1)
+
+    if params["model"]["dimension"] == 2:
+        image = torch.squeeze(image, -1)
+        if "value_keys" in params:
+            if label is not None:
+                if len(label.shape) > 1:
+                    label = torch.squeeze(label, -1)
+
+    if not (train) and params["model"]["type"].lower() == "openvino":
+        output = torch.from_numpy(
+            model(inputs={params["model"]["IO"][0][0]: image.cpu().numpy()})[
+                params["model"]["IO"][1][0]
+            ]
+        )
+        output = output.to(params["device"])
+    else:
+        if params["model"]["amp"]:
+            with torch.cuda.amp.autocast():
+                output = model(image)
+        else:
+            output = model(image)
+
+    attention_map = None
+    if "medcam_enabled" in params and params["medcam_enabled"]:
+        output, attention_map = output
+
+    # one-hot encoding of 'label' will probably be needed for segmentation
+    if label is not None:
+        loss, metric_output = get_loss_and_metrics(image, label, output, params)
+    else:
+        loss, metric_output = None, None
+
+    if len(output) > 1:
+        output = output[0]
+
+    if params["model"]["dimension"] == 2:
+        output = torch.unsqueeze(output, -1)
+        if "medcam_enabled" in params and params["medcam_enabled"]:
+            attention_map = torch.unsqueeze(attention_map, -1)
+
+    return loss, metric_output, output, attention_map
---
+++ packages/GANDLF/GANDLF-0.0.20.dev20240331.tar.gz/GANDLF-0.0.20.dev20240331/GANDLF/inference_manager.py
@@ -0,0 +1,121 @@
+import os
+from typing import Optional
+from pathlib import Path
+import pandas as pd
+import torch
+import torch.nn.functional as F
+
+from GANDLF.compute import inference_loop
+from GANDLF.utils import get_unique_timestamp
+
+
+def InferenceManager(
+    dataframe: pd.DataFrame,
+    modelDir: str,
+    parameters: dict,
+    device: str,
+    outputDir: Optional[str] = None,
+) -> None:
+    """
+    This function is used to perform inference on a model using a dataframe.
+
+    Args:
+        dataframe (pd.DataFrame): The dataframe containing the data to be used for inference.
+        modelDir (str): The path to the model directory.
+        parameters (dict): The parameters to be used for inference.
+        device (str): The device to be used for inference.
+        outputDir (Optional[str], optional): The output directory for the inference results. Defaults to None.
+    """
+    # get the indeces for kfold splitting
+    inferenceData_full = dataframe
+
+    # if outputDir is not provided, create a new directory with a unique timestamp
+    if outputDir is None:
+        outputDir = os.path.join(modelDir, get_unique_timestamp())
+        print(
+            "Output directory not provided, creating a new directory with a unique timestamp: ",
+            outputDir,
+        )
+    Path(outputDir).mkdir(parents=True, exist_ok=True)
+
+    parameters["output_dir"] = outputDir
+
+    # initialize parameters for inference
+    for key in ["penalty_weights", "sampling_weights", "class_weights"]:
+        parameters[key] = parameters.get(key, None)
+
+    n_folds = parameters["nested_training"]["validation"]
+
+    modelDir_split = [modelDir]
+    if "," in modelDir:
+        modelDir_split = modelDir.split(",")
+
+    averaged_probs_list = []
+    for current_modelDir in modelDir_split:
+        fold_dirs = []
+        if n_folds > 1:
+            directories = sorted(os.listdir(current_modelDir))
+            for d in directories:
+                if d.isdigit():
+                    fold_dirs.append(os.path.join(current_modelDir, d, ""))
+        else:
+            fold_dirs = [current_modelDir]
+
+        # this is for the case where inference is happening using a single model
+        if len(fold_dirs) == 0:
+            fold_dirs = [current_modelDir]
+
+        probs_list = []
+        class_list = None
+        is_classification = parameters["problem_type"] == "classification"
+
+        # initialize model type for processing: if not defined, default to torch
+        if not ("type" in parameters["model"]):
+            parameters["model"]["type"] = "torch"
+
+        for fold_dir in fold_dirs:
+            parameters["current_fold_dir"] = fold_dir
+            inference_loop(
+                inferenceDataFromPickle=inferenceData_full,
+                modelDir=fold_dir,
+                device=device,
+                parameters=parameters,
+                outputDir=outputDir,
+            )
+
+            if is_classification:
+                logits_dir = os.path.join(fold_dir, "logits.csv")
+                is_logits_dir_exist = os.path.isfile(logits_dir)
+
+                if is_logits_dir_exist:
+                    # fold_logits = np.genfromtxt(logits_dir, delimiter=",")
+                    class_list = [str(c) for c in parameters["model"]["class_list"]]
+                    fold_logits = pd.read_csv(logits_dir)[class_list].values
+                    fold_logits = torch.from_numpy(fold_logits)
+                    fold_probs = F.softmax(fold_logits, dim=1)
+                    probs_list.append(fold_probs)
+
+        if is_classification and (n_folds > 1):
+            probs_list = torch.stack(probs_list)
+            averaged_probs_list.append(torch.mean(probs_list, 0))
+
+    # this logic should be changed if we want to do multi-fold inference for histo images
+    if (parameters["modality"] == "rad") and averaged_probs_list and is_classification:
+        columns = ["SubjectID", "PredictedClass"] + parameters["model"]["class_list"]
+        averaged_probs_df = pd.DataFrame(columns=columns)
+        averaged_probs_df.SubjectID = dataframe[0]
+
+        averaged_probs_across_models = torch.stack(averaged_probs_list)
+        averaged_probs_across_models = torch.mean(
+            averaged_probs_across_models, 0
+        ).numpy()
+        averaged_probs_df[class_list] = averaged_probs_across_models
+        averaged_probs_df.PredictedClass = [
+            class_list[a] for a in averaged_probs_across_models.argmax(1)
+        ]
+        filepath_to_save = os.path.join(outputDir, "final_preds_and_avg_probs.csv")
+        if os.path.isfile(filepath_to_save):
+            filepath_to_save = os.path.join(
+                outputDir, "final_preds_and_avg_probs" + get_unique_timestamp() + ".csv"
+            )
+        averaged_probs_df.to_csv(filepath_to_save, index=False)
