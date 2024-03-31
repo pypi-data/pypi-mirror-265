f1840e1b588a397fbbfd86174675b79ba1f9df00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-31 04:04:12 +0000

    Add package gameInvasion3/1.1.3/gameInvasion3-1.1.3.tar.gz

---
+++ packages/gameInvasion3/gameInvasion3-1.1.3.tar.gz/gameInvasion3-1.1.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import codecs
+
+with open('requirements.txt',encoding='utf-8') as f:
+    requirements = f.read().splitlines()
+
+# 使用codecs模块打开MANIFEST.in文件，并指定编码为utf-8
+with codecs.open('MANIFEST.in', 'r', 'utf-8') as f:
+    manifest = f.read()
+
+setup(
+    name='gameInvasion3',
+    version='1.1.3',
+    url='https://github.com/MaoJiayang/gameInvasion3',
+    author='Jiayang Mao',
+    author_email='Nucleon_17th@njust.edu.cn',
+    description='a python game engine for my 2D game development',
+    packages=find_packages(),    
+    install_requires=requirements,
+    # 添加manifest参数
+    manifest=manifest,
+        entry_points={
+        'console_scripts': [
+            'gameInvasion3=invasion_game_demo.main:main', 
+        ],
+    },
+)
