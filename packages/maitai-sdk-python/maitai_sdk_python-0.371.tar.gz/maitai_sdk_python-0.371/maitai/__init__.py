96bf6161dc369b6b5ae8d5d05eca9a959c9d0ce7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-31 04:03:29 +0000

    Add package maitai-sdk-python/0.371/maitai_sdk_python-0.371.tar.gz

---
+++ packages/maitai-sdk-python/maitai_sdk_python-0.371.tar.gz/maitai_sdk_python-0.371/maitai/__init__.py
@@ -0,0 +1,5 @@
+from maitai._config import config
+from maitai._eval_request import EvalRequest as EvalRequest
+from maitai._loadable import Loadable as Loadable
+from maitai._maitai_object import MaiTaiObject as MaiTaiObject
+from maitai._evaluator import Evaluator as Evaluator
