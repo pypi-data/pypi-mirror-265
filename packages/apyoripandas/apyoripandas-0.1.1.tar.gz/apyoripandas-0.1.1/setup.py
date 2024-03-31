592483b3e7dfa1cfb0ac5c891e58601246c612f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-31 14:07:33 +0000

    Add package apyoripandas/0.1.1/apyoripandas-0.1.1.tar.gz

---
+++ packages/apyoripandas/apyoripandas-0.1.1.tar.gz/apyoripandas-0.1.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+
+
+
+setup(
+    name="apyoripandas",
+    version="0.1.1",
+    description="Make easy user Apyori for Pandas",
+    author="Matheus de Sá",
+    author_email="matheusdesa55@gmail.com",
+    url="https://github.com/matheus0sa/apyoripandas",
+    packages=["apyoripandas"],
+    python_requires=">=3.6",
+    long_description=open("README.md", "r", encoding="utf-8").read()
+)
