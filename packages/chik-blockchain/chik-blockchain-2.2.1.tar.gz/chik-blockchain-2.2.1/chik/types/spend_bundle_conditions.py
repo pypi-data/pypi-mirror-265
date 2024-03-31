dd0c31c8c5773f5866bb10c0a1399abcf729eace
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-31 14:07:17 +0000

    Add package chik-blockchain/2.2.1/chik-blockchain-2.2.1.tar.gz

---
+++ packages/chik-blockchain/chik-blockchain-2.2.1.tar.gz/chik-blockchain-2.2.1/chik/types/spend_bundle_conditions.py
@@ -0,0 +1,5 @@
+from __future__ import annotations
+
+from chik_rs import ELIGIBLE_FOR_DEDUP, Spend, SpendBundleConditions
+
+__all__ = ["Spend", "SpendBundleConditions", "ELIGIBLE_FOR_DEDUP"]
---
+++ packages/chik-blockchain/chik-blockchain-2.2.1.tar.gz/chik-blockchain-2.2.1/chik/wallet/cat_wallet/cat_constants.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+
+CHIK_CAT = {
+    "asset_id": "5ceb7442d9168f0fc0d459011f39129d8fd137f1900e9895fd791351cb105030",
+    "name": "Tiny Chik Token",
+    "symbol": "TCT",
+}
+
+STABLY_USDS = {
+    "asset_id": "6d95dae356e32a71db5ddcb42224754a02524c615c5fc35f568c2af04774e589",
+    "name": "Stably USD Classic",
+    "symbol": "USDSC",
+}
+
+DEFAULT_CATS = {
+    CHIK_CAT["asset_id"]: CHIK_CAT,
+    STABLY_USDS["asset_id"]: STABLY_USDS,
+}
