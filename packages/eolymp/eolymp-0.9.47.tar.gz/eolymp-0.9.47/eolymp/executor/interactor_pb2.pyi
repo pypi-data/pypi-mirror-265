7c4535549309a21406834f67a415369b619fd5bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-31 14:07:05 +0000

    Add package eolymp/0.9.47/eolymp-0.9.47.tar.gz

---
+++ packages/eolymp/eolymp-0.9.47.tar.gz/eolymp-0.9.47/eolymp/executor/interactor_pb2.pyi
@@ -0,0 +1,32 @@
+from google.protobuf.internal import containers as _containers
+from google.protobuf.internal import enum_type_wrapper as _enum_type_wrapper
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Iterable as _Iterable, Mapping as _Mapping, Optional as _Optional, Union as _Union
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class Interactor(_message.Message):
+    __slots__ = ["files", "lang", "source", "type"]
+    class Type(int, metaclass=_enum_type_wrapper.EnumTypeWrapper):
+        __slots__ = []
+    class File(_message.Message):
+        __slots__ = ["path", "source_ern", "source_url"]
+        PATH_FIELD_NUMBER: _ClassVar[int]
+        SOURCE_ERN_FIELD_NUMBER: _ClassVar[int]
+        SOURCE_URL_FIELD_NUMBER: _ClassVar[int]
+        path: str
+        source_ern: str
+        source_url: str
+        def __init__(self, path: _Optional[str] = ..., source_ern: _Optional[str] = ..., source_url: _Optional[str] = ...) -> None: ...
+    FILES_FIELD_NUMBER: _ClassVar[int]
+    LANG_FIELD_NUMBER: _ClassVar[int]
+    NONE: Interactor.Type
+    PROGRAM: Interactor.Type
+    SOURCE_FIELD_NUMBER: _ClassVar[int]
+    TYPE_FIELD_NUMBER: _ClassVar[int]
+    files: _containers.RepeatedCompositeFieldContainer[Interactor.File]
+    lang: str
+    source: str
+    type: Interactor.Type
+    def __init__(self, type: _Optional[_Union[Interactor.Type, str]] = ..., lang: _Optional[str] = ..., source: _Optional[str] = ..., files: _Optional[_Iterable[_Union[Interactor.File, _Mapping]]] = ...) -> None: ...
