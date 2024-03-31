0ec0a897e74e99afe0df91eba936aefaf2ea967b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-31 14:07:17 +0000

    Add package genzql/0.1.1/genzql-0.1.1-py3-none-any.whl

---
+++ packages/genzql/genzql-0.1.1-py3-none-any.whl/genzql/types.py
@@ -0,0 +1,22 @@
+from enum import Enum
+from typing import Dict
+
+
+ZqlQuery = str
+SqlQuery = str
+AstNode = Dict
+Token = str
+MaybeDialect = str | None
+
+
+class NodeType(Enum):
+    QUERY = "query"
+    SELECT = "select"
+    FROM = "from"
+    WHERE = "where"
+    LIMIT = "limit"
+    FILTER = "filter"
+    FILTER_BRANCH = "filter_branch"
+    EXPRESSION = "expression"
+    INTEGER = "integer"
+    TERMINAL = "terminal"
