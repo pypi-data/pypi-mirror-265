85d0c2894e6848cfdf396a18cd1d7d0e57bea9a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-31 03:03:37 +0000

    Add package jump-wsgi/0.1.0/jump-wsgi-0.1.0.tar.gz

---
+++ packages/jump-wsgi/jump-wsgi-0.1.0.tar.gz/jump-wsgi-0.1.0/README.md
@@ -0,0 +1,14 @@
+<h3 align="center"><img src="IMG_0760.png" alt="logo" height="250px"></h3>
+
+<p align="center">
+    <b>Bucket WSGI</b><br>
+    <br>
+    The Bucket WSGI is a compex program that lets the user create outstanding.
+    <br>Web applications with such simplicity.
+    <br>
+    <br>
+    <b>Installation</b><br>
+    <br>
+    <code>pip3 install jump-wsgi</code>
+</p>
+
---
+++ packages/jump-wsgi/jump-wsgi-0.1.0.tar.gz/jump-wsgi-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+name='jump-wsgi',
+version='0.1.0',
+author='Rayan Haddda',
+author_email='rayan.m.haddad@proton.me',
+description='The Python Micro Framework for building web applications',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.6',
+)
