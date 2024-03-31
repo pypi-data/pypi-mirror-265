73605a75029dd41dbf159c02f3407599c99cf6b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-31 03:03:21 +0000

    Add package disdial/1.3.0/disdial-1.3.0.tar.gz

---
+++ packages/disdial/disdial-1.3.0.tar.gz/disdial-1.3.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="disdial",
+    version="1.3.0",
+    author="Arpit Sengar (arpy8)",
+    description="A terminal-based IRC-inspired package that enables users to chat on a single server with everyone..",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=["wheel", "termcolor", "setuptools", "argparse", "requests", "pytz", "argparse", "datetime", "keyboard"],
+    entry_points={
+        "console_scripts": [
+            "disdial=disdial.main:main",
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    include_package_data=True,
+    packages=find_packages(),
+    package_data={'disdial': ['*.txt', '*.json']},
+)
