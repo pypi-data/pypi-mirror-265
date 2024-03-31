04b8b1f620619dcb060984726f183ed9756f009d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-31 03:03:07 +0000

    Add package pydvpl/1.2.1/pydvpl-1.2.1.tar.gz

---
+++ packages/pydvpl/pydvpl-1.2.1.tar.gz/pydvpl-1.2.1/src/pydvpl/dvpl/__init__.py
@@ -0,0 +1,10 @@
+from ._dvpl import (
+    read_dvpl_footer,
+    create_dvpl_footer,
+    compress_dvpl,
+    decompress_dvpl,
+    DVPL_FOOTER_SIZE,
+    DVPL_TYPE_NONE,
+    DVPL_TYPE_LZ4,
+    __LZ4_VERSION__
+)
