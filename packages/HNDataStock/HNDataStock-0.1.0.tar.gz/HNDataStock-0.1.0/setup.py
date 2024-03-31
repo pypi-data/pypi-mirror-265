0e8aec9da78c67be1cb9f06480476d62fc9c5989
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-31 15:04:02 +0000

    Add package HNDataStock/0.1.0/HNDataStock-0.1.0.tar.gz

---
+++ packages/HNDataStock/HNDataStock-0.1.0.tar.gz/HNDataStock-0.1.0/setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name='HNDataStock',
+    version='0.1.0',
+    author='Ten tac gia',
+    author_email='email@example.com',
+    description='Mo ta ngan gon',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='http://www.huongnghiepdulieu.com',
+    packages=find_packages(),
+    install_requires=[
+        # Danh sách các phụ thuộc
+    ],
+    classifiers=[
+        # Danh sách các phân loại
+    ],
+)
