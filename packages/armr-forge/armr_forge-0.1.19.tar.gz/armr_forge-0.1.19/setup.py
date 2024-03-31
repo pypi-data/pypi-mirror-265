e0acc18b935e90a37c91ff5931903a3bc2d898e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-31 14:07:15 +0000

    Add package armr-forge/0.1.19/armr_forge-0.1.19.tar.gz

---
+++ packages/armr-forge/armr_forge-0.1.19.tar.gz/armr_forge-0.1.19/setup.py
@@ -0,0 +1,23 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='armr_forge',
+    version='0.1.19',
+    packages=['armr_forge'],
+    install_requires=[
+	'bcrypt>=4.0.1',
+	'cryptography>=41.0.5',
+	'numpy>=1.26.1',
+	'pandas>=2.1.2',
+    ],
+    # Metadata
+    author='StalwartBI',
+    license='Apache License 2.0',
+    author_email='StalwartBI@outlook.com',
+    description='The Adanced Record Management Resource',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/StalwartBI/armr_forge/armr_forge",
+
+)
