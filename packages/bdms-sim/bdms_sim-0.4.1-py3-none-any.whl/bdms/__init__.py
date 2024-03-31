14c32fef2a3de3eddfc2ea282e93340fd5cf7e7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-31 04:04:08 +0000

    Add package bdms-sim/0.4.1/bdms_sim-0.4.1-py3-none-any.whl

---
+++ packages/bdms-sim/bdms_sim-0.4.1-py3-none-any.whl/bdms/__init__.py
@@ -0,0 +1,9 @@
+__version__ = "0.4.1"
+
+from .tree import TreeNode as _TreeNode
+
+TreeNode = _TreeNode
+"""Alias for :py:class:`bdms.tree.TreeNode`."""
+
+Tree = _TreeNode
+"""Alias for :py:class:`bdms.tree.TreeNode`."""
