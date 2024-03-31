ffffb1ca7fc905503b4955259f9721653cde286b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-31 14:07:35 +0000

    Add package meta-edc/0.3.9/meta_edc-0.3.9-py3-none-any.whl

---
+++ packages/meta-edc/meta_edc-0.3.9-py3-none-any.whl/meta_subject/admin/dm_referral_followup_admin.py
@@ -0,0 +1,89 @@
+from django.contrib import admin
+from django_audit_fields.admin import audit_fieldset_tuple
+from edc_adherence.model_admin_mixin import get_visual_score_fieldset_tuple
+from edc_crf.fieldset import crf_status_fieldset
+from edc_form_label.form_label_modeladmin_mixin import FormLabelModelAdminMixin
+from edc_model_admin.history import SimpleHistoryAdmin
+
+from ..admin_site import meta_subject_admin
+from ..forms.dm_referral_followup import DmReferralFollowupForm
+from ..models import DmReferralFollowup
+from .modeladmin import CrfModelAdminMixin
+
+
+@admin.register(DmReferralFollowup, site=meta_subject_admin)
+class DmReferralFollowupAdmin(
+    CrfModelAdminMixin, FormLabelModelAdminMixin, SimpleHistoryAdmin
+):
+    form = DmReferralFollowupForm
+
+    fieldsets = (
+        (None, {"fields": ("subject_visit", "report_datetime")}),
+        ("Referral to Diabetes clinic", {"fields": ("referral_date",)}),
+        (
+            "Diabetes clinic attendance",
+            {
+                "fields": (
+                    "attended",
+                    "facility_attended",
+                    "missed_referral_reasons",
+                    "other_missed_referral_reason",
+                    "attended_date",
+                    "healthcare_workers",
+                    "other_healthcare_workers",
+                ),
+            },
+        ),
+        (
+            "Investigations",
+            {
+                "fields": (
+                    "investigations_performed",
+                    "investigations",
+                    "other_investigations",
+                    "complications_checks",
+                ),
+            },
+        ),
+        (
+            "Diabetes treatment",
+            {
+                "fields": (
+                    "dm_treatments",
+                    "on_dm_medications",
+                    "dm_medications_init_date",
+                    "dm_medications",
+                    "other_dm_medications",
+                ),
+            },
+        ),
+        (
+            "Diabetes Medication Adherence",
+            {
+                "fields": (
+                    "medications_adherent",
+                    "last_missed_pill",
+                ),
+            },
+        ),
+        get_visual_score_fieldset_tuple(),
+        crf_status_fieldset,
+        audit_fieldset_tuple,
+    )
+
+    filter_horizontal = (
+        "healthcare_workers",
+        "investigations",
+        "complications_checks",
+        "dm_treatments",
+        "dm_medications",
+        "missed_referral_reasons",
+    )
+
+    radio_fields = {
+        "attended": admin.VERTICAL,
+        "investigations_performed": admin.VERTICAL,
+        "on_dm_medications": admin.VERTICAL,
+        "medications_adherent": admin.VERTICAL,
+        "last_missed_pill": admin.VERTICAL,
+    }
