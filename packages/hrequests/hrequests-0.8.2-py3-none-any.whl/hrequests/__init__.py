3ba317fb4777cd65a4ba3f9fe1fe49465bdb0ebe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-03-31 14:07:17 +0000

    Add package hrequests/0.8.2/hrequests-0.8.2-py3-none-any.whl

---
+++ packages/hrequests/hrequests-0.8.2-py3-none-any.whl/hrequests/__init__.py
@@ -0,0 +1,52 @@
+'''
+Hrequests initializer
+'''
+
+import os
+
+
+def detect_module() -> bool:
+    '''
+    Hacky way to detect if hrequests is being ran as a module
+    '''
+    import inspect
+
+    stack: list = inspect.stack(2)
+    if len(stack) < 2:
+        return False
+    prev, launch = stack[-2:]
+    try:
+        if (launch.function, prev.function) == ('_run_module_as_main', '_get_module_details'):
+            return True
+    except AttributeError:
+        pass
+    return False
+
+
+if detect_module():
+    os.environ['HREQUESTS_MODULE'] = '1'
+
+
+from .response import Response, ProcessResponse
+from .session import Session, TLSSession, chrome, firefox
+from .reqs import *
+from .headers import Headers
+
+
+# attempt to import headless browsing dependencies
+try:
+    from .playwright_mock import ChromeBrowser, FirefoxBrowser
+    from .browser import BrowserSession, render
+
+    os.environ['HREQUESTS_PW'] = '1'
+except ModuleNotFoundError:
+    from rich import print as rprint
+
+    if not os.getenv('HREQUESTS_MODULE'):
+        rprint(
+            r'[bright_yellow]WARNING: Please run [white]pip install hrequests\[all][/] for automated browsing support.'
+        )
+
+from .parser import HTML
+from .__version__ import __version__
+from .__version__ import __author__
---
+++ packages/hrequests/hrequests-0.8.2-py3-none-any.whl/hrequests/__version__.py
@@ -0,0 +1,8 @@
+__title__ = "hrequests"
+__description__ = "Webscraping for humans"
+__version__ = "0.8.2"
+__author__ = "daijro"
+__license__ = "Apache 2.0"
+
+# version of hrequests-cgo library
+BRIDGE_VERSION = '2.'
