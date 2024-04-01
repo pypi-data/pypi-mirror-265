fe1a1f1371ce6cfb4aab1bb4a958ae7ef6a42d5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-01 14:04:21 +0000

    Add package jupyterlab-skip-traceback/5.1.0/jupyterlab_skip_traceback-5.1.0.tar.gz

---
+++ packages/jupyterlab-skip-traceback/jupyterlab_skip_traceback-5.1.0.tar.gz/jupyterlab_skip_traceback-5.1.0/CHANGELOG.md
@@ -0,0 +1,44 @@
+## [5.1.0](https://github.com/deshaw/jupyterlab-skip-traceback/compare/v5.0.0...v5.1.0) (20240331)
+
+### Added
+
+- Support for clicking on paths in tracebacks (requires >=jupyterlab@4.1) #20
+
+## [5.0.0](https://github.com/deshaw/jupyterlab-skip-traceback/compare/v4.0.4...v5.0.0) (20220519)
+
+### Changed
+
+- **Breaking**: Ported to JupyterLab 4.x
+
+## [4.0.4](https://github.com/deshaw/jupyterlab-skip-traceback/compare/v4.0.3...v4.0.4) (20220621)
+
+### Fixed
+
+- Collapsed state renders text instead of HTML
+
+## [4.0.3](https://github.com/deshaw/jupyterlab-skip-traceback/compare/v4.0.1...v4.0.3) (20220613)
+
+### Fixed
+
+- Default rendering when collapsed is set to `true` from Settings
+
+## [4.0.1](https://github.com/deshaw/jupyterlab-skip-traceback/compare/v4.0.0...v4.0.1) (20220526)
+
+### Fixed
+
+- Improved rendering of multi-line tracebacks
+
+## 4.0.0 (20210317)
+
+### Changed
+
+- **BREAKING** Jupyterlab 3 Upgrade
+
+## 3.0.0 (20200713)
+
+### Added
+
+- Public release
+
+The project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html) and
+this CHANGELOG follows the [Keep a Changelog](https://keepachangelog.com/en/1.0.0/) standard.
---
+++ packages/jupyterlab-skip-traceback/jupyterlab_skip_traceback-5.1.0.tar.gz/jupyterlab_skip_traceback-5.1.0/src/SkipTracebackWidget.ts
@@ -0,0 +1,143 @@
+import '../style/index.css';
+
+import { Widget } from '@lumino/widgets';
+import { IRenderMime, renderText, renderError } from '@jupyterlab/rendermime';
+import { Clipboard } from '@jupyterlab/apputils/lib/clipboard';
+
+const BTN_BASE_CLASS = 'minimal jp-Button';
+const COPY_CLASS = `fa fa-fw fa-copy ${BTN_BASE_CLASS} right-align`;
+const TOGGLE_CLOSED_CLASS = `fa fa-caret-right jp-ToolbarButtonComponent ${BTN_BASE_CLASS}`;
+const TOGGLE_OPENED_CLASS = `fa fa-caret-down jp-ToolbarButtonComponent ${BTN_BASE_CLASS}`;
+const SHORT_ERROR_CLASS = 'short-error';
+const RED_BOLD_TEXT_CLASS = 'red-bold-text';
+
+// Defined via: https://nbformat.readthedocs.io/en/latest/format_description.html#error
+interface IError {
+  output_type: string;
+  ename: string; // Exception name, as a string
+  evalue: string; // Exception value, as a string
+  traceback: string[]; // The traceback will contain a list of frames, represented each as a string.
+}
+
+const setMessageInCollapsedState = (
+  shortError: HTMLPreElement,
+  data: IError,
+) => {
+  const eName = document.createElement('span');
+  eName.className = RED_BOLD_TEXT_CLASS;
+  eName.textContent = data.ename;
+  const eValue = document.createTextNode(`: ${data.evalue}`);
+  shortError.innerHTML = '';
+  shortError.appendChild(eName);
+  shortError.appendChild(eValue);
+};
+
+// prettier-ignore
+export default class SkipTracebackWidget
+  extends Widget
+  implements IRenderMime.IRenderer {
+  constructor(options: IRenderMime.IRendererOptions) {
+    super();
+    this._mimeType = options.mimeType;
+    this._options = options;
+  }
+
+  private static _defaults = {
+    collapsed: true,
+  };
+
+  static setDefaults(newDefaults: Record<string, unknown>): void {
+    SkipTracebackWidget._defaults = {
+      ...SkipTracebackWidget._defaults,
+      ...newDefaults,
+    };
+  }
+
+  _toggleTraceback(): void {
+    if (this._toggleBtn && this._tracebackNode && this._shortError && this._data) {
+      const isToggled = this._toggleBtn.className === TOGGLE_CLOSED_CLASS;
+      if (isToggled) {
+        this._toggleBtn.className = TOGGLE_OPENED_CLASS;
+        this._shortError.innerHTML = '';
+        this.node.appendChild(this._tracebackNode);
+      } else {
+        this._toggleBtn.className = TOGGLE_CLOSED_CLASS;
+        setMessageInCollapsedState(this._shortError, this._data);
+        this.node.removeChild(this._tracebackNode);
+      }
+    }
+  }
+
+  _copyTraceback(): void {
+    if (this._tracebackNode) {
+      Clipboard.copyToSystem(this._tracebackNode.textContent || '');
+    }
+  }
+
+  renderModel(model: IRenderMime.IMimeModel): Promise<void> {
+    this._data = model.data[this._mimeType] as unknown as IError;
+
+    const toggleBtn = document.createElement('button');
+    toggleBtn.className = TOGGLE_CLOSED_CLASS;
+    toggleBtn.onclick = this._toggleTraceback.bind(this);
+    this._toggleBtn = toggleBtn;
+
+    const shortError = document.createElement('pre');
+    shortError.className = SHORT_ERROR_CLASS;
+    setMessageInCollapsedState(shortError, this._data);
+    shortError.onclick = this._toggleTraceback.bind(this);
+    this._shortError = shortError;
+
+    const copyBtn = document.createElement('button');
+    copyBtn.className = COPY_CLASS;
+    copyBtn.onclick = this._copyTraceback.bind(this);
+    copyBtn.title = 'Copy full traceback to clipboard';
+
+    const span = document.createElement('div');
+    span.className = 'skip-traceback';
+    span.appendChild(copyBtn);
+    span.appendChild(toggleBtn);
+    span.appendChild(shortError);
+
+    const traceback = document.createElement('pre');
+    // It should look like stderr
+    const source = (model.data['application/vnd.jupyter.stderr'] as string) ||
+          this._data.traceback.join('\n');
+
+    let renderedPromise: Promise<void>;
+    if (typeof renderError !== 'undefined') {
+      renderedPromise = renderError({
+        ...this._options,
+        host: traceback,
+        source
+      });
+    } else {
+      renderedPromise = renderText({
+        ...this._options,
+        host: traceback,
+        source
+      });
+    }
+    const tbDiv = document.createElement('div');
+    tbDiv.className = 'jp-RenderedText';
+    tbDiv.setAttribute('data-mime-type', 'application/vnd.jupyter.stderr');
+    tbDiv.appendChild(traceback);
+    // End hack due to issue
+    this._tracebackNode = tbDiv;
+
+    this.node.appendChild(span);
+
+    if (!SkipTracebackWidget._defaults.collapsed) {
+      this._toggleTraceback();
+    }
+    // Don't finish until we render the text
+    return renderedPromise;
+  }
+
+  private _toggleBtn?: HTMLButtonElement;
+  private _tracebackNode?: HTMLDivElement;
+  private _options: IRenderMime.IRendererOptions;
+  private _data?: IError;
+  private _mimeType: string;
+  private _shortError?: HTMLPreElement;
+}
