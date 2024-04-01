36d58da6e0d7e6154f60edc8cd6811c6f53a9df7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-01 15:04:11 +0000

    Add package okx-sdk/1.2.5/okx_sdk-1.2.5-py3-none-any.whl

---
+++ packages/okx-sdk/okx_sdk-1.2.5-py3-none-any.whl/okx/restapi/SignalTrading.py
@@ -0,0 +1,8 @@
+from .BaseClient import OkxBaseClient
+from ..constants import *
+
+
+class SignalTradingClient(OkxBaseClient):
+    def __init__(self, apikey='', apisecret='', passphrase='',
+                 use_server_time=False, simulation=False, domain=API_URL, debug=False, proxy=None):
+        OkxBaseClient.__init__(self, apikey, apisecret, passphrase, use_server_time, simulation, domain, debug, proxy)
