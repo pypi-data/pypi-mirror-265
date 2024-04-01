01f27c472300c0da39572c9244193266958efe84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-01 14:04:27 +0000

    Add package csv2database/0.0.4/csv2database-0.0.4.tar.gz

---
+++ packages/csv2database/csv2database-0.0.4.tar.gz/csv2database-0.0.4/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='csv2database',
+    version='0.0.4',
+    author='Md Arfaan Baig',
+    author_email='mdarfaanbaig@gmail.com',
+    description='A package for importing CSV files into a MySQL database',
+    long_description='This package provides functionality to import CSV files into a MySQL database, supporting both local and Google Drive sources.',
+    url='https://github.com/mdarfaanbaig/csv2database',
+    license='MIT',
+    package_dir={'': 'src'},  # Source code directory
+    packages=find_packages(where='src'),  # Find packages under src directory
+    install_requires=[
+        'numpy>=1.0.0',
+        'pandas>=1.0.0',
+        'mysql-connector-python>=8.0.0',
+        'google-api-python-client>=2.0.0',
+    ],
+    entry_points={
+        'console_scripts': [
+            'csv_import=csv2database.main:main',
+        ],
+    },
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',  
+)
