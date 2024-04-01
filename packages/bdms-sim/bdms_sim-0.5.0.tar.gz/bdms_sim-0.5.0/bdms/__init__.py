7a8eb52cf6b99057d41b3f82155399706d9de953
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-01 04:03:47 +0000

    Add package bdms-sim/0.5.0/bdms_sim-0.5.0.tar.gz

---
+++ packages/bdms-sim/bdms_sim-0.5.0.tar.gz/bdms_sim-0.5.0/bdms/__init__.py
@@ -0,0 +1,9 @@
+__version__ = "0.5.0"
+
+from .tree import TreeNode as _TreeNode
+
+TreeNode = _TreeNode
+"""Alias for :py:class:`bdms.tree.TreeNode`."""
+
+Tree = _TreeNode
+"""Alias for :py:class:`bdms.tree.TreeNode`."""
