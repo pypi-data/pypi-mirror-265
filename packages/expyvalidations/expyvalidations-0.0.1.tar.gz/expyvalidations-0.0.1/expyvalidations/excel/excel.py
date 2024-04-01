66db94d10faf248242e9c46ac94345674e9af275
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-01 04:03:53 +0000

    Add package expyvalidations/0.0.1/expyvalidations-0.0.1.tar.gz

---
+++ packages/expyvalidations/expyvalidations-0.0.1.tar.gz/expyvalidations-0.0.1/expyvalidations/excel/excel.py
@@ -0,0 +1,332 @@
+import re
+import sys
+from typing import Callable, Any, Literal, Union
+from alive_progress import alive_bar
+
+import pandas as pd
+from expyvalidations import config
+from expyvalidations.config import CheckException
+from expyvalidations.excel.checks import CheckTypes
+from expyvalidations.utils import string_normalize
+
+
+class ExpyValidations:
+
+    def __init__(
+        self,
+        path_file: str,
+        sheet_name: str = "sheet",
+        book: pd.ExcelFile = None,
+        header_row: int = 1,
+        verbose: bool = False,
+    ):
+
+        self.column_details: list = []
+        self.verbose: bool = verbose
+        self.__book = pd.ExcelFile(path_file)
+
+        self.__errors_list: list = []
+        self.erros: bool = False
+        """
+        indica se ouve algum erro nas validações da planilha
+        se tiver erro o método data_all não deve
+        retornar dados
+        """
+
+        self.excel: pd.DataFrame
+        try:
+            excel = pd.read_excel(self.__book, self.sheet_name(sheet_name), header=header_row)
+            # retirando linhas e colunas em brando do Data Frame
+            excel = excel.dropna(how="all")
+            excel.columns = excel.columns.astype("string")
+            excel = excel.loc[:, ~excel.columns.str.contains("^Unnamed")]
+            excel = excel.astype(object)
+            excel = excel.where(pd.notnull(excel), None)
+            self.excel = excel
+
+        except ValueError as exp:
+            raise ValueError(exp)
+
+        self.__header_row = header_row
+        self.add_row_column()
+
+        self.checks = CheckTypes()
+
+    def sheet_name(self, search: str) -> str:
+        """
+        Função responsável por pesquisa a string do parâmetro 'search'
+        nas planilhas (sheets) do 'book' especificado no __init__
+        e retornar o 1º nome de planilha que encontrar na pesquisa
+
+        Caso tenha apenas 1 planilha no arquivo ela é retornada
+        """
+        if len(self.__book.sheet_names) == 1:
+            return self.__book.sheet_names[0]
+
+        for names in self.__book.sheet_names:
+            name = string_normalize(names)
+            if re.search(search, name, re.IGNORECASE):
+                return names
+        raise ValueError(f"ERROR! Sheet '{search}' not found! Rename your sheet!")
+
+    def column_name(self, column_name: Union[str, list[str]]) -> str:
+        """
+        Resquias e retorna o nome da coluna da planilha,
+        se não encontrar, retorna ValueError
+        """
+        excel = self.excel
+        if isinstance(column_name, str):
+            column_name = [column_name]
+
+        for header in excel.keys():
+            header_name = string_normalize(header)
+            count = 0
+            for name in column_name:
+                if re.search(name, header_name, re.IGNORECASE):
+                    count += 1
+            if count == len(column_name):
+                return header
+
+        column_formated = " ".join(column_name)
+        raise ValueError(f"Column '{column_formated}' not found!")
+
+    def add_column(
+        self,
+        key: str,
+        name: Union[str, list[str]],
+        required: bool = True,
+        default: Any = None,
+        types: Literal[
+            "string",
+            "int",
+            "float",
+            "bool",
+            "date",
+            "time",
+            "email",
+            "cel",
+            "cpf",
+            "sex",
+        ] = "string",
+        length: int = 0,
+        custom_function_before: Callable = None,
+        custom_function_after: Callable = None,
+    ):
+        """
+        Função responsável por adicionar as colunas que serão lidas
+        da planilha \n
+        Parâmetros: \n
+        key: nome da chave do dicionario com os dados da coluna \n
+        name: nome da coluna da planilha, não é necessário informar o
+        nome completo da coluna, apenas uma palavra para busca, se o nome da
+        coluna não foi encontrado o programa fechará \n
+        default: se a coluna não for encontrada ou o valor não foi informado
+        então será considerado o valor default \n
+        types: tipo de dado que deve ser retirado da coluna \n
+        required: define se a coluna é obrigatória na planilha \n
+        length: Número máximo de caracteres que o dado pode ter,
+        padrão 1 ou seja ilimitado \n
+
+        custom_function: recebe a referencia de uma função que sera executada
+            apos as verificações padrão, essa função deve conter os parametros:
+            value: (valor que sera verificado),
+            key: (Chave do valor que sera verificado, para fins de log),
+            row: (Linha da planilha que esta o valor que sera verificado,
+                para fins de log),
+            default: (Valor padrão que deve ser usado caso caso ocorra algum
+                erro na verificação, para resolução de problemas).
+            Essa custom_funcition deverá retornar o valor (value) verificado
+            em caso de sucesso na verficação/tratamento, caso contratio,
+            deve retornar uma Exception
+        """
+        excel = self.excel
+
+        try:
+            column_name = self.column_name(name)
+        except ValueError as exp:
+            if required:
+                print(f"ERROR! Required {exp}")
+                self.erros = True
+            elif self.verbose and not config.NO_WARNING:
+                print(f"WARNING! {exp}")
+            excel[key] = default
+        else:
+            excel.rename({column_name: key}, axis="columns", inplace=True)
+
+        self.column_details.append(
+            {
+                "key": key,
+                "types": types,
+                "default": default,
+                "length": length,
+                "custom_function_before": custom_function_before,
+                "custom_function_after": custom_function_after,
+            }
+        )
+
+    def check_all(
+        self,
+        check_row: Callable = None,
+        check_duplicated_keys: list[str] = None,
+        checks_final: list[Callable] = None,
+    ) -> bool:
+        """
+        Função responsável por verificar todas as colunas
+        da planilha
+
+        Parâmetros:
+        check_row: função que será executada para cada linha da planilha
+        checks_final: lista de funções que serão executadas
+            considerando todos os dados da planilha
+
+        Retorno:
+        False se NÃO ouve erros na verificação
+        True se ouve erros na verificação
+        """
+        if self.erros:
+            head = self.__header_row + 1
+            print(f"ERROS FOUND! REMENBER HEADER ROW = {head}")
+            sys.exit(1)
+
+        excel = self.excel
+
+        # configuração padrão da barra de progresso
+        config.config_bar_excel()
+
+        # verificando todas as colunas
+        with alive_bar(
+            len(excel.index) * len(self.column_details), title="Checking for columns..."
+        ) as pbar:
+            # Verificações por coluna
+            for column in self.column_details:
+                for index in excel.index:
+                    value = excel.at[index, column["key"]]
+                    invalid = self.check_value(value=value, index=index, **column)
+                    if invalid and not self.erros:
+                        self.erros = True
+                    pbar()
+
+        # Verificações por linha
+        with alive_bar(len(excel.index), title="Checking for rows...") as pbar:
+            if check_row is not None:
+                for row in excel.index:
+                    try:
+                        data = check_row(excel.at[row, "_row"], excel.loc[row].copy())
+                        for key, value in data.items():
+                            excel.at[row, key] = value
+
+                    except CheckException:
+                        self.erros = True
+                    pbar()
+
+        # Verificações totais (duplicação de dados)
+        if check_duplicated_keys is not None:
+            try:
+                excel = self.checks.check_duplications(
+                    data=excel, keys=check_duplicated_keys
+                )
+            except CheckException:
+                self.erros = True
+
+        if checks_final is not None:
+            for check in checks_final:
+                try:
+                    excel = check(excel)
+                except CheckException:
+                    self.erros = True
+                pbar()
+
+        self.excel = excel
+        return self.erros
+
+    def check_value(
+        self,
+        value: Any,
+        key: str,
+        types: str,
+        index: int,
+        default: Any = None,
+        length: int = 0,
+        custom_function_before: Callable = None,
+        custom_function_after: Callable = None,
+    ) -> bool:
+        """Executa todas as verificações em um valor especifico,
+        retorna True um False para caso as verificações passarem ou não
+        """
+        excel = self.excel
+        erros = False
+
+        # Pega referencia fa função de verificação padrão
+        check_function = self.checks.get_type_function(types=types)
+
+        # Verificações customizadas que serão feitas antes
+        # das verificações padrões
+        if custom_function_before is not None:
+            try:
+                value = custom_function_before(
+                    value=value, key=key, default=default, row=self.row(index)
+                )
+            except CheckException:
+                erros = True
+
+        # Executa a verificação padrão
+        try:
+            value = check_function(value, key=key, default=default, row=self.row(index))
+        except CheckException:
+            erros = True
+
+        # Verificação de tamanho de string
+        if length not in (0, None):
+            try:
+                value = self.checks.check_length(
+                    value, key=key, row=self.row(index), length=length
+                )
+            except CheckException:
+                erros = True
+
+        # Executa a função de verificações customizada
+        # depois das verificações padrão
+        if custom_function_after is not None:
+            try:
+                value = custom_function_after(
+                    value=value, key=key, default=default, row=self.row(index)
+                )
+            except CheckException:
+                erros = True
+
+        excel.at[index, key] = value
+        return erros
+
+    def clean_columns(self):
+        """
+        Deleta as colunas do data frame que não foram adicionadas pelo
+        'add_column'
+        """
+        columns = list(map(lambda col: col["key"], self.column_details))
+        columns.append("_row")
+        self.excel = self.excel[columns]
+
+    def row(self, index: int) -> int:
+        """
+        Retorna a linha do respectivo index passado
+        """
+        return index + self.__header_row + 2
+
+    def add_row_column(self):
+        """Adicionar uma columa ao datafreme chamada 'row'
+        com o número de cada linha
+        """
+        excel = self.excel
+        rows = []
+        for i in excel.index:
+            rows.append(self.row(i))
+        excel["_row"] = rows
+
+    def data_all(self) -> dict:
+        excel = self.excel
+        if excel.empty:
+            return {}
+
+        excel = excel.drop(columns="_row")
+        excel = excel.where(pd.notnull(excel), None)
+        return excel.to_dict("records")
---
+++ packages/expyvalidations/expyvalidations-0.0.1.tar.gz/expyvalidations-0.0.1/expyvalidations/excel/servicos.py
@@ -0,0 +1,130 @@
+import sys
+from typing import Any
+
+# from alive_progress import alive_bar
+from pandas import ExcelFile
+# from oneparams.api.gservs import Gservis
+# from oneparams.api.servicos import ApiServicos
+from expyvalidations.config import CheckException
+from expyvalidations.excel.excel import ExpyValidations
+
+
+def servico(book: ExcelFile, header: int = 1, reset: bool = False):
+    """
+    Book: planilha com todos os dados \n
+    reset: True se todos os serviços do sistema
+    serão excluídos para cadastrar os serviços da planilha \n
+
+    Nessa função tem toda a descrição do json que vai ser enviado
+    para as rotas de cadastro do sistema
+
+    Return None
+    """
+    # one = ApiServicos()
+    print("analyzing spreadsheet")
+
+    ex = ExpyValidations(path_file=book, sheet_name="Servico", header_row=header)
+
+    ex.add_column(key="flagAtivo", name="ativo", required=False, default=True)
+    ex.add_column(key="descricao",
+                  name="nome",
+                  length=50,
+                  custom_function_after=check_descricao)
+    ex.add_column(key="gservId",
+                  name="grupo",
+                  length=50,
+                  custom_function_after=check_descricao)
+    ex.add_column(key="preco", name="valor", default=1, types="float")
+    ex.add_column(key="comissao",
+                  name="comissao",
+                  default=0,
+                  types="float",
+                  custom_function_after=check_comissao)
+    ex.add_column(key="tempoExecucao",
+                  name="execucao",
+                  default="00:30:00",
+                  types="time")
+    ex.add_column(key="custosGerais",
+                  name="custo",
+                  required=False,
+                  default=0,
+                  types="float")
+    ex.add_column(key="intervaloMarcacao",
+                  name="intervalo",
+                  required=False,
+                  default="00:10:00",
+                  types="time")
+    ex.add_column(key="permiteEncaixe",
+                  name="encaixe",
+                  required=False,
+                  default=True,
+                  types="bool")
+    ex.add_column(key="permiteSimultaneidade",
+                  name="simultaneidade",
+                  required=False,
+                  default=True,
+                  types="bool")
+    ex.add_column(key="valPercComissao",
+                  name="tipo comissao",
+                  required=False,
+                  default="P")
+    ex.add_column(key="valPercCustos",
+                  name="tipo custo",
+                  required=False,
+                  default="P")
+    ex.add_column(key="flagMobilidade",
+                  name="mobilidade",
+                  required=False,
+                  default=True,
+                  types="bool")
+    ex.clean_columns()
+
+    invalid = ex.check_all(check_duplicated_keys=["descricao"])
+    if invalid:
+        sys.exit(1)
+
+    print("creating services")
+    data = ex.data_all()
+    print(data)
+
+    # len_data = len(data)
+    # if reset:
+    #     len_data += len(one.items)
+
+    # config_bar_api()
+    # with alive_bar(len_data) as pbar:
+    #     if reset:
+    #         for i in list(one.items):
+    #             one.delete(i)
+    #             pbar()
+
+    #     for row in data:
+    #         one.diff_item(row)
+    #         pbar()
+
+    # with alive_bar() as pbar:
+    #     grupo = Gservis()
+    #     grupo.get_all()
+    #     grupo.clear()
+
+
+def check_descricao(value: Any, key: str, row: int, default: Any) -> Any:
+    if value is None:
+        print(f"ERROR! in line {row}, Column {key}, empty value")
+        raise CheckException
+    return value
+
+
+def check_comissao(value: Any, key: str, row: int, default: Any) -> float:
+    try:
+        value = float(value)
+    except ValueError as exp:
+        raise CheckException from exp
+
+    if value <= 1:
+        value = value * 100
+    return value
+
+
+if __name__ == "__main__":
+    servico("Parametrização Sistema One Beleza.xlsx")
