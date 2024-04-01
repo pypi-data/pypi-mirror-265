45e0d0a8f2b4cbbdba565c112c3b306427df9900
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-01 03:04:58 +0000

    Add package kss/5.0.0/kss-5.0.0.tar.gz

---
+++ packages/kss/kss-5.0.0.tar.gz/kss-5.0.0/kss/__init__.py
@@ -0,0 +1,9 @@
+# Copyright (C) 2021 Hyunwoong Ko <kevin.brain@kakaobrain.com> and Sang-Kil Park <skpark1224@hyundai.com>
+# All rights reserved.
+
+from kss._modules.morphemes.split_morphemes import split_morphemes
+from kss._modules.sentences.split_sentences import split_sentences
+from kss._modules.summarization.summarize_sentences import summarize_sentences
+
+__ALL__ = [split_sentences, split_morphemes, summarize_sentences]
+__version__ = "5.0.0"
---
+++ packages/kss/kss-5.0.0.tar.gz/kss-5.0.0/setup.py
@@ -0,0 +1,67 @@
+import codecs
+import os
+import subprocess
+import sys
+from contextlib import suppress
+
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+
+
+class InstallCommand(install):
+    def run(self):
+        try:
+            import mecab
+        except:
+            with suppress():
+                subprocess.call(
+                    [sys.executable, "-m", "pip", "install", "python-mecab-kor"],
+                    stderr=subprocess.DEVNULL,
+                )
+
+        super().run()
+
+
+def read_file(filename, cb):
+    with codecs.open(filename, "r", "utf8") as f:
+        return cb(f)
+
+
+version = None
+with open(os.path.join("kss", "__init__.py"), encoding="utf-8") as f:
+    for line in f:
+        if line.strip().startswith("__version__"):
+            version = line.split("=")[1].strip().replace('"', "").replace("'", "")
+
+with open("README.md", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="kss",
+    version=version,
+    author="Hyunwoong Ko",
+    author_email="kevin.ko@tunib.ai",
+    url="https://github.com/hyunwoongko/kss",
+    license='BSD 3-Clause "New" or "Revised" License',
+    description="A Toolkit for Korean sentence segmentation",
+    long_description_content_type="text/markdown",
+    platforms=["any"],
+    install_requires=["emoji==1.2.0", "regex", "pecab", "networkx"],
+    long_description=long_description,
+    packages=find_packages(exclude=["bench", "assets", ".java", ".pytest_cache"]),
+    python_requires=">=3",
+    zip_safe=False,
+    package_data={"": ["kss/pynori/resources/*"]},
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    cmdclass={"install": InstallCommand},
+)
