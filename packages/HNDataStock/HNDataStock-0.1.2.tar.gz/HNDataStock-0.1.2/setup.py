f4584af0cb29a6823d6dedfd48c65523b5c64c94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-01 03:04:34 +0000

    Add package HNDataStock/0.1.2/HNDataStock-0.1.2.tar.gz

---
+++ packages/HNDataStock/HNDataStock-0.1.2.tar.gz/HNDataStock-0.1.2/setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name='HNDataStock',
+    version='0.1.2',
+    author='Hướng Nghiệp Dữ Liệu',
+    author_email='daotao@huongnghiepdulieu.com',
+    description='Trường đào tạo lập trình Forex, Crypto, Chứng Khoán',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='http://www.huongnghiepdulieu.com',
+    packages=find_packages(),
+    install_requires=[
+        
+    ],
+    classifiers=[
+        
+    ],
+)
