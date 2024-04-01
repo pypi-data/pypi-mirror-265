2e163986e53d0b5617239a69bc8b3c60d7a638b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-01 03:04:58 +0000

    Add package HNDataStock/0.1.7/HNDataStock-0.1.7.tar.gz

---
+++ packages/HNDataStock/HNDataStock-0.1.7.tar.gz/HNDataStock-0.1.7/hn_data_stock/finance_data.py
@@ -0,0 +1,13 @@
+# hn_data_stock/finance_data.py
+
+# import requests
+# import yfinance as yf
+
+# def get_stock_data(ticker):
+
+#     # Lấy dữ liệu cổ phiếu Apple
+#     history = yf.Ticker(ticker)
+#     # Lấy dữ liệu giá trong vòng period tháng gần nhất
+#     history_data = history.history(period='1mo')
+    
+#     return history_data
