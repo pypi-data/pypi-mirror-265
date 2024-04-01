7d09a225135def4945c14fed9f221d08abdec42f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-01 15:03:46 +0000

    Add package fr24/0.1.1/fr24-0.1.1.tar.gz

---
+++ packages/fr24/fr24-0.1.1.tar.gz/fr24-0.1.1/src/fr24/base.py
@@ -0,0 +1,157 @@
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+from pathlib import Path
+from typing import Any, Generic, TypeVar
+
+import httpx
+import pyarrow as pa
+import pyarrow.parquet as pq
+from loguru import logger
+from typing_extensions import Self
+
+import pandas as pd
+
+from .authentication import login
+from .types.fr24 import Authentication
+
+
+class HTTPClient:
+    """
+    An HTTPX client for making requests to the API and storing
+    authentication data.
+    """
+
+    def __init__(self, *, retries: int = 5) -> None:
+        transport = httpx.AsyncHTTPTransport(retries=retries)
+        self.client = httpx.AsyncClient(transport=transport)
+        self.auth: Authentication | None = None
+
+    async def __aenter__(self) -> Self:
+        self.auth = await login(self.client)
+        return self
+
+    async def __aexit__(self, *args: Any) -> None:
+        if self.client is not None:
+            await self.client.aclose()
+
+
+ApiRsp = TypeVar("ApiRsp")
+"""Type returned by the API, e.g. [fr24.types.fr24.FlightList][]"""
+Ctx = TypeVar("Ctx")
+"""Type of the context for the service, a TypedDict"""
+
+
+class APIBase(Generic[ApiRsp, Ctx], ABC):
+    def __init__(self, http: HTTPClient, ctx: Ctx) -> None:
+        self.http = http
+        self.ctx = ctx
+
+    @abstractmethod
+    async def fetch(self, *args: Any, **kwargs: Any) -> ApiRsp:
+        """Fetch data from the API."""
+        ...
+
+
+class ArrowBase(Generic[ApiRsp, Ctx], ABC):
+    """A base class for handling arrow tables."""
+
+    schema: pa.Schema | None = None
+
+    def __init__(self, base_dir: str, ctx: Ctx) -> None:
+        self.base_dir = Path(base_dir)
+        self._table: pa.Table | None = None
+        self.ctx = ctx
+
+    @property
+    @abstractmethod
+    def fp(self) -> Path:
+        """Path to the parquet file."""
+        ...
+
+    @property
+    def table(self) -> pa.Table | None:
+        return self._table
+
+    @table.setter
+    def table(self, table: pa.Table) -> None:
+        """Set the arrow table or extend it if it already exists."""
+        self._table = (
+            table
+            if self._table is None
+            else self._concat_tables(self._table, table)
+        )
+
+    @staticmethod
+    def _concat_tables(tbl_old: pa.Table, tbl_new: pa.Table) -> pa.Table:
+        """Override this to customize how tables should be concatenated."""
+        return pa.concat_tables([tbl_old, tbl_new])
+
+    @abstractmethod
+    def add_api_response(self, data: ApiRsp) -> Self:
+        """Parse API response and add to the arrow table."""
+
+    def add_parquet(self, fp: Path | None = None) -> Self:
+        """
+        Read parquet file and add to the arrow table.
+
+        :param fp: Path to the parquet file - if not provided, the default cache
+        directory is used.
+        """
+        out_fp = fp if fp is not None else self.fp
+
+        # pq.read_table apparently doesn't read schema metadata:
+        # overwriting self.schema using pq.read_schema instead.
+        sch = pq.read_schema(out_fp)
+        if sch_diffs := set(sch).difference(set(self.schema or [])):
+            logger.warning(f"Parquet schema mismatch:\n{sch_diffs}, will cast.")
+            sch = sch.cast(self.schema)  # attempt to cast the schema
+        self.schema = sch
+
+        self.table = pq.read_table(out_fp, **self._schema_kwargs)
+        num_rows = self._table.num_rows if self._table is not None else 0
+        logger.debug(f"Read {out_fp}: {num_rows=}")
+        return self
+
+    def save_parquet(self, fp: Path | None = None) -> Self:
+        """
+        Write the arrow table to a parquet file.
+        :param fp: Path to the parquet file - if not provided, the default cache
+        directory is used.
+        """
+        if self._table is None:
+            logger.warning("No data to write.")
+            return self
+        out_fp = fp if fp is not None else self.fp
+        out_fp.parent.mkdir(parents=True, exist_ok=True)
+        with pq.ParquetWriter(out_fp, **self._schema_kwargs) as writer:
+            writer.write_table(self._table)
+        logger.debug(f"Saved {self._table.num_rows} rows to {out_fp}")
+        return self
+
+    def clear(self) -> None:
+        """Clear the arrow table."""
+        self._table = None
+
+    @property
+    def _schema_kwargs(self) -> dict[str, Any]:
+        return {} if self.schema is None else {"schema": self.schema}
+
+    @property
+    def df(self) -> pd.DataFrame | None:
+        return self._table.to_pandas() if self._table is not None else None
+
+
+ApiBaseT = TypeVar("ApiBaseT")
+"""`APIBase[ApiRsp]`"""
+FileBaseT = TypeVar("FileBaseT")
+"""`FileBase[ApiRsp]`"""
+
+
+class ServiceBase(Generic[ApiBaseT, FileBaseT, Ctx]):
+    """A service to handle the API and file operations."""
+
+    def __init__(self, api: ApiBaseT, data: FileBaseT, ctx: Ctx) -> None:
+        self.api = api
+        self.data = data
+        self.ctx = ctx
---
+++ packages/fr24/fr24-0.1.1.tar.gz/fr24-0.1.1/src/fr24/cli.py
@@ -0,0 +1,102 @@
+from __future__ import annotations
+
+import asyncio
+from datetime import datetime
+from pathlib import Path
+from typing import Annotated, Optional
+
+import rich
+import typer
+from appdirs import user_cache_dir, user_config_dir
+
+from .core import FR24, FlightListService, LiveFeedService, PlaybackService
+from .tui.tui import main as tui_main
+
+app = typer.Typer(no_args_is_help=True)
+
+
+@app.command()
+def dirs() -> None:
+    """Shows relevant directories"""
+    rich.print(f"Config: {user_config_dir('fr24')}")
+    rich.print(f" Cache: {user_cache_dir('fr24')}")
+
+
+@app.command()
+def tui() -> None:
+    """Starts the TUI"""
+
+    tui_main()
+
+
+app_auth = typer.Typer()
+app.add_typer(
+    app_auth,
+    name="auth",
+    no_args_is_help=True,
+    help="Commands for authentication",
+)
+
+
+@app_auth.command()
+def show() -> None:
+    """Shows authentication status"""
+
+    async def show_() -> None:
+        async with FR24() as fr24:
+            if fr24.http.auth is None:
+                rich.print(
+                    "[bold red]You are not authenticated.[/bold red]\n"
+                    "Provide credentials in environment variables: either \n"
+                    "- fr24_username + fr24_password or \n"
+                    "- fr24_subscription_key + fr24_token\n"
+                    "Alternatively, copy the example config file to "
+                    f"{Path(user_config_dir('fr24')) / 'fr24.conf'}."
+                )
+            else:
+                rich.print("[bold green]Authenticated[/bold green]")
+                rich.print(fr24.http.auth)
+
+    asyncio.run(show_())
+
+
+def get_success_message(
+    service: FlightListService | LiveFeedService | PlaybackService,
+) -> str:
+    num_rows = t.num_rows if (t := service.data.table) is not None else 0
+    size = service.data.fp.stat().st_size
+    return (
+        f"[bold green]Success: Saved {num_rows} rows ({size} bytes) "
+        f"to {service.data.fp}.[/bold green]"
+    )
+
+
+@app.command()
+def feed(
+    timestamp: Annotated[
+        Optional[int], typer.Option(help="Start unix timestamp (s, UTC)")
+    ] = None,
+    time: Annotated[
+        Optional[datetime], typer.Option(help="Start datetime (UTC)")
+    ] = None,
+) -> None:
+    """Fetches current livefeed / playback of live feed at a given time"""
+    # if time is None and timestamp is None:
+    #     raise typer.BadParameter("Either time or timestamp must be set")
+    if time is not None and timestamp is not None:
+        raise typer.BadParameter("Only one of time and timestamp can be set")
+    if time is not None:
+        timestamp = int(time.timestamp())
+
+    async def feed_() -> None:
+        async with FR24() as fr24:
+            lf = fr24.livefeed(timestamp)
+            lf.data.add_api_response(await lf.api.fetch())
+            lf.data.save_parquet()
+            rich.print(get_success_message(lf))
+
+    asyncio.run(feed_())
+
+
+if __name__ == "__main__":
+    app()
