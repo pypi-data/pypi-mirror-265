b9873e11a37a7fa999c2508a172711881cdf878f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-01 15:03:49 +0000

    Add package cdk-cloudformation-paloaltonetworks-cloudngfw-ngfw/2.0.0a7/cdk-cloudformation-paloaltonetworks-cloudngfw-ngfw-2.0.0a7.tar.gz

---
+++ packages/cdk-cloudformation-paloaltonetworks-cloudngfw-ngfw/cdk-cloudformation-paloaltonetworks-cloudngfw-ngfw-2.0.0a7.tar.gz/cdk-cloudformation-paloaltonetworks-cloudngfw-ngfw-2.0.0a7/setup.py
@@ -0,0 +1,64 @@
+import json
+import setuptools
+
+kwargs = json.loads(
+    """
+{
+    "name": "cdk-cloudformation-paloaltonetworks-cloudngfw-ngfw",
+    "version": "2.0.0.a7",
+    "description": "A Firewall resource offers Palo Alto Networks next-generation firewall capabilities with built-in resiliency, scalability, and life-cycle management.",
+    "license": "Apache-2.0",
+    "url": "https://github.com/aws-cloudformation/aws-cloudformation-rpdk.git",
+    "long_description_content_type": "text/markdown",
+    "author": "Amazon Web Services",
+    "bdist_wheel": {
+        "universal": true
+    },
+    "project_urls": {
+        "Source": "https://github.com/cdklabs/cdk-cloudformation.git"
+    },
+    "package_dir": {
+        "": "src"
+    },
+    "packages": [
+        "cdk_cloudformation_paloaltonetworks_cloudngfw_ngfw",
+        "cdk_cloudformation_paloaltonetworks_cloudngfw_ngfw._jsii"
+    ],
+    "package_data": {
+        "cdk_cloudformation_paloaltonetworks_cloudngfw_ngfw._jsii": [
+            "paloaltonetworks-cloudngfw-ngfw@2.0.0-alpha.7.jsii.tgz"
+        ],
+        "cdk_cloudformation_paloaltonetworks_cloudngfw_ngfw": [
+            "py.typed"
+        ]
+    },
+    "python_requires": "~=3.8",
+    "install_requires": [
+        "aws-cdk-lib>=2.134.0, <3.0.0",
+        "constructs>=10.3.0, <11.0.0",
+        "jsii>=1.96.0, <2.0.0",
+        "publication>=0.0.3",
+        "typeguard~=2.13.3"
+    ],
+    "classifiers": [
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: JavaScript",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Typing :: Typed",
+        "License :: OSI Approved"
+    ],
+    "scripts": []
+}
+"""
+)
+
+with open("README.md", encoding="utf8") as fp:
+    kwargs["long_description"] = fp.read()
+
+
+setuptools.setup(**kwargs)
