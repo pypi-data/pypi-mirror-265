2f919a75d4dc3cb7603c74e62a864dabc85b682b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-01 04:06:22 +0000

    Add package my-first-python-package-0/0.1.3/my_first_python_package_0-0.1.3.tar.gz

---
+++ packages/my-first-python-package-0/my_first_python_package_0-0.1.3.tar.gz/my_first_python_package_0-0.1.3/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': '.'}
+
+packages = \
+['my_first_python_package']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pytest>=8.1.1,<9.0.0']
+
+setup_kwargs = {
+    'name': 'my-first-python-package-0',
+    'version': '0.1.3',
+    'description': 'A simple package to demonstrate how to create a python package',
+    'long_description': '# my-first-python-package',
+    'author': 'Hassan Abedi',
+    'author_email': 'hassan.abedi.t@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
