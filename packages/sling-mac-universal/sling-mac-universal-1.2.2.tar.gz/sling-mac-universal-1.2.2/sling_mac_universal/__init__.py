3fbbbe4661a3de76065797f589cfbcb66e483d25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-01 04:04:21 +0000

    Add package sling-mac-universal/1.2.2/sling-mac-universal-1.2.2.tar.gz

---
+++ packages/sling-mac-universal/sling-mac-universal-1.2.2.tar.gz/sling-mac-universal-1.2.2/sling_mac_universal/__init__.py
@@ -0,0 +1,17 @@
+
+import os, platform, pathlib
+
+# set binary
+BIN_FOLDER = os.path.join(os.path.dirname(__file__), 'bin')
+
+if platform.system() == 'Darwin':
+  SLING_BIN = os.path.join(BIN_FOLDER,'sling-mac')
+else:
+  SLING_BIN = ''
+
+SLING_VERSION = '0.0.0dev'
+
+version_path = pathlib.Path(os.path.join(BIN_FOLDER,'VERSION'))
+if version_path.exists():
+  with version_path.open() as file:
+    SLING_VERSION = file.read().strip()
