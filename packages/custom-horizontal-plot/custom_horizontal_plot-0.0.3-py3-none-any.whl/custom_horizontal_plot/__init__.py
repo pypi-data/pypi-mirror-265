17cb70ab4d77376c9c0fb6c86f7d6e9c98dfe94c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-01 03:04:23 +0000

    Add package custom-horizontal-plot/0.0.3/custom_horizontal_plot-0.0.3-py3-none-any.whl

---
+++ packages/custom-horizontal-plot/custom_horizontal_plot-0.0.3-py3-none-any.whl/custom_horizontal_plot/__init__.py
@@ -0,0 +1,24 @@
+import os
+import streamlit.components.v1 as components
+
+_RELEASE = True 
+
+if not _RELEASE:
+    _horizontal_plot = components.declare_component(
+
+        "horizontal_plot",
+
+        url="http://localhost:3001",
+    )
+else:
+
+    parent_dir = os.path.dirname(os.path.abspath(__file__))
+    build_dir = os.path.join(parent_dir, "frontend/build")
+    _horizontal_plot = components.declare_component("horizontal_plot", path=build_dir)
+
+
+def horizontal_plot(data=None, titlesForChart=None, widthSet=None, useImg=None, styles=None, key=None, default=None):
+
+    component_value = _horizontal_plot(data=data, titlesForChart=titlesForChart, widthSet=widthSet, useImg=useImg, styles=styles, key=key, default=default)
+
+    return component_value
