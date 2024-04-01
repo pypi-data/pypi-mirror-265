6ac30032cf5edf20d7c8ee3c2962fb47541023d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-01 04:04:03 +0000

    Add package keyboard-extended/0.2.3/keyboard_extended-0.2.3.tar.gz

---
+++ packages/keyboard-extended/keyboard_extended-0.2.3.tar.gz/keyboard_extended-0.2.3/setup.py
@@ -0,0 +1,50 @@
+# following https://www.youtube.com/watch?v=tEFkHEKypLI&t=176s und https://www.youtube.com/watch?v=GIF3LaRqgXo 12:48 / 29:26
+
+# cd -> current dir
+# cd "C:\Users\Creed\OneDrive\Schul-Dokumente\Programmieren\Python\Code Sammlung\Packages\creating_keyboard_extended"
+
+# python setup.py sdist bdist_wheel
+
+# install current package but not in site-packages -> link to this
+# pip install -e .
+
+# pip install keyboard_extended
+
+# twine upload dist/*
+# twine upload --skip-existing dist/*
+# twine upload --verbose --skip-existing dist/*
+# twine upload --verbose dist/*
+
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+DESCRIPTION = "Extension for the keyboard package"
+
+# Setting up
+setup(
+    name="keyboard_extended",
+    version="0.2.3",
+    author="André Herber",
+    author_email="andre.herber.programming@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=["keyboard"],
+    keywords=["python"],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows",
+    ],
+)
