1f375780ab92df495613dfa068768ed14997b9b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-01 14:03:50 +0000

    Add package language-formatters-pre-commit-hooks/2.13.0/language_formatters_pre_commit_hooks-2.13.0-py2.py3-none-any.whl

---
+++ packages/language-formatters-pre-commit-hooks/language_formatters_pre_commit_hooks-2.13.0-py2.py3-none-any.whl/language_formatters_pre_commit_hooks/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import pkg_resources
+
+
+__version__ = pkg_resources.get_distribution("language_formatters_pre_commit_hooks").version
+
+
+def _get_default_version(tool_name: str) -> str:  # pragma: no cover
+    """
+    Read tool_name default version.
+    The method is intended to be used only from language_formatters_pre_commit_hooks modules
+    """
+    try:
+        with open(
+            pkg_resources.resource_filename(
+                "language_formatters_pre_commit_hooks",
+                "{tool_name}.version".format(tool_name=tool_name),
+            )
+        ) as f:
+            return f.readline().split()[0]
+    except:  # noqa: E722 (allow usage of bare 'except')  # pragma: no cover
+        raise RuntimeError("No default version found for {tool_name}".format(tool_name=tool_name))
