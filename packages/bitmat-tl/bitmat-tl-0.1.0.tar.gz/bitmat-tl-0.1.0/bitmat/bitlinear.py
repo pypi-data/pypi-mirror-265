3f21bbbf039f06ef7346502c2bbf7c0ba7eb22c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-01 03:04:25 +0000

    Add package bitmat-tl/0.1.0/bitmat-tl-0.1.0.tar.gz

---
+++ packages/bitmat-tl/bitmat-tl-0.1.0.tar.gz/bitmat-tl-0.1.0/bitmat/bitlinear.py
@@ -0,0 +1,28 @@
+import torch
+
+from utils.bitmat import bitmat
+from utils.rmsnorm import RMSLayerNorm
+
+
+class BitLinear(torch.nn.Module):
+    """
+    A linear layer that uses packed terniary matrix multiplication.
+    """
+    def __init__(self, in_features, out_features, eps, bias=False):
+        super(BitLinear, self).__init__()
+        self.eps = eps
+        self.weight = torch.nn.Parameter(torch.zeros(out_features, in_features))
+        if bias:
+            self.bias = torch.nn.Parameter(torch.tensor(out_features))
+        else:
+            self.bias = None
+        self.norm = RMSLayerNorm(out_features, 1e-5)
+        self._post_init()
+    def _post_init(self):
+        torch.nn.init.kaiming_normal_(self.weight, mode='fan_out', nonlinearity='relu')
+
+    def forward(self, x):
+        x = self.norm(x)
+        return bitmat(self.weight, x)
+
+
