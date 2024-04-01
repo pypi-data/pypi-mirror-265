19953b3a5c9287f0fad1b37b199b2f1944b24980
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-01 04:03:48 +0000

    Add package yacv-server/0.8.8/yacv_server-0.8.8.tar.gz

---
+++ packages/yacv-server/yacv_server-0.8.8.tar.gz/yacv_server-0.8.8/yacv_server/frontend/___vite-browser-external_commonjs-proxy-CDvJFezp.js
@@ -0,0 +1,2 @@
+import{bK as e}from"./index-B9MDeHxu.js";const t={},r=Object.freeze(Object.defineProperty({__proto__:null,default:t},Symbol.toStringTag,{value:"Module"})),a=e(r);export{a as r};
+//# sourceMappingURL=___vite-browser-external_commonjs-proxy-CDvJFezp.js.map
