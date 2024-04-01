8bb11941c59a77df6e74f2f0750d7a71fd8c7cfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-01 15:04:05 +0000

    Add package whru/0.3/whru-0.3.tar.gz

---
+++ packages/whru/whru-0.3.tar.gz/whru-0.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='whru',
+    version='0.3',
+    description='My custom Python module',
+    author='whr819987540',
+    author_email='steeliron550@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        "torch>=1.7.0",
+        "tensorflow>=2.0.2",
+    ],
+)
