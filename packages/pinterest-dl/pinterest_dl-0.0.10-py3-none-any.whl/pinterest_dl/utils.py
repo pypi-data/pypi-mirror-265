2cdeaf2fb1444aed5c29a85eafe712bfa5364bbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-01 03:04:49 +0000

    Add package pinterest-dl/0.0.10/pinterest_dl-0.0.10-py3-none-any.whl

---
+++ packages/pinterest-dl/pinterest_dl-0.0.10-py3-none-any.whl/pinterest_dl/utils.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+
+from PIL import Image
+
+
+def get_root():
+    return Path(__file__).parent.absolute()
+
+
+def prune_by_resolution(input_file, resolution):
+    size = (0, 0)
+    with Image.open(input_file) as im:
+        size = im.size
+
+    res = tuple(map(int, resolution.split("x")))
+
+    if size < res:
+        input_file.unlink()
+        print(f"Removed {input_file}, resolution: {size} < {res}")
+
+
+def get_appdata_dir(path_under=None):
+    if path_under:
+        return Path.home().joinpath("AppData", "Local", "pinterest-dl", path_under)
+    return Path.home().joinpath("AppData", "Local", "pinterest-dl")
