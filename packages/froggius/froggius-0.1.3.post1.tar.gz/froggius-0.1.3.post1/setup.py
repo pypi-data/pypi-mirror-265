7f0fbc917f0182291ddafa4a6477daafecbb9ac1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-01 15:03:57 +0000

    Add package froggius/0.1.3.post1/froggius-0.1.3.post1.tar.gz

---
+++ packages/froggius/froggius-0.1.3.post1.tar.gz/froggius-0.1.3.post1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="froggius",
+    packages=find_packages(include=["froggius"]),
+    description="Froggius is a dumb easy logging tool for python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    version="0.1.3-1",
+    author="zlElo",
+    author_email="mail@zlelo.de",
+    url="https://github.com/zlElo/Froggius",
+    keywords=["logging", "logger", "easy-to-use", "log"],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+)
