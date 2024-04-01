1b2e113434b407889dfb90cbc0105c4be5ca26b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-01 03:04:21 +0000

    Add package hw2latex/0.1.2/hw2latex-0.1.2-py3-none-any.whl

---
+++ packages/hw2latex/hw2latex-0.1.2-py3-none-any.whl/hw2latex/hw2latex.py
@@ -0,0 +1,35 @@
+def generate_latex_table(data):
+    # Начало таблицы
+    latex_code = "\\begin{table}[h!]\n"
+    latex_code += "\\centering\n"
+    latex_code += "\\begin{tabular}{|" + "|".join(["c"] * len(data[0])) + "|}\n"
+    latex_code += "\\hline\n"
+
+    # Заполнение таблицы данными
+    for row in data:
+        latex_code += " & ".join(map(str, row)) + " \\\\ \n"
+        latex_code += "\\hline\n"
+
+    # Завершение таблицы
+    latex_code += "\\end{tabular}\n"
+    latex_code += "\\caption{Пример таблицы}\n"
+    latex_code += "\\label{tab:example}\n"
+    latex_code += "\\end{table}\n"
+
+    return latex_code
+
+
+def imagetolatex(image_path, caption='caption'):
+    latex_code = f"""
+\\begin{{figure}}
+    \\centering
+    \\includegraphics{{{image_path}}}
+    \\caption{{{caption}}}
+    \\label{{fig:image}}
+\\end{{figure}}
+"""
+    return latex_code
+
+def test():
+    print('hello ')
+    
