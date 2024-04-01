f67f6e1faa6e7bb04e3e644dbc6c10bbaa06b704
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-01 14:03:49 +0000

    Add package aibridge-test/0.2.0/aibridge_test-0.2.0.tar.gz

---
+++ packages/aibridge-test/aibridge_test-0.2.0.tar.gz/aibridge_test-0.2.0/AIBridge/ai_services/process_mq.py
@@ -0,0 +1,50 @@
+from AIBridge.ai_services.cohere_llm import CohereApi
+from AIBridge.ai_services.palm_chat import PalmChat
+from AIBridge.ai_services.palm_text import PalmText
+from AIBridge.queue_integration.response_class import (
+    OpenAiImageRes,
+    PalmTextRes,
+    OpenAiRes,
+    PalmChatRes,
+    StableDuffusionRes,
+    CohereRes,
+    JarasicTextRes,
+    GeminiAiRes,
+)
+from AIBridge.ai_services.openai_images import OpenAIImage
+from AIBridge.ai_services.stable_diffusion_image import StableDiffusion
+from AIBridge.exceptions import ProcessMQException
+from AIBridge.ai_services.ai21labs_text import JurasicText
+from AIBridge.ai_services.geminin_services import GeminiAIService
+
+
+class ProcessMQ:
+    @classmethod
+    def get_process_mq(self, process_name):
+        from AIBridge.ai_services.openai_services import OpenAIService
+
+        process_obj = {
+            "open_ai": OpenAIService(),
+            "open_ai_image": OpenAIImage(),
+            "palm_api": PalmText(),
+            "palm_chat": PalmChat(),
+            "stable_diffusion": StableDiffusion(),
+            "cohere_api": CohereApi(),
+            "ai21_api": JurasicText(),
+            "gemini_ai": GeminiAIService(),
+        }
+        response_obj = {
+            "open_ai": OpenAiRes(),
+            "open_ai_image": OpenAiImageRes(),
+            "palm_api": PalmTextRes(),
+            "palm_chat": PalmChatRes(),
+            "stable_diffusion": StableDuffusionRes(),
+            "cohere_api": CohereRes(),
+            "ai21_api": JarasicTextRes(),
+            "gemini_ai": GeminiAiRes(),
+        }
+        if process_name not in process_obj:
+            raise ProcessMQException(
+                f"Process of message queue Not Found process->{process_name}"
+            )
+        return process_obj[process_name], response_obj[process_name]
