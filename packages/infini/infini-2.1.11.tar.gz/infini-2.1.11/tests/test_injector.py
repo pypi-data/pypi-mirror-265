5fff81fec5085f7fc5f3b758b9acd6a4ae4830da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-01 14:04:08 +0000

    Add package infini/2.1.11/infini-2.1.11.tar.gz

---
+++ packages/infini/infini-2.1.11.tar.gz/infini-2.1.11/tests/test_injector.py
@@ -0,0 +1,67 @@
+from typing import Optional
+from infini.handler import Handler
+from infini.injector import Injector
+from infini.input import Input
+from infini.loader import Loader
+from infini.output import Output
+from infini.router import Startswith
+
+
+def test_injector():
+    def name(name: str):
+        return name
+
+    def add(a: int, b: int = 0):
+        return a + b
+
+    injector = Injector()
+    injector.parameters = {"a": 12, "b": 20, "c": 0, "card_name": name}
+    assert injector.inject(add)() == 32
+    assert injector.output(add) == 32
+
+
+def test_handler_injector():
+    input = Input("test_message")
+
+    def absolute(input: Input[str], plain_text: str) -> Output:
+        return input.output(
+            "text",
+            "absolute",
+            block=False,
+            variables={
+                "text": plain_text,
+            },
+        )
+    
+    def absolute_2(input: Input[str], plain_text: Optional[str]) -> Output:
+        return input.output(
+            "text",
+            "absolute",
+            block=False,
+            variables={
+                "text": plain_text,
+            },
+        )
+
+    handler = Handler()
+    handler.handlers = [
+        {
+            "priority": 2,
+            "router": Startswith(""),
+            "handler": absolute,
+        },
+        {
+            "priority": 2,
+            "router": Startswith(""),
+            "handler": absolute_2,
+        },
+    ]
+
+    core = Loader().into_core()
+    core.handler = handler
+    core.generator.events = {
+        "absolute": "{{ text }}",
+    }
+
+    for output in core.input(input):
+        assert output == "test_message"
