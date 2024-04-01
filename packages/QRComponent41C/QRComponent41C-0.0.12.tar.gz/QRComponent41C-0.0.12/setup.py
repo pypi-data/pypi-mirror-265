84a011680d8a24ea25d4b922fb3e7e5a8c4ad86e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-01 03:04:20 +0000

    Add package QRComponent41C/0.0.12/QRComponent41C-0.0.12.tar.gz

---
+++ packages/QRComponent41C/QRComponent41C-0.0.12.tar.gz/QRComponent41C-0.0.12/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+
+def readme():
+    with open('README.md', 'r') as f:
+        return f.read()
+
+
+setup(
+    name='QRComponent41C',
+    version='0.0.12',
+    author='vissarion249',
+    author_email='vissarion249@gmail.com',
+    description='QRComponent41C is a Python library for working with QR codes in 1C:Enterprise 8.3.',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Absolemus/QRfor1CComponent',
+    packages=find_packages(),
+    install_requires=['qrcode>=7.4.2', 'Pillow>=10.2.0'],
+    classifiers=[
+        'Programming Language :: Python :: 3.12',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'
+    ],
+    keywords='1C:Enterprise 8.3, QR code, qrcode, Pillow',
+    project_urls={
+        'GitHub': 'https://github.com/Absolemus/QRfor1CComponent'
+    },
+    python_requires='>=3.12'
+)
