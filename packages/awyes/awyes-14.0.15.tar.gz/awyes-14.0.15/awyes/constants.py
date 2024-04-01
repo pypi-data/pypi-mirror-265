753038d2575f05a2a117c8e9af1e6cdb448dfe8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-01 14:03:52 +0000

    Add package awyes/14.0.15/awyes-14.0.15.tar.gz

---
+++ packages/awyes/awyes-14.0.15.tar.gz/awyes-14.0.15/awyes/constants.py
@@ -0,0 +1,17 @@
+X = "✗"
+DOT = "."
+CHECK = "✓"
+ARROW = "→"
+QUOTE = '"'
+NEW_LINE = "\n"
+ANIMATION_SLEEP = 1.5
+MATCH_REF = "reference"
+CACHE_REF = r"\$\((?P<reference>.*?)\)"
+
+USER_ENV_PATH = ".env*"
+USER_PIPFILE_PATH = "Pipfile"
+USER_CLIENT_MODULE_NAME = "local"
+USER_CLIENT_PATH_PREFIXES = "awyes.*"
+USER_CLIENT_PATH_SUFFIXES = (".py",)
+USER_CONFIG_PATH_PREFIXES = "awyes.*"
+USER_CONFIG_PATH_SUFFIXES = (".yaml", ".yml")
