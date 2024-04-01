fbd18f8602ac381dcbc8edeac9990117f1bc61f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-01 03:04:21 +0000

    Add package boto3form/0.0.2/boto3form-0.0.2.tar.gz

---
+++ packages/boto3form/boto3form-0.0.2.tar.gz/boto3form-0.0.2/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="boto3form",
+    version="0.0.2",
+    author="Rukmal Senavirathne",
+    description="Create and delete aws ecr repository using python boto3 like terraform ",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    # packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(),
+    # package_dir={"": "boto3form"},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=['boto3'],
+    url="https://github.com/rukmalsenavirathne/aws_python_boto3"
+)
