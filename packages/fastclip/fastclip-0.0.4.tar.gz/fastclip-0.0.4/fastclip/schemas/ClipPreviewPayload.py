fe8348b61f9614eea884b17fc60f67d65b6cd597
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-01 04:04:38 +0000

    Add package fastclip/0.0.4/fastclip-0.0.4.tar.gz

---
+++ packages/fastclip/fastclip-0.0.4.tar.gz/fastclip-0.0.4/fastclip/schemas/ClipPreviewPayload.py
@@ -0,0 +1,23 @@
+from typing import List
+from pydantic import BaseModel
+
+from fastclip.enums.ClipFormat import ClipFormat
+from fastclip.schemas.Word import Word
+
+
+class ClipPreviewPayload(BaseModel):
+    start: float
+    end: float
+    subtitles_settings: dict
+    subtitles: List[Word] = []
+    format: ClipFormat = ClipFormat.HORIZONTAL
+    has_subtitles: bool = False
+
+    def to_json(self):
+        return {
+            "start": self.start,
+            "end": self.end,
+            "subtitles_settings": self.subtitles_settings,
+            "subtitles": [word.to_json() for word in self.subtitles],
+            "format": self.format.value,
+        }
