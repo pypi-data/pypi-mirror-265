a4f58cfe823180b28d52ba75c92d20daa23381c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-01 03:05:07 +0000

    Add package vastdata/0.0.0/vastdata-0.0.0.tar.gz

---
+++ packages/vastdata/vastdata-0.0.0.tar.gz/vastdata-0.0.0/setup.py
@@ -0,0 +1,16 @@
+import os
+from setuptools import setup, find_packages
+
+setup(
+    name='vastdata',
+    python_requires='>=3.9.0',
+    description='VAST Data SDK',
+    version='0.0.0',
+    url='https://github.com/vast-data/',
+    author='VAST DATA',
+    author_email='hello@vastdata.com',
+    license='Copyright (C) VAST Data Ltd.',
+    packages=find_packages(),
+    long_description='',
+    long_description_content_type='text/markdown',
+)
