dc84fab4cdf673cbbd8a5b1e3263033f95cfa625
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-01 04:04:16 +0000

    Add package real-pyMate/1.0.0/real_pyMate-1.0.0.tar.gz

---
+++ packages/real-pyMate/real_pyMate-1.0.0.tar.gz/real_pyMate-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='real_pyMate',
+    version='1.0.0',
+    packages=[''],
+    url='https://github.com/akcarsten/pyMate',
+    license='MIT',
+    author='Carsten Klein',
+    description='pyMate is a Python package that provides basic functionalities to process functional magnetic '
+                'resonance imaging (fMRI) and electro-physiological (e-phys) data.'
+)
