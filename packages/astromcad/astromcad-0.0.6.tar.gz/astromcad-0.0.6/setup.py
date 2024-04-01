d4ae775c08f8044a05d71cb908d3e4d9f70a8b24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-01 14:04:31 +0000

    Add package astromcad/0.0.6/astromcad-0.0.6.tar.gz

---
+++ packages/astromcad/astromcad-0.0.6.tar.gz/astromcad-0.0.6/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+
+VERSION = '0.0.6'
+DESCRIPTION = 'Anomaly Detection for Astronomical Transients'
+LONG_DESCRIPTION = 'Train custom classifiers to be used as anomaly detectors for astronmical transients'
+
+# Setting up
+setup(
+    name="astromcad",
+    version=VERSION,
+    author="Rithwik Gupta",
+    author_email="<rithwikca2020@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['numpy', 'keras', 'tensorflow', 'matplotlib', 'scikit-learn'],
+    keywords=[],
+    
+)
