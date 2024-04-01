dd97619ef2f29364d34b10ef4e1943f4b7cdd569
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-01 14:03:57 +0000

    Add package netpanda/0.0.5/netpanda-0.0.5-py2.py3-none-any.whl

---
+++ packages/netpanda/netpanda-0.0.5-py2.py3-none-any.whl/netpanda/__init__.py
@@ -0,0 +1,7 @@
+"""Top-level package for netpanda."""
+
+__author__ = """Shabiha Hossain"""
+__email__ = "sabihahossain4105@gmail.com"
+__version__ = "0.0.5"
+
+from .netpanda import AirplaneRouteMap
