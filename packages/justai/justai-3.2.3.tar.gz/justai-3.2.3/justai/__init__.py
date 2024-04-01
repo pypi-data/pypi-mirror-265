05573e2a6ce869f77c3374c878eee1175ad8cc08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-01 15:03:56 +0000

    Add package justai/3.2.3/justai-3.2.3.tar.gz

---
+++ packages/justai/justai-3.2.3.tar.gz/justai-3.2.3/justai/__init__.py
@@ -0,0 +1,8 @@
+from justai.agent.agent import Agent
+from justai.translator.translator import Translator
+from justai.interactive.repl import Repl
+from justai.interactive.commands import CommandHandler
+from justai.tools.prompts import get_prompt, set_prompt_file, add_prompt_file
+
+if __name__ == '__main__':
+    pass
