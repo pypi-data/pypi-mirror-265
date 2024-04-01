3d00001628f2f66de9010ad9c212e7b93d41cd41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-01 03:05:03 +0000

    Add package python-sts-payone/0.0.7/python_sts_payone-0.0.7.tar.gz

---
+++ packages/python-sts-payone/python_sts_payone-0.0.7.tar.gz/python_sts_payone-0.0.7/src/python_sts_payone/redirection_model/utils.py
@@ -0,0 +1,4 @@
+SR_URL_TEST: str = 'https://smartroute-test.payone.io/SmartRoutePaymentWeb/SRPayMsgHandler'
+SR_URL_LIVE: str = 'https://smartroute-test.payone.io/SmartRoutePaymentWeb/SRPayMsgHandler'
+REDIRECT_MESSAGE_ID: str = '1'
+REDIRECT_VERIFY_MESSAGE_ID: str = '14'
