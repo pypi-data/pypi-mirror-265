6d40e5e502f1fb5f0e22d46593d884df363e68b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-01 14:03:54 +0000

    Add package simple-form/1.1/simple-form-1.1.tar.gz

---
+++ packages/simple-form/simple-form-1.1.tar.gz/simple-form-1.1/setup.py
@@ -0,0 +1,21 @@
+import os
+from setuptools import setup
+
+if os.path.isfile("README.md"):
+    with open("README.md", "r", encoding="utf-8") as readme:
+        long_description = readme.read()
+
+setup(
+    name="simple-form",
+    version=1.1,
+    description="Pequena biblioteca para facilitar formulários em CLI com Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Alan Reis Anjos",
+    author_email="alanreisanjo@gmail.com",
+    url="https://github.com/Hoyasumii/SimpleForm",
+    packages=['simpleForm', 'simpleForm.scripts'],
+    install_requires=['keyboard==0.13.5', 'setuptools==69.0.3'],
+    license="GPL-3.0",
+    entry_points={'console_scripts': []}
+)
