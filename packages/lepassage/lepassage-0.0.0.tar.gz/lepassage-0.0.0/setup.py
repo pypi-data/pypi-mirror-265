f68da1a746d4feabc07fc023747ab1892aebce45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-01 03:04:36 +0000

    Add package lepassage/0.0.0/lepassage-0.0.0.tar.gz

---
+++ packages/lepassage/lepassage-0.0.0.tar.gz/lepassage-0.0.0/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+import progress
+
+
+setup(
+    name='lepassage',
+    description='The simple yet elegant Progress Bar',
+    author='Rayan Haddad',
+    author_email='rayan.m.haddad@proton.me',
+    license='MIT',
+    packages=['progress'],
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: ISC License (ISCL)',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ]
+)
