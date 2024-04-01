dd715ec0557edddeacd388baf7b096864a7cbf4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-01 03:05:29 +0000

    Add package acddaun/2.4/acddaun-2.4.tar.gz

---
+++ packages/acddaun/acddaun-2.4.tar.gz/acddaun-2.4/setup.py
@@ -0,0 +1,16 @@
+# setup.py
+from setuptools import setup
+
+setup(
+    name='acddaun',
+    version='2.4',
+    description='acddaun',
+    long_description='Описание съел билибоба :)',
+    author='Sasibacka',
+    author_email='sergeycikkatkovcik@gmail.com',
+    packages=['acddaun'],
+    install_requires=[
+        'pyautogui',
+    ]
+)
+
