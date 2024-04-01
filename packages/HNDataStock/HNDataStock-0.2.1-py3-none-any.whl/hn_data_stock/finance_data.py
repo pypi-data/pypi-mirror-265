c65301d093ce2807d62c555d4ed7723e279f6b01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-01 03:04:26 +0000

    Add package HNDataStock/0.2.1/HNDataStock-0.2.1-py3-none-any.whl

---
+++ packages/HNDataStock/HNDataStock-0.2.1-py3-none-any.whl/hn_data_stock/finance_data.py
@@ -0,0 +1,23 @@
+# hn_data_stock/finance_data.py
+
+import requests
+import yfinance as yf
+
+import yfinance as yf
+
+def get_stock_data(ticker, period='1mo'):
+    """
+    Lấy dữ liệu lịch sử cổ phiếu.
+
+    :param ticker: Mã cổ phiếu.
+    :param period: Khoảng thời gian lịch sử để truy xuất.
+                   Ví dụ: '1d' cho một ngày, '1mo' cho một tháng, '1y' cho một năm, v.v.
+    :return: DataFrame chứa dữ liệu lịch sử cổ phiếu, với cột 'Date' được đổi tên thành 'Datetime'.
+    """
+    history = yf.Ticker(ticker)
+    history_data = history.history(period=period)
+    
+    # Chuyển chỉ mục 'Date' thành một cột và đổi tên cột đó thành 'Datetime'
+    history_data = history_data.reset_index().rename(columns={'Date': 'Datetime'})
+    
+    return history_data
