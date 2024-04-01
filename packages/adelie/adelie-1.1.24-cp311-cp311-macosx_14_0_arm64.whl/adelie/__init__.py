f95a7c889df85d21f1c859e60400b423f5b5b3c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-01 14:04:32 +0000

    Add package adelie/1.1.24/adelie-1.1.24-cp311-cp311-macosx_14_0_arm64.whl

---
+++ packages/adelie/adelie-1.1.24-cp311-cp311-macosx_14_0_arm64.whl/adelie/__init__.py
@@ -0,0 +1,19 @@
+from . import adelie_core
+from . import bcd
+from . import configs
+from . import cv
+from . import data
+from . import diagnostic
+from . import glm
+from . import io
+from . import matrix
+from . import optimization
+from . import state
+from . import solver
+from .cv import (
+    cv_grpnet,
+)
+from .solver import (
+    gaussian_cov, 
+    grpnet,
+)
