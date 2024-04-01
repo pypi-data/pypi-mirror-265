6eea521a85c676030e99a1e116b183343e2e24be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-01 03:04:21 +0000

    Add package quant2/0.0.11/quant2-0.0.11.tar.gz

---
+++ packages/quant2/quant2-0.0.11.tar.gz/quant2-0.0.11/quant/football/transforms/table20240326.py
@@ -0,0 +1,155 @@
+import time
+import numpy as np
+from typing import Union
+from ..data.stats import mean_std, normlize
+from ..data.utils import label_encoder, odds_avg_max_min, one_hot_encoder
+from ...io.utils import make_dir, save_json
+
+
+def parse_attrs_data(data: dict):
+    data = data["titan"]
+
+    keys = [
+        "match_id",
+        "match_time",
+        "match_state",
+        "season_name",
+        "home_score",
+        "visiting_score",
+        "home_half_score",
+        "visiting_half_score",
+        "home_team_name",
+        "visiting_team_name",
+    ]
+
+    # 10: 10(n-key) * 1(none) + 0(none)
+    xs = [data[k] for k in keys]
+    return xs
+
+
+def parse_event_data(data: dict):
+    data = data["events"]
+
+    keys = [
+        ("goalIn", 0),  # 进球
+        ("changePlayer", 1),  # 换人
+        ("yellowCard", 2),  # 黄牌
+        ("redCard", 3),  # 红牌
+        ("ownGoal", 4),  # 乌龙
+        ("penaltyKick", 5),  # 点球
+        ("penaltyKickMiss", 6),  # 射失点球
+        ("doubleYellowToRed", 7),  # 两黄变红
+    ]
+
+    xs_home = np.zeros((6, len(keys)), dtype=int)
+    xs_away = np.zeros((6, len(keys)), dtype=int)
+    for event in data:
+        for key, idx in keys:
+            if key in event:
+                kind = event["kind"].lower()
+                cycle = event["time"][:-1].split("+")[0]
+                cycle = min(max(0, (int(cycle) - 1) // 15), 5)
+                if kind == "home":
+                    xs_home[cycle][idx] += 1
+                if kind == "away":
+                    xs_away[cycle][idx] += 1
+                break
+
+    # 96: 8(n-key) * 6(n-cycle) + 8(n-key) * 6(n-cycle)
+    xs = xs_home.reshape(-1).tolist() + xs_away.reshape(-1).tolist()
+    return xs
+
+
+def parse_odds_data(data: dict):
+    first_1x2 = [v[-1] for v in data["odds_1x2"].values() if v]
+    first_overunder = [v[-1] for v in data["odds_overunder"].values() if v]
+    first_1x2 = odds_avg_max_min(first_1x2)
+    first_overunder = odds_avg_max_min(first_overunder)
+    # 18: 3(avg-max-min) * 3(1x2) + 3(avg-max-min) * 3(overunder)
+    xs = first_1x2 + first_overunder
+    return xs
+
+
+def extract_features(data: dict):
+    attrs_data = parse_attrs_data(data["mapping_match"])
+    event_data = parse_event_data(data["event_titan"])
+    odds_data = parse_odds_data(data["bets_titan"])
+    # 124: 10(attrs) + 96(event) + 18(odds)
+    xs = attrs_data + event_data + odds_data
+    return xs
+
+
+def check_samples(data: list[list]):
+    good_row, bad_row, bad_msg = [], [], []
+    for row in data:
+        a = int(row[4])  # 主队得分
+        b = int(row[5])  # 客队得分
+        c = sum([row[i] for i in range(10, 58, 8)])  # 进球
+        c += sum([row[i] for i in range(14, 58, 8)])  # 乌龙
+        c += sum([row[i] for i in range(15, 58, 8)])  # 点球
+        d = sum([row[i] for i in range(58, 106, 8)])  # 进球
+        d += sum([row[i] for i in range(62, 106, 8)])  # 乌龙
+        d += sum([row[i] for i in range(63, 106, 8)])  # 点球
+        if (a + b) == (c + d):
+            good_row.append(row)
+            continue
+        bad_row.append(row)
+        bad_msg.append((a, b, c, d))
+    return good_row, bad_row, bad_msg
+
+
+def export_samples(data: list[list], encoder: Union[str, dict], mean: list, std: list, norm: bool, path: str, stages: int, whole: bool):
+    # [0:4] : [比赛ID,比赛时间,比赛状态,联赛名]
+    # [4:8] : [主队得分,客队得分,主队半场得分,客队半场得分]
+    # [8:10] : [主队名称,客队名称]
+    # [10:58] : [进球,换人,黄牌,红牌,乌龙,点球,射失点球,两黄变红]x6
+    # [58:106] : [进球,换人,黄牌,红牌,乌龙,点球,射失点球,两黄变红]x6
+    # [106:124] : [初赔均值,初赔最大,初赔最小]x[胜平负,进球数]
+    assert (-1 < stages < 6) if whole else (-1 < stages < 3)
+    scores = []
+    team_home = []
+    team_away = []
+    xfeatures = []
+    len_slice = 8 * stages
+    for row in data:
+        scores.append(row[4:8])
+        team_home.append(row[8])
+        team_away.append(row[9])
+        xfeatures.append(row[10:(10 + len_slice)] +
+                         row[58:(58 + len_slice)] + row[106:124])
+
+    if isinstance(encoder, str):
+        if encoder == "label":
+            encoder = label_encoder(team_home + team_away)
+        elif encoder == "one_hot":
+            encoder = one_hot_encoder(team_home + team_away)
+        else:
+            raise NotImplementedError(f"Not supported <{encoder=}>.")
+    assert isinstance(encoder, dict), f"{type(encoder)=} is not <dict>."
+
+    X = []
+    for _home, _away, _vars in zip(team_home, team_away, xfeatures):
+        X.append(encoder[_home] + encoder[_away] + _vars)
+
+    if mean is None or std is None:
+        start_idx, end_idx = encoder["__dim__"] * 2, len(X[0])
+        mean, std = mean_std(X, [start_idx], [end_idx])
+
+    if norm:
+        X = normlize(X, mean, std)
+
+    if whole:
+        Y = [int(s[0]) + int(s[1]) for s in scores]
+        Y = [min(9, y) for y in Y]
+    else:
+        Y = [int(s[2]) + int(s[3]) for s in scores]
+        Y = [min(9, y) for y in Y]
+
+    path = make_dir(path)
+    ts = time.strftime("%Y%m%d_%H%M%S")
+
+    # .dat: [X, Y]
+    # .cfg: [encoder, mean, std]
+    save_json(path / f"{ts}.dat", [X, Y], indent=4)
+    save_json(path / f"{ts}.cfg", [encoder, mean, std], indent=4)
+    return X, Y, encoder, mean, std
---
+++ packages/quant2/quant2-0.0.11.tar.gz/quant2-0.0.11/tools/fb_cls_train.py
@@ -0,0 +1,198 @@
+# PYTHONPATH=$(pwd) python tools/fb_cls_train.py
+import numpy as np
+import random
+import sys
+import time
+import torch
+from pathlib import Path
+from quant.io.logging import get_logger, print_log
+from quant.io.utils import load_json, save_json
+
+
+def train(model, device, train_loader, criterion, optimizer, epoch, log_interval, logger):
+    model.train()
+    dataloader_size = len(train_loader)
+    for batch_idx, (data, target) in enumerate(train_loader, 1):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = criterion(output, target)
+        loss.backward()
+        optimizer.step()
+        if batch_idx % log_interval == 0:
+            print_log(
+                f"Train Epoch: {epoch} [{batch_idx}/{dataloader_size}] Loss: {loss.item():.6f}", logger)
+
+
+def test(model, device, test_loader, criterion, logger):
+    model.eval()
+    test_loss, correct = 0, 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += criterion(output, target).item()
+            pred = output.argmax(dim=1, keepdim=True)
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader)
+    dataset_size = len(test_loader.dataset)
+    test_acc = correct / dataset_size
+
+    print_log(
+        f"\nTest set: Avg loss: {test_loss:.6f}, Acc: {correct}/{dataset_size} ({test_acc:.4f})\n", logger)
+    return test_acc
+
+
+def main(config):
+    data, model, loss, optimizer, scheduler, runtime = \
+        config["data"], config["model"], config["loss"], config["optimizer"], config["scheduler"], config["runtime"]
+
+    seed = runtime.get("seed", 1)
+    epochs = runtime.get("epochs", 90)
+    device = runtime.get("device", "cuda")
+    log_interval = runtime.get("log_interval", 10)
+
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+    if device == "cuda" and torch.cuda.is_available():
+        device = torch.device("cuda")
+    else:
+        device = torch.device("cpu")
+
+    dataset_type = data.pop("type")
+    data_root = Path(data["data_root"])
+    if dataset_type == "ClassificationDataset":
+        from quant.football.data.dataset import ClassificationDataset
+        train_dataset = ClassificationDataset(data_root / data["train_file"])
+        test_dataset = ClassificationDataset(data_root / data["test_file"])
+    elif dataset_type == "FootballDataset":
+        from quant.football.data.dataset import FootballDataset
+        train_dataset = FootballDataset(data_root / data["train_file"])
+        test_dataset = FootballDataset(data_root / data["test_file"])
+    else:
+        raise NotImplementedError(f"Not supported <{dataset_type}>.")
+
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=data["batch_size"], shuffle=True
+    )
+    test_loader = torch.utils.data.DataLoader(
+        test_dataset, batch_size=1
+    )
+
+    model_type = model.pop("type")
+    if model_type == "STNetV1":
+        from quant.football.models.stnet import STNetV1
+        model = STNetV1(**model).to(device)
+    elif model_type == "STNetV2":
+        from quant.football.models.stnet import STNetV2
+        model = STNetV2(**model).to(device)
+    elif model_type == "STNetV3":
+        from quant.football.models.stnet import STNetV3
+        model = STNetV3(**model).to(device)
+    else:
+        raise NotImplementedError(f"Not supported <{model_type}>.")
+
+    loss_type = loss.pop("type")
+    if loss_type == "CrossEntropyLoss":
+        from torch.nn import CrossEntropyLoss
+        criterion = CrossEntropyLoss(**loss).to(device)
+    else:
+        raise NotImplementedError(f"Not supported <{loss_type}>.")
+
+    optimizer_type = optimizer.pop("type")
+    if optimizer_type == "SGD":
+        from torch.optim import SGD
+        optimizer = SGD(model.parameters(), **optimizer)
+    else:
+        raise NotImplementedError(f"Not supported <{optimizer_type}>.")
+
+    scheduler_type = scheduler.pop("type")
+    if scheduler_type == "StepLR":
+        from torch.optim.lr_scheduler import StepLR
+        scheduler = StepLR(optimizer, **scheduler)
+    else:
+        raise NotImplementedError(f"Not supported <{scheduler_type}>.")
+
+    out_dir = Path("runs") / time.strftime("%m%d%H%M%S")
+    out_dir.mkdir(parents=True, exist_ok=True)
+
+    logger = get_logger(__name__, False, out_dir / "log.txt")
+
+    save_json(out_dir / "config.json", config, indent=4)
+
+    best_acc, best_epoch = 0.0, -1
+    for epoch in range(1, epochs + 1):
+        train(model, device, train_loader, criterion, optimizer,
+              epoch, log_interval, logger)
+        curr_acc = test(model, device, test_loader, criterion,
+                        logger)
+        if curr_acc > best_acc:
+            best_acc, best_epoch = curr_acc, epoch
+            torch.save(model.state_dict(), out_dir / f"model{epoch:03d}.pt")
+        scheduler.step()
+    torch.save(model.state_dict(), out_dir / "last.pt")
+
+    print_log("[best model]", logger)
+    print_log(f"\n{best_acc=:.4f}, {best_epoch=:03d}\n", logger)
+
+    print_log("[check train dataset]", logger)
+    check_loader = torch.utils.data.DataLoader(train_dataset, batch_size=1)
+    test(model, device, check_loader, criterion, logger)
+
+
+config = dict(
+    data=dict(
+        type="ClassificationDataset",
+        data_root="/workspace/xlab/datasets/table20240326_03281154_torch_norm",
+        train_file="20240329_101534.dat",
+        test_file="20240329_101539.dat",
+        batch_size=128),
+    model=dict(
+        type="STNetV2",
+        in_features=806,
+        hidden_features=384,
+        out_features=10,
+        n_layers=1,
+        bias=True,
+        enable_skip=False,
+        num_embeddings=1000,
+        embedding_dim=100),
+    loss=dict(type="CrossEntropyLoss", reduction="mean"),
+    optimizer=dict(
+        type="SGD",
+        lr=0.1,
+        momentum=0.9,
+        weight_decay=0.0001),
+    scheduler=dict(
+        type="StepLR",
+        step_size=30,
+        gamma=0.1),
+    runtime=dict(
+        seed=1,
+        epochs=90,
+        device="cuda",
+        log_interval=10))
+
+
+if __name__ == "__main__":
+    kwargs = {}
+    for arg in sys.argv[1:]:
+        key, value = arg.split("=")
+        kwargs[key] = eval(value)
+
+    if "config" in kwargs:
+        config = load_json(kwargs["config"])
+
+    for key, value in kwargs.items():
+        names = key.split(".")
+        d = config
+        for name in names[:-1]:
+            d = d[name]
+        d[names[-1]] = value
+
+    main(config)
