f2d0d234f56be19649a346d1e0c446eb27b7522c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-01 15:03:53 +0000

    Add package geemlib/1.0.3/geemlib-1.0.3-py3-none-any.whl

---
+++ packages/geemlib/geemlib-1.0.3-py3-none-any.whl/geemlib/__init__.py
@@ -0,0 +1,6 @@
+"""Main init file"""
+from . import geemlib
+from . import Web
+
+from .Changelog import GeemlibChangelog as __changelog__
+__version__ = __changelog__.version
---
+++ packages/geemlib/geemlib-1.0.3-py3-none-any.whl/geemlib/geemlib.py
@@ -0,0 +1,9 @@
+"""Main geemlib script"""
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
