4efb4dcd9800301d68a63ec7da3c24c29d65c27c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-01 15:03:48 +0000

    Add package pipxu/1.3/pipxu-1.3-py3-none-any.whl

---
+++ packages/pipxu/pipxu-1.3-py3-none-any.whl/pipxu/commands/reinstall-all.py
@@ -0,0 +1,27 @@
+# Author: Mark Blakeney, Feb 2024.
+'Reinstall all packages and their executables.'
+from __future__ import annotations
+
+from argparse import ArgumentParser, Namespace
+from typing import Optional
+
+from .. import utils
+from . import reinstall
+
+def init(parser: ArgumentParser) -> None:
+    'Called to add command arguments to parser at init'
+    xgroup = parser.add_mutually_exclusive_group()
+    xgroup.add_argument('-p', '--python',
+                        help='specify explicit python executable path')
+    xgroup.add_argument('-P', '--pyenv',
+                        help='pyenv python version to use, '
+                        'i.e. from `pyenv versions`, e.g. "3.9".')
+    parser.add_argument('-v', '--verbose', action='store_true',
+                        help='give more output')
+    parser.add_argument('-s', '--skip', nargs='*',
+                        help='skip these packages, e.g. package1 package2')
+
+def main(args: Namespace) -> Optional[str]:
+    'Called to action this command'
+    args.package = utils.get_all_package_names(args)
+    return reinstall.main(args)
