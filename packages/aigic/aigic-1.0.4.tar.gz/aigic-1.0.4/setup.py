704892bedc3630b3ac8933808854a0f7f2cb648d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-01 15:04:01 +0000

    Add package aigic/1.0.4/aigic-1.0.4.tar.gz

---
+++ packages/aigic/aigic-1.0.4.tar.gz/aigic-1.0.4/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='aigic',
+    version='1.0.4',
+    description='agin python SDK',
+    packages=find_packages(),
+    author='',
+    author_email='',
+    install_requires=[
+        'http.client'
+        # ... other dependencies ...
+    ]
+)
