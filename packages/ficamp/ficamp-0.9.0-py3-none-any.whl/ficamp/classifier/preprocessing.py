f4e430d275649f1854364e1cdef6cb8e95785838
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-01 03:04:58 +0000

    Add package ficamp/0.9.0/ficamp-0.9.0-py3-none-any.whl

---
+++ packages/ficamp/ficamp-0.9.0-py3-none-any.whl/ficamp/classifier/preprocessing.py
@@ -0,0 +1,44 @@
+def remove_digits(s: str) -> str:
+    """
+    Return string without words that have more that 2 digits.
+    examples:
+        like SEPA 12312321 BIC --> SEPA BIC
+        like SEPA12 --> SEPA12
+    """
+    clean = []
+    words = s.split()
+
+    for word in words:
+        n_char = 0
+        for char in word:
+            n_char += char.isdigit()
+        if n_char <= 2:
+            clean.append(word)
+    return " ".join(clean)
+
+
+def remove_pipes(s: str) -> str:
+    return " ".join(s.split("|"))
+
+
+def remove_colon(s: str) -> str:
+    return " ".join(s.split(":"))
+
+
+def remove_comma(s: str) -> str:
+    return " ".join(s.split(","))
+
+
+def preprocess(s: str) -> str:
+    "Clean up transaction description"
+    steps = (
+        lambda s: s.lower(),
+        remove_pipes,
+        remove_colon,
+        remove_comma,
+        remove_digits,
+    )
+    out = s
+    for func in steps:
+        out = func(out)
+    return out
