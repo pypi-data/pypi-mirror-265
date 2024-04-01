17f070c871484078bc12be51c9c6576bedbfa097
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-01 04:04:28 +0000

    Add package hydrotoolbox/2.0.10/hydrotoolbox-2.0.10-py3-none-any.whl

---
+++ packages/hydrotoolbox/hydrotoolbox-2.0.10-py3-none-any.whl/hydrotoolbox/__init__.py
@@ -0,0 +1,11 @@
+"""Define hydrotoolbox package."""
+
+from .hydrotoolbox import (
+    about,
+    baseflow_sep,
+    exceedance_time,
+    flow_duration,
+    indices,
+    recession,
+    storm_events,
+)
