93490dd5445efca8a1208f0ef7fa91487afa0da0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-01 14:03:39 +0000

    Add package piah/0.1.1/piah-0.1.1-py3-none-any.whl

---
+++ packages/piah/piah-0.1.1-py3-none-any.whl/piah/__init__.py
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: 2024-present fabiobarkoski <fabiobarkoskii@gmail.com>
+
+# SPDX-License-Identifier: MIT
+
+from dotenv import load_dotenv
+from piah.parser import Piah as Piah
+
+load_dotenv()
