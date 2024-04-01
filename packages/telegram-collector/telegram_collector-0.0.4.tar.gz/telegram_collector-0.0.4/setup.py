f756b2f6f81ae60e12a821963f78131072e692db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-01 15:04:02 +0000

    Add package telegram-collector/0.0.4/telegram_collector-0.0.4.tar.gz

---
+++ packages/telegram-collector/telegram_collector-0.0.4.tar.gz/telegram_collector-0.0.4/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name='telegram_collector',
+    version='0.0.4',
+    author='fengleicn',
+    author_email='fengleisemail@gmail.com',
+    url='https://github.com/fengleicn/telegram_collector',
+    description=u'收集电报群组的视频图片消息',
+    packages=['telegram_collector'],
+    install_requires=['telethon', 'python_socks'],
+    entry_points={
+        'console_scripts': [
+            'telegram_collector_send_current_message_src_to_dest=telegram_collector.__init__:send_current_message_src_to_dest',
+            'telegram_collector_send_history_message_src_to_dest=telegram_collector.__init__:send_history_message_src_to_dest']
+    }
+)
---
+++ packages/telegram-collector/telegram_collector-0.0.4.tar.gz/telegram_collector-0.0.4/telegram_collector/__main__.py
@@ -0,0 +1,17 @@
+from .__init__ import TelegramCollector
+
+
+def send_current_message_src_to_dest():
+    collector = TelegramCollector()
+    collector.do_init()
+    collector.send_current_message_src_to_dest()
+
+
+def send_history_message_src_to_dest():
+    collector = TelegramCollector()
+    collector.do_init()
+    collector.send_history_message_src_to_dest()
+
+
+if __name__ == '__main__':
+    send_current_message_src_to_dest()
