b6601a5a28e0a824b4a15f8ff0a8493a6924e90f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-02 04:06:55 +0000

    Add package harp-proxy/0.5.0b5/harp_proxy-0.5.0b5-py3-none-any.whl

---
+++ packages/harp-proxy/harp_proxy-0.5.0b5-py3-none-any.whl/harp/settings.py
@@ -0,0 +1,16 @@
+"""
+The Settings (:mod:`harp.settings`) module contains hardcoded configuration for Harp. This is probably only handy
+for the young days of harp, and will be replaced by something less hard-coded later.
+
+Contents
+--------
+
+"""
+
+__title__ = "Settings"
+
+#: Pagination size for api endpoints
+PAGE_SIZE = 20
+
+#: Default timeout for http requests
+DEFAULT_TIMEOUT = 10.0
