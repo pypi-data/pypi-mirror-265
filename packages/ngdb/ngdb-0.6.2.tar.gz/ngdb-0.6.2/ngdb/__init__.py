5c7545768d8f29c60b5b59b94c41925042dae1e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-02 16:04:43 +0000

    Add package ngdb/0.6.2/ngdb-0.6.2.tar.gz

---
+++ packages/ngdb/ngdb-0.6.2.tar.gz/ngdb-0.6.2/ngdb/__init__.py
@@ -0,0 +1,37 @@
+"""ngdb - Norton Guide database reading library."""
+
+######################################################################
+# Main library information.
+__author__ = "Dave Pearson"
+__copyright__ = "Copyright 2021-2024, Dave Pearson"
+__credits__ = ["Dave Pearson"]
+__maintainer__ = "Dave Pearson"
+__email__ = "davep@davep.org"
+__version__ = "0.6.2"
+__licence__ = "GPLv3+"
+
+##############################################################################
+# Import things for easier access.
+from .dosify import make_dos_like
+from .entry import Entry, Long, Short
+from .guide import NortonGuide
+from .parser import BaseParser, MarkupText, PlainText
+from .types import NGEOF, NGDBError, UnknownEntryType
+
+##############################################################################
+# Define what importing * means.
+__all__ = (
+    "NGDBError",
+    "UnknownEntryType",
+    "NGEOF",
+    "NortonGuide",
+    "Entry",
+    "Short",
+    "Long",
+    "BaseParser",
+    "PlainText",
+    "MarkupText",
+    "make_dos_like",
+)
+
+### __init__.py ends here
