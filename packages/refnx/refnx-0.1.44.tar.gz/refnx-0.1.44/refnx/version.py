add61162bf4e9ee24f14efd19f8c0797ea87c51b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-02 16:04:44 +0000

    Add package refnx/0.1.44/refnx-0.1.44.tar.gz

---
+++ packages/refnx/refnx-0.1.44.tar.gz/refnx-0.1.44/refnx/version.py
@@ -0,0 +1,9 @@
+
+# THIS FILE IS GENERATED FROM REFNX SETUP.PY
+short_version = '0.1.44'
+version = '0.1.44'
+full_version = '0.1.44'
+git_revision = '25b25942b26a18ca0cd33e6f9ca49c2b0e613550'
+release = True
+if not release:
+    version = full_version
