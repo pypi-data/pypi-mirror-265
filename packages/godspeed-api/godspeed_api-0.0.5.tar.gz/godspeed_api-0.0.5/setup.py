201f59445d6baab8dc87eeff80fb74d6a44feb34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-02 03:04:30 +0000

    Add package godspeed-api/0.0.5/godspeed_api-0.0.5.tar.gz

---
+++ packages/godspeed-api/godspeed_api-0.0.5.tar.gz/godspeed_api-0.0.5/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+VERSION = "0.0.5"
+DESCRIPTION = "A Godspeed task manager API wrapper"
+
+setup(
+    name="godspeed_api",
+    version=VERSION,
+    author="Artem Trubacheev",
+    author_email="almaz5200@gmail.com",
+    description=DESCRIPTION,
+    long_description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[],
+    keywords=["python", "godspeed"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+)
