20268a1440b9b1649057d7de8c36708322f3879e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-02 06:04:53 +0000

    Add package model-explorer/0.0.63/model_explorer-0.0.63-py3-none-any.whl

---
+++ packages/model-explorer/model_explorer-0.0.63-py3-none-any.whl/model_explorer/builtin_tf_mlir_adapter.py
@@ -0,0 +1,43 @@
+import os
+from typing import Dict
+
+from model_explorer_adapter import \
+    _pywrap_convert_wrapper as convert_wrapper  # type: ignore
+
+from .adapter import Adapter, AdapterMetadata
+from .types import ModelExplorerGraphs
+from .utils import convert_builtin_resp
+
+
+class BuiltinTfMlirAdapter(Adapter):
+  """Built-in tf adapter using MLIR."""
+
+  metadata = AdapterMetadata(id='builtin_tf_mlir',
+                             name='TF adapter (MLIR)',
+                             description='A built-in adapter that converts a TF saved model to Model Explorer format through MLIR.',
+                             fileExts=['pb'])
+
+  def __init__(self):
+    super().__init__()
+
+  def convert(self, model_path: str, settings: Dict) -> ModelExplorerGraphs:
+    # Construct config.
+    config = convert_wrapper.VisualizeConfig()
+    if 'const_element_count_limit' in settings:
+      config.const_element_count_limit = settings['const_element_count_limit']
+
+    # Normalize model_path
+    model_dir = model_path
+    if model_path.endswith('.pb'):
+      model_dir = os.path.dirname(model_path)
+      file_name = os.path.basename(model_path)
+
+      # Check file name.
+      if file_name != 'saved_model.pb':
+        raise Exception(
+            f'The model file has to be named "saved_model.pb" to work with this adapter')
+
+    # Run
+    resp_json_str = convert_wrapper.ConvertSavedModelToJson(
+        config, model_dir)
+    return {'graphCollections': convert_builtin_resp(resp_json_str)}
