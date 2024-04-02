39fcdb1356da0a7c8fc9ce23fc8f32c959fc0e94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-02 03:03:44 +0000

    Add package pvp-backend-api/0.7/pvp-backend-api-0.7.tar.gz

---
+++ packages/pvp-backend-api/pvp-backend-api-0.7.tar.gz/pvp-backend-api-0.7/setup.py
@@ -0,0 +1,25 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='pvp-backend-api',
+    version='0.7',
+    packages=find_packages(),
+    install_requires=[
+        'click',
+    ],
+    entry_points='''
+        [console_scripts]
+        pvp-api=pvp_api:cli
+        pvp-api-scenario-library=pvp_api_scenario_library:cli
+        pvp-api-pe-decomposition=pvp_api_pe_decomposition:cli
+        pvp-create-project=pvp_create_project:cli
+    ''',
+    author='Sachin Raghav',
+    author_email='sraghav872@gmail.com',
+    description='A Python package to generate a PVP API template',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/pvp-api-package',
+)
