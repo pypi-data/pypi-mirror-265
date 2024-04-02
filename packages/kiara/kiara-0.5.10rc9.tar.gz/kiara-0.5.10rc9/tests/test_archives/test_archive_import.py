4c77091a1446e88379f2e87f9ec43b704727137c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-02 16:05:14 +0000

    Add package kiara/0.5.10rc9/kiara-0.5.10rc9.tar.gz

---
+++ packages/kiara/kiara-0.5.10rc9.tar.gz/kiara-0.5.10rc9/tests/test_archives/test_archive_import.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+import os
+import uuid
+from pathlib import Path
+
+from kiara.interfaces.python_api.base_api import BaseAPI
+
+ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
+TEST_RESOURCES_FOLDER = os.path.join(ROOT_DIR, "tests", "resources")
+
+
+def test_archive_import_values_no_alias(api: BaseAPI):
+
+    resources_folder = Path(TEST_RESOURCES_FOLDER)
+
+    archive_file = resources_folder / "archives" / "export_test.kiarchive"
+
+    assert not api.list_all_value_ids()
+
+    result = api.import_archive(archive_file, no_aliases=True)
+
+    assert not result.errors
+
+    assert len(result) == 8
+    assert "af83495c-9fbf-4155-a9ce-29f1e8be4da9" in result.keys()
+
+    assert uuid.UUID("af83495c-9fbf-4155-a9ce-29f1e8be4da9") in api.list_all_value_ids()
+
+    assert ["export_test#y"] == api.list_alias_names()
+
+
+def test_archive_import_values_with_alias(api: BaseAPI):
+
+    resources_folder = Path(TEST_RESOURCES_FOLDER)
+
+    archive_file = resources_folder / "archives" / "export_test.kiarchive"
+
+    assert not api.list_all_value_ids()
+
+    result = api.import_archive(archive_file, no_aliases=False)
+
+    assert not result.errors
+
+    assert len(result) == 8
+    assert "af83495c-9fbf-4155-a9ce-29f1e8be4da9" in result.keys()
+
+    assert uuid.UUID("af83495c-9fbf-4155-a9ce-29f1e8be4da9") in api.list_all_value_ids()
+
+    assert {"y", "export_test#y"} == set(api.list_alias_names())
