9b4291b66ae5619c24d56b73329338c0a5612f28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-02 05:05:14 +0000

    Add package nutshell/0.2.3/nutshell-0.2.3-py3-none-any.whl

---
+++ packages/nutshell/nutshell-0.2.3-py3-none-any.whl/nutshell/__init__.py
@@ -0,0 +1,5 @@
+from .methods import FindUsers, GetUser, FindTeams, FindActivityTypes, ReportType, FilterEntity, ReportFilter, \
+    GetAnalyticsReport
+from .nutshell_api import NutshellAPI
+from .responses import FindUsersResult, GetUserResult, FindTeamsResult, FindActivityTypesResult, \
+    GetAnalyticsReportResult
