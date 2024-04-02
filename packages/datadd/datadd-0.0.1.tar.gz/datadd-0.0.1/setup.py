7fffa3607f8cfca0df80539d310a0f812ab26ebc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-02 04:07:22 +0000

    Add package datadd/0.0.1/datadd-0.0.1.tar.gz

---
+++ packages/datadd/datadd-0.0.1.tar.gz/datadd-0.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="datadd",
+    version="0.0.1",
+    author="Sam Hjelmfelt",
+    author_email="sam@helm.news",
+    description="",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://helm.news",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+)
