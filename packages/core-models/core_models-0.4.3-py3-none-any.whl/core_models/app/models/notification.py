578a3a23aeec49a13a34d24e2338116ac157ab56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-02 04:05:16 +0000

    Add package core-models/0.4.3/core_models-0.4.3-py3-none-any.whl

---
+++ packages/core-models/core_models-0.4.3-py3-none-any.whl/core_models/app/models/notification.py
@@ -0,0 +1,41 @@
+from django.db import models
+
+from . import Company, ProfileApplication, Contract, Invoice
+from .base import BaseModelAbstract
+from ... import constants
+
+
+class Notification(BaseModelAbstract, models.Model):
+    company = models.ForeignKey(Company, models.CASCADE, null=True, blank=True)
+    notice_type = models.CharField(max_length=50, null=False, blank=False)
+    object_id = models.UUIDField(null=True, blank=True)
+    seen = models.BooleanField(default=False)
+
+    def __unicode__(self):
+        email = self.created_by.email if self.created_by else '-'
+        return f"Notification type: {self.notice_type} for: {email}"
+
+    @property
+    def object(self):
+        maps = {
+            constants.ADMIN_NEW_PROFILE_NOTIF_TYPE: ProfileApplication,
+            constants.ADMIN_CONTRACT_DD_NOTIF_TYPE: Contract,
+            constants.ADMIN_VAL_INVOICE_NOTIF_TYPE: Invoice,
+        }
+        if m := maps.get(self.notice_type):
+            return m.objects.get(id=self.object_id)
+        return None
+
+    @property
+    def text(self):
+        for item in constants.NOTIFICATION_TYPES:
+            if item[0] == self.notice_type:
+                return item[1]
+        return '-'
+
+    @property
+    def description(self):
+        for item in constants.NOTIFICATION_TYPES:
+            if item[0] == self.notice_type:
+                return item[1]
+        return '-'
