b93597e5915302d09f3d45d683ae2edb80d2e8fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-02 15:04:25 +0000

    Add package proteinnpt/1.5/proteinnpt-1.5.tar.gz

---
+++ packages/proteinnpt/proteinnpt-1.5.tar.gz/proteinnpt-1.5/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup,find_packages
+
+with open("README.md") as f:
+    readme = f.read()
+
+setup(
+    name="proteinnpt",
+    description="ProteinNPT: Improving Protein Property Prediction and Design with Non-Parametric Transformers",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author="Pascal Notin and Ruben Weitzman",
+    version="1.5",
+    license="MIT",
+    url="https://github.com/OATML-Markslab/ProteinNPT",    
+    packages=find_packages()
+)
