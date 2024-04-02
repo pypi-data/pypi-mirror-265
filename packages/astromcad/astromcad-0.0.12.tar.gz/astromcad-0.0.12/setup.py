e464b9c506eb97d51e9301266c2e58508e5951b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-02 15:05:05 +0000

    Add package astromcad/0.0.12/astromcad-0.0.12.tar.gz

---
+++ packages/astromcad/astromcad-0.0.12.tar.gz/astromcad-0.0.12/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+with open("READMEPyPI.md", "r") as fh:
+    long_description = fh.read();
+
+VERSION = '0.0.12'
+DESCRIPTION = 'Anomaly Detection for Astronomical Transients'
+LONG_DESCRIPTION = long_description
+
+# Setting up
+setup(
+    name="astromcad",
+    version=VERSION,
+    author="Rithwik Gupta",
+    author_email="<rithwikca2020@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['numpy', 'keras', 'tensorflow', 'matplotlib', 'scikit-learn'],
+    keywords=[],
+    
+)
