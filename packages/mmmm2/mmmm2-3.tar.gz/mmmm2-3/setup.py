eb47fc176c117897b82c114863e284aa53e9a803
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-02 05:04:11 +0000

    Add package mmmm2/3/mmmm2-3.tar.gz

---
+++ packages/mmmm2/mmmm2-3.tar.gz/mmmm2-3/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name='mmmm2',
+      version='3',
+      description='none',
+      packages=['mmmm2'],
+      author_email='antoha123_3@mail.ru',
+      zip_safe=False)
