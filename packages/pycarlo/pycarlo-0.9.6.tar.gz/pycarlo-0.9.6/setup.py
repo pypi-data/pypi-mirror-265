67e4ca88e53c2b4428d71a76869d018d48b86f69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-02 04:04:06 +0000

    Add package pycarlo/0.9.6/pycarlo-0.9.6.tar.gz

---
+++ packages/pycarlo/pycarlo-0.9.6.tar.gz/pycarlo-0.9.6/setup.py
@@ -0,0 +1,46 @@
+import distutils.text_file
+from pathlib import Path
+from typing import List
+
+from setuptools import find_packages, setup
+
+
+def parse_requirements(file_name: str = "requirements.txt") -> List[str]:
+    return distutils.text_file.TextFile(
+        filename=str(Path(__file__).with_name(file_name))
+    ).readlines()
+
+
+def get_long_description():
+    with open("README.md", "r") as fh:
+        return fh.read()
+
+
+setup(
+    name="pycarlo",
+    use_scm_version=True,
+    license="Apache Software License (Apache 2.0)",
+    description="Monte Carlo's Python SDK",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    author="Monte Carlo Data, Inc",
+    author_email="info@montecarlodata.com",
+    url="https://www.montecarlodata.com/",
+    packages=find_packages(exclude=["tests*", "utils*", "examples*"]),
+    include_package_data=True,
+    install_requires=parse_requirements(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    python_requires=">=3.7",
+    setup_requires=["setuptools", "setuptools_scm"],
+)
