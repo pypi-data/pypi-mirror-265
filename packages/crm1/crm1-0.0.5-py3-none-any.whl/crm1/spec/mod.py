99cecd2a69186bb60daf8623f55ee346612dcf42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-02 05:04:10 +0000

    Add package crm1/0.0.5/crm1-0.0.5-py3-none-any.whl

---
+++ packages/crm1/crm1-0.0.5-py3-none-any.whl/crm1/spec/mod.py
@@ -0,0 +1,33 @@
+"""Don't import this module directly."""
+
+from dataclasses import dataclass
+
+from dataclasses_json import LetterCase, dataclass_json
+
+from .dependency import RDependency
+from .unsafe_modext import UnsafeModExt
+
+
+@dataclass_json(letter_case=LetterCase.CAMEL)
+@dataclass
+class RMod:
+    """Raw mod data. This is used for deserialization."""
+
+    id: str
+    """Mod ID. This is in the format of group.id, like `com.example.mod`."""
+    name: str
+    """Mod name."""
+    desc: str
+    """A short description of the mod."""
+    authors: list[str]
+    """A list of the authors' names of the mod."""
+    version: str
+    """The version of the mod."""
+    game_version: str
+    """The version of the game that the mod is compatible with."""
+    url: str
+    """The download URL of the mod's jar."""
+    deps: list[RDependency]
+    """A list of dependencies of the mod."""
+    ext: dict | UnsafeModExt
+    """Some optional additional info of the mod."""
