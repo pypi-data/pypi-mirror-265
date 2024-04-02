315532fb4028a81f771dceda9b17ce9a109122b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-02 14:05:01 +0000

    Add package sajjad/1.7/sajjad-1.7.tar.gz

---
+++ packages/sajjad/sajjad-1.7.tar.gz/sajjad-1.7/sajjad/sajjad.py
@@ -0,0 +1,8 @@
+from colorama import Fore
+class get_number:
+    def add(num1,num2):
+        if str(type(num1)) == str(type(num2)) == "<class 'int'>":
+            print(num1 + num2)
+        else:
+            print(Fore.RED,"you shoud type numbers",Fore.RESET)
+get_number.add(1,2)
