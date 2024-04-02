6c6f8817c6dc9d2f344afc5a09f630227c57a03c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-02 03:04:03 +0000

    Add package hw2latex/0.1.5/hw2latex-0.1.5.tar.gz

---
+++ packages/hw2latex/hw2latex-0.1.5.tar.gz/hw2latex-0.1.5/README.md
@@ -0,0 +1,3 @@
+## Description
+
+My hw2 module
---
+++ packages/hw2latex/hw2latex-0.1.5.tar.gz/hw2latex-0.1.5/hw2latex/hw2latex.py
@@ -0,0 +1,49 @@
+def generate_latex_table(data):
+    # Начало таблицы
+    latex_code = "\\begin{table}[h!]\n"
+    latex_code += "\\centering\n"
+    latex_code += "\\begin{tabular}{|" + "|".join(["c"] * len(data[0])) + "|}\n"
+    latex_code += "\\hline\n"
+
+    # Заполнение таблицы данными
+    for row in data:
+        latex_code += " & ".join(map(str, row)) + " \\\\ \n"
+        latex_code += "\\hline\n"
+
+    # Завершение таблицы
+    latex_code += "\\end{tabular}\n"
+    latex_code += "\\caption{table}\n"
+    latex_code += "\\label{tab:example}\n"
+    latex_code += "\\end{table}\n"
+
+    return latex_code
+
+
+def imagetolatex(image_path, caption='image'):
+    latex_code = rf"""
+\begin{{figure}}
+    \centering
+    \includegraphics[width=0.5\textwidth]{{{image_path}}}
+    \caption{{{caption}}}
+    \label{{fig:image}}
+\end{{figure}}
+"""
+    return latex_code
+
+def file_latex(f_table, f_image, data, image_path):
+    latex_code = '''
+\\documentclass{article}
+\\usepackage{graphicx}
+\\usepackage[utf8x]{inputenc}
+\\graphicspath{{./img}}
+\\begin{document}
+'''
+    latex_code+= f_table(data)
+    latex_code+= f_image(image_path)
+    
+    latex_code+= '\end{document}'
+    return latex_code
+   
+
+    
+    
