2e2f2f85b49779a04bc261e368ece5bf85761534
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-02 15:04:47 +0000

    Add package ft2bt/0.1.2/ft2bt-0.1.2.tar.gz

---
+++ packages/ft2bt/ft2bt-0.1.2.tar.gz/ft2bt-0.1.2/ft2bt/scripts/behavior_trees/behavior_tree_node.py
@@ -0,0 +1,10 @@
+class BehaviorTreeNode:
+    """
+    Behavior tree node class. 
+    Each node has a node ID, node type, and a list of children
+    """
+    def __init__(self, node_id, node_type, label=None):
+        self.node_id = node_id
+        self.node_type = node_type
+        self.children = list()
+        self.label = label
