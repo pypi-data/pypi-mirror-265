a304061ce2b057bfc2a6688381eca7de3ddd9aa2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-02 14:05:04 +0000

    Add package Eporner-API/1.6.1/Eporner_API-1.6.1.tar.gz

---
+++ packages/Eporner-API/Eporner_API-1.6.1.tar.gz/Eporner_API-1.6.1/eporner_api/modules/errors.py
@@ -0,0 +1,13 @@
+class InvalidURL(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+
+
+class HTML_IS_DISABLED(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+
+
+class NotAvailable(Exception):
+    def __init__(self, msg):
+        self.msg = msg
