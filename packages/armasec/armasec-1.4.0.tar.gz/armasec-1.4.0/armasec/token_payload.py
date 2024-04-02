926228c466faa82839abdd2e68a846729ace7dab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-02 05:04:10 +0000

    Add package armasec/1.4.0/armasec-1.4.0.tar.gz

---
+++ packages/armasec/armasec-1.4.0.tar.gz/armasec-1.4.0/armasec/token_payload.py
@@ -0,0 +1,41 @@
+"""
+This module defines a pydantic schema for the payload of a jwt.
+"""
+
+from datetime import datetime
+from typing import List, Optional
+
+from pydantic import BaseModel, Field
+
+
+class TokenPayload(BaseModel):
+    """
+    A convenience class that can be used to access parts of a decoded jwt.
+
+    Attributes:
+        sub:            The "sub" claim from a JWT.
+        permissions:    The permissions claims extracted from a JWT.
+        expire:         The "exp" claim extracted from a JWT.
+        client_id:      The "azp" claim extracted from a JWT.
+        original_token: The original token value
+    """
+
+    sub: str
+    permissions: List[str] = Field(list())
+    expire: datetime = Field(None, alias="exp")
+    client_id: str = Field(None, alias="azp")
+    original_token: Optional[str] = None
+
+    class Config:
+        extra = "allow"
+
+    def to_dict(self):
+        """
+        Convert a TokenPayload to the equivalent dictionary returned by `jwt.decode()`.
+        """
+        return dict(
+            sub=self.sub,
+            permissions=self.permissions,
+            exp=int(self.expire.timestamp()),
+            client_id=self.client_id,
+        )
