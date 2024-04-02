e0c351a81cd09da29afb250e93df6d54ea853f2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-02 16:05:12 +0000

    Add package iec-api/0.2.13/iec_api-0.2.13.tar.gz

---
+++ packages/iec-api/iec_api-0.2.13.tar.gz/iec_api-0.2.13/iec_api/data.py
@@ -0,0 +1,199 @@
+import base64
+import logging
+from datetime import datetime
+from typing import Optional, TypeVar
+
+from aiohttp import ClientSession
+from mashumaro.codecs import BasicDecoder
+
+from iec_api import commons
+from iec_api.const import (
+    GET_ACCOUNTS_URL,
+    GET_BILLING_INVOICES,
+    GET_CONSUMER_URL,
+    GET_CONTRACTS_URL,
+    GET_DEFAULT_CONTRACT_URL,
+    GET_DEVICE_BY_DEVICE_ID_URL,
+    GET_DEVICE_TYPE_URL,
+    GET_DEVICES_URL,
+    GET_ELECTRIC_BILL_URL,
+    GET_KWH_TARIFF_URL,
+    GET_LAST_METER_READING_URL,
+    GET_REQUEST_READING_URL,
+    HEADERS_WITH_AUTH,
+)
+from iec_api.models.account import Account
+from iec_api.models.account import decoder as account_decoder
+from iec_api.models.contract import Contract, Contracts
+from iec_api.models.contract import decoder as contract_decoder
+from iec_api.models.customer import Customer
+from iec_api.models.device import Device, Devices
+from iec_api.models.device import decoder as devices_decoder
+from iec_api.models.device_type import DeviceType
+from iec_api.models.device_type import decoder as device_type_decoder
+from iec_api.models.electric_bill import ElectricBill
+from iec_api.models.electric_bill import decoder as electric_bill_decoder
+from iec_api.models.exceptions import IECError
+from iec_api.models.invoice import GetInvoicesBody
+from iec_api.models.invoice import decoder as invoice_decoder
+from iec_api.models.jwt import JWT
+from iec_api.models.meter_reading import MeterReadings
+from iec_api.models.meter_reading import decoder as meter_reading_decoder
+from iec_api.models.remote_reading import ReadingResolution, RemoteReadingRequest, RemoteReadingResponse
+from iec_api.models.response_descriptor import ResponseWithDescriptor
+
+T = TypeVar("T")
+logger = logging.getLogger(__name__)
+
+
+async def _get_response_with_descriptor(
+    session: ClientSession, jwt_token: JWT, request_url: str, decoder: BasicDecoder[ResponseWithDescriptor[T]]
+) -> T:
+    """
+    A function to retrieve a response with a descriptor using a JWT token and a URL.
+
+    Args:
+        jwt_token (JWT): The JWT token used for authentication.
+        request_url (str): The URL to send the request to.
+
+    Returns:
+        T: The response with a descriptor, with its type specified by the return type annotation.
+    """
+    headers = commons.add_auth_bearer_to_headers(HEADERS_WITH_AUTH, jwt_token.id_token)
+    response = await commons.send_get_request(session=session, url=request_url, headers=headers)
+
+    response_with_descriptor = decoder.decode(response)
+
+    if not response_with_descriptor.data and not response_with_descriptor.response_descriptor.is_success:
+        raise IECError(
+            response_with_descriptor.response_descriptor.code, response_with_descriptor.response_descriptor.description
+        )
+
+    return response_with_descriptor.data
+
+
+async def get_accounts(session: ClientSession, token: JWT) -> Optional[list[Account]]:
+    """Get Accounts response from IEC API."""
+    return await _get_response_with_descriptor(session, token, GET_ACCOUNTS_URL, account_decoder)
+
+
+async def get_customer(session: ClientSession, token: JWT) -> Optional[Customer]:
+    """Get customer data response from IEC API."""
+    headers = commons.add_auth_bearer_to_headers(HEADERS_WITH_AUTH, token.id_token)
+    # sending get request and saving the response as response object
+    response = await commons.send_get_request(session=session, url=GET_CONSUMER_URL, headers=headers)
+
+    logger.debug(f"Response: {response}")
+    return Customer.from_dict(response)
+
+
+async def get_remote_reading(
+    session: ClientSession,
+    token: JWT,
+    contract_id: str,
+    meter_serial_number: str,
+    meter_code: int,
+    last_invoice_date: datetime,
+    from_date: datetime,
+    resolution: ReadingResolution = ReadingResolution.DAILY,
+) -> Optional[RemoteReadingResponse]:
+    req = RemoteReadingRequest(
+        meter_serial_number=meter_serial_number,
+        meter_code=str(meter_code),
+        last_invoice_date=last_invoice_date.strftime("%Y-%m-%d"),
+        from_date=from_date.strftime("%Y-%m-%d"),
+        resolution=resolution,
+    )
+
+    url = GET_REQUEST_READING_URL.format(contract_id=contract_id)
+    headers = commons.add_auth_bearer_to_headers(HEADERS_WITH_AUTH, token.id_token)
+
+    response = await commons.send_post_request(session=session, url=url, headers=headers, json_data=req.to_dict())
+
+    return RemoteReadingResponse.from_dict(response)
+
+
+async def get_electric_bill(
+    session: ClientSession, token: JWT, bp_number: str, contract_id: str
+) -> Optional[ElectricBill]:
+    """Get Electric Bill data response from IEC API."""
+    return await _get_response_with_descriptor(
+        session,
+        token,
+        GET_ELECTRIC_BILL_URL.format(contract_id=contract_id, bp_number=bp_number),
+        electric_bill_decoder,
+    )
+
+
+async def get_default_contract(session: ClientSession, token: JWT, bp_number: str) -> Optional[Contract]:
+    """Get Contract data response from IEC API."""
+    return await _get_response_with_descriptor(
+        session, token, GET_DEFAULT_CONTRACT_URL.format(bp_number=bp_number), contract_decoder
+    )
+
+
+async def get_contracts(session: ClientSession, token: JWT, bp_number: str) -> Optional[Contracts]:
+    """Get all user's Contracts from IEC API."""
+    return await _get_response_with_descriptor(
+        session, token, GET_CONTRACTS_URL.format(bp_number=bp_number), contract_decoder
+    )
+
+
+async def get_last_meter_reading(
+    session: ClientSession, token: JWT, bp_number: str, contract_id: str
+) -> Optional[MeterReadings]:
+    """Get Last Meter Reading data response from IEC API."""
+    return await _get_response_with_descriptor(
+        session,
+        token,
+        GET_LAST_METER_READING_URL.format(contract_id=contract_id, bp_number=bp_number),
+        meter_reading_decoder,
+    )
+
+
+async def get_devices(session: ClientSession, token: JWT, contract_id: str) -> list[Device]:
+    """Get Device data response from IEC API."""
+    headers = commons.add_auth_bearer_to_headers(HEADERS_WITH_AUTH, token.id_token)
+    # sending get request and saving the response as response object
+    response = await commons.send_get_request(
+        session=session, url=GET_DEVICES_URL.format(contract_id=contract_id), headers=headers
+    )
+
+    return [Device.from_dict(device) for device in response]
+
+
+async def get_device_by_device_id(
+    session: ClientSession, token: JWT, contract_id: str, device_id: str
+) -> Optional[Devices]:
+    """Get Device data response from IEC API."""
+    return await _get_response_with_descriptor(
+        session,
+        token,
+        GET_DEVICE_BY_DEVICE_ID_URL.format(device_id=device_id, contract_id=contract_id),
+        devices_decoder,
+    )
+
+
+async def get_device_type(session: ClientSession, token: JWT, bp_number: str, contract_id: str) -> Optional[DeviceType]:
+    """Get Device Type data response from IEC API."""
+    # sending get request and saving the response as response object
+    return await _get_response_with_descriptor(
+        session, token, GET_DEVICE_TYPE_URL.format(bp_number=bp_number, contract_id=contract_id), device_type_decoder
+    )
+
+
+async def get_billing_invoices(
+    session: ClientSession, token: JWT, bp_number: str, contract_id: str
+) -> Optional[GetInvoicesBody]:
+    """Get Device Type data response from IEC API."""
+    return await _get_response_with_descriptor(
+        session, token, GET_BILLING_INVOICES.format(bp_number=bp_number, contract_id=contract_id), invoice_decoder
+    )
+
+
+async def get_kwh_tariff(session: ClientSession) -> float:
+    """Get Device Type data response from IEC API."""
+    response = await commons.send_get_request(session=session, url=GET_KWH_TARIFF_URL)
+    kwh_tariff_str = response["components"][1]["table"][1][2]["value"]
+
+    return float(base64.b64decode(kwh_tariff_str).decode("utf-8"))
---
+++ packages/iec-api/iec_api-0.2.13.tar.gz/iec_api-0.2.13/iec_api/iec_client.py
@@ -0,0 +1,436 @@
+import asyncio
+import atexit
+import logging
+from datetime import datetime, timedelta
+from typing import Optional
+
+import aiohttp
+import jwt
+from aiohttp import ClientSession
+
+from iec_api import data, login
+from iec_api.commons import is_valid_israeli_id
+from iec_api.models.contract import Contract
+from iec_api.models.customer import Account, Customer
+from iec_api.models.device import Device, Devices
+from iec_api.models.device_type import DeviceType
+from iec_api.models.electric_bill import ElectricBill
+from iec_api.models.exceptions import IECLoginError
+from iec_api.models.invoice import GetInvoicesBody
+from iec_api.models.jwt import JWT
+from iec_api.models.meter_reading import MeterReadings
+from iec_api.models.remote_reading import ReadingResolution, RemoteReadingResponse
+
+logger = logging.getLogger(__name__)
+
+
+class IecClient:
+    """IEC API Client."""
+
+    def __init__(self, user_id: str | int, session: Optional[ClientSession] = None):
+        """
+        Initializes the class with the provided user ID and optionally logs in automatically.
+
+        Args:
+        session (ClientSession): The aiohttp ClientSession object.
+        user_id (str): The user ID (SSN) to be associated with the instance.
+        automatically_login (bool): Whether to automatically log in the user. Default is False.
+        """
+
+        self._kwh_tariff: Optional[float] = None
+        self._kwh_tariff_fetch_time: Optional[datetime.datetime] = None
+        if not is_valid_israeli_id(user_id):
+            raise ValueError("User ID must be a valid Israeli ID.")
+
+        if not session:
+            session = aiohttp.ClientSession()
+            atexit.register(self._shutdown)
+
+        self._session = session
+
+        self._state_token: Optional[str] = None  # Token for maintaining the state of the user's session
+        self._factor_id: Optional[str] = None  # Factor ID for multifactor authentication
+        self._session_token: Optional[str] = None  # Token for maintaining the user's session
+        self.logged_in: bool = False  # Flag to indicate if the user is logged in
+        self._token: JWT = JWT(
+            access_token="", refresh_token="", token_type="", expires_in=0, scope="", id_token=""
+        )  # Token for authentication
+        self._user_id: str = str(user_id)  # User ID associated with the instance
+        self._login_response: Optional[str] = None  # Response from the login attempt
+        self._bp_number: Optional[str] = None  # BP Number associated with the instance
+        self._contract_id: Optional[str] = None  # Contract ID associated with the instance
+
+    def _shutdown(self):
+        if not self._session.closed:
+            asyncio.run(self._session.close())
+
+    # -------------
+    # Data methods:
+    # -------------
+
+    async def get_customer(self) -> Optional[Customer]:
+        """
+        Get consumer data response from IEC API.
+        :return: Customer data
+        """
+        await self.check_token()
+        customer = await data.get_customer(self._session, self._token)
+        if customer:
+            self._bp_number = customer.bp_number
+        return customer
+
+    async def get_accounts(self) -> Optional[list[Account]]:
+        """
+        Get consumer data response from IEC API.
+        :return: Customer data
+        """
+        await self.check_token()
+        accounts = await data.get_accounts(self._session, self._token)
+
+        if accounts and len(accounts) > 0:
+            self._bp_number = accounts[0].account_number
+
+        return accounts
+
+    async def get_default_account(self) -> Account:
+        """
+        Get consumer data response from IEC API.
+        :return: Customer data
+        """
+        accounts = await self.get_accounts()
+        return accounts[0]
+
+    async def get_default_contract(self, bp_number: str = None) -> Optional[Contract]:
+        """
+        This function retrieves the default contract based on the given BP number.
+        :param bp_number: A string representing the BP number
+        :return: Contract object containing the contract information
+        """
+
+        await self.check_token()
+
+        if not bp_number:
+            bp_number = self._bp_number
+
+        assert bp_number, "BP number must be provided"
+
+        get_contract_response = await data.get_contracts(self._session, self._token, bp_number)
+        if get_contract_response:
+            contracts = get_contract_response.contracts
+            if contracts and len(contracts) > 0:
+                self._contract_id = contracts[0].contract_id
+            return contracts[0]
+        return None
+
+    async def get_contracts(self, bp_number: str = None) -> list[Contract]:
+        """
+        This function retrieves a contract based on the given BP number.
+        :param bp_number: A string representing the BP number
+        :return: list of Contract objects
+        """
+
+        await self.check_token()
+
+        if not bp_number:
+            bp_number = self._bp_number
+
+        assert bp_number, "BP number must be provided"
+
+        get_contract_response = await data.get_contracts(self._session, self._token, bp_number)
+        if get_contract_response:
+            contracts = get_contract_response.contracts
+            if contracts and len(contracts) > 0:
+                self._contract_id = contracts[0].contract_id
+            return contracts
+        return []
+
+    async def get_last_meter_reading(
+        self, bp_number: Optional[str] = None, contract_id: Optional[str] = None
+    ) -> Optional[MeterReadings]:
+        """
+        Retrieves a last meter reading for a specific contract and user.
+        Args:
+            self: The instance of the class.
+            bp_number (str): The BP number of the meter.
+            contract_id (str): The contract ID associated with the meter.
+        Returns:
+            MeterReadings: The response containing the meter readings.
+        """
+        await self.check_token()
+        if not bp_number:
+            bp_number = self._bp_number
+
+        assert bp_number, "BP number must be provided"
+
+        if not contract_id:
+            contract_id = self._contract_id
+
+        assert contract_id, "Contract ID must be provided"
+
+        return await data.get_last_meter_reading(self._session, self._token, bp_number, contract_id)
+
+    async def get_electric_bill(
+        self, bp_number: Optional[str] = None, contract_id: Optional[str] = None
+    ) -> Optional[ElectricBill]:
+        """
+        Retrieves a remote reading for a specific meter using the provided parameters.
+        Args:
+            self: The instance of the class.
+            bp_number (str): The BP number of the meter.
+            contract_id (str): The contract ID associated with the meter.
+        Returns:
+            ElectricBill: The Invoices/Electric Bills for the user with the contract_id
+        """
+        await self.check_token()
+
+        if not bp_number:
+            bp_number = self._bp_number
+
+        assert bp_number, "BP number must be provided"
+
+        if not contract_id:
+            contract_id = self._contract_id
+
+        assert contract_id, "Contract ID must be provided"
+
+        return await data.get_electric_bill(self._session, self._token, bp_number, contract_id)
+
+    async def get_devices(self, contract_id: Optional[str] = None) -> Optional[list[Device]]:
+        """
+        Get a list of devices for the user
+        Args:
+            self: The instance of the class.
+            contract_id (str): The Contract ID of the meter.
+        Returns:
+            list[Device]: List of devices
+        """
+        await self.check_token()
+
+        if not contract_id:
+            contract_id = self._contract_id
+
+        assert contract_id, "Contract Id must be provided"
+
+        return await data.get_devices(self._session, self._token, contract_id)
+
+    async def get_device_by_device_id(self, device_id: str, contract_id: Optional[str] = None) -> Optional[Devices]:
+        """
+        Get a list of devices for the user
+        Args:
+            self: The instance of the class.
+            device_id (str): The Device code.
+            contract_id (str): The Contract ID of the user.
+        Returns:
+            list[Device]: List of devices
+        """
+        await self.check_token()
+
+        if not contract_id:
+            contract_id = self._contract_id
+
+        assert contract_id, "Contract ID must be provided"
+
+        return await data.get_device_by_device_id(self._session, self._token, contract_id, device_id)
+
+    async def get_remote_reading(
+        self,
+        meter_serial_number: str,
+        meter_code: int,
+        last_invoice_date: datetime,
+        from_date: datetime,
+        resolution: ReadingResolution = ReadingResolution.DAILY,
+        contract_id: Optional[str] = None,
+    ) -> Optional[RemoteReadingResponse]:
+        """
+        Retrieves a remote reading for a specific meter using the provided parameters.
+        Args:
+            self: The instance of the class.
+            meter_serial_number (str): The serial number of the meter.
+            meter_code (int): The code associated with the meter.
+            last_invoice_date (str): The date of the last invoice.
+            from_date (str): The start date for the remote reading.
+            resolution (int): The resolution of the remote reading.
+            contract_id (str): The contract id.
+        Returns:
+            RemoteReadingResponse: The response containing the remote reading or None if not found
+        """
+        await self.check_token()
+        if not contract_id:
+            contract_id = self._contract_id
+
+        return await data.get_remote_reading(
+            self._session,
+            self._token,
+            contract_id,
+            meter_serial_number,
+            meter_code,
+            last_invoice_date,
+            from_date,
+            resolution,
+        )
+
+    async def get_device_type(
+        self, bp_number: Optional[str] = None, contract_id: Optional[str] = None
+    ) -> Optional[DeviceType]:
+        """
+        Get a list of devices for the user
+        Args:
+            self: The instance of the class.
+            bp_number (str): The BP number of the meter.
+            contract_id (str: The Contract ID
+        Returns:
+            DeviceType
+        """
+        await self.check_token()
+
+        if not bp_number:
+            bp_number = self._bp_number
+
+        assert bp_number, "BP number must be provided"
+
+        if not contract_id:
+            contract_id = self._contract_id
+
+        assert contract_id, "Contract ID must be provided"
+
+        return await data.get_device_type(self._session, self._token, bp_number, contract_id)
+
+    async def get_billing_invoices(
+        self, bp_number: Optional[str] = None, contract_id: Optional[str] = None
+    ) -> Optional[GetInvoicesBody]:
+        """
+        Get a list of devices for the user
+        Args:
+            self: The instance of the class.
+            bp_number (str): The BP number of the meter.
+            contract_id (str: The Contract ID
+        Returns:
+            Billing Invoices data
+        """
+        await self.check_token()
+
+        if not bp_number:
+            bp_number = self._bp_number
+
+        assert bp_number, "BP number must be provided"
+
+        if not contract_id:
+            contract_id = self._contract_id
+
+        assert contract_id, "Contract ID must be provided"
+
+        return await data.get_billing_invoices(self._session, self._token, bp_number, contract_id)
+
+    async def get_kwh_tariff(self) -> float:
+        if not self._kwh_tariff or self._kwh_tariff_fetch_time < datetime.now() - timedelta(hours=1):
+            logger.debug("Tariff is missing or expired, fetching kwh tariff from IEC API")
+            self._kwh_tariff_fetch_time = datetime.now()
+            self._kwh_tariff = await data.get_kwh_tariff(self._session)
+
+        return self._kwh_tariff
+
+    # ----------------
+    # Login/Token Flow
+    # ----------------
+
+    async def login_with_id(self):
+        """
+        Login with ID and wait for OTP
+        """
+        state_token, factor_id, session_token = self._login_response = await login.first_login(
+            self._session, self._user_id
+        )
+        self._state_token = state_token
+        self._factor_id = factor_id
+        self._session_token = session_token
+
+    async def verify_otp(self, otp_code: str | int) -> bool:
+        """
+        Verify the OTP code and return the token
+        :param otp_code: The OTP code to be verified
+        :return: The token
+        """
+        jwt_token = await login.verify_otp_code(self._session, self._factor_id, self._state_token, str(otp_code))
+        self._token = jwt_token
+        self.logged_in = True
+        return True
+
+    async def manual_login(self):
+        """
+        Logs the user in by obtaining an authorization token, setting the authorization header,
+        and updating the login status and token attribute.
+        """
+        token = await login.manual_authorization(self._session, self._user_id)
+        self.logged_in = True
+        self._token = token
+
+    def get_token(self) -> JWT:
+        """
+        Return the JWT token.
+        """
+        return self._token
+
+    async def load_jwt_token(self, token: JWT):
+        """
+        Set the token and mark the user as logged in.
+        :param token: The new token to be set.
+        :return: None
+        """
+        self._token = token
+        if await self.check_token():
+            self.logged_in = True
+        else:
+            raise IECLoginError(-1, "Invalid JWT token")
+
+    async def override_id_token(self, id_token):
+        """
+        Set the token and mark the user as logged in.
+        :param id_token: The new token to be set.
+        :return: None
+        """
+        logger.debug(f"Overriding jwt.py token: {id_token}")
+        self._token = JWT(access_token="", refresh_token="", token_type="", expires_in=0, scope="", id_token=id_token)
+        self._token.id_token = id_token
+        self.logged_in = True
+
+    async def check_token(self) -> bool:
+        """
+        Check the validity of the jwt.py token and refresh in the case of expired signature errors.
+        """
+        should_refresh = False
+
+        try:
+            remaining_to_expiration = login.get_token_remaining_time_to_expiration(self._token)
+            if remaining_to_expiration < 0:
+                should_refresh = True
+
+        except jwt.exceptions.ExpiredSignatureError as e:
+            raise IECLoginError(-1, "Expired JWT token") from e
+
+        if should_refresh:
+            logger.debug("jwt.py token expired, refreshing token")
+            self.logged_in = False
+            await self.refresh_token()
+
+        return True
+
+    async def refresh_token(self):
+        """
+        Refresh IEC JWT token.
+        """
+        self._token = await login.refresh_token(self._session, self._token)
+        if self._token:
+            self.logged_in = True
+
+    async def load_token_from_file(self, file_path: str = "token.json"):
+        """
+        Load token from file.
+        """
+        self._token = await login.load_token_from_file(file_path)
+        self.logged_in = True
+
+    async def save_token_to_file(self, file_path: str = "token.json"):
+        """
+        Save token to file.
+        """
+        await login.save_token_to_file(self._token, file_path)
