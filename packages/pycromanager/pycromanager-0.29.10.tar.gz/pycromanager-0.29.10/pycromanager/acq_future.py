e565df0bfd50d05e13e9a34f18dd0ceac09db066
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-02 14:04:24 +0000

    Add package pycromanager/0.29.10/pycromanager-0.29.10.tar.gz

---
+++ packages/pycromanager/pycromanager-0.29.10.tar.gz/pycromanager-0.29.10/pycromanager/acq_future.py
@@ -0,0 +1,79 @@
+import threading
+from pycromanager.acquisition.acq_eng_py.main.acq_notification import AcqNotification
+
+def _axes_to_key(axes):
+    """ Turn axes into a hashable key """
+    return None if axes is None else frozenset(axes.items())
+
+class AcquisitionFuture:
+
+    def __init__(self, acq, axes_or_axes_list):
+        """
+        :param event_or_events: a single event (dictionary) or a list of events
+        """
+        self._acq = acq
+        self._condition = threading.Condition()
+        self._notification_recieved = {}
+        if isinstance(axes_or_axes_list, dict):
+            axes_or_axes_list = [axes_or_axes_list]
+        for axes in axes_or_axes_list:
+            # single event
+            # TODO maybe unify snap and sequence cause this is confusing
+            self._notification_recieved[_axes_to_key(axes)] = {
+                AcqNotification.Hardware.PRE_HARDWARE: False,
+                AcqNotification.Hardware.POST_HARDWARE: False,
+
+                AcqNotification.Camera.PRE_SNAP: False,
+                AcqNotification.Camera.PRE_SEQUENCE_STARTED: False,
+                AcqNotification.Camera.POST_EXPOSURE: False,
+
+                AcqNotification.Image.IMAGE_SAVED: False,
+            }
+
+
+    def _notify(self, notification):
+        """
+        Called by the internal notification dispatcher in order so that it can check off that the notification was
+        received. Want to store this, rather than just waiting around for it, in case the await methods are called
+        after the notification has already been sent.
+        """
+        if notification.phase == AcqNotification.Acquisition.ACQ_EVENTS_FINISHED or \
+            notification.phase == AcqNotification.Image.DATA_SINK_FINISHED:
+            return # ignore for now...
+        if isinstance(notification.id, list):
+            keys = [_axes_to_key(ax) for ax in notification.id]
+        else:
+            keys = [_axes_to_key(notification.id)]
+        # check if any keys are present in the notification_recieved dict
+        if not any([key in self._notification_recieved.keys() for key in keys]):
+            return # ignore notifications that aren't relevant to this future
+        for key in keys:
+            self._notification_recieved[key][notification.phase] = True
+        with self._condition:
+            self._condition.notify_all()
+
+    def await_execution(self, axes, phase):
+        key = _axes_to_key(axes)
+        if key not in self._notification_recieved.keys() or phase not in self._notification_recieved[key].keys():
+            notification = AcqNotification(None, axes, phase)
+            raise ValueError("this future is not expecting a notification for: " + str(notification.to_json()))
+        with self._condition:
+            while not self._notification_recieved[key][phase]:
+                self._condition.wait()
+
+    def await_image_saved(self, axes, return_image=False):
+        if isinstance(axes, list):
+            keys = [_axes_to_key(ax) for ax in axes]
+        else:
+            keys = [_axes_to_key(axes)]
+        for key in keys:
+            with self._condition:
+                while not self._notification_recieved[key][AcqNotification.Image.IMAGE_SAVED]:
+                    self._condition.wait()
+        if return_image:
+            if isinstance(axes, list):
+                return [self._acq.get_dataset().read_image(**ax) for ax in axes]
+            else:
+                return self._acq.get_dataset().read_image(**axes)
+
+
---
+++ packages/pycromanager/pycromanager-0.29.10.tar.gz/pycromanager-0.29.10/pycromanager/acquisition/acq_eng_py/main/acq_notification.py
@@ -0,0 +1,100 @@
+import json
+
+class AcqNotification:
+
+    class Acquisition:
+        ACQ_STARTED = "acq_started"
+        ACQ_EVENTS_FINISHED = "acq_events_finished"
+
+        @staticmethod
+        def to_string():
+            return "global"
+
+    class Hardware:
+        PRE_HARDWARE = "pre_hardware"
+        POST_HARDWARE = "post_hardware"
+
+        @staticmethod
+        def to_string():
+            return "hardware"
+
+    class Camera:
+        PRE_SEQUENCE_STARTED = "pre_sequence_started"
+        PRE_SNAP = "pre_snap"
+        POST_EXPOSURE = "post_exposure"
+
+        @staticmethod
+        def to_string():
+            return "camera"
+
+    class Image:
+        IMAGE_SAVED = "image_saved"
+        DATA_SINK_FINISHED = "data_sink_finished"
+
+        @staticmethod
+        def to_string():
+            return "image"
+
+    def __init__(self, type, id, phase=None):
+        if type == AcqNotification.Acquisition or type == AcqNotification.Acquisition.to_string():
+            self.type = AcqNotification.Acquisition
+            self.id = id
+            self.phase = phase
+        elif (type == AcqNotification.Image or type == AcqNotification.Image.to_string()) and \
+              phase == AcqNotification.Image.DATA_SINK_FINISHED:
+            self.type = AcqNotification.Image
+            self.id = id
+            self.phase = phase
+        elif phase in [AcqNotification.Camera.PRE_SNAP, AcqNotification.Camera.POST_EXPOSURE,
+                     AcqNotification.Camera.PRE_SEQUENCE_STARTED]:
+            self.type = AcqNotification.Camera
+            self.id = json.loads(id) if isinstance(id, str) else id # convert from '{'time': 5}' to {'time': 5}
+        elif phase in [AcqNotification.Hardware.PRE_HARDWARE, AcqNotification.Hardware.POST_HARDWARE]:
+            self.type = AcqNotification.Hardware
+            self.id = json.loads(id) if isinstance(id, str) else id # convert from '{'time': 5}' to {'time': 5}
+        elif phase == AcqNotification.Image.IMAGE_SAVED:
+            self.type = AcqNotification.Image
+            self.id = id
+        else:
+            raise ValueError("Unknown phase")
+        self.phase = phase
+
+
+    @staticmethod
+    def create_acq_events_finished_notification():
+       return AcqNotification(AcqNotification.Acquisition, None, AcqNotification.Acquisition.ACQ_EVENTS_FINISHED)
+
+    @staticmethod
+    def create_acq_started_notification():
+        return AcqNotification(AcqNotification.Acquisition, None, AcqNotification.Acquisition.ACQ_STARTED)
+
+    @staticmethod
+    def create_data_sink_finished_notification():
+        return AcqNotification(AcqNotification.Image, None, AcqNotification.Image.DATA_SINK_FINISHED)
+
+    @staticmethod
+    def create_image_saved_notification(image_descriptor):
+        return AcqNotification(AcqNotification.Image, image_descriptor, AcqNotification.Image.IMAGE_SAVED)
+
+    def to_json(self):
+        n = {}
+        n['type'] = self.type
+        n['phase'] = self.phase
+        if self.id:
+            n['id'] = self.id
+        return n
+
+    @staticmethod
+    def from_json(json):
+        return AcqNotification(json['type'],
+                               json['id'] if 'id' in json else None,
+                               json['phase'] if 'phase' in json else None)
+
+    def is_acquisition_finished_notification(self):
+        return self.phase == AcqNotification.Acquisition.ACQ_EVENTS_FINISHED
+
+    def is_data_sink_finished_notification(self):
+        return self.phase == AcqNotification.Image.DATA_SINK_FINISHED
+
+    def is_image_saved_notification(self):
+        return self.phase == AcqNotification.Image.IMAGE_SAVED
