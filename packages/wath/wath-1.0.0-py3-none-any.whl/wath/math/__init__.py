115c4184923831525f1a54005b82580ac6bfbdd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-02 16:05:21 +0000

    Add package wath/1.0.0/wath-1.0.0-py3-none-any.whl

---
+++ packages/wath/wath-1.0.0-py3-none-any.whl/wath/math/__init__.py
@@ -0,0 +1,4 @@
+from .bayes import bayesian_correction
+from .fit import (fit_circle, fit_cross_point, fit_pole, goodness_of_fit,
+                  lin_fit)
+from .signal.demodulate import getFTMatrix
