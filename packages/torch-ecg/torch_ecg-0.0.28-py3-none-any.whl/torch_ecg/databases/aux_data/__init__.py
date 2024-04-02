2613ce4a83ff2d927200117667b0048ee8f09d73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-02 15:04:18 +0000

    Add package torch-ecg/0.0.28/torch_ecg-0.0.28-py3-none-any.whl

---
+++ packages/torch-ecg/torch_ecg-0.0.28-py3-none-any.whl/torch_ecg/databases/aux_data/__init__.py
@@ -0,0 +1,23 @@
+"""
+"""
+
+from pathlib import Path
+
+import pandas as pd
+
+from . import aha, cinc2020_aux_data, cinc2021_aux_data
+
+__all__ = [
+    "cinc2020_aux_data",
+    "cinc2021_aux_data",
+    "get_physionet_dbs",
+    "aha",
+]
+
+
+def get_physionet_dbs() -> pd.DataFrame:
+    """
+    load the list of PhysioNet databases,
+    locally stored in the file "./physionet_dbs.csv.gz"
+    """
+    return pd.read_csv(Path(__file__).absolute().parent / "physionet_dbs.csv.gz")
