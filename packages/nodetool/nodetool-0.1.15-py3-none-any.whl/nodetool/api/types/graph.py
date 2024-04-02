ddfa1504d02caebd77dbcc9935841f0d51ad846f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-02 04:04:29 +0000

    Add package nodetool/0.1.15/nodetool-0.1.15-py3-none-any.whl

---
+++ packages/nodetool/nodetool-0.1.15-py3-none-any.whl/nodetool/api/types/graph.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel
+
+
+from typing import Any, List
+
+
+class Node(BaseModel):
+    id: str
+    type: str = "default"
+    data: Any = {}
+    ui_properties: Any = {}
+
+
+class Edge(BaseModel):
+    id: str | None = None
+    source: str
+    sourceHandle: str
+    target: str
+    targetHandle: str
+    ui_properties: dict[str, str] | None = None
+
+
+class Graph(BaseModel):
+    nodes: List[Node]
+    edges: List[Edge]
