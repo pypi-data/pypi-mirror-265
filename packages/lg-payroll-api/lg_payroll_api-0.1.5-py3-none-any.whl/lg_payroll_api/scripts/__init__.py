606eadd045d7ee7411290e3254ec360a622204a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-02 06:05:16 +0000

    Add package lg-payroll-api/0.1.5/lg_payroll_api-0.1.5-py3-none-any.whl

---
+++ packages/lg-payroll-api/lg_payroll_api-0.1.5-py3-none-any.whl/lg_payroll_api/scripts/__init__.py
@@ -0,0 +1,5 @@
+from lg_payroll_api.scripts.company import LgApiCompanyClient
+from lg_payroll_api.scripts.cost_center import LgApiCostCenterClient
+from lg_payroll_api.scripts.employee import LgApiEmployee
+from lg_payroll_api.scripts.employment_contract import LgApiEmploymentContract
+from lg_payroll_api.scripts.organizational_unit import LgApiOrganizationalUnitClient
