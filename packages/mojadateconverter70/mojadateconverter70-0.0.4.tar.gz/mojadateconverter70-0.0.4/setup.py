e61d1bc55212d74c6959039dff9e1aa264f02d1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-02 16:05:21 +0000

    Add package mojadateconverter70/0.0.4/mojadateconverter70-0.0.4.tar.gz

---
+++ packages/mojadateconverter70/mojadateconverter70-0.0.4.tar.gz/mojadateconverter70-0.0.4/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+
+
+
+setup(
+    name="mojadateconverter70",
+    version="0.0.4",
+    packages= find_packages(),
+    author="Mohammadali Jalili",
+    description="some functions related to date, with not much accuracy",
+    install_requires = [],
+  
+)
