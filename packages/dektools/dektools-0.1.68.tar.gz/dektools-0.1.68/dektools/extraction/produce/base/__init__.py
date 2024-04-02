fcefc579a6d8b8526ded9dc8e1927c953566d988
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-02 15:04:15 +0000

    Add package dektools/0.1.68/dektools-0.1.68.tar.gz

---
+++ packages/dektools/dektools-0.1.68.tar.gz/dektools-0.1.68/dektools/extraction/produce/base/__init__.py
@@ -0,0 +1,18 @@
+class ProduceBase:
+    @classmethod
+    def wrapper(cls, name=None):
+        def w(d):
+            return cls(d, name)
+
+        return w
+
+    def __init__(self, data, name=None):
+        self.data = data
+        self.name = name
+        self._broadcast()
+
+    def _broadcast(self):
+        for k in list(self.data):
+            v = self.data[k]
+            if hasattr(v, 'produce_notify'):
+                v.produce_notify(self, k)
