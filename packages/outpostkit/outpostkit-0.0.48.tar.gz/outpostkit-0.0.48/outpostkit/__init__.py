e5c6f6d6db1da0e88992a07585700e97505c9ff5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-02 03:03:41 +0000

    Add package outpostkit/0.0.48/outpostkit-0.0.48.tar.gz

---
+++ packages/outpostkit/outpostkit-0.0.48.tar.gz/outpostkit-0.0.48/outpostkit/__init__.py
@@ -0,0 +1,6 @@
+__version__ = "0.0.48"
+from outpostkit.client import Client as Client
+from outpostkit.endpoints import Endpoint as Endpoint
+from outpostkit.endpoints import Endpoints as Endpoints
+from outpostkit.team import Team as Team
+from outpostkit.user import User as User
