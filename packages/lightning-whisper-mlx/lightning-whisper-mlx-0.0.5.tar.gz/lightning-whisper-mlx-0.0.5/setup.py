fe2a4d174c1b75db830b1fad206a3362cf81239a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-02 16:05:08 +0000

    Add package lightning-whisper-mlx/0.0.5/lightning-whisper-mlx-0.0.5.tar.gz

---
+++ packages/lightning-whisper-mlx/lightning-whisper-mlx-0.0.5.tar.gz/lightning-whisper-mlx-0.0.5/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lightning-whisper-mlx',
+    version='0.0.5',
+    packages=find_packages(),
+    install_requires=[
+        'huggingface_hub',
+        "mlx",
+        "numba",
+        "numpy",
+        "torch",
+        "tqdm",
+        "more-itertools",
+        "tiktoken==0.3.3",
+        "scipy"
+    ],
+    entry_points={
+        'console_scripts': [
+            'lightning-whisper-mlx=lighting_whisper_mlx:main'
+        ]
+    }
+)
