2fb3fdad9acfacadddc4db3514d6253e0a85469a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-02 16:05:14 +0000

    Add package glasflow/0.3.1/glasflow-0.3.1.tar.gz

---
+++ packages/glasflow/glasflow-0.3.1.tar.gz/glasflow-0.3.1/tests/conftest.py
@@ -0,0 +1,10 @@
+"""
+General configuration for tests.
+"""
+
+import glasflow
+
+
+def pytest_sessionstart():
+    """Log which nflows backend is being using"""
+    print(f"glasflow config: USE_NFLOWS={glasflow.USE_NFLOWS}")
---
+++ packages/glasflow/glasflow-0.3.1.tar.gz/glasflow-0.3.1/tests/test_integration.py
@@ -0,0 +1,11 @@
+"""
+Overall integration tests.
+"""
+
+import pytest
+
+
+@pytest.mark.slow_integration_test
+def test_placeholder_slow_integration():
+    """Placeholder so that CI passes"""
+    pass
