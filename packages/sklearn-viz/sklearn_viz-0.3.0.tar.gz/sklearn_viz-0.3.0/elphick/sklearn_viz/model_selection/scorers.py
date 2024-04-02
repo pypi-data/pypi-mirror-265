fa0de359656faa00a8c59ec59775ac8672f9ea99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-02 03:03:50 +0000

    Add package sklearn-viz/0.3.0/sklearn_viz-0.3.0.tar.gz

---
+++ packages/sklearn-viz/sklearn_viz-0.3.0.tar.gz/sklearn_viz-0.3.0/elphick/sklearn_viz/model_selection/scorers.py
@@ -0,0 +1,7 @@
+classification_scorers = {'acc': 'accuracy',
+                          'prec_macro': 'precision_macro',
+                          'rec_macro': 'recall_macro'}
+
+regression_scorers = {'r2_score': 'r2',
+                      'mae': 'neg_mean_absolute_error',
+                      'mse': 'neg_mean_squared_error'}
