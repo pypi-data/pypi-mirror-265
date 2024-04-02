7f195ba734694f13146444827a5e7e1595289207
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-02 05:04:47 +0000

    Add package MobileInventoryCLI/0.4.32/MobileInventoryCLI-0.4.32.tar.gz

---
+++ packages/MobileInventoryCLI/MobileInventoryCLI-0.4.32.tar.gz/MobileInventoryCLI-0.4.32/MobileInventoryCLI/CodeProcessing/RecordCodesAndBarcodes/POS/POS.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import csv
+from datetime import datetime
+from pathlib import Path
+from colored import Fore,Style,Back
+from barcode import Code39,UPCA,EAN8,EAN13
+import barcode,qrcode,os,sys,argparse
+from datetime import datetime,timedelta,date
+import zipfile,tarfile
+import base64,json
+from ast import literal_eval
+import sqlalchemy
+from sqlalchemy import *
+from sqlalchemy.orm import *
+from sqlalchemy.ext.declarative import declarative_base as dbase
+from sqlalchemy.ext.automap import automap_base
+from pathlib import Path
+import upcean
+from MobileInventoryCLI.CodeProcessing.RecordCodesAndBarcodes.ExtractPkg.ExtractPkg2 import *
+from MobileInventoryCLI.CodeProcessing.RecordCodesAndBarcodes.Lookup.Lookup import *
+from MobileInventoryCLI.CodeProcessing.RecordCodesAndBarcodes.DayLog.DayLogger import *
+from MobileInventoryCLI.CodeProcessing.RecordCodesAndBarcodes.DB.db import *
+from MobileInventoryCLI.CodeProcessing.RecordCodesAndBarcodes.ConvertCode.ConvertCode import *
+from MobileInventoryCLI.CodeProcessing.RecordCodesAndBarcodes.setCode.setCode import *
+from MobileInventoryCLI.CodeProcessing.RecordCodesAndBarcodes.Locator.Locator import *
+from MobileInventoryCLI.CodeProcessing.RecordCodesAndBarcodes.ListMode2.ListMode2 import *
+from MobileInventoryCLI.CodeProcessing.RecordCodesAndBarcodes.TasksMode.Tasks import *
+from MobileInventoryCLI.CodeProcessing.RecordCodesAndBarcodes.ExportList.ExportListCurrent import *
+from MobileInventoryCLI.CodeProcessing.RecordCodesAndBarcodes.DB.Prompt import *
+
+
+import MobileInventoryCLI.CodeProcessing.RecordCodesAndBarcodes.possibleCode as pc
+
+class POS:
+	def __init__(self,engine,parent):
+		self.engine=engine
+		self.parent=parent
+
+		print("under dev!")
