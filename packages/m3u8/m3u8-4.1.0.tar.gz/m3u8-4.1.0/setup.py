171f808a5873e7cf5c843294321cc0e4402f3eb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-02 05:05:22 +0000

    Add package m3u8/4.1.0/m3u8-4.1.0.tar.gz

---
+++ packages/m3u8/m3u8-4.1.0.tar.gz/m3u8-4.1.0/setup.py
@@ -0,0 +1,27 @@
+from os.path import abspath, dirname, exists, join
+
+from setuptools import setup
+
+long_description = None
+if exists("README.rst"):
+    with open("README.rst") as file:
+        long_description = file.read()
+
+install_reqs = [
+    req for req in open(abspath(join(dirname(__file__), "requirements.txt")))
+]
+
+setup(
+    name="m3u8",
+    author="Globo.com",
+    version="4.1.0",
+    license="MIT",
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=install_reqs,
+    packages=["m3u8"],
+    url="https://github.com/globocom/m3u8",
+    description="Python m3u8 parser",
+    long_description=long_description,
+    python_requires=">=3.7",
+)
