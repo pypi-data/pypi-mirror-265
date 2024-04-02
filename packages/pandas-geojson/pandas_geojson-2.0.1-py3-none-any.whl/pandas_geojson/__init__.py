4c8220261ee5840993686a82faed7e069ba109cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-02 15:04:06 +0000

    Add package pandas-geojson/2.0.1/pandas_geojson-2.0.1-py3-none-any.whl

---
+++ packages/pandas-geojson/pandas_geojson-2.0.1-py3-none-any.whl/pandas_geojson/__init__.py
@@ -0,0 +1,22 @@
+__version__ = "2.0.1"
+
+from pandas_geojson.core import(
+    GeoJSON
+
+)
+
+
+from pandas_geojson.io import (
+    save_geojson,
+    read_geojson,
+    read_geojson_url,
+
+)
+
+__all__ =[
+    "GeoJSON",
+    "read_geojson",
+    "read_geojson_url",
+    "save_geojson",
+
+]
