d007d8281277a4a9f6b5d8a6e3f66ec04d580a08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-02 05:04:23 +0000

    Add package sajjad/1.1/sajjad-1.1.tar.gz

---
+++ packages/sajjad/sajjad-1.1.tar.gz/sajjad-1.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='sajjad',
+    version='1.1',
+    py_modules=['sajjad'],
+    description='A simple python library',
+    author='Your Name',
+    author_email='your.email@example.com',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
