9c54a5a552a254dcbc9700cfbb5354001e90147c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-02 14:04:43 +0000

    Add package testebiblioteca/1.0.0/testebiblioteca-1.0.0.tar.gz

---
+++ packages/testebiblioteca/testebiblioteca-1.0.0.tar.gz/testebiblioteca-1.0.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='testebiblioteca',
+    version='1.0.0',
+    packages=find_packages(),
+    install_requires=[
+        # Lista de dependências
+    ],
+    author='Alex',
+    author_email='abattialex1999@gmail.com',
+    description='Descrição da sua biblioteca',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        # Outros classificadores conforme necessário
+    ],
+)
