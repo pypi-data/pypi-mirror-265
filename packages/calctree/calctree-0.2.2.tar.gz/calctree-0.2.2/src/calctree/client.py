5105eb9e065b5bd01afdc48ac3b94d97fa4aef18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-02 14:05:07 +0000

    Add package calctree/0.2.2/calctree-0.2.2.tar.gz

---
+++ packages/calctree/calctree-0.2.2.tar.gz/calctree-0.2.2/src/calctree/client.py
@@ -0,0 +1,67 @@
+import json
+from urllib import request
+
+from calculation_result import CalculationResult
+
+
+class CalcTreeClient:
+    """Client for interacting with the CalcTree API.
+
+    This client allows you to perform calculations using the CalcTree API.
+
+    Args:
+        api_key (str): The API key for authentication.
+
+    Attributes:
+        _api_key (str): The API key for authentication.
+    """
+
+    def __init__(self, api_key: str):
+        self._api_key = api_key
+        self._calctree_host = 'https://api.calctree.com'
+        self._run_calculation_endpoint = '/api/calctree-cell/run-calculation'
+
+    def run_calculation(self, page_id: str, ct_cells, host: str = 'https://api.calctree.com') -> CalculationResult:
+        """Run a calculation using the CalcTree API.
+
+        Args:
+            page_id (str): The ID of the page containing the calculation.
+            ct_cells (List[Parameters]): A list of Parameters instances representing the calculation parameters.
+                Each Parameters instance has the following attributes:
+                    name (str): The parameter name, the same as on the page.
+                    formula (str): The value associated with the parameter.
+
+        Returns:
+            CalculationResult: The result of the calculation.
+        """  # noqa
+        calculation_request_response = self._request_calculation(ct_cells, page_id)
+        return CalculationResult(calculation_request_response)
+
+    def _request_calculation(self, ct_cells, page_id: str):
+        url = f'{self._calctree_host}{self._run_calculation_endpoint}'
+        headers = self._prepare_headers()
+        body = self._prepare_body(ct_cells, page_id)
+
+        payload = json.dumps(body).encode('utf-8')
+
+        req = request.Request(url, payload, headers)
+        with request.urlopen(req) as request_result:  # noqa
+            self._raise_exception_for_failed_request(request_result)
+            return request_result.read()
+
+    def _raise_exception_for_failed_request(self, request_result):
+        if not (200 <= request_result.status < 300):
+            error_message = request_result.read().decode('utf-8')
+            raise Exception(f'HTTP request failed with status code {request_result.status}: {error_message}')
+
+    def _prepare_body(self, ct_cells, page_id: str):
+        return {
+            'pageId': page_id,
+            'ctCells': ct_cells,
+        }
+
+    def _prepare_headers(self):
+        return {
+            'x-api-key': self._api_key,
+            'content-type': 'application/json',
+        }
