c225f9b9c27d5586c2778caf31429e14b08f6906
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-02 15:05:00 +0000

    Add package deadline-cloud-worker-agent/0.26.0/deadline_cloud_worker_agent-0.26.0.tar.gz

---
+++ packages/deadline-cloud-worker-agent/deadline_cloud_worker_agent-0.26.0.tar.gz/deadline_cloud_worker_agent-0.26.0/src/deadline_worker_agent/sessions/errors.py
@@ -0,0 +1,62 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+from __future__ import annotations
+
+from .._version import version
+
+from ..log_messages import SessionActionLogKind
+
+
+class CancelationError(Exception):
+    """Raised when there was an error trying to cancel a session action"""
+
+    pass
+
+
+class SessionActionError(Exception):
+    """Captures the action_id of an action that failed"""
+
+    action_id: str
+    action_log_kind: SessionActionLogKind
+    message: str
+
+    def __init__(self, action_id: str, action_log_kind: SessionActionLogKind, message: str):
+        super().__init__()
+        self.action_id = action_id
+        self.message = message
+
+    def __str__(self) -> str:
+        return self.message
+
+
+class EnvironmentDetailsError(SessionActionError):
+    """Raised when environment_details fails in an unrecoverable way"""
+
+    pass
+
+
+class JobAttachmentDetailsError(SessionActionError):
+    """Raised when job_attachments_details fails in an unrecoverable way"""
+
+    pass
+
+
+class StepDetailsError(SessionActionError):
+    """Raised when step_details fails in an unrecoverable way"""
+
+    pass
+
+
+class JobEntityUnsupportedSchemaError(SessionActionError):
+    """Raised when the worker agent does support the schema version
+    of a job entity that turns into a SessionAction"""
+
+    schema_version: str
+
+    def __init__(self, action_id: str, action_log_kind: SessionActionLogKind, schema_version: str):
+        self.schema_version = schema_version
+        self.message = (
+            f"Worker Agent: {version} does not support Open Job Description Schema Version {self.schema_version}. "
+            f"Consider upgrading to a newer Worker Agent."
+        )
+        super().__init__(action_id=action_id, action_log_kind=action_log_kind, message=self.message)
