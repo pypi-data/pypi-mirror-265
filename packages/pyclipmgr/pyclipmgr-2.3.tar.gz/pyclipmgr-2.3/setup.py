b5caf63f5268d0967a401bb99417d0569f2b09f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-02 15:04:46 +0000

    Add package pyclipmgr/2.3/pyclipmgr-2.3.tar.gz

---
+++ packages/pyclipmgr/pyclipmgr-2.3.tar.gz/pyclipmgr-2.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+from pyclipmgr import copy, paste
+
+setup(
+    name='pyclipmgr',
+    packages=find_packages(),
+    scripts=['pyclipmgr.py'],  # List of script files to be installed
+    version='2.3',
+    entry_points={
+        'console_scripts': [
+            'pyclipmgr-copy = pyclipmgr:copy',
+            'pyclipmgr-paste = pyclipmgr:paste',
+        ]
+    },
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='sudo man',
+    author_email='supertechman@yahoo.com',
+    description="this lib check for your machine's platform and use system commands to contact with clipboard (copy or paste) in python",
+    url='https://github.com/mohammed-saleh2007/py-clip-mgr',
+)
