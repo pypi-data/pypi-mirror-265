41c99d3fd034361565cfa771d4a4222fd4ffdb3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-02 06:04:36 +0000

    Add package sagemaker-schema-inference-artifacts/0.0.2/sagemaker_schema_inference_artifacts-0.0.2-py3-none-any.whl

---
+++ packages/sagemaker-schema-inference-artifacts/sagemaker_schema_inference_artifacts-0.0.2-py3-none-any.whl/sagemaker_schema_inference_artifacts/huggingface/config.py
@@ -0,0 +1,6 @@
+SUPPORTED_TASKS = ['question-answering']
+PATH_TO_HF_TASKS_NPM = 'huggingface/node_modules/@huggingface/tasks/src/tasks'
+SAMPLE_AUDIO = 'mp3.mp3'
+SAMPLE_IMAGE = 'png-truncated.png'
+PATH_TO_SAMPLE_AUDIO = 'huggingface/sample_data/audio'
+PATH_TO_SAMPLE_IMAGE = 'huggingface/sample_data/image'
