0a726f1866b3f02ed62fb9bb36e972f3306d354a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-02 06:04:57 +0000

    Add package secureli/0.33.0/secureli-0.33.0-py3-none-any.whl

---
+++ packages/secureli/secureli-0.33.0-py3-none-any.whl/secureli/settings.py
@@ -0,0 +1,59 @@
+from pathlib import Path
+from typing import Any
+
+import pydantic
+import yaml
+
+import secureli.repositories.secureli_config as SecureliConfig
+from secureli.repositories import repo_settings
+
+
+def secureli_yaml_settings(
+    settings: pydantic.BaseSettings,
+) -> dict[str, Any]:
+    """
+    Loads the .secureli.yaml file as the source for settings.
+    """
+
+    encoding = settings.__config__.env_file_encoding
+    path_to_settings = Path(SecureliConfig.FOLDER_PATH / ".secureli.yaml")
+    if not path_to_settings.exists():
+        return {}
+    with open(
+        path_to_settings,
+        "r",
+        encoding=encoding,
+    ) as f:
+        data = yaml.safe_load(f)
+        return data
+
+
+class Settings(pydantic.BaseSettings):
+    """
+    The settings loaded from a collection of data sources, but most notably
+    the .secureli.yaml file.
+    """
+
+    repo_files: repo_settings.RepoFilesSettings = repo_settings.RepoFilesSettings()
+    echo: repo_settings.EchoSettings = repo_settings.EchoSettings()
+    language_support: repo_settings.LanguageSupportSettings = (
+        repo_settings.LanguageSupportSettings()
+    )
+    telemetry: repo_settings.TelemetrySettings = repo_settings.TelemetrySettings()
+
+    class Config:
+        env_file_encoding = "utf-8"
+
+        @classmethod
+        def customise_sources(
+            cls,
+            init_settings,
+            env_settings,
+            file_secret_settings,
+        ):
+            return (
+                init_settings,
+                secureli_yaml_settings,
+                env_settings,
+                file_secret_settings,
+            )
