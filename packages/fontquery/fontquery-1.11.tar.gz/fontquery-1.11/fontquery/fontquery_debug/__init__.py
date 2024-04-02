404ac89572d8476755d7592b5741321025a13677
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-02 15:04:37 +0000

    Add package fontquery/1.11/fontquery-1.11.tar.gz

---
+++ packages/fontquery/fontquery-1.11.tar.gz/fontquery-1.11/fontquery/fontquery_debug/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (C) 2023 Red Hat, Inc.
+# SPDX-License-Identifier: MIT
+
+import os
+import sys
+
+sys.path.insert(0,
+                os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir)))
