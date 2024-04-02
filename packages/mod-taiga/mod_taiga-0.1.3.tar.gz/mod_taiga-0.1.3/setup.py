cbae7e3d46073d0244224abdf6f52ab6c87810d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-02 15:04:18 +0000

    Add package mod-taiga/0.1.3/mod_taiga-0.1.3.tar.gz

---
+++ packages/mod-taiga/mod_taiga-0.1.3.tar.gz/mod_taiga-0.1.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='mod_taiga',
+    packages=find_packages(),
+    version='0.1.3',
+    description='Taiga Library',
+    author='shikhaverma'
+)
