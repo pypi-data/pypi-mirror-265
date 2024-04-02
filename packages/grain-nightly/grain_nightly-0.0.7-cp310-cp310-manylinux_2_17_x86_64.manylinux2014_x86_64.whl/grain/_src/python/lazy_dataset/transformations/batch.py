a7e86672157f0730bf8b70f433061a62ed2adf80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-02 04:04:09 +0000

    Add package grain-nightly/0.0.7/grain_nightly-0.0.7-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/grain-nightly/grain_nightly-0.0.7-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/grain/_src/python/lazy_dataset/transformations/batch.py
@@ -0,0 +1,139 @@
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Implements batch transformations."""
+
+from collections.abc import Sequence
+import math
+from typing import TypeVar
+
+from grain._src.core import tree
+from grain._src.python.lazy_dataset import lazy_dataset
+import numpy as np
+
+T = TypeVar("T")
+
+
+def _make_batch(values: Sequence[T]) -> T:
+  num_values = len(values)
+  if num_values == 0:
+    return ()
+  elif num_values == 1:
+    return tree.map_structure(lambda x: np.expand_dims(x, axis=0), values[0])
+  else:
+    return tree.map_structure(lambda *xs: np.stack(xs), values[0], *values[1:])
+
+
+class _BatchLazyDatasetIterator(lazy_dataset.LazyDatasetIterator[T]):
+  """Iterator that batches elements."""
+
+  def __init__(
+      self,
+      parent: lazy_dataset.LazyDatasetIterator,
+      batch_size: int,
+      drop_remainder: bool,
+  ):
+    super().__init__()
+    self._parent = parent
+    self._batch_size = batch_size
+    self._drop_remainder = drop_remainder
+
+  def __next__(self):
+    values = []
+    for _ in range(self._batch_size):
+      try:
+        values.append(next(self._parent))
+      except StopIteration as e:
+        if self._drop_remainder:
+          raise e
+        break
+    if not values:
+      raise StopIteration
+    return _make_batch(values)
+
+  def get_state(self):
+    return self._parent.get_state()
+
+  def set_state(self, state):
+    self._parent.set_state(state)
+
+  def __str__(self) -> str:
+    return (
+        f"BatchLazyDatasetIterator(parent={self._parent},"
+        f" batch_size={self._batch_size},"
+        f" drop_remainder={self._drop_remainder})"
+    )
+
+
+@lazy_dataset.lazy_map_dataset_function("batch")
+class BatchLazyMapDataset(lazy_dataset.LazyMapDataset[T]):
+  """Batch transformation for non-sparse LazyMapDatasets."""
+
+  def __init__(
+      self,
+      parent: lazy_dataset.LazyMapDataset,
+      batch_size: int,
+      drop_remainder: bool = False,
+  ):
+    super().__init__(parent)
+    self._batch_size = batch_size
+    self._drop_remainder = drop_remainder
+    if self._drop_remainder:
+      self._length = len(self._parent) // self._batch_size
+    else:
+      self._length = math.ceil(len(self._parent) / self._batch_size)
+
+  def __len__(self):
+    return self._length
+
+  def __getitem__(self, index):
+    if isinstance(index, slice):
+      return self.slice(index)
+    start = index * self._batch_size
+    stop = min(len(self._parent), (index + 1) * self._batch_size)
+    values = [self._parent[i] for i in range(start, stop)]
+    return _make_batch(values)
+
+  def __str__(self) -> str:
+    return (
+        f"BatchMapLazyDataset(parent={self._parent},"
+        f" batch_size={self._batch_size})"
+    )
+
+
+@lazy_dataset.lazy_iter_dataset_function("batch")
+class BatchLazyIterDataset(lazy_dataset.LazyIterDataset[T]):
+  """Batch transformation for LazyIterDatasets."""
+
+  def __init__(
+      self,
+      parent: lazy_dataset.LazyIterDataset,
+      batch_size: int,
+      drop_remainder: bool = False,
+  ):
+    super().__init__(parent)
+    self._batch_size = batch_size
+    self._drop_remainder = drop_remainder
+
+  def __iter__(self) -> _BatchLazyDatasetIterator[T]:
+    parent_iter = self._parent.__iter__()
+    return _BatchLazyDatasetIterator(
+        parent_iter, self._batch_size, drop_remainder=self._drop_remainder
+    )
+
+  def __str__(self) -> str:
+    return (
+        f"BatchIterLazyDataset(parent={self._parent},"
+        f" batch_size={self._batch_size},"
+        f" drop_remainder={self._drop_remainder})"
+    )
---
+++ packages/grain-nightly/grain_nightly-0.0.7-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/grain/_src/python/lazy_dataset/transformations/map_test.py
@@ -0,0 +1,204 @@
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Tests for map transformation."""
+
+import dataclasses
+from absl.testing import absltest
+from absl.testing import parameterized
+import cloudpickle
+from grain._src.core import transforms
+from grain._src.python.lazy_dataset import lazy_dataset
+from grain._src.python.lazy_dataset.transformations import map as ldmap
+import numpy as np
+
+
+@dataclasses.dataclass(frozen=True)
+class MapWithNoTransform(transforms.MapTransform):
+
+  def map(self, element: int):
+    return element
+
+
+@dataclasses.dataclass(frozen=True)
+class MapWithTransform(transforms.MapTransform):
+
+  def map(self, element: int):
+    return element + 1
+
+
+@dataclasses.dataclass(frozen=True)
+class RandomMapWithTransform(transforms.RandomMapTransform):
+
+  def random_map(self, element: int, rng: np.random.Generator):
+    delta = 0.1
+    return element + rng.uniform(-delta, delta)
+
+
+@dataclasses.dataclass(frozen=True)
+class RandomMapWithDeterminismTransform(transforms.RandomMapTransform):
+
+  def random_map(self, element: int, rng: np.random.Generator):
+    return element + rng.integers(0, 10)
+
+
+@dataclasses.dataclass(frozen=True)
+class AddIndexTransform(transforms.MapWithIndexTransform):
+
+  def map_with_index(self, index: int, element: int):
+    return (index, element)
+
+
+class MapLazyMapDatasetTest(parameterized.TestCase):
+
+  def setUp(self):
+    super().setUp()
+    self.range_ds = lazy_dataset.RangeLazyMapDataset(0, 10)
+
+  def test_map_size(self):
+    map_ds_no_transform = ldmap.MapLazyMapDataset(
+        self.range_ds, MapWithNoTransform()
+    )
+    map_ds_with_transform = ldmap.MapLazyMapDataset(
+        self.range_ds, MapWithTransform()
+    )
+    map_ds_with_random_transform = ldmap.MapLazyMapDataset(
+        self.range_ds, RandomMapWithTransform(), seed=0
+    )
+    self.assertLen(map_ds_no_transform, len(self.range_ds))
+    self.assertLen(map_ds_with_transform, len(self.range_ds))
+    self.assertLen(map_ds_with_random_transform, len(self.range_ds))
+
+  @parameterized.parameters(
+      MapWithNoTransform,
+      MapWithTransform,
+      RandomMapWithTransform,
+  )
+  def test_map_picklable(self, map_cls):
+    ds = ldmap.MapLazyMapDataset(self.range_ds, map_cls(), seed=0)
+    ds = cloudpickle.loads(cloudpickle.dumps(ds))
+    self.assertLen(ds, len(self.range_ds))
+
+  def test_map_data_no_transform(self):
+    map_ds_no_transform = ldmap.MapLazyMapDataset(
+        self.range_ds, MapWithNoTransform()
+    )
+    expected_data = [i for i in range(10)]
+    actual_data = [
+        map_ds_no_transform[i] for i in range(len(map_ds_no_transform))
+    ]
+    self.assertEqual(expected_data, actual_data)
+
+  def test_map_data_with_transform(self):
+    map_ds_with_transform = ldmap.MapLazyMapDataset(
+        self.range_ds, MapWithTransform()
+    )
+    expected_data = [i + 1 for i in range(10)]
+    actual_data = [
+        map_ds_with_transform[i] for i in range(len(map_ds_with_transform))
+    ]
+    self.assertEqual(expected_data, actual_data)
+
+  def test_random_map_data_with_transform(self):
+    map_ds_with_random_transform = ldmap.MapLazyMapDataset(
+        self.range_ds, RandomMapWithTransform(), seed=0
+    )
+    expected_data = [_ for _ in range(10)]
+    actual_data = [
+        map_ds_with_random_transform[i]
+        for i in range(len(map_ds_with_random_transform))
+    ]
+    np.testing.assert_almost_equal(expected_data, actual_data, decimal=1)
+
+
+class MapLazyIterDatasetTest(absltest.TestCase):
+
+  def setUp(self):
+    super().setUp()
+    self.range_iter_ds = lazy_dataset.RangeLazyMapDataset(
+        0, 10
+    ).to_iter_dataset()
+
+  def test_map_data_no_transform(self):
+    map_no_transform_iter_ds = iter(
+        ldmap.MapLazyIterDataset(self.range_iter_ds, MapWithNoTransform())
+    )
+    expected_data = [_ for _ in range(10)]
+    actual_data = [next(map_no_transform_iter_ds) for _ in range(10)]
+    self.assertEqual(expected_data, actual_data)
+
+  def test_map_data_with_transform(self):
+    map_with_transform_iter_ds = iter(
+        ldmap.MapLazyIterDataset(self.range_iter_ds, MapWithTransform())
+    )
+    expected_data = [i + 1 for i in range(10)]
+    actual_data = [next(map_with_transform_iter_ds) for _ in range(10)]
+    self.assertEqual(expected_data, actual_data)
+
+  def test_random_map_data_with_transform(self):
+    map_with_random_transform_iter_ds = iter(
+        ldmap.MapLazyIterDataset(
+            self.range_iter_ds, RandomMapWithTransform(), seed=0
+        )
+    )
+    expected_data = [_ for _ in range(10)]
+    actual_data = [next(map_with_random_transform_iter_ds) for _ in range(10)]
+    np.testing.assert_almost_equal(expected_data, actual_data, decimal=1)
+
+  def test_random_map_data_with_transform_deterministic_with_seed(self):
+    expected_data = [9, 3, 6, 11, 4, 7, 9, 9, 8, 9]
+    # check if elements are reproducible for multiple runs
+    for _ in range(10):
+      map_with_random_transform_iter_ds = iter(
+          ldmap.MapLazyIterDataset(
+              self.range_iter_ds, RandomMapWithDeterminismTransform(), seed=42
+          )
+      )
+      actual_data = [next(map_with_random_transform_iter_ds) for _ in range(10)]
+      np.testing.assert_equal(expected_data, actual_data)
+
+  def test_map_past_one_epoch_raises_exception(self):
+    map_no_transform_iter_ds = iter(
+        ldmap.MapLazyIterDataset(self.range_iter_ds, MapWithNoTransform())
+    )
+    with self.assertRaises(StopIteration):
+      next(map_no_transform_iter_ds)
+      _ = [next(map_no_transform_iter_ds) for _ in range(20)]
+
+
+class MapWithIndexLazyMapDatasetTest(absltest.TestCase):
+
+  def setUp(self):
+    super().setUp()
+    self.range_ds = lazy_dataset.RangeLazyMapDataset(3, 6)
+
+  def test_length(self):
+    map_ds = ldmap.MapWithIndexLazyMapDataset(
+        self.range_ds, AddIndexTransform()
+    )
+    self.assertLen(map_ds, len(self.range_ds))
+
+  def test_getitem(self):
+    map_ds = ldmap.MapWithIndexLazyMapDataset(
+        self.range_ds, AddIndexTransform()
+    )
+    self.assertEqual(map_ds[0], (0, 3))
+    self.assertEqual(map_ds[1], (1, 4))
+    self.assertEqual(map_ds[2], (2, 5))
+    self.assertEqual(map_ds[3], (3, 3))
+    self.assertEqual(map_ds[4], (4, 4))
+    self.assertEqual(map_ds[5], (5, 5))
+
+
+if __name__ == "__main__":
+  absltest.main()
