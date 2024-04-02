86364202882386ca6a2808d11ac0ce53ab331977
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-02 04:04:54 +0000

    Add package RipeDB/0.3.6/RipeDB-0.3.6.tar.gz

---
+++ packages/RipeDB/RipeDB-0.3.6.tar.gz/RipeDB-0.3.6/ripedb/utils/helper.py
@@ -0,0 +1,15 @@
+def show_general_help():
+    help_text = """
+    RipeDB - A tool for performing queries and analysis on RipeDB
+
+    Usage:
+        ripedb [command]
+
+    Commands:
+        -q, --query   Executes a query on RipeDB.
+        -h, --help    Shows this help message or details about a specific command.
+        -em, --editing-mode Go to Editing Mode and perform DNS Search.
+
+    """
+    print(help_text)
+
