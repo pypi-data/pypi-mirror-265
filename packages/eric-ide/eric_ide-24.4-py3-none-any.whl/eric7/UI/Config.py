a796ab3f02207275f2a2928e3907db66a745d606
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-02 16:05:22 +0000

    Add package eric-ide/24.4/eric_ide-24.4-py3-none-any.whl

---
+++ packages/eric-ide/eric_ide-24.4-py3-none-any.whl/eric7/UI/Config.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2006 - 2024 Detlev Offenbach <detlev@die-offenbachs.de>
+#
+
+"""
+Module defining common data to be used by all windows..
+"""
+
+from PyQt6.QtCore import QSize
+
+ToolBarIconSize = QSize(20, 18)
