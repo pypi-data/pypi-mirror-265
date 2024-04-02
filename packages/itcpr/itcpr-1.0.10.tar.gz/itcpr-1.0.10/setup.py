ef283bc7abd479e7781d1656444ba390091230d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-02 16:04:44 +0000

    Add package itcpr/1.0.10/itcpr-1.0.10.tar.gz

---
+++ packages/itcpr/itcpr-1.0.10.tar.gz/itcpr-1.0.10/setup.py
@@ -0,0 +1,26 @@
+import pathlib
+from setuptools import setup
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="itcpr",
+    version="1.0.10",
+    description="Your tools for ITCPR",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ITCPR/itcpr-tools",
+    author="Md. Abdus Sami Akanda",
+    author_email="abdussamiakanda@gmail.com",
+    license="MIT",
+    classifiers=[
+    'Programming Language :: Python :: 3',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    ],
+    py_modules=["spintronics"],
+    package_dir={'': 'src'},
+    install_requires=['numpy', 'matplotlib', 'scipy', 'pandas', 'jupyterlab'],
+)
