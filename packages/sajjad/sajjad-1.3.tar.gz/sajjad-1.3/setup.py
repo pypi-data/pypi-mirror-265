28667d63ed7b865170b6e953f624d15a463144d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-02 05:04:01 +0000

    Add package sajjad/1.3/sajjad-1.3.tar.gz

---
+++ packages/sajjad/sajjad-1.3.tar.gz/sajjad-1.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sajjad',
+    version='1.3',
+    packages=find_packages(),
+    description='sajjad created that',
+    author='sajjad seyedi',
+    author_email='sajjad.seyedi.88@gmail.com',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
