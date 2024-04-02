c7d7f928d83e454f28edf86c81955076d1e1b686
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-02 04:04:25 +0000

    Add package RipeDB/0.3.5/RipeDB-0.3.5.tar.gz

---
+++ packages/RipeDB/RipeDB-0.3.5.tar.gz/RipeDB-0.3.5/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="RipeDB",
+    version="0.3.5",
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'pandas',
+        'openpyxl',
+    ],
+    entry_points={
+        'console_scripts': [
+            'ripedb=ripedb.main:main',
+        ],
+    },
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="APT-0-Blog",
+    author_email="cryptovortex@outlook.com",
+    description="A tool for making analyssis on RipeDB",
+    keywords="ripe ripedb",
+    url="https://github.com/apt-0/RipeDB",
+)
