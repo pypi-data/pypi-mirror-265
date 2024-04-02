ab380ebf747da641239044530c5959bc414ea653
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-02 15:04:40 +0000

    Add package tokengrams/0.1.0/tokengrams-0.1.0-cp310-cp310-manylinux_2_31_x86_64.whl

---
+++ packages/tokengrams/tokengrams-0.1.0-cp310-cp310-manylinux_2_31_x86_64.whl/tokengrams/__init__.py
@@ -0,0 +1,4 @@
+from .tokengrams import (
+    InMemoryIndex,
+    MemmapIndex,
+)
---
+++ packages/tokengrams/tokengrams-0.1.0-cp310-cp310-manylinux_2_31_x86_64.whl/tokengrams/tokengrams.pyi
@@ -0,0 +1,61 @@
+class InMemoryIndex:
+    """An n-gram index."""
+
+    def __init__(self, tokens: list[int]) -> None:
+        ...
+    
+    @staticmethod
+    def from_token_file(path: str, token_limit: int | None) -> "InMemoryIndex":
+        """Construct a `InMemoryIndex` from a file containing raw little-endian tokens."""
+
+    def contains(self, query: list[int]) -> bool:
+        """Check if `query` has nonzero count. Faster than `count(query) > 0`."""
+    
+    def count(self, query: list[int]) -> int:
+        """Count the number of occurrences of a query in the index."""
+
+    def batch_next_token_counts(self, query: list[list[int]], vocab: int | None) -> list[list[int]]:
+        """Count the occurrences of each token that directly follows each query sequence."""
+
+    def sample(self, query: list[int], n: int, k: int) -> list[int]:
+        """Autoregressively k characters from conditional distributions based 
+        on the previous (n - 1) characters (n-gram prefix) in the sequence.""" 
+    
+    def batch_sample(self, query: list[int], n: int, k: int, num_samples: int) -> list[list[int]]:
+        """Autoregressively samples num_samples of k characters each from conditional distributions based 
+        on the previous (n - 1) characters (n-gram prefix) in the sequence."""
+
+    def is_sorted(self) -> bool:
+        """Check if the suffix table is sorted lexicographically. 
+        This is always true for valid suffix tables."""
+
+class MemmapIndex:
+    """An n-gram index backed by a memory-mapped file."""
+
+    def __init__(self, token_file: str, index_file: str) -> None:
+        """Load a prebuilt memory-mapped index from a pair of files."""
+
+    @staticmethod
+    def build(token_file: str, index_file: str) -> "MemmapIndex":
+        """Build a memory-mapped index from a token file."""
+    
+    def contains(self, query: list[int]) -> bool:
+        """Check if `query` has nonzero count. Faster than `count(query) > 0`."""
+    
+    def count(self, query: list[int]) -> int:
+        """Count the number of occurrences of a query in the index."""
+
+    def batch_next_token_counts(self, query: list[list[int]], vocab: int | None) -> list[list[int]]:
+        """Count the occurrences of each token that directly follows each query sequence."""
+
+    def sample(self, query: list[int], n: int, k: int) -> list[int]:
+        """Autoregressively k characters from conditional distributions based 
+        on the previous (n - 1) characters (n-gram prefix) in the sequence.""" 
+   
+    def batch_sample(self, query: list[int], n: int, k: int, num_samples: int) -> list[list[int]]:
+        """Autoregressively samples num_samples of k characters each from conditional distributions based 
+        on the previous (n - 1) characters (n-gram prefix) in the sequence."""
+   
+    def is_sorted(self) -> bool:
+        """Check if the suffix table is lexicographically sorted. 
+        This is always true for valid suffix tables."""
