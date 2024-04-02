185567ab9f6bd8618c8f2811bccfa69f2cf3f4a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-02 06:05:14 +0000

    Add package CustomChat/1.0.4/CustomChat-1.0.4.tar.gz

---
+++ packages/CustomChat/CustomChat-1.0.4.tar.gz/CustomChat-1.0.4/CustomChat/main.py
@@ -0,0 +1,28 @@
+def run():
+    import CustomChat.app
+def change_name(name):
+    import os
+    file1 = open(os.path.join(os.path.dirname(__file__), "Name"), "w")
+    file1.write(name)
+    file1.close()
+def set_config():
+    import os
+    import CustomChat.configuration as config
+    command=input('Would you like the Command Line to be enabled? ')
+    if 'yes' in command.lower() or 'true' in command.lower():
+        command=True
+    else:
+        command=False
+    Web=input('Would you like to enable web scraping? ')
+    if 'yes' in Web.lower() or 'true' in Web.lower():
+        Web=True
+    else:
+        Web=False
+    Edit=input('Would you like to lock your edits? ')
+    if 'yes' in Edit.lower() or 'false' in Edit.lower():
+        Edit=False
+    else:
+        Edit=True
+    file1 = open(os.path.join(os.path.dirname(__file__), "configuration.py"), "w")
+    file1.write("Command_Line="+command+"\nWeb_Scrape="+Web+"\nEdit="+Edit+"")
+    file1.close()
