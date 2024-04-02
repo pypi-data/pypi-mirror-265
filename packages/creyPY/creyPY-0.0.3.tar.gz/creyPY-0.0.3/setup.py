0a648fbb247cc16399ab20c73df6f6c525129f36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-02 04:05:44 +0000

    Add package creyPY/0.0.3/creyPY-0.0.3.tar.gz

---
+++ packages/creyPY/creyPY-0.0.3.tar.gz/creyPY-0.0.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="creyPY",
+    version="0.0.3",
+    description="My collection of Python and FastAPI shortcuts etc.",
+    author="Conrad Großer",
+    author_email="conrad@noah.tech",
+    packages=find_packages(),
+    home_page="https://github.com/creyD/creyPY",
+    license="MIT",
+    python_requires=">=3.12",
+)
