337624ef2b5ed1050b90238156fbea484c4f60d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-02 15:04:42 +0000

    Add package genice2-svg/2.4.1/genice2_svg-2.4.1.tar.gz

---
+++ packages/genice2-svg/genice2_svg-2.4.1.tar.gz/genice2_svg-2.4.1/genice2_svg/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# even: stable; odd: develop
+__version__ = "2.4.1"
+
+# 2.4 with poetry
+# 2.3 for GenIce2.2
+
+# 1st: major version
+# 2nd: minor changes
+# 3rd: bug fix.
+
+# next bug fix will be 2.4.1
+# next version candidates will be 2.4-alpha.1
+# next version with some new features will be 2.4
