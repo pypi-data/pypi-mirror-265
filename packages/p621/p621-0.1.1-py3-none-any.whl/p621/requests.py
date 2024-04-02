4e93021955389e65b7a49b1635ff1b46a9933445
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-02 04:05:28 +0000

    Add package p621/0.1.1/p621-0.1.1-py3-none-any.whl

---
+++ packages/p621/p621-0.1.1-py3-none-any.whl/p621/requests.py
@@ -0,0 +1,42 @@
+from p621.posts import Post
+
+import requests
+from requests import Response
+
+USER_AGENT: str = 'p621/0.1.1'
+
+def search_posts(api_key: str, username: str, limit: int = None, tags: list[str] = None, page: int = None) -> list[Post]:
+    parameters: dict = {'api_key': api_key, 'login': username}
+    if limit:
+        parameters['limit'] = limit
+    if tags:
+        parameters['tags'] = ' '.join(tags)
+    if page:
+        parameters['page'] = page
+
+    response: Response = requests.get('https://e621.net/posts.json', params = parameters, headers = {'User-Agent': USER_AGENT})
+    
+    match response.status_code:
+        case 200:
+            posts: dict = response.json()['posts']
+            return [Post(post) for post in posts]
+        case 401:
+            raise Exception("failed with invalid authorization")
+        case status_code:
+            raise Exception("failed with status code: " + status_code)
+
+def list_favorites(api_key: str, username: str, user_id: int = None) -> list[Post]:
+    parameters: dict = {'api_key': api_key, 'login': username}
+    if user_id:
+        parameters['user_id'] = user_id
+
+    response: Response = requests.get('https://e621.net/favorites.json', params = parameters, headers = {'User-Agent': USER_AGENT})
+
+    match response.status_code:
+        case 200:
+            posts: dict = response.json()['posts']
+            return [Post(post) for post in posts]
+        case 401:
+            raise Exception("failed with invalid authorization")
+        case status_code:
+            raise Exception("failed with status code: " + status_code)
