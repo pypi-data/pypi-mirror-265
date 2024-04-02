de704d6a1822377fab3aff9d0c4297f75608d2ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-02 15:04:24 +0000

    Add package LedgePythonSdk/1.0.0/LedgePythonSdk-1.0.0.tar.gz

---
+++ packages/LedgePythonSdk/LedgePythonSdk-1.0.0.tar.gz/LedgePythonSdk-1.0.0/ledge-python-api-client/api/__init__.py
@@ -0,0 +1,19 @@
+# flake8: noqa
+
+# import apis into api package
+from ledge-python-api-client.api.case_reward_api import CaseRewardApi
+from ledge-python-api-client.api.draw_api import DrawApi
+from ledge-python-api-client.api.external_api import ExternalApi
+from ledge-python-api-client.api.games_api import GamesApi
+from ledge-python-api-client.api.guest_api import GuestApi
+from ledge-python-api-client.api.inventory_api import InventoryApi
+from ledge-python-api-client.api.leaderboard_api import LeaderboardApi
+from ledge-python-api-client.api.listings_api import ListingsApi
+from ledge-python-api-client.api.notification_api import NotificationApi
+from ledge-python-api-client.api.product_api import ProductApi
+from ledge-python-api-client.api.quests_api import QuestsApi
+from ledge-python-api-client.api.screens_api import ScreensApi
+from ledge-python-api-client.api.social_api import SocialApi
+from ledge-python-api-client.api.transaction_api import TransactionApi
+from ledge-python-api-client.api.user_api import UserApi
+
