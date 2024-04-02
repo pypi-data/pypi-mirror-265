9e76eb7c23a313fe5ce42561bd5eec60bb3a8c6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-02 15:05:10 +0000

    Add package fastapi-validation-i18n/0.4.1/fastapi_validation_i18n-0.4.1.tar.gz

---
+++ packages/fastapi-validation-i18n/fastapi_validation_i18n-0.4.1.tar.gz/fastapi_validation_i18n-0.4.1/fastapi_validation_i18n/handler.py
@@ -0,0 +1,20 @@
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
+from pydantic import ValidationError
+from starlette.requests import Request
+
+from ._helpers import translate_errors
+from .translator import Translator
+
+
+async def i18n_exception_handler(
+        r: Request, e: RequestValidationError | ValidationError) -> JSONResponse:
+    t = Translator(r.state.locale, locale_path=r.state.locale_path)
+    errors = translate_errors(t, e.errors())
+    return JSONResponse(
+        {'errors': errors},
+        status_code=422,
+    )
+
+
+__all__ = ['i18n_exception_handler']
