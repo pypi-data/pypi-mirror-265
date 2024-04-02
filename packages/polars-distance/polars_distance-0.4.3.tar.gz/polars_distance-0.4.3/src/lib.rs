84c48c67b4979f584200fccf9bd44a22a317a685
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-02 16:05:16 +0000

    Add package polars-distance/0.4.3/polars_distance-0.4.3.tar.gz

---
+++ packages/polars-distance/polars_distance-0.4.3.tar.gz/polars_distance-0.4.3/src/lib.rs
@@ -0,0 +1,20 @@
+mod array;
+mod expressions;
+mod list;
+mod other_dist;
+mod string;
+use pyo3::types::PyModule;
+use pyo3::{pymodule, PyResult, Python};
+
+#[cfg(target_os = "linux")]
+use jemallocator::Jemalloc;
+
+#[global_allocator]
+#[cfg(target_os = "linux")]
+static ALLOC: Jemalloc = Jemalloc;
+
+#[pymodule]
+fn _internal(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add("__version__", env!("CARGO_PKG_VERSION"))?;
+    Ok(())
+}
