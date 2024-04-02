a46a32855a640e8c0db2eaaa8c4fc4620ca3d46d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-02 16:05:13 +0000

    Add package excell-functions/0.6/excell_functions-0.6-py3-none-any.whl

---
+++ packages/excell-functions/excell_functions-0.6-py3-none-any.whl/excell_functions/__init__.py
@@ -0,0 +1,21 @@
+#exec(open('requirements.txt').read())
+
+import re
+import math
+import numpy as np
+import pandas as pd
+
+from excell_functions.compatibility import *
+from excell_functions.cube import *
+from excell_functions.database import *
+from excell_functions.datetime import *
+from excell_functions.engineering import *
+from excell_functions.financial import *
+from excell_functions.information import *
+from excell_functions.logical import *
+from excell_functions.lookup import *
+from excell_functions.math import *
+from excell_functions.statistical import *
+from excell_functions.text import *
+from excell_functions.users import *
+from excell_functions.web import *
---
+++ packages/excell-functions/excell_functions-0.6-py3-none-any.whl/excell_functions/lookup/__init__.py
@@ -0,0 +1,260 @@
+# lookup
+# ADDRESS function
+def ADDRESS():
+    '''Returns a reference as text to a single cell in a worksheet'''
+    pass
+    return
+
+
+# AREAS function
+def AREAS():
+    '''Returns the number of areas in a reference'''
+    pass
+    return
+
+
+# CHOOSE function
+def CHOOSE():
+    '''Chooses a value from a list of values'''
+    pass
+    return
+
+
+# CHOOSECOLS function
+def CHOOSECOLS():
+    '''Returns the specified columns from an array'''
+    pass
+    return
+
+
+# CHOOSEROWS function
+def CHOOSEROWS():
+    '''Returns the specified rows from an array'''
+    pass
+    return
+
+
+# COLUMN function
+def COLUMN():
+    '''Returns the column number of a reference'''
+    pass
+    return
+
+
+# COLUMNS function
+def COLUMNS():
+    '''Returns the number of columns in a reference'''
+    pass
+    return
+
+
+# DROP function
+def DROP():
+    '''Excludes a specified number of rows or columns from the start or end of an array'''
+    pass
+    return
+
+
+# EXPAND function
+def EXPAND():
+    '''Expands or pads an array to specified row and column dimensions'''
+    pass
+    return
+
+
+# FILTER function
+def FILTER():
+    '''Filters a range of data based on criteria you define'''
+    pass
+    return
+
+
+# FORMULATEXT function
+def FORMULATEXT():
+    '''Returns the formula at the given reference as text'''
+    pass
+    return
+
+
+# GETPIVOTDATA function
+def GETPIVOTDATA():
+    '''Returns data stored in a PivotTable report'''
+    pass
+    return
+
+
+# HLOOKUP function
+def HLOOKUP():
+    '''Looks in the top row of an array and returns the value of the indicated cell'''
+    pass
+    return
+
+
+# HSTACK function
+def HSTACK():
+    '''Appends arrays horizontally and in sequence to return a larger array'''
+    pass
+    return
+
+
+# HYPERLINK function
+def HYPERLINK():
+    '''Creates a shortcut or jump that opens a document stored on a network server, an intranet, or the Internet'''
+    pass
+    return
+
+
+# IMAGE function
+def IMAGE():
+    '''Returns an image from a given source'''
+    pass
+    return
+
+
+# INDEX function
+def INDEX():
+    '''Uses an index to choose a value from a reference or array'''
+    pass
+    return
+
+
+# INDIRECT function
+def INDIRECT():
+    '''Returns a reference indicated by a text value'''
+    pass
+    return
+
+
+# LOOKUP function
+def LOOKUP():
+    '''Looks up values in a vector or array'''
+    pass
+    return
+
+
+# MATCH function
+def MATCH():
+    '''Looks up values in a reference or array'''
+    pass
+    return
+
+
+# OFFSET function
+def OFFSET():
+    '''Returns a reference offset from a given reference'''
+    pass
+    return
+
+
+# ROW function
+def ROW():
+    '''Returns the row number of a reference'''
+    pass
+    return
+
+
+# ROWS function
+def ROWS():
+    '''Returns the number of rows in a reference'''
+    pass
+    return
+
+
+# RTD function
+def RTD():
+    '''Retrieves real-time data from a program that supports COM automation'''
+    pass
+    return
+
+
+# SORT function
+def SORT():
+    '''Sorts the contents of a range or array'''
+    pass
+    return
+
+
+# SORTBY function
+def SORTBY():
+    '''Sorts the contents of a range or array based on the values in a corresponding range or array'''
+    pass
+    return
+
+
+# TAKE function
+def TAKE():
+    '''Returns a specified number of contiguous rows or columns from the start or end of an array'''
+    pass
+    return
+
+
+# TOCOL function
+def TOCOL():
+    '''Returns the array in a single column'''
+    pass
+    return
+
+
+# TOROW function
+def TOROW():
+    '''Returns the array in a single row'''
+    pass
+    return
+
+
+# TRANSPOSE function
+def TRANSPOSE():
+    '''Returns the transpose of an array'''
+    pass
+    return
+
+
+# UNIQUE function
+def UNIQUE():
+    '''Returns a list of unique values in a list or range'''
+    pass
+    return
+
+
+# VLOOKUP function
+def VLOOKUP():
+    '''Looks in the first column of an array and moves across the row to return the value of a cell'''
+    pass
+    return
+
+
+# VSTACK function
+def VSTACK():
+    '''Appends arrays vertically and in sequence to return a larger array'''
+    pass
+    return
+
+
+# WRAPCOLS function
+def WRAPCOLS():
+    '''Wraps the provided row or column of values by columns after a specified number of elements'''
+    pass
+    return
+
+
+# WRAPROWS function
+def WRAPROWS():
+    '''Wraps the provided row or column of values by rows after a specified number of elements'''
+    pass
+    return
+
+
+# XLOOKUP function
+def XLOOKUP():
+    '''Searches a range or an array, and returns an item corresponding to the first match it finds. If a match doesn't exist, then XLOOKUP can return the closest (approximate) match.'''
+    pass
+    return
+
+
+# XMATCH function
+def XMATCH():
+    '''Returns the relative position of an item in an array or range of cells.'''
+    pass
+    return
+
+
