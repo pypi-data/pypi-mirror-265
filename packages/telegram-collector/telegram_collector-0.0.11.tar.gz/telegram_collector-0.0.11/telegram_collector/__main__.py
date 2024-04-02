06707c2b485e9588fa852eff43a64a3636d1abeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-02 16:05:15 +0000

    Add package telegram-collector/0.0.11/telegram_collector-0.0.11.tar.gz

---
+++ packages/telegram-collector/telegram_collector-0.0.11.tar.gz/telegram_collector-0.0.11/telegram_collector/__main__.py
@@ -0,0 +1,14 @@
+from .__init__ import TelegramCollector
+
+
+def send_current_message_src_to_dest():
+    collector = TelegramCollector()
+    collector.send_current_message_src_to_dest()
+
+
+def send_history_message_src_to_dest():
+    collector = TelegramCollector()
+    collector.send_history_message_src_to_dest()
+
+
+
