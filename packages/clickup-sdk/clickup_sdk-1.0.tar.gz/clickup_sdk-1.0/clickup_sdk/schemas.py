00984e920f31667d5bd3a506c3a867a6f6912c5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-02 05:05:13 +0000

    Add package clickup-sdk/1.0/clickup_sdk-1.0.tar.gz

---
+++ packages/clickup-sdk/clickup_sdk-1.0.tar.gz/clickup_sdk-1.0/clickup_sdk/schemas.py
@@ -0,0 +1,46 @@
+import datetime as dt
+
+from pydantic import BaseModel
+from pydantic.types import UUID4
+
+# Extras
+
+
+class DropDownOption(BaseModel):
+    id: UUID4
+    name: str
+    color: str
+    orderindex: int
+
+
+class DropDownTypeConfig(BaseModel):
+    default: int
+    options: list[DropDownOption]
+
+
+class CustomField(BaseModel):
+    name: str
+    value: str | float | dt.date | None
+
+
+class Task(BaseModel):
+    id: str
+    name: str
+    status: str
+    date_updated: dt.date | None
+    date_created: dt.date | None
+    custom_fields: list[CustomField]
+
+
+# Parameters
+class GetTasksParameters(BaseModel):
+    page: int
+    include_closed: bool
+
+
+# Payloads
+
+
+# Responses
+class GetTasksResponse(BaseModel):
+    tasks: list[Task]
