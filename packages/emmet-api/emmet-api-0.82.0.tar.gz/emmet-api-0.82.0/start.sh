9a6077867ba45b1f90b50f082ecb8b361ec34d6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-02 06:04:34 +0000

    Add package emmet-api/0.82.0/emmet-api-0.82.0.tar.gz

---
+++ packages/emmet-api/emmet-api-0.82.0.tar.gz/emmet-api-0.82.0/start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -x
+
+pmgrc=$HOME/.pmgrc.yaml
+[[ ! -e $pmgrc ]] && echo "PMG_DUMMY_VAR: dummy" > $pmgrc
+
+exec ddtrace-run gunicorn --statsd-host $DD_AGENT_HOST:8125 \
+    -b 0.0.0.0:$PORT -k uvicorn.workers.UvicornWorker -w $NUM_WORKERS \
+    --access-logfile - --error-logfile - $RELOAD \
+    --max-requests $MAX_REQUESTS --max-requests-jitter $MAX_REQUESTS_JITTER \
+    --timeout 120 \
+    app:app
