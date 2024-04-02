52543ffffdb3bc4617c0d14b940817670b87a3f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-02 05:04:36 +0000

    Add package nodestream/0.12.0rc1/nodestream-0.12.0rc1.tar.gz

---
+++ packages/nodestream/nodestream-0.12.0rc1.tar.gz/nodestream-0.12.0rc1/nodestream/project/target.py
@@ -0,0 +1,37 @@
+from dataclasses import dataclass
+from typing import Any, Dict
+
+from ..file_io import LazyLoadedArgument
+from ..schema.migrations import Migrator
+
+
+@dataclass(slots=True, frozen=True)
+class Target:
+    name: str
+    connector_config: Dict[str, Any]
+
+    @property
+    def resolved_connector_config(self):
+        return LazyLoadedArgument.resolve_if_needed(self.connector_config)
+
+    @property
+    def connector(self):
+        from ..databases import DatabaseConnector
+
+        return DatabaseConnector.from_database_args(**self.resolved_connector_config)
+
+    def make_writer(self, **writer_args):
+        from ..databases import GraphDatabaseWriter
+
+        return GraphDatabaseWriter.from_file_data(
+            **writer_args, **self.resolved_connector_config
+        )
+
+    def make_type_retriever(self):
+        return self.connector.make_type_retriever()
+
+    def make_migrator(self) -> "Migrator":
+        return self.connector.make_migrator()
+
+    def to_file_data(self):
+        return self.connector_config
