094081d910053dd1440afa39f79a76ab5a5464d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-02 04:04:13 +0000

    Add package EasyOutput/0.0.5/EasyOutput-0.0.5.tar.gz

---
+++ packages/EasyOutput/EasyOutput-0.0.5.tar.gz/EasyOutput-0.0.5/pkg/__init__.py
@@ -0,0 +1,10 @@
+from easyoutput import Success_Message
+from easyoutput import Successful_Connection_Message
+from easyoutput import Error_Message
+from easyoutput import Connection_Error_Message
+from easyoutput import Wait_Message
+from easyoutput import Note_Message
+from easyoutput import Title_Print
+from easyoutput import Info_Message
+
+__version__ = "0.0.5"
