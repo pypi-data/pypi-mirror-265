388b6f21c23b64d51b835bf9c2e71ba404a71ae1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-02 16:05:06 +0000

    Add package alohapy/0.1/alohapy-0.1-py3-none-any.whl

---
+++ packages/alohapy/alohapy-0.1-py3-none-any.whl/alohapy/__init__.py
@@ -0,0 +1,6 @@
+# __init__.py: tell python the whole directory here should be treated as a package
+from .main import aloha # this line is more like a function registry for letting developer to import o the function aloha and use it ~
+
+# when we use the package, how?
+# do the following import
+# from alohapy import aloha
