d81d6770a9316b2686479326742e674911f2cdee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-02 16:05:21 +0000

    Add package RTSAI/0.1.3/RTSAI-0.1.3.tar.gz

---
+++ packages/RTSAI/RTSAI-0.1.3.tar.gz/RTSAI-0.1.3/setup.py
@@ -0,0 +1,30 @@
+
+import os, setuptools
+from RTSAI.config import PACKAGE_NAME, PACKAGE_VERSION, DATA_PATH
+
+setuptools.setup (
+    name = PACKAGE_NAME, 
+    version = PACKAGE_VERSION, 
+    description = 'The recreated RTSAI package with pip install. ', 
+    long_description = 'The recreated RTSAI package with pip install. ', 
+    long_description_content_type = 'text/x-rst', 
+    author = 'wukunhuan', 
+    author_email = 'u3577163@connect.hku.hk', 
+    url = 'https://github.com/WuKunhuan/RTSAI', 
+    entry_points = {
+        'console_scripts': [
+            'RTSAI = RTSAI.main:main'
+        ]
+    }, 
+    project_urls = {
+        "Source": "https://github.com/WuKunhuan/RTSAI", 
+    }, 
+    python_requires=">=3.10.0", 
+    install_requires = [
+        # "kgtk-wukunhuan", 
+        # "appdirs", 
+        "pyautogui", 
+        # "pillow", 
+        # "pytest-shutil", 
+    ]
+)
