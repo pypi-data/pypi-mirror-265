a77690e236a1ca4173b18022d32b23431d79e97c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-02 16:05:23 +0000

    Add package eric-ide/24.4/eric-ide-24.4.tar.gz

---
+++ packages/eric-ide/eric-ide-24.4.tar.gz/eric-ide-24.4/src/eric7/UI/Info.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2002 - 2024 Detlev Offenbach <detlev@die-offenbachs.de>
+#
+
+"""
+Module defining some informational strings.
+"""
+
+
+Program = "eric7"
+Version = "24.4 (rev. e6c9ab1dc168)"
+VersionOnly = "24.4"
+Copyright = "Copyright (c) 2002 - 2024 Detlev Offenbach <detlev@die-offenbachs.de>"
+CopyrightShort = "(c) 2002 - 2024 Detlev Offenbach"
+BugAddress = "eric-bugs@eric-ide.python-projects.org"
+FeatureAddress = "eric-featurerequest@eric-ide.python-projects.org"
+Homepage = "https://eric-ide.python-projects.org/index.html"
