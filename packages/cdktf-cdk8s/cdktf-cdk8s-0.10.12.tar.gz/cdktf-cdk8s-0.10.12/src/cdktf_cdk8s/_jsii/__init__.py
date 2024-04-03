bee3e0d298b75e691a66d442ed49741e0b1d985e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-03 03:05:41 +0000

    Add package cdktf-cdk8s/0.10.12/cdktf-cdk8s-0.10.12.tar.gz

---
+++ packages/cdktf-cdk8s/cdktf-cdk8s-0.10.12.tar.gz/cdktf-cdk8s-0.10.12/src/cdktf_cdk8s/_jsii/__init__.py
@@ -0,0 +1,26 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from typeguard import check_type
+
+import cdk8s._jsii
+import cdktf._jsii
+import cdktf_cdktf_provider_kubernetes._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "cdktf-cdk8s", "0.10.12", __name__[0:-6], "cdktf-cdk8s@0.10.12.jsii.tgz"
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
