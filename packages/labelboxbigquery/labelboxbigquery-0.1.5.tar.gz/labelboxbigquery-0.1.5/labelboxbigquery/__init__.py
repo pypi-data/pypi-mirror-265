bb8b082a436d40a0673e4e34317272aa5c92c3fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-03 04:05:39 +0000

    Add package labelboxbigquery/0.1.5/labelboxbigquery-0.1.5.tar.gz

---
+++ packages/labelboxbigquery/labelboxbigquery-0.1.5.tar.gz/labelboxbigquery-0.1.5/labelboxbigquery/__init__.py
@@ -0,0 +1,1 @@
+from labelboxbigquery.client import Client
---
+++ packages/labelboxbigquery/labelboxbigquery-0.1.5.tar.gz/labelboxbigquery-0.1.5/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="labelboxbigquery",
+    version="0.1.05",
+    author="Labelbox",
+    author_email="raphael@labelbox.com",
+    description="Labelbox Connector for BigQuery",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://labelbox.com",
+    packages=setuptools.find_packages(),
+    install_requires=["labelbox", "google-cloud-bigquery", "labelbase"],
+    keywords=["labelbox", "bigquery", "labelboxbigquery", "labelbase"],
+)
