51c6a9a7d783dc4e8d70e52f66f1f53417f167f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-03 06:06:04 +0000

    Add package bughunter9/9.23/bughunter9-9.23.tar.gz

---
+++ packages/bughunter9/bughunter9-9.23.tar.gz/bughunter9-9.23/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='bughunter9',
+    version='9.23',
+    py_modules=['bughunter9'],
+    entry_points={
+        'console_scripts': [
+            'bughunter9 = bughunter9:main',
+        ],
+    },
+    install_requires=[],
+)
