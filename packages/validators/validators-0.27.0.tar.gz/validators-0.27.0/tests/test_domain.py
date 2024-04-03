d497ee1e83e7623abce717131e2e26f9a1a51718
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-03 16:05:17 +0000

    Add package validators/0.27.0/validators-0.27.0.tar.gz

---
+++ packages/validators/validators-0.27.0.tar.gz/validators-0.27.0/tests/test_domain.py
@@ -0,0 +1,57 @@
+"""Test Domain."""
+
+# external
+import pytest
+
+# local
+from validators import ValidationError, domain
+
+
+@pytest.mark.parametrize(
+    ("value", "rfc_1034", "rfc_2782"),
+    [
+        ("example.com", False, False),
+        ("exa_mple.com", False, True),
+        ("xn----gtbspbbmkef.xn--p1ai", False, False),
+        ("underscore_subdomain.example.com", False, True),
+        ("something.versicherung", False, False),
+        ("someThing.versicherung.", True, False),
+        ("11.com", False, False),
+        ("3.cn.", True, False),
+        ("_example.com", False, True),
+        ("example_.com", False, True),
+        ("a.cn", False, False),
+        ("sub1.sub2.sample.co.uk", False, False),
+        ("somerandomexample.xn--fiqs8s", False, False),
+        ("kräuter.com.", True, False),
+        ("über.com", False, False),
+    ],
+)
+def test_returns_true_on_valid_domain(value: str, rfc_1034: bool, rfc_2782: bool):
+    """Test returns true on valid domain."""
+    assert domain(value, rfc_1034=rfc_1034, rfc_2782=rfc_2782)
+
+
+@pytest.mark.parametrize(
+    ("value", "rfc_1034", "rfc_2782"),
+    [
+        ("example.com/.", True, False),
+        ("example.com:4444", False, False),
+        ("example.-com", False, False),
+        ("example.", False, False),
+        ("-example.com", False, False),
+        ("example-.com.", True, False),
+        ("_example.com", False, False),
+        ("_example._com", False, False),
+        ("example_.com", False, False),
+        ("example", False, False),
+        ("a......b.com", False, False),
+        ("a.123", False, False),
+        ("123.123", False, False),
+        ("123.123.123.", True, False),
+        ("123.123.123.123", False, False),
+    ],
+)
+def test_returns_failed_validation_on_invalid_domain(value: str, rfc_1034: bool, rfc_2782: bool):
+    """Test returns failed validation on invalid domain."""
+    assert isinstance(domain(value, rfc_1034=rfc_1034, rfc_2782=rfc_2782), ValidationError)
