83c8dbfdb95f4aed5a80045c7a8df8fe97d6fb53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-03 06:06:05 +0000

    Add package edx-organizations/6.13.0/edx-organizations-6.13.0.tar.gz

---
+++ packages/edx-organizations/edx-organizations-6.13.0.tar.gz/edx-organizations-6.13.0/organizations/__init__.py
@@ -0,0 +1,4 @@
+"""
+edx-organizations app initialization module
+"""
+__version__ = '6.13.0'  # pragma: no cover
---
+++ packages/edx-organizations/edx-organizations-6.13.0.tar.gz/edx-organizations-6.13.0/setup.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+import os
+import re
+
+from setuptools import find_packages, setup
+
+
+def load_requirements(*requirements_paths):
+    """
+    Load all requirements from the specified requirements files.
+    Returns a list of requirement strings.
+    """
+    requirements = set()
+    for path in requirements_paths:
+        with open(path) as reqs:
+            requirements.update(
+                line.split('#')[0].strip() for line in reqs
+                if is_requirement(line.strip())
+            )
+    return list(requirements)
+
+
+def is_requirement(line):
+    """
+    Return True if the requirement line is a package requirement;
+    that is, it is not blank, a comment, a URL, or an included file.
+    """
+    return line and not line.startswith(('-r', '#', '-e', 'git+', '-c'))
+
+
+def get_version(*file_paths):
+    """
+    Extract the version string from the file at the given relative path fragments.
+    """
+    filename = os.path.join(os.path.dirname(__file__), *file_paths)
+    with open(filename, encoding='utf-8') as opened_file:
+        version_file = opened_file.read()
+        version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                                  version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError('Unable to find version string.')
+
+
+VERSION = get_version("organizations", "__init__.py")
+
+
+setup(
+    name='edx-organizations',
+    version=VERSION,
+    description='Organization management module for Open edX',
+    long_description=open('README.rst').read(),
+    author='edX',
+    url='https://github.com/openedx/edx-organizations',
+    license='AGPL',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 4.2',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+    ],
+    packages=find_packages(exclude=['tests']),
+    install_requires=load_requirements('requirements/base.in'),
+)
