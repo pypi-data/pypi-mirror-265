caec0c369694dbb14839745452d221bf28de8d8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-03 17:05:32 +0000

    Add package cellacdc/1.4.27/cellacdc-1.4.27-py3-none-any.whl

---
+++ packages/cellacdc/cellacdc-1.4.27-py3-none-any.whl/cellacdc/urls.py
@@ -0,0 +1,19 @@
+contribute_url = 'https://github.com/SchmollerLab/Cell_ACDC/blob/main/cellacdc/docs/source/contributing.rst'
+
+github_url = 'https://github.com/SchmollerLab/Cell_ACDC'
+
+issues_url = 'https://github.com/SchmollerLab/Cell_ACDC/issues'
+
+forum_url = 'https://github.com/SchmollerLab/Cell_ACDC/discussions'
+
+resources_url = 'https://github.com/SchmollerLab/Cell_ACDC#resources'
+
+my_contact_url = 'https://www.helmholtz-munich.de/ife/about-us/people/staff-detail/ma/8873/Dr.-Padovani/index.html'
+
+user_manual_url = 'https://github.com/SchmollerLab/Cell_ACDC/blob/main/UserManual/Cell-ACDC_User_Manual.pdf'
+
+cite_url = 'https://bmcbiol.biomedcentral.com/articles/10.1186/s12915-022-01372-6'
+
+dataprep_docs = 'https://cell-acdc.readthedocs.io/en/latest/getting-started.html#preparing-data-for-further-analysis-data-prep'
+
+docs_homepage = 'https://cell-acdc.readthedocs.io/en/latest'
