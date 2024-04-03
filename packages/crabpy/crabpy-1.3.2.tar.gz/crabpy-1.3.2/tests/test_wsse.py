3ed93fbd4a987eb5e4d8604309aed3eba2e80195
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-03 03:05:08 +0000

    Add package crabpy/1.3.2/crabpy-1.3.2.tar.gz

---
+++ packages/crabpy/crabpy-1.3.2.tar.gz/crabpy-1.3.2/tests/test_wsse.py
@@ -0,0 +1,38 @@
+import unittest
+
+from crabpy.wsse import UsernameDigestToken
+
+from base64 import b64encode
+
+from suds.wsse import wssens
+
+from suds.sax.element import Element
+
+
+class UsernameDigestTokenTests(unittest.TestCase):
+    def setUp(self):
+        self.token = UsernameDigestToken("myself", "mypassword")
+
+    def tearDown(self):
+        self.token = None
+
+    def test_simple(self):
+        self.assertIsInstance(self.token, UsernameDigestToken)
+        xml = self.token.xml()
+        self.assertIsInstance(xml, Element)
+        self.assertIsInstance(xml.getChild("Username", ns=wssens), Element)
+        self.assertIsInstance(xml.getChild("Password", ns=wssens), Element)
+        self.assertIsInstance(xml.getChild("Nonce", ns=wssens), Element)
+
+    def test_set_custom_nonce(self):
+        self.assertIsInstance(self.token, UsernameDigestToken)
+        self.token.setnonce(b"NONCE")
+        xml = self.token.xml()
+        self.assertIsInstance(xml, Element)
+        self.assertIsInstance(xml.getChild("Username", ns=wssens), Element)
+        self.assertIsInstance(xml.getChild("Password", ns=wssens), Element)
+        self.assertIsInstance(xml.getChild("Nonce", ns=wssens), Element)
+        self.assertEqual(
+            xml.getChild("Nonce", ns=wssens).getText(),
+            b64encode(b"NONCE").decode("utf-8"),
+        )
