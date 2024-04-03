18da3017928f245bd8d2757af35b3154975297b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-03 04:05:57 +0000

    Add package booookscore/0.1.2/booookscore-0.1.2.tar.gz

---
+++ packages/booookscore/booookscore-0.1.2.tar.gz/booookscore-0.1.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='booookscore',
+    version='0.1.2',
+    description='Official package for our ICLR 2024 paper, "BooookScore: A systematic exploration of book-length summarization in the era of LLMs".',
+    author='Yapei Chang',
+    author_email='yapeichang@umass.edu',
+    url='https://github.com/lilakk/BooookScore',
+    packages=find_packages(),
+    install_requires=[
+        # List your package dependencies here
+    ],
+    # Additional package metadata
+)
