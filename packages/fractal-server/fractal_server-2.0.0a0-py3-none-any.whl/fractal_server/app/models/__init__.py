9146b0d469208e70d7c7de2fcfaff024794d5e20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-03 16:05:24 +0000

    Add package fractal-server/2.0.0a0/fractal_server-2.0.0a0-py3-none-any.whl

---
+++ packages/fractal-server/fractal_server-2.0.0a0-py3-none-any.whl/fractal_server/app/models/__init__.py
@@ -0,0 +1,8 @@
+"""
+`models` module
+"""
+from ..schemas.v1 import *  # noqa F401, F403  # FIXME: remove this
+from .security import *  # noqa: F401, F403
+from .state import State  # noqa: F401
+from .v1 import *  # noqa: F401, F403
+from .v2 import *  # noqa: F401, F403
