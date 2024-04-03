9d9b2e0bb5b3c8a5653118276bdfdefcd09c3aaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-03 17:05:37 +0000

    Add package qtlink/1.0.0/qtlink-1.0.0.tar.gz

---
+++ packages/qtlink/qtlink-1.0.0.tar.gz/qtlink-1.0.0/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+setuptools.setup(
+    name="qtlink",
+    version="1.0.0",
+    author="NanHaiLoong",
+    author_email="nanhai@163.com",
+    description="a ui framework based on pyside6",
+    long_description='a ui framework based on pyside6',
+    long_description_content_type="text/markdown",
+    url="https://gitee.com/darlingxyz/qtlink",
+    packages=['qtlink'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
+
+
+
