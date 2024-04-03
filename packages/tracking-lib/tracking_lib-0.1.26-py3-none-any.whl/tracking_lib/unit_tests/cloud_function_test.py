957ef9997a5749a6e39912272a34bd5458bcd7a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-03 16:05:22 +0000

    Add package tracking-lib/0.1.26/tracking_lib-0.1.26-py3-none-any.whl

---
+++ packages/tracking-lib/tracking_lib-0.1.26-py3-none-any.whl/tracking_lib/unit_tests/cloud_function_test.py
@@ -0,0 +1,57 @@
+import os
+import sys
+import subprocess
+import time
+import pytest
+import requests
+from tracking_lib.transaction import Transaction
+
+# Get the current directory
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Get the parent directory
+parent_dir = os.path.dirname(current_dir)
+
+# Add the parent directory to PATH
+sys.path.append(parent_dir)
+
+
+def interactor_runc():
+    # print(f"transaction_id in interactor func: {Transaction.read_transaction_id()}")
+    return Transaction.read_transaction_id()
+
+
+def hello_world(request):
+    request_args = request.args
+    print(request_args)
+    Transaction.set_transaction_id("123456")
+    tr_id = interactor_runc()
+    return {"message": f"{tr_id}"}
+
+
+@pytest.fixture(scope="module")
+def server():
+    # Start the server
+    server = subprocess.Popen(
+        [
+            "functions-framework",
+            "--target=hello_world",
+            f"--source={current_dir}/cloud_function_test.py",
+            "--port=8000",
+        ]
+    )
+    time.sleep(5)  # Wait for the server to start
+    yield server
+    # Teardown : Stop the server
+    server.terminate()
+
+
+def test_server(server):
+    # Send a request to the server
+    response = requests.get("http://0.0.0.0:8000")
+    print(response.json())
+    # Check that the server responded with status code 200
+    assert response.status_code == 200
+
+    # Check the content of the response
+    assert response.json() == {"message": "123456"}
