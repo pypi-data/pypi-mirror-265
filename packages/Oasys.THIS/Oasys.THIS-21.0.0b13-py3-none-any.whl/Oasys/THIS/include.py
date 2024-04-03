e3bd7062cf919027de6a07c6d57e1428763dfaf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-03 03:05:52 +0000

    Add package Oasys.THIS/21.0.0b13/Oasys.THIS-21.0.0b13-py3-none-any.whl

---
+++ packages/Oasys.THIS/Oasys.THIS-21.0.0b13-py3-none-any.whl/Oasys/THIS/include.py
@@ -0,0 +1,48 @@
+import Oasys.gRPC
+
+
+# Metaclass for static properties and constants
+class IncludeType(type):
+    _consts = {'NATIVE', 'UNIX', 'WINDOWS'}
+
+    def __getattr__(cls, name):
+        if name in IncludeType._consts:
+            return Oasys.THIS._connection.classGetter(cls.__name__, name)
+
+        raise AttributeError("Include class attribute '{}' does not exist".format(name))
+
+
+    def __setattr__(cls, name, value):
+# If one of the constants we define then error
+        if name in IncludeType._consts:
+            raise AttributeError("Cannot set Include class constant '{}'".format(name))
+
+# Set the property locally
+        cls.__dict__[name] = value
+
+
+class Include(Oasys.gRPC.OasysItem, metaclass=IncludeType):
+
+
+    def __del__(self):
+        if not Oasys.THIS._connection:
+            return
+
+        if self._handle is None:
+            return
+
+        Oasys.THIS._connection.destructor(self.__class__.__name__, self._handle)
+
+
+    def __getattr__(self, name):
+# If constructor for an item fails in program, then _handle will not be set and when
+# __del__ is called to return the object we will call this to get the (undefined) value
+        if name == "_handle":
+            return None
+
+        raise AttributeError("Include instance attribute '{}' does not exist".format(name))
+
+
+    def __setattr__(self, name, value):
+# Set the property locally
+        self.__dict__[name] = value
