8d8c33a7ff76f6403f5805547544c94324f2a7f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-03 14:05:19 +0000

    Add package biothings/0.12.4/biothings-0.12.4.tar.gz

---
+++ packages/biothings/biothings-0.12.4.tar.gz/biothings-0.12.4/biothings/cli/__init__.py
@@ -0,0 +1,113 @@
+import importlib
+import importlib.util
+import logging
+import os
+import pathlib
+import sys
+
+try:
+    import typer
+    from rich.logging import RichHandler
+
+    typer_avail = True
+except ImportError:
+    typer_avail = False
+
+from biothings.utils.common import DummyConfig
+from biothings.utils.configuration import ConfigurationError
+
+CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"])
+
+if typer_avail:
+    # prevent dimming the help text from the 2nd line
+    # see: https://github.com/tiangolo/typer/issues/437#issuecomment-1224149402
+    typer.rich_utils.STYLE_HELPTEXT = ""
+
+    # Typer already support an env variable to disable rich tracebacks using _TYPER_STANDARD_TRACEBACK=1
+    # This supports a similar BTCLI_RICH_TRACEBACK=1 env variable to turn it on when default is off in our case
+    # Relevant ref: https://github.com/tiangolo/typer/issues/525 and https://github.com/tiangolo/typer/discussions/612
+    # and BTCLI_DEBUG=1 env varible to turn on both rich tracebacks and show locals
+    if os.environ.get("BTCLI_DEBUG"):
+        pretty_exceptions_enable = True
+        pretty_exceptions_show_locals = True
+        default_logging_level = logging.DEBUG
+    else:
+        if os.environ.get("BTCLI_RICH_TRACEBACK"):
+            pretty_exceptions_enable = True
+        else:
+            pretty_exceptions_enable = False
+            sys.tracebacklimit = 1  # only show the last traceback, default is 1000
+        pretty_exceptions_show_locals = False
+        default_logging_level = logging.INFO
+
+    cli = typer.Typer(
+        help="[green]BioThings Admin CLI to test your local data plugins. See helps for each command for specific usage.[/green]",
+        rich_help_panel="Help and Others",
+        rich_markup_mode="rich",
+        context_settings=CONTEXT_SETTINGS,
+        no_args_is_help=True,
+        pretty_exceptions_show_locals=pretty_exceptions_show_locals,
+        pretty_exceptions_enable=pretty_exceptions_enable,
+    )
+
+    logging.basicConfig(
+        level="INFO",
+        format="%(message)s",
+        datefmt="[%X]",
+        handlers=[
+            # we don't need to turn on rich_tracebacks since typer creates it already
+            RichHandler(
+                level=default_logging_level,
+                rich_tracebacks=False,
+                tracebacks_suppress=[typer],
+                show_path=False,
+            ),
+        ],
+    )
+
+logger = logging.getLogger("cli")
+
+
+def setup_config():
+    """Setup a config module necessary to launch the CLI"""
+    working_dir = pathlib.Path().resolve()
+    _config = DummyConfig("config")
+    _config.HUB_DB_BACKEND = {
+        "module": "biothings.utils.sqlite3",
+        "sqlite_db_folder": ".biothings_hub",
+    }
+    _config.DATA_SRC_DATABASE = ".data_src_database"
+    _config.DATA_ARCHIVE_ROOT = ".biothings_hub/archive"
+    # _config.LOG_FOLDER = ".biothings_hub/logs"
+    _config.LOG_FOLDER = None  # disable file logging, only log to stdout
+    _config.DATA_PLUGIN_FOLDER = f"{working_dir}"
+    _config.hub_db = importlib.import_module(_config.HUB_DB_BACKEND["module"])
+    try:
+        config_mod = importlib.import_module("config")
+        for attr in dir(config_mod):
+            value = getattr(config_mod, attr)
+            if isinstance(value, ConfigurationError):
+                raise ConfigurationError("%s: %s" % (attr, str(value)))
+            setattr(_config, attr, value)
+    except ModuleNotFoundError:
+        logger.debug("The config.py does not exists in the working directory, use default biothings.config")
+
+    sys.modules["config"] = _config
+    sys.modules["biothings.config"] = _config
+
+
+def main():
+    """The main entry point for running the BioThings CLI to test your local data plugins."""
+    if not typer_avail:
+        logger.error(
+            '"typer" package is required for CLI feature. Use "pip install biothings[cli]" or "pip install typer[all]" to install.'
+        )
+        return
+
+    setup_config()
+    from .dataplugin import app as dataplugin_app
+    from .dataplugin_hub import app as dataplugin_hub_app
+
+    cli.add_typer(dataplugin_app, name="dataplugin")
+    cli.add_typer(dataplugin_hub_app, name="dataplugin-hub")
+    return cli()
---
+++ packages/biothings/biothings-0.12.4.tar.gz/biothings-0.12.4/biothings/hub/api/manager.py
@@ -0,0 +1,127 @@
+import socket
+import types
+from datetime import datetime
+
+from biothings import config as btconfig
+from biothings.utils.hub_db import get_api
+from biothings.utils.loggers import get_logger
+from biothings.utils.manager import BaseManager
+from biothings.web.launcher import BiothingsAPILauncher
+
+
+class APIManagerException(Exception):
+    pass
+
+
+class APIManager(BaseManager):
+    def __init__(self, log_folder=None, *args, **kwargs):
+        self.api = get_api()
+        self.register = {}
+        self.timestamp = datetime.now()
+        self.log_folder = log_folder or btconfig.LOG_FOLDER
+        self.setup()
+        self.restore_running_apis()
+
+    def setup(self):
+        self.setup_log()
+
+    def setup_log(self):
+        self.logger, _ = get_logger("apimanager")
+
+    def restore_running_apis(self):
+        """
+        If some APIs were running but the hub stopped, re-start APIs
+        as hub restarts
+        """
+        apis = self.get_apis()
+        # these were running but had to stop when hub stopped
+        running_apis = [api for api in apis if api.get("status") == "running"]
+        for api in running_apis:
+            self.logger.info("Restart API '%s'" % api["_id"])
+            self.start_api(api["_id"])
+
+    def register_status(self, api_id, status, **extra):
+        apidoc = self.api.find_one({"_id": api_id})
+        apidoc.update(extra)
+        # clean according to status
+        if status == "running":
+            apidoc.pop("err", None)
+        else:
+            apidoc.pop("url", None)
+        apidoc["status"] = status
+        self.api.save(apidoc)
+
+    def get_apis(self):
+        return [d for d in self.api.find()]
+
+    def start_api(self, api_id):
+        apidoc = self.api.find_one({"_id": api_id})
+        if not apidoc:
+            raise APIManagerException("No such API with ID '%s'" % api_id)
+        if "entry_point" in apidoc:
+            raise NotImplementedError(
+                "Custom entry point not implemented yet, " + "only basic generated APIs are currently supported"
+            )
+
+        config_mod = types.ModuleType("config_mod")
+        config_mod.ES_HOST = apidoc["config"]["es_host"]
+        config_mod.ES_INDEX = apidoc["config"]["index"]
+        config_mod.ES_DOC_TYPE = apidoc["config"]["doc_type"]
+
+        launcher = BiothingsAPILauncher(config_mod)
+        port = int(apidoc["config"]["port"])
+        try:
+            server = launcher.get_server()
+            server.listen(port)
+            self.register[api_id] = server
+            self.logger.info("Running API '%s' on port %s" % (api_id, port))
+            url = "http://%s:%s" % (socket.gethostname(), port)
+            self.register_status(api_id, "running", url=url)
+        except Exception as e:
+            self.logger.exception("Failed to start API '%s'" % api_id)
+            self.register_status(api_id, "failed", err=str(e))
+            raise
+
+    def stop_api(self, api_id):
+        try:
+            assert api_id in self.register, "API '%s' is not running" % api_id
+            server = self.register.pop(api_id)
+            server.stop()
+            if server._stopped:
+                self.register_status(api_id, "stopped")
+        except Exception as e:
+            self.logger.exception("Failed to stop API '%s'" % api_id)
+            self.register_status(api_id, "failed", err=str(e))
+            raise
+
+    def delete_api(self, api_id):
+        try:
+            self.stop_api(api_id)
+        except Exception as e:
+            self.logger.warning("While trying to stop API '%s': %s" % (api_id, e))
+        finally:
+            self.api.remove({"_id": api_id})
+
+    def create_api(
+        self,
+        api_id,
+        es_host,
+        index,
+        doc_type,
+        port,
+        description=None,
+        **kwargs,
+    ):
+        apidoc = {
+            "_id": api_id,
+            "config": {
+                "port": port,
+                "es_host": es_host,
+                "index": index,
+                "doc_type": doc_type,
+            },
+            "description": description,
+        }
+        apidoc.update(kwargs)
+        self.api.save(apidoc)
+        return apidoc
