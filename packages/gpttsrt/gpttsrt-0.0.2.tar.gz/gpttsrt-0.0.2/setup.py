72abc0e3680c0e5e80e8639e35cbd0f12b3736c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-03 03:05:04 +0000

    Add package gpttsrt/0.0.2/gpttsrt-0.0.2.tar.gz

---
+++ packages/gpttsrt/gpttsrt-0.0.2.tar.gz/gpttsrt-0.0.2/setup.py
@@ -0,0 +1,30 @@
+# coding: utf-8
+
+from distutils.core import setup
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setup(
+    name='gpttsrt',
+    version='0.0.2',
+    description='Gpt powered subtitle translation tool.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Seeker472',
+    author_email='gmx472@qq.com',
+    url='https://github.com/Seeker0472/gpttsrt',
+    packages=['gpttsrt'],
+    entry_points={
+        'console_scripts': [
+            'gpttsrt = gpttsrt.gpttsrt:main',
+        ],
+    },
+    install_requires=[
+        'pysrt~=1.1.2',
+        'requests',
+        'tqdm~=4.65.0',
+        'configargparse~=1.4',
+        'openai~=1.9.0',
+    ],
+)
