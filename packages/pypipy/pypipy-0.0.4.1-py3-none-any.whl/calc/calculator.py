9005ae117590239bbcef57b12f168ddf0b85830b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-03 15:05:19 +0000

    Add package pypipy/0.0.4.1/pypipy-0.0.4.1-py3-none-any.whl

---
+++ packages/pypipy/pypipy-0.0.4.1-py3-none-any.whl/calc/calculator.py
@@ -0,0 +1,17 @@
+def addAll(*args):
+    return sum(args)
+
+
+def mulAll(*args):
+    result = 1
+    for i in range(len(args)):
+        result *= args[i]
+    return result
+
+
+def aMinusB(a, b):
+    return a - b
+
+
+def info():
+    print("나만의 패키지")
