e068dec793e4b3e550792c9fa7489c481d9ef3d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-03 15:05:05 +0000

    Add package seatable-api-meta/0.0.1/seatable-api-meta-0.0.1.tar.gz

---
+++ packages/seatable-api-meta/seatable-api-meta-0.0.1.tar.gz/seatable-api-meta-0.0.1/setup_meta.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+__version__ = '0.0.1'
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setup(
+    name='seatable-api-meta',
+    version=__version__,
+    license='Apache Licence',
+    description='Python client for SeaTable web api -- meta version',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='seatable',
+    author_email='support@seafile.com',
+    url='https://github.com/seatable/seatable-api-python',
+    platforms='any',
+    packages=find_packages(),  # folder with __init__.py
+    install_requires=['requests', 'python-socketio<5', 'ply', 'python_dateutil'],
+    classifiers=['Programming Language :: Python'],
+)
