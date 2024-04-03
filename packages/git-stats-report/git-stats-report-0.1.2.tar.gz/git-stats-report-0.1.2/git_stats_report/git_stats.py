2325a3b951ab95819c5a48ed6e760e3d819d1f17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-03 14:04:57 +0000

    Add package git-stats-report/0.1.2/git-stats-report-0.1.2.tar.gz

---
+++ packages/git-stats-report/git-stats-report-0.1.2.tar.gz/git-stats-report-0.1.2/git_stats_report/git_stats.py
@@ -0,0 +1,56 @@
+import json
+from datetime import datetime
+from typing import List, cast
+
+from typing_extensions import TypedDict
+
+from .utils import run_command
+
+NUM_CHARS = [str(n) for n in range(10)]
+
+
+class AuthorStats(TypedDict):
+    value: int
+    label: str
+
+
+class GitStatsRawOutput(TypedDict):
+    authors: List[AuthorStats]
+
+
+def get_git_stats(since: datetime) -> GitStatsRawOutput:
+    command: str = ""
+    try:
+        command = f"git-stats --raw --authors --since {since}"
+    except Exception:  # noqa
+        try:
+            command = f"npx git-stats --raw --authors --since {since}"
+        except Exception as exc:  # noqa
+            msg = "Either git-stats or node.js must be installed"
+            raise Exception(msg) from exc  # noqa
+    return cast(GitStatsRawOutput, json.loads(run_command(command)))
+
+
+def get_total_commits(git_stats: GitStatsRawOutput) -> int:
+    return sum(author["value"] for author in git_stats["authors"] if author["value"])
+
+
+def get_author_info(author: AuthorStats, since: datetime) -> str:
+    author_name: str = author["label"]
+    command: str = (
+        f'git log --author="{author_name}" --numstat --since="{since}" --format=""'
+    )
+    log: str = run_command(command)
+    files_changed: int = 0
+    lines_added: int = 0
+    lines_deleted: int = 0
+    for line in log.splitlines():
+        if not line:
+            continue
+        added, deleted, _ = line.split("\t", 2)
+        files_changed += 1
+        if added[-1] in NUM_CHARS:
+            lines_added += int(added)
+        if added[-1] in NUM_CHARS:
+            lines_deleted += int(deleted)
+    return f"\n* {files_changed} Files Changed, - {lines_deleted} Lines Deleted, + {lines_added} Lines Added\n\n"  # pylint: disable=line-too-long
