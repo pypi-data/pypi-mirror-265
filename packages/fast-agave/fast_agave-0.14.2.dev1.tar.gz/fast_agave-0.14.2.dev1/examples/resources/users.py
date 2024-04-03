59382a297a88a99a8433645f89d97a6f2e4c5a7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-03 06:05:45 +0000

    Add package fast-agave/0.14.2.dev1/fast_agave-0.14.2.dev1.tar.gz

---
+++ packages/fast-agave/fast_agave-0.14.2.dev1.tar.gz/fast_agave-0.14.2.dev1/examples/resources/users.py
@@ -0,0 +1,26 @@
+from typing import cast
+
+from fast_agave.filters import generic_query
+from ..models import User as UserModel
+from ..validators import UserQuery, UserUpdateRequest
+from .base import app
+from fastapi.responses import JSONResponse as Response
+from fastapi import Request
+from starlette.datastructures import Address
+
+
+@app.resource('/users')
+class User:
+    model = UserModel
+    query_validator = UserQuery
+    get_query_filter = generic_query
+    update_validator = UserUpdateRequest
+
+    @staticmethod
+    async def update(
+        user: UserModel, request: UserUpdateRequest, api_request: Request
+    ) -> Response:
+        user.name = request.name
+        user.ip = cast(Address, api_request.client).host
+        await user.async_save()
+        return Response(content=user.to_dict(), status_code=200)
