ff6c16f4b2561b77d0317fea9c6c5fcdd7667907
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-03 16:05:21 +0000

    Add package facebook-graphql-scraper/0.7/facebook_graphql_scraper-0.7-py3-none-any.whl

---
+++ packages/facebook-graphql-scraper/facebook_graphql_scraper-0.7-py3-none-any.whl/fb_graphql_scraper/test_program.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+#%%
+import os
+print(os.getcwd())
+from facebook_graphql_scraper import FacebookGraphqlScraper as fb_graphql_scraper
+from dotenv import load_dotenv
+import os
+
+
+# ## Load account info
+# load_dotenv()
+# username_or_userid = os.getenv("FB_USERNAME_OR_USERID")
+# pwd = os.getenv("FBPASSWORD")
+
+# #%%
+# ## Log in account version
+# fb_user_id = "KaiCenatOfficial"
+# url = "https://www.facebook.com/"
+# res = fb_graphql_scraper.get_user_posts(
+#     fb_username_or_userid=fb_user_id, 
+#     loop_times=50,
+#     username_or_userid=username_or_userid,
+#     pwd=pwd
+# )
+# res
+#%%
+## Without logging in account version
+fb_user_id = "KaiCenatOfficial"
+url = "https://www.facebook.com/"
+res = fb_graphql_scraper.get_user_posts(
+    fb_username_or_userid=fb_user_id, 
+    loop_times=50
+)
+res
+# %%
