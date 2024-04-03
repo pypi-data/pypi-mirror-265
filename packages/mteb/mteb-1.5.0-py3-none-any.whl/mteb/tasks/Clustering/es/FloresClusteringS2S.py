388aa677f2255039fb4fcef6e43070585a4d1f86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-03 05:04:57 +0000

    Add package mteb/1.5.0/mteb-1.5.0-py3-none-any.whl

---
+++ packages/mteb/mteb-1.5.0-py3-none-any.whl/mteb/tasks/Clustering/es/FloresClusteringS2S.py
@@ -0,0 +1,34 @@
+from __future__ import annotations
+
+from mteb.abstasks.TaskMetadata import TaskMetadata
+
+from ....abstasks.AbsTaskClustering import AbsTaskClustering
+
+
+class FloresClusteringS2S(AbsTaskClustering):
+    metadata = TaskMetadata(
+        name="FloresClusteringS2S",
+        description="Clustering of sentences from various web articles, 32 topics in total.",
+        reference="https://huggingface.co/datasets/facebook/flores",
+        dataset={
+            "path": "mteb/flores",
+            "revision": "480b580487f53a46f881354a8348335d4edbb2de",
+        },
+        type="Clustering",
+        category="s2s",
+        eval_splits=["test"],
+        eval_langs=["es"],
+        main_score="v_measure",
+        date=None,
+        form=None,
+        domains=None,
+        task_subtypes=None,
+        license=None,
+        socioeconomic_status=None,
+        annotations_creators=None,
+        dialect=None,
+        text_creation=None,
+        bibtex_citation=None,
+        n_samples=None,
+        avg_character_length=None,
+    )
---
+++ packages/mteb/mteb-1.5.0-py3-none-any.whl/mteb/tasks/PairClassification/zh/CMTEBPairClassification.py
@@ -0,0 +1,62 @@
+from __future__ import annotations
+
+from mteb.abstasks.AbsTaskPairClassification import AbsTaskPairClassification
+from mteb.abstasks.TaskMetadata import TaskMetadata
+
+
+class Ocnli(AbsTaskPairClassification):
+    metadata = TaskMetadata(
+        name="Ocnli",
+        description="Original Chinese Natural Language Inference dataset",
+        reference="https://arxiv.org/abs/2010.05444",
+        dataset={
+            "path": "C-MTEB/OCNLI",
+            "revision": "7bd1ee0ae3a820d8a4b6f8a624063f8504a3564d",
+        },
+        type="PairClassification",
+        category="s2s",
+        eval_splits=["validation", "test"],
+        eval_langs=["zh"],
+        main_score="accuracy",
+        date=None,
+        form=None,
+        domains=None,
+        task_subtypes=None,
+        license=None,
+        socioeconomic_status=None,
+        annotations_creators=None,
+        dialect=None,
+        text_creation=None,
+        bibtex_citation=None,
+        n_samples=None,
+        avg_character_length=None,
+    )
+
+
+class Cmnli(AbsTaskPairClassification):
+    metadata = TaskMetadata(
+        name="Cmnli",
+        description="Chinese Multi-Genre NLI",
+        reference="https://huggingface.co/datasets/clue/viewer/cmnli",
+        dataset={
+            "path": "C-MTEB/CMNLI",
+            "revision": "41bc36f332156f7adc9e38f53777c959b2ae9766",
+        },
+        type="PairClassification",
+        category="s2s",
+        eval_splits=["validation", "test"],
+        eval_langs=["zh"],
+        main_score="accuracy",
+        date=None,
+        form=None,
+        domains=None,
+        task_subtypes=None,
+        license=None,
+        socioeconomic_status=None,
+        annotations_creators=None,
+        dialect=None,
+        text_creation=None,
+        bibtex_citation=None,
+        n_samples=None,
+        avg_character_length=None,
+    )
