d5a76a806a64e25c827f449d7bf5ac7921c6432c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-03 15:05:33 +0000

    Add package paibox/1.0.0b1/paibox-1.0.0b1-py3-none-any.whl

---
+++ packages/paibox/paibox-1.0.0b1-py3-none-any.whl/paibox/synapses/base.py
@@ -0,0 +1,260 @@
+from typing import ClassVar, Optional, Tuple, Union
+
+import numpy as np
+from paicorelib import HwConfig
+from paicorelib import WeightPrecision as WP
+
+from paibox.base import NeuDyn, SynSys
+from paibox.exceptions import ShapeError
+from paibox.neuron import Neuron
+from paibox.projection import InputProj
+from paibox.types import DataArrayType, SynOutType, WeightType
+
+from .conv_utils import (
+    _fm_ndim1_check,
+    _fm_ndim2_check,
+    _KOrder3d,
+    _KOrder4d,
+    _Order2d,
+    _Order3d,
+)
+from .transforms import AllToAll, Conv1dForward, Conv2dForward
+from .transforms import GeneralConnType as GConnType
+from .transforms import Identity, MaskedLinear, OneToOne, Transform
+
+RIGISTER_MASTER_KEY_FORMAT = "{0}.output"
+
+
+def _check_equal(num_in: int, num_out: int) -> int:
+    if num_in != num_out:
+        raise ShapeError(
+            f"the number of source & destination neurons must be equal: {num_in} != {num_out}."
+        )
+
+    return num_in
+
+
+class Synapses:
+    def __init__(
+        self,
+        source: Union[NeuDyn, InputProj],
+        dest: NeuDyn,
+    ) -> None:
+        self._source = source
+        self._dest = dest
+
+    @property
+    def source(self) -> Union[NeuDyn, InputProj]:
+        return self._source
+
+    @property
+    def dest(self) -> NeuDyn:
+        return self._dest
+
+    @property
+    def shape_in(self) -> Tuple[int, ...]:
+        return self._source.shape_out
+
+    @property
+    def shape_out(self) -> Tuple[int, ...]:
+        return self._dest.shape_in
+
+    @property
+    def num_in(self) -> int:
+        return self._source.num_out
+
+    @property
+    def num_out(self) -> int:
+        return self._dest.num_in
+
+
+class FullConnectedSyn(Synapses, SynSys):
+    def __init__(
+        self,
+        source: Union[NeuDyn, InputProj],
+        dest: NeuDyn,
+        name: Optional[str] = None,
+    ) -> None:
+        super(Synapses, self).__init__(name)
+        super().__init__(source, dest)
+
+        self.set_memory("_synout", np.zeros((self.num_out,), dtype=np.int32))
+
+        # Register `self` for the destination `NeuDyn`.
+        dest.register_master(RIGISTER_MASTER_KEY_FORMAT.format(self.name), self)
+
+    def __call__(self, *args, **kwargs) -> SynOutType:
+        return self.update(*args, **kwargs)
+
+    def update(self, spike: Optional[np.ndarray] = None, *args, **kwargs) -> SynOutType:
+        # Retrieve the spike at index `timestamp` of the dest neurons
+        if self.dest.is_working:
+            if isinstance(self.source, InputProj):
+                synin = self.source.output.copy() if spike is None else spike
+            else:
+                idx = self.dest.timestamp % HwConfig.N_TIMESLOT_MAX
+                synin = self.source.output[idx].copy() if spike is None else spike
+        else:
+            # Retrieve 0 to the dest neurons if it is not working
+            synin = np.zeros_like(self.source.spike, dtype=np.bool_)
+
+        self._synout = self.comm(synin).astype(np.int32)
+        return self._synout
+
+    def reset_state(self, *args, **kwargs) -> None:
+        # TODO Add other initialization methods in the future.
+        self.reset_memory()  # Call reset of `StatusMemory`.
+
+    def _set_comm(self, comm: Transform) -> None:
+        self.comm = comm
+
+    @property
+    def output(self) -> SynOutType:
+        return self._synout
+
+    @property
+    def weights(self) -> WeightType:
+        return self.comm.weights
+
+    @property
+    def weight_precision(self) -> WP:
+        return self.comm._get_wp(self.CFLAG_ENABLE_WP_OPTIMIZATION)
+
+    @property
+    def connectivity(self) -> WeightType:
+        """The connectivity matrix in `np.bool_` or `np.int8` format."""
+        return self.comm.connectivity
+
+
+class FullConnSyn(FullConnectedSyn):
+    def __init__(
+        self,
+        source: Union[NeuDyn, InputProj],
+        dest: NeuDyn,
+        weights: DataArrayType,
+        conn_type: GConnType,
+        name: Optional[str] = None,
+    ) -> None:
+        super().__init__(source, dest, name)
+
+        if conn_type is GConnType.One2One:
+            comm = OneToOne(_check_equal(self.num_in, self.num_out), weights)
+        elif conn_type is GConnType.Identity:
+            if not isinstance(weights, (int, np.bool_, np.integer)):
+                raise TypeError(
+                    f"expected type int, np.bool_, np.integer, but got type {type(weights)}."
+                )
+            comm = Identity(_check_equal(self.num_in, self.num_out), weights)
+        elif conn_type is GConnType.All2All:
+            comm = AllToAll((self.num_in, self.num_out), weights)
+        else:  # MatConn
+            if not isinstance(weights, np.ndarray):
+                raise TypeError(
+                    f"expected type np.ndarray, but got type {type(weights)}."
+                )
+            comm = MaskedLinear((self.num_in, self.num_out), weights)
+
+        self._set_comm(comm)
+
+
+class Conv1dSyn(FullConnectedSyn):
+    _spatial_ndim: ClassVar[int] = 1
+
+    def __init__(
+        self,
+        source: Union[NeuDyn, InputProj],
+        dest: Neuron,
+        kernel: np.ndarray,
+        stride: Tuple[int],
+        # padding: Tuple[int],
+        fm_order: _Order2d,
+        order: _KOrder3d,
+        name: Optional[str] = None,
+    ) -> None:
+        super().__init__(source, dest, name)
+
+        if kernel.ndim != self._spatial_ndim + 2:
+            raise ShapeError(
+                f"convolution kernel dimension must be {self._spatial_ndim + 2}, but got {kernel.ndim}."
+            )
+
+        if order == "IOL":
+            _kernel = kernel.transpose(1, 0, 2)
+        else:
+            _kernel = kernel.copy()
+
+        # O,I,L
+        out_channels, in_channels, kernel_l = _kernel.shape
+
+        # C,L
+        in_ch, in_l = _fm_ndim1_check(source.shape_out, fm_order)
+        out_ch, out_l = _fm_ndim1_check(dest.shape_out, fm_order)
+
+        if in_ch != in_channels:
+            raise ShapeError(f"input channels mismatch: {in_ch} != {in_channels}.")
+
+        if out_ch != out_channels:
+            raise ShapeError(f"output channels mismatch: {out_ch} != {out_channels}.")
+
+        # If padding is considered, the implementation of convolution unrolling
+        # is extremely complex, so fix it.
+        padding = (0,)
+
+        assert (in_l + 2 * padding[0] - kernel_l) // stride[0] + 1 == out_l
+
+        comm = Conv1dForward((in_l,), (out_l,), _kernel, stride, padding, fm_order)
+
+        self.comm = comm
+
+
+class Conv2dSyn(FullConnectedSyn):
+    _spatial_ndim: ClassVar[int] = 2
+
+    def __init__(
+        self,
+        source: Union[NeuDyn, InputProj],
+        dest: Neuron,
+        kernel: np.ndarray,
+        stride: Tuple[int, int],
+        # padding: Tuple[int, int],
+        fm_order: _Order3d,
+        order: _KOrder4d,
+        name: Optional[str] = None,
+    ) -> None:
+        super().__init__(source, dest, name)
+
+        if kernel.ndim != self._spatial_ndim + 2:
+            raise ShapeError(
+                f"convolution kernel dimension must be {self._spatial_ndim + 2}, but got {kernel.ndim}."
+            )
+
+        if order == "IOHW":
+            _kernel = kernel.transpose(1, 0, 2, 3)
+        else:
+            _kernel = kernel.copy()
+
+        # O,I,H,W
+        out_channels, in_channels, kernel_h, kernel_w = _kernel.shape
+
+        # C,H,W
+        in_ch, in_h, in_w = _fm_ndim2_check(source.shape_out, fm_order)
+        out_ch, out_h, out_w = _fm_ndim2_check(dest.shape_out, fm_order)
+
+        if in_ch != in_channels:
+            raise ShapeError(f"input channels mismatch: {in_ch} != {in_channels}.")
+
+        if out_ch != out_channels:
+            raise ShapeError(f"output channels mismatch: {out_ch} != {out_channels}.")
+
+        # If padding is considered, the implementation of convolution unrolling
+        # is extremely complex, so fix it.
+        padding = (0, 0)
+
+        assert (in_h + 2 * padding[0] - kernel_h) // stride[0] + 1 == out_h
+        assert (in_w + 2 * padding[1] - kernel_w) // stride[1] + 1 == out_w
+
+        comm = Conv2dForward(
+            (in_h, in_w), (out_h, out_w), _kernel, stride, padding, fm_order
+        )
+
+        self._set_comm(comm)
---
+++ packages/paibox/paibox-1.0.0b1-py3-none-any.whl/paibox/synapses/conv_utils.py
@@ -0,0 +1,252 @@
+from functools import partial
+from itertools import repeat
+from typing import Any, Iterable, Literal, Tuple, TypeVar, Union
+
+import numpy as np
+from numpy.typing import NDArray
+
+from paibox.exceptions import ShapeError
+from paibox.types import SynOutType, WeightType
+
+T = TypeVar("T")
+
+_TupleAnyType = Union[T, Tuple[T, ...]]
+_Tuple1Type = Union[T, Tuple[T]]
+_Tuple2Type = Union[T, Tuple[T, T]]
+_Tuple3Type = Union[T, Tuple[T, T, T]]
+
+_SizeAnyType = _TupleAnyType[int]
+_Size1Type = _Tuple1Type[int]
+_Size2Type = _Tuple2Type[int]
+_Size3Type = _Tuple3Type[int]
+
+SizeAnyType = Tuple[int, ...]
+Size1Type = Tuple[int]
+Size2Type = Tuple[int, int]
+Size3Type = Tuple[int, int, int]
+
+_Order2d = Literal["CL", "LC"]  # Feature map order in 2d
+_Order3d = Literal["CHW", "HWC"]  # Feature map order in 3d
+_KOrder3d = Literal["OIL", "IOL"]  # Kernel order in 3d
+_KOrder4d = Literal["OIHW", "IOHW"]  # Kernel order in 4d
+
+
+def _ntuple(x, n: int) -> Tuple[Any, ...]:
+    if isinstance(x, Iterable):
+        return tuple(x)
+
+    return tuple(repeat(x, n))
+
+
+_single = partial(_ntuple, n=1)
+_pair = partial(_ntuple, n=2)
+_triple = partial(_ntuple, n=3)
+_quadruple = partial(_ntuple, n=4)
+
+
+def _fm_ndim1_check(fm_shape: SizeAnyType, fm_order: _Order2d) -> Size2Type:
+    if len(fm_shape) < 1 or len(fm_shape) > 2:
+        raise ShapeError(f"expected shape of 1 or 2, but got {len(fm_shape)}.")
+
+    if len(fm_shape) == 1:
+        channels, l = (1,) + fm_shape
+    else:
+        if fm_order == "CL":
+            channels, l = fm_shape
+        else:
+            l, channels = fm_shape
+
+    return channels, l
+
+
+def _fm_ndim2_check(fm_shape: SizeAnyType, fm_order: _Order3d) -> Size3Type:
+    if len(fm_shape) < 2 or len(fm_shape) > 3:
+        raise ShapeError(f"expected shape of 2 or 3, but got {len(fm_shape)}.")
+
+    if len(fm_shape) == 2:
+        channels, h, w = (1,) + fm_shape
+    else:
+        if fm_order is "CHW":
+            channels, h, w = fm_shape
+        else:
+            h, w, channels = fm_shape
+
+    return channels, h, w
+
+
+def _conv1d_unroll(
+    in_shape: Size1Type,
+    out_shape: Size1Type,
+    kernel: WeightType,
+    stride: Size1Type,
+    # padding: Size1Type,
+) -> WeightType:
+    """Unroll the convolution kernel of 1d convolution into a matrix."""
+    cout, cin, kl = kernel.shape
+    il = in_shape[0]
+    ol = out_shape[0]
+
+    w_unrolled = np.zeros((cin * il, cout * ol), dtype=kernel.dtype)
+    zeros_image = np.zeros((cin * il, cout, ol), dtype=kernel.dtype)
+
+    for i in range(ol):
+        for ch_idx in np.ndindex(kernel.shape[:2]):
+            # [0] -> o_ch, [1] -> i_ch
+            zeros_image[
+                i * stride[0] + ch_idx[1] * il : i * stride[0] + ch_idx[1] * il + kl,
+                ch_idx[0],
+                i,
+            ] = kernel[ch_idx[0], ch_idx[1], :]
+
+        t = zeros_image[:, :, i].T
+        for o_ch in range(cout):
+            w_unrolled[:, i + o_ch * ol] = t[o_ch].flatten()
+
+    return w_unrolled
+
+
+def _conv2d_unroll(
+    in_shape: Size2Type,
+    out_shape: Size2Type,
+    kernel: WeightType,
+    stride: Size2Type,
+    # padding: Size2Type,
+) -> WeightType:
+    """Unroll the convolution kernel of 2d convolution into a matrix."""
+    cout, cin, kh, kw = kernel.shape
+
+    ih, iw = in_shape
+    oh, ow = out_shape
+    in_size = ih * iw
+    out_size = oh * ow
+
+    w_unrolled = np.zeros((cin * in_size, cout * out_size), dtype=kernel.dtype)
+    zeros_image = np.zeros((cin * ih, iw * cout, out_size), dtype=kernel.dtype)
+
+    for i in range(oh):
+        for j in range(ow):
+            for ch_idx in np.ndindex(kernel.shape[:2]):
+                # [0] -> o_ch, [1] -> i_ch
+                zeros_image[
+                    i * stride[0]
+                    + ch_idx[1] * ih : i * stride[0]
+                    + ch_idx[1] * ih
+                    + kh,
+                    j * stride[1]
+                    + ch_idx[0] * iw : j * stride[1]
+                    + ch_idx[0] * iw
+                    + kw,
+                    i * ow + j,
+                ] = kernel[ch_idx[0], ch_idx[1], :, :]
+
+            t = (
+                zeros_image[:, :, i * ow + j]
+                .reshape(cin * ih, cout, iw)
+                .transpose(1, 0, 2)
+            )
+            for o_ch in range(cout):
+                w_unrolled[:, i * ow + j + o_ch * out_size] = t[o_ch].flatten()
+
+    return w_unrolled
+
+
+def _conv1d_faster(
+    x_cl: np.ndarray,
+    out_shape: Size1Type,
+    kernel: WeightType,
+    stride: Size1Type,
+    padding: Size1Type,
+) -> SynOutType:
+    xc, xl = x_cl.shape
+
+    # (O, I, L)
+    cout, cin, kl = kernel.shape
+    assert xc == cin
+
+    x_padded = np.pad(x_cl, ((0, 0), (padding[0], padding[0])), mode="constant")
+
+    assert (xl + padding[0] * 2 - kl) // stride[0] + 1 == out_shape[0]
+
+    # kernel: (cout, cin, kl) -> (cin*kl, cout)
+    col_kernel = kernel.transpose(1, 2, 0).reshape(-1, cout)
+
+    # padded: (cin, xl+2*p[0]-kl) -> (ol, cin*kl)
+    col_fm = _1d_im2col(x_padded, out_shape[0], kl, stride)
+
+    # out = np.zeros((cout,) + out_shape, dtype=np.int64)
+    # (ol, cin*kl) * (cin*kl, cout) = (ol, cout)
+    out = col_fm @ col_kernel  # + self.bias
+
+    # (ol, cout) -> (ol, cout) -> (cout, ol)
+    out = out.reshape(out_shape + (cout,)).T
+
+    return out.astype(np.int32)
+
+
+def _conv2d_faster(
+    x_chw: np.ndarray,
+    out_shape: Size2Type,
+    kernel: WeightType,
+    stride: Size2Type,
+    padding: Size2Type,
+) -> SynOutType:
+    xc, xh, xw = x_chw.shape
+
+    # (O, I, H, W)
+    cout, cin, kh, kw = kernel.shape
+    assert xc == cin
+
+    x_padded = np.pad(
+        x_chw,
+        ((0, 0), (padding[0], padding[0]), (padding[1], padding[1])),
+        mode="constant",
+    )
+
+    assert (xh + padding[0] * 2 - kh) // stride[0] + 1 == out_shape[0]
+    assert (xw + padding[1] * 2 - kw) // stride[1] + 1 == out_shape[1]
+
+    # kernel: (cout, cin, kh, kw) -> (cin*kh*kw, cout)
+    col_kernel = kernel.transpose(1, 2, 3, 0).reshape(-1, cout)
+
+    # padded: (cin, xh+2*p[0]-kh, xw+2*p[1]-kw) -> (oh*ow, cin*kh*kw)
+    col_fm = _2d_im2col(x_padded, out_shape[0], out_shape[1], kh, kw, stride)
+
+    # out = np.zeros((cout,) + out_shape, dtype=np.int64)
+    # (oh*ow, cin*kh*kw) * (cin*kh*kw, cout) = (oh*ow, cout)
+    out = col_fm @ col_kernel  # + self.bias
+
+    # (oh*ow, cout) -> (oh, ow, cout) -> (cout, oh, ow)
+    out = out.reshape(out_shape + (cout,)).transpose(2, 0, 1)
+
+    return out.astype(np.int32)
+
+
+def _1d_im2col(
+    x_padded: np.ndarray, ol: int, kl: int, stride: Size1Type
+) -> NDArray[np.int64]:
+    cols = np.zeros((ol, x_padded.shape[0] * kl), dtype=np.int64)
+
+    _, pl = x_padded.shape
+
+    idx = 0
+    for i in range(0, pl - kl + 1, stride[0]):
+        cols[idx] = x_padded[:, i : i + kl].reshape(-1)
+        idx += 1
+
+    return cols
+
+
+def _2d_im2col(
+    x_padded: np.ndarray, oh: int, ow: int, kh: int, kw: int, stride: Size2Type
+) -> NDArray[np.int64]:
+    cols = np.zeros((oh * ow, x_padded.shape[0] * kh * kw), dtype=np.int64)
+
+    _, ph, pw = x_padded.shape
+
+    idx = 0
+    for i in range(0, ph - kh + 1, stride[0]):
+        for j in range(0, pw - kw + 1, stride[1]):
+            cols[idx] = x_padded[:, i : i + kh, j : j + kw].reshape(-1)
+            idx += 1
+
+    return cols
