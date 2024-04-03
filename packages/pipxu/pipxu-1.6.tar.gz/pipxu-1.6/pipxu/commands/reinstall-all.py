368b8f9b35a0edda9548793abb0df5ddc3ec5ffd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-03 14:05:00 +0000

    Add package pipxu/1.6/pipxu-1.6.tar.gz

---
+++ packages/pipxu/pipxu-1.6.tar.gz/pipxu-1.6/pipxu/commands/reinstall-all.py
@@ -0,0 +1,28 @@
+# Author: Mark Blakeney, Feb 2024.
+'Reinstall all applications.'
+from __future__ import annotations
+
+from argparse import ArgumentParser, Namespace
+from typing import Optional
+
+from .. import utils
+from . import reinstall
+
+def init(parser: ArgumentParser) -> None:
+    'Called to add command arguments to parser at init'
+    xgroup = parser.add_mutually_exclusive_group()
+    xgroup.add_argument('-p', '--python',
+                        help='specify explicit python executable path')
+    xgroup.add_argument('-P', '--pyenv',
+                        help='pyenv python version to use, '
+                        'i.e. from `pyenv versions`, e.g. "3.9".')
+    parser.add_argument('-v', '--verbose', action='store_true',
+                        help='give more output')
+    parser.add_argument('-s', '--skip', nargs='*',
+                        help='skip these applications, '
+                        'e.g. "-s package1 package2"')
+
+def main(args: Namespace) -> Optional[str]:
+    'Called to action this command'
+    args.package = utils.get_all_package_names(args)
+    return reinstall.main(args)
