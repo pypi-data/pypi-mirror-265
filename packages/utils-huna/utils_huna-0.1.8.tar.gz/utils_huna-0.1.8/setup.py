40cbfd39f95c5be2ce338892d043a13cb6aac7fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-03 06:06:01 +0000

    Add package utils-huna/0.1.8/utils_huna-0.1.8.tar.gz

---
+++ packages/utils-huna/utils_huna-0.1.8.tar.gz/utils_huna-0.1.8/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='utils_huna',
+    packages=find_packages(),
+    version='0.1.8',
+    description='HUNA helper functions',
+    author='Huna',
+    license='',
+)
