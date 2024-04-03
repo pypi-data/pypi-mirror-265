8522c74576c2e43a2aa0bd099ffe142eb4bb7739
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-03 04:04:49 +0000

    Add package django-crud-manager/0.10.0/django_crud_manager-0.10.0.tar.gz

---
+++ packages/django-crud-manager/django_crud_manager-0.10.0.tar.gz/django_crud_manager-0.10.0/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="django-crud-manager",
+    version="0.10",
+    author="Amir Pourjafari",
+    author_email="amir.pourjafari@gmail.com",
+    description="Django Crudd Manager",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sauditore/SARequest/",
+    project_urls={
+        "Bug Tracker": "https://github.com/sauditore/SARequest/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+    install_requires=["khayyam", "django", "pydantic"]
+)
