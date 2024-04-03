0341ca2b2780341774c146bef5c5345195d2c6e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-03 14:05:18 +0000

    Add package pemdas/1.1/pemdas-1.1.tar.gz

---
+++ packages/pemdas/pemdas-1.1.tar.gz/pemdas-1.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+from pemdas import __version__
+
+setup(
+    name='pemdas',
+    version=__version__,
+
+    url='https://github.com/elizabethratterree/ecr',
+    author='Elizabeth Ratterree',
+    author_email='elizabeth.ratterree@my.utsa.edu',
+
+    packages=find_packages(),
+)
