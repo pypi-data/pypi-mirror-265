2a27218b1f07410283ab82d7ab350bd85321af2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-03 05:06:41 +0000

    Add package re-gpt/4.0.0/re_gpt-4.0.0.tar.gz

---
+++ packages/re-gpt/re_gpt-4.0.0.tar.gz/re_gpt-4.0.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="re_gpt",
+    version="4.0.0",
+    author="Zai-Kun",
+    description="Unofficial reverse-engineered ChatGPT API in Python.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Zai-Kun/reverse-engineered-chatgpt",
+    project_urls={
+        "Bug Tracker": "https://github.com/Zai-Kun/reverse-engineered-chatgpt/issues",
+    },
+    packages=find_packages(),
+    install_requires=["curl_cffi==0.5.9", "websockets==12.0"],
+)
