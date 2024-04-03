d23ae10faee7ac2ce27b1b60e50f91112a18937a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-03 03:04:28 +0000

    Add package PYield/0.3.7/pyield-0.3.7.tar.gz

---
+++ packages/PYield/pyield-0.3.7.tar.gz/pyield-0.3.7/pyield/__init__.py
@@ -0,0 +1,15 @@
+from .__about__ import __version__
+from .di_futures import get_di, read_di
+from .br_calendar import count_bdays, generate_bdays, offset_bdays
+from .anbima import get_treasury_rates, calculate_treasury_di_spreads
+
+__all__ = [
+    "__version__",
+    "get_di",
+    "read_di",
+    "count_bdays",
+    "generate_bdays",
+    "offset_bdays",
+    "get_treasury_rates",
+    "calculate_treasury_di_spreads",
+]
