9675897870aec4c56e7b109ff33280913354d616
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-03 17:05:28 +0000

    Add package Yugioh-database/1.0.0/Yugioh_database-1.0.0.tar.gz

---
+++ packages/Yugioh-database/Yugioh_database-1.0.0.tar.gz/Yugioh_database-1.0.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Yugioh_database',
+    version='1.0.0',
+    author='K.Womack',
+    author_email='womack5706@gmail.com',
+    description='A program for creating a list of your own Yu-Gi-Oh! TCG Card Collection.',
+    url='https://github.com/womack5706/yugioh_database',
+    packages=find_packages(),
+    install_requires=[
+    'setuptools',
+    ],
+)
