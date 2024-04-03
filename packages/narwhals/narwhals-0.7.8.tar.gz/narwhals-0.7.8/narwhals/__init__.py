8511780cf0eb14d683037babfb61153b23e7c844
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-03 17:05:28 +0000

    Add package narwhals/0.7.8/narwhals-0.7.8.tar.gz

---
+++ packages/narwhals/narwhals-0.7.8.tar.gz/narwhals-0.7.8/narwhals/__init__.py
@@ -0,0 +1,36 @@
+from narwhals.dataframe import DataFrame
+from narwhals.dataframe import LazyFrame
+from narwhals.dtypes import *  # noqa: F403
+from narwhals.expression import Expr
+from narwhals.expression import all
+from narwhals.expression import col
+from narwhals.expression import len
+from narwhals.expression import max
+from narwhals.expression import mean
+from narwhals.expression import min
+from narwhals.expression import sum
+from narwhals.expression import sum_horizontal
+from narwhals.functions import concat
+from narwhals.series import Series
+from narwhals.translate import from_native
+from narwhals.translate import to_native
+
+__version__ = "0.7.8"
+
+__all__ = [
+    "concat",
+    "to_native",
+    "from_native",
+    "all",
+    "col",
+    "len",
+    "min",
+    "max",
+    "mean",
+    "sum",
+    "sum_horizontal",
+    "DataFrame",
+    "LazyFrame",
+    "Series",
+    "Expr",
+]
