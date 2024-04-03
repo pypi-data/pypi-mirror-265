e64f5f4435043311e6324c7c85325127221b2f8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-03 06:06:12 +0000

    Add package coordinax/0.4.3/coordinax-0.4.3-py3-none-any.whl

---
+++ packages/coordinax/coordinax-0.4.3-py3-none-any.whl/coordinax/_typing.py
@@ -0,0 +1,28 @@
+"""Representation of coordinates in different systems."""
+
+__all__: list[str] = []
+
+from typing import TypeAlias
+
+import astropy.units as u
+from jaxtyping import Float, Int, Shaped
+
+from unxt import Distance, Quantity
+
+Unit: TypeAlias = u.Unit | u.UnitBase | u.CompositeUnit
+
+FloatScalarQ = Float[Quantity, ""]
+BatchFloatScalarQ = Shaped[FloatScalarQ, "*batch"]
+BatchableFloatScalarQ = Shaped[FloatScalarQ, "*#batch"]
+
+ScalarTime = Float[Quantity["time"], ""] | Int[Quantity["time"], ""]
+
+BatchableAngle = Shaped[Quantity["angle"], "*#batch"]
+BatchableLength = Shaped[Quantity["length"], "*#batch"]
+BatchableDistance = Shaped[Distance, "*#batch"]
+BatchableSpeed = Shaped[Quantity["speed"], "*#batch"]
+BatchableTime = Shaped[Quantity["time"], "*#batch"]
+BatchableAngularSpeed = Shaped[Quantity["angular speed"], "*#batch"]
+
+
+TimeBatchOrScalar = ScalarTime | BatchableTime
