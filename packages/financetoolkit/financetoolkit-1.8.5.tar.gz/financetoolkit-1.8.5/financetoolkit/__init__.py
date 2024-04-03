d24f8dd36fd6fac8f9c746e33f90c2d3bcf40bad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-03 04:05:12 +0000

    Add package financetoolkit/1.8.5/financetoolkit-1.8.5.tar.gz

---
+++ packages/financetoolkit/financetoolkit-1.8.5.tar.gz/financetoolkit-1.8.5/financetoolkit/__init__.py
@@ -0,0 +1,6 @@
+"""FinanceToolkit Initialization"""
+
+# flake8: noqa
+from .toolkit_controller import Toolkit
+from .economics.economics_controller import Economics
+from .discovery.discovery_controller import Discovery
