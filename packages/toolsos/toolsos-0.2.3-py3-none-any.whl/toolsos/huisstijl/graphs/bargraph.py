1df1ddf8a606c95a182ff94dbc691fec29bf1733
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-03 03:05:03 +0000

    Add package toolsos/0.2.3/toolsos-0.2.3-py3-none-any.whl

---
+++ packages/toolsos/toolsos-0.2.3-py3-none-any.whl/toolsos/huisstijl/graphs/bargraph.py
@@ -0,0 +1,133 @@
+import plotly.express as px
+from .styler import BaseStyle
+
+basestyle = BaseStyle()
+
+
+def bar(
+    data,
+    x: str,
+    y: str,
+    color: str = None,
+    color_discrete_sequence: list = None,
+    orientation: str = None,
+    barmode=None,
+    width=750,
+    height=490,
+    **kwargs,
+):
+    fig = px.bar(
+        data_frame=data,
+        x=x,
+        y=y,
+        color=color,
+        template=basestyle.get_base_template("bar", orientation=orientation),
+        width=width,
+        color_discrete_sequence=color_discrete_sequence,
+        height=height,
+        barmode=barmode,
+        **kwargs,
+    )
+
+    fig.update_layout(
+        dict(xaxis_title_text="", yaxis_title_text="", legend_title_text="")
+    )
+
+    return fig
+
+
+def stacked_single(
+    data,
+    x: str,
+    y: str,
+    color: str = None,
+    color_discrete_sequence: list = None,
+    orientation="v",
+    **kwargs,
+):
+    fig = bar(
+        data=data,
+        x=x,
+        y=y,
+        color=color,
+        color_discrete_sequence=color_discrete_sequence,
+        barmode="relative",
+        orientation=orientation,
+        **kwargs,
+    )
+
+    if orientation == "v":
+        fig.update_xaxes(showticklabels=False)
+    if orientation == "h":
+        fig.update_yaxes(showticklabels=False)
+
+    return fig
+
+
+def stacked_multiple(
+    data,
+    x: str,
+    y: str,
+    color: str = None,
+    color_discrete_sequence: list = None,
+    orientation="v",
+    **kwargs,
+):
+    fig = bar(
+        data=data,
+        x=x,
+        y=y,
+        color=color,
+        color_discrete_sequence=color_discrete_sequence,
+        barmode="stack",
+        orientation=orientation,
+        **kwargs,
+    )
+
+    return fig
+
+
+def grouped(
+    data,
+    x: str,
+    y: str,
+    color: str = None,
+    color_discrete_sequence: list = None,
+    orientation="v",
+    **kwargs,
+):
+    fig = bar(
+        data=data,
+        x=x,
+        y=y,
+        color=color,
+        color_discrete_sequence=color_discrete_sequence,
+        barmode="group",
+        orientation=orientation,
+        **kwargs,
+    )
+
+    return fig
+
+
+def single(
+    data,
+    x: str,
+    y: str,
+    color_discrete_sequence: list = None,
+    orientation="v",
+    **kwargs,
+):
+    fig = bar(
+        data=data,
+        x=x,
+        y=y,
+        color_discrete_sequence=color_discrete_sequence,
+        orientation=orientation,
+        **kwargs,
+    )
+
+    if orientation == "h":
+        fig.update_yaxes(automargin=True)
+
+    return fig
---
+++ packages/toolsos/toolsos-0.2.3-py3-none-any.whl/toolsos/huisstijl/graphs/graph_styles.py
@@ -0,0 +1,30 @@
+font = "Amsterdam Sans ExtraBold, Corbel"
+# font = "Arial"
+
+STYLE_OLD = {
+    "font": {"family": "Corbel", "size": 15},
+    "plot_bgcolor": "#FFFFFF",
+    "gridline_color": "#E8E8E8",
+    "gridlinewidth": 0.75,
+    "showline": True,
+    "showgrid": False,
+    "zerolinecolor": "#E8E8E8",
+    "gridcolor": "#E8E8E8",
+    "separators": ",",
+}
+
+
+STYLE_NEW = {
+    "font_bold": {"family": "Amsterdam Sans ExtraBold, Corbel", "size": 15},
+    "font": {"family": "Amsterdam Sans, Corbel", "size": 15},
+    "axis_font": {"family": font, "size": 15},
+    "plot_bgcolor": "#FFFFFF",
+    "gridline_color": "#dbdbdb",
+    "gridline_width": 0.75,
+    "showline": True,
+    "showgrid": False,
+    "zerolinecolor": "#dbdbdb",
+    "gridcolor": "#dbdbdb",
+    "gridlinecolor": "#dbdbdb",
+    "separators": ",",
+}
