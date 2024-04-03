199e56f39c1c239e0866fb87d9434450ed554fd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-03 06:06:03 +0000

    Add package coordinax/0.4.2/coordinax-0.4.2-py3-none-any.whl

---
+++ packages/coordinax/coordinax-0.4.2-py3-none-any.whl/coordinax/_d1/base.py
@@ -0,0 +1,72 @@
+"""Representation of coordinates in different systems."""
+
+__all__ = ["Abstract1DVector", "Abstract1DVectorDifferential"]
+
+
+from abc import abstractmethod
+from dataclasses import fields
+
+from jaxtyping import Shaped
+
+from unxt import Quantity
+
+from coordinax._base import AbstractVectorBase
+from coordinax._base_dif import AbstractVectorDifferential
+from coordinax._base_vec import AbstractVector
+from coordinax._utils import classproperty
+
+
+class Abstract1DVector(AbstractVector):
+    """Abstract representation of 1D coordinates in different systems."""
+
+    @classproperty
+    @classmethod
+    def _cartesian_cls(cls) -> type[AbstractVectorBase]:
+        from .builtin import Cartesian1DVector
+
+        return Cartesian1DVector
+
+    @classproperty
+    @classmethod
+    @abstractmethod
+    def differential_cls(cls) -> type["Abstract1DVectorDifferential"]:
+        raise NotImplementedError
+
+
+# TODO: move to the class in py3.11+
+@AbstractVector.constructor._f.dispatch  # type: ignore[attr-defined, misc]  # noqa: SLF001
+def constructor(
+    cls: "type[Abstract1DVector]", x: Shaped[Quantity["length"], ""], /
+) -> "Abstract1DVector":
+    """Construct a 1D vector.
+
+    Examples
+    --------
+    >>> from unxt import Quantity
+    >>> from coordinax import Cartesian1DVector
+
+    >>> q = Cartesian1DVector.constructor(Quantity(1, "kpc"))
+    >>> q
+    Cartesian1DVector(
+        x=Quantity[PhysicalType('length')](value=f32[1], unit=Unit("kpc"))
+    )
+
+    """
+    return cls(**{fields(cls)[0].name: x.reshape(1)})
+
+
+class Abstract1DVectorDifferential(AbstractVectorDifferential):
+    """Abstract representation of 1D differentials in different systems."""
+
+    @classproperty
+    @classmethod
+    def _cartesian_cls(cls) -> type[AbstractVectorBase]:
+        from .builtin import CartesianDifferential1D
+
+        return CartesianDifferential1D
+
+    @classproperty
+    @classmethod
+    @abstractmethod
+    def integral_cls(cls) -> type[Abstract1DVector]:
+        raise NotImplementedError
