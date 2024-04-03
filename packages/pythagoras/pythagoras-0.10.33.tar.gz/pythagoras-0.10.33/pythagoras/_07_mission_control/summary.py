2ea02831a14ba058a94b71ea43ac43d055a230eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-03 15:05:24 +0000

    Add package pythagoras/0.10.33/pythagoras-0.10.33.tar.gz

---
+++ packages/pythagoras/pythagoras-0.10.33.tar.gz/pythagoras-0.10.33/pythagoras/_07_mission_control/summary.py
@@ -0,0 +1,41 @@
+# from pythagoras._07_mission_control.global_state_management import (
+#   is_fully_unitialized, is_correctly_initialized)
+import pythagoras as pth
+
+def summary(include_current_session:bool = True, print_result:bool = False):
+    """ Get summary of Pythagoras' current state ."""
+
+    # if is_fully_unitialized():
+    #     return "Pythagoras is not initialized."
+    # if not is_correctly_initialized():
+    #     return "Pythagoras is not correctly initialized."
+
+
+    result = "\n"
+    result += 20*"~" + " PERSISTENT STATE: " + 20*"~" + "\n"
+    result += f"Base directory: {pth.base_dir} \n"
+    result += f"# of values stored: {len(pth.value_store)} \n"
+    result += f"# of function execution results cached: {len(pth.execution_results)} \n"
+    result += f"# of exceptions recorded: {len(pth.crash_history)} \n"
+    result += f"# of events recorded: {len(pth.event_log)} \n"
+    result += f"Execution queue size: {len(pth.execution_requests)} \n"
+    result += f"# of active nodes:{len(pth.compute_nodes.pkl)} \n"
+
+
+    if include_current_session:
+
+        result += 21*"~" +  " CURRENT SESSION: " + 21*"~" + "\n"
+        result += f"{len(pth.all_autonomous_functions)=} \n"
+        result += f"{[island for island in pth.all_autonomous_functions]=} \n"
+        result += f"{pth.default_island_name=} \n"
+        default_island = pth.all_autonomous_functions[pth.default_island_name]
+        result += f"{len(default_island)=} \n"
+        result += f"{[func for func in default_island]=} \n"
+
+    result += 60*"~" + "\n\n"
+
+    if print_result:
+        print(result)
+
+    return result
+
