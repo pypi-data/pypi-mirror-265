df42b7988da10aa23e0f7c638ed32b207bf5825a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-03 16:05:20 +0000

    Add package isar/1.16.17/isar-1.16.17.tar.gz

---
+++ packages/isar/isar-1.16.17.tar.gz/isar-1.16.17/src/robot_interface/telemetry/mqtt_client.py
@@ -0,0 +1,81 @@
+import time
+from abc import ABCMeta, abstractmethod
+from queue import Queue
+from typing import Callable, Tuple
+
+from robot_interface.models.exceptions.robot_exceptions import (
+    RobotTelemetryException,
+)
+
+
+class MqttClientInterface(metaclass=ABCMeta):
+    @abstractmethod
+    def publish(
+        self, topic: str, payload: str, qos: int = 0, retain: bool = False
+    ) -> None:
+        """
+        Parameters
+        ----------
+        topic : string
+            MQTT topic to publish to
+        payload : string
+            Payload to send to publish on the topic
+        qos : integer
+            Quality of Service
+        retain : boolean
+            Retain on topic
+
+        Returns
+        -------
+
+        """
+        pass
+
+
+class MqttPublisher(MqttClientInterface):
+    def __init__(self, mqtt_queue: Queue) -> None:
+        self.mqtt_queue: Queue = mqtt_queue
+
+    def publish(
+        self, topic: str, payload: str, qos: int = 0, retain: bool = False
+    ) -> None:
+        queue_message: Tuple[str, str, int, bool] = (topic, payload, qos, retain)
+        self.mqtt_queue.put(queue_message)
+
+
+class MqttTelemetryPublisher(MqttClientInterface):
+    def __init__(
+        self,
+        mqtt_queue: Queue,
+        telemetry_method: Callable,
+        topic: str,
+        interval: float,
+        qos: int = 0,
+        retain: bool = False,
+    ) -> None:
+        self.mqtt_queue: Queue = mqtt_queue
+        self.telemetry_method: Callable = telemetry_method
+        self.topic: str = topic
+        self.interval: float = interval
+        self.qos: int = qos
+        self.retain: bool = retain
+
+    def run(self, isar_id: str, robot_name: str) -> None:
+        while True:
+            try:
+                payload: str = self.telemetry_method(
+                    isar_id=isar_id, robot_name=robot_name
+                )
+            except RobotTelemetryException:
+                continue
+
+            self.publish(
+                topic=self.topic, payload=payload, qos=self.qos, retain=self.retain
+            )
+            time.sleep(self.interval)
+
+    def publish(
+        self, topic: str, payload: str, qos: int = 0, retain: bool = False
+    ) -> None:
+        queue_message: Tuple[str, str, int, bool] = (topic, payload, qos, retain)
+        self.mqtt_queue.put(queue_message)
---
+++ packages/isar/isar-1.16.17.tar.gz/isar-1.16.17/tests/isar/state_machine/states/test_monitor.py
@@ -0,0 +1,53 @@
+import pytest
+
+from isar.state_machine.states.monitor import Monitor
+from robot_interface.models.mission.mission import Mission
+from robot_interface.models.mission.status import StepStatus
+from robot_interface.models.mission.step import Step, TakeImage
+from robot_interface.models.mission.task import Task
+from tests.mocks.step import MockStep
+
+
+@pytest.mark.parametrize(
+    "mock_status, expected_output",
+    [
+        (StepStatus.Successful, True),
+        (StepStatus.Successful, True),
+        (StepStatus.Failed, True),
+    ],
+)
+def test_step_finished(monitor: Monitor, mock_status, expected_output):
+    step: Step = MockStep.drive_to()
+    step.status = mock_status
+    step_completed: bool = monitor._step_finished(
+        step=step,
+    )
+
+    assert step_completed == expected_output
+
+
+@pytest.mark.parametrize(
+    "mock_status, should_queue_upload",
+    [
+        (StepStatus.Successful, True),
+        (StepStatus.Failed, False),
+    ],
+)
+def test_should_only_upload_if_status_is_completed(
+    monitor: Monitor, mock_status, should_queue_upload
+):
+    step: TakeImage = MockStep.take_image_in_coordinate_direction()
+    step.status = mock_status
+    task: Task = Task(steps=[step])
+    mission: Mission = Mission(tasks=[task])
+
+    monitor.state_machine.current_mission = mission
+    monitor.state_machine.current_task = task
+    monitor.state_machine.current_step = step
+
+    if monitor._should_upload_inspections():
+        monitor._queue_inspections_for_upload(mission, step)
+
+    assert monitor.state_machine.queues.upload_queue.empty() == (
+        not should_queue_upload
+    )
