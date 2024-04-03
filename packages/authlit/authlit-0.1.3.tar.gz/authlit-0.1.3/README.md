4b3dcd2cf92e13b79ec7ec8b124376624c65f508
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-03 17:05:28 +0000

    Add package authlit/0.1.3/authlit-0.1.3.tar.gz

---
+++ packages/authlit/authlit-0.1.3.tar.gz/authlit-0.1.3/README.md
@@ -0,0 +1,12 @@
+# Authlit
+
+## Overview
+
+A streamlit library which provides a Login/Sign-Up UI with an option to reset password with Email OTP Validation, also supports cookies.
+
+## Installation
+
+You can install the package via pip:
+
+```bash
+pip install authlit
---
+++ packages/authlit/authlit-0.1.3.tar.gz/authlit-0.1.3/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="authlit",
+    version="0.1.3",
+    author="Badal Sahani",
+    author_email="badalsahani8381@gmail.com",
+    description="A streamlit library which provides a Login/Sign-Up UI with an option to reset password, also supports cookies.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AGI-24/st-auth.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        "streamlit",
+        "extra_streamlit_components",
+        "streamlit_option_menu",
+        "streamlit_cookies_manager",
+        "sqlalchemy",
+        "pymysql",
+        "pymongo",
+        "argon2-cffi",
+        "bcrypt",
+        "PyYAML",
+    ],
+)
