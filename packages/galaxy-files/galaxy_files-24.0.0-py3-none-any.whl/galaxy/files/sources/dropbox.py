ca3211634ccc19139eb1f61204a2e31e863c34bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-03 15:05:12 +0000

    Add package galaxy-files/24.0.0/galaxy_files-24.0.0-py3-none-any.whl

---
+++ packages/galaxy-files/galaxy_files-24.0.0-py3-none-any.whl/galaxy/files/sources/dropbox.py
@@ -0,0 +1,34 @@
+try:
+    from fs.dropboxfs import DropboxFS
+except ImportError:
+    DropboxFS = None
+
+from typing import (
+    Optional,
+    Union,
+)
+
+from . import (
+    FilesSourceOptions,
+    FilesSourceProperties,
+)
+from ._pyfilesystem2 import PyFilesystem2FilesSource
+
+
+class DropboxFilesSource(PyFilesystem2FilesSource):
+    plugin_type = "dropbox"
+    required_module = DropboxFS
+    required_package = "fs.dropboxfs"
+
+    def _open_fs(self, user_context=None, opts: Optional[FilesSourceOptions] = None):
+        props = self._serialization_props(user_context)
+        extra_props: Union[FilesSourceProperties, dict] = opts.extra_props or {} if opts else {}
+        # accessToken has been renamed to access_token in fs.dropboxfs 1.0
+        if "accessToken" in props:
+            props["access_token"] = props.pop("accessToken")
+
+        handle = DropboxFS(**{**props, **extra_props})
+        return handle
+
+
+__all__ = ("DropboxFilesSource",)
