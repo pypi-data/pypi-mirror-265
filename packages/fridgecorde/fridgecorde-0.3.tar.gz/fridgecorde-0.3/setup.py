2f66268801c70d745931657946ad013af68fbdee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-03 06:06:05 +0000

    Add package fridgecorde/0.3/fridgecorde-0.3.tar.gz

---
+++ packages/fridgecorde/fridgecorde-0.3.tar.gz/fridgecorde-0.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='fridgecorde',
+    version='0.3',
+    packages=find_packages(),
+    description='A custom Discord bot module',
+    author='Ebola',
+    author_email='soulspeed.contact@gmail.com',
+    url='https://github.com/Ebolaishere/discord-e',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
