fab1e4b15ec6d02ac40e46d9c46e8c84ac7c4aee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-03 06:05:44 +0000

    Add package rapids-dependency-file-generator/1.13.0/rapids_dependency_file_generator-1.13.0-py3-none-any.whl

---
+++ packages/rapids-dependency-file-generator/rapids_dependency_file_generator-1.13.0-py3-none-any.whl/rapids_dependency_file_generator/constants.py
@@ -0,0 +1,27 @@
+from enum import Enum
+
+
+class OutputTypes(Enum):
+    CONDA = "conda"
+    REQUIREMENTS = "requirements"
+    PYPROJECT = "pyproject"
+    NONE = "none"
+
+    def __str__(self):
+        return self.value
+
+
+cli_name = "rapids-dependency-file-generator"
+
+default_channels = [
+    "rapidsai",
+    "rapidsai-nightly",
+    "dask/label/dev",
+    "conda-forge",
+    "nvidia",
+]
+
+default_conda_dir = "conda/environments"
+default_requirements_dir = "python"
+default_pyproject_dir = "python"
+default_dependency_file_path = "dependencies.yaml"
