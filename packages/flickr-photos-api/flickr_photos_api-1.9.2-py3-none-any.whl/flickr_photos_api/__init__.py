f2f2af23e203f2dd549316bf4920f13d4d5713cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-03 03:05:28 +0000

    Add package flickr-photos-api/1.9.2/flickr_photos_api-1.9.2-py3-none-any.whl

---
+++ packages/flickr-photos-api/flickr_photos_api-1.9.2-py3-none-any.whl/flickr_photos_api/__init__.py
@@ -0,0 +1,58 @@
+from .api import FlickrPhotosApi
+from .exceptions import (
+    FlickrApiException,
+    InvalidApiKey,
+    InvalidXmlException,
+    ResourceNotFound,
+    LicenseNotFound,
+)
+from .types import (
+    License,
+    User,
+    UserInfo,
+    TakenGranularity,
+    DateTaken,
+    LocationInfo,
+    Size,
+    SafetyLevel,
+    SinglePhoto,
+    CollectionOfPhotos,
+    PhotosFromUrl,
+    PhotosInAlbum,
+    PhotosInGallery,
+    PhotosInGroup,
+    AlbumInfo,
+    GalleryInfo,
+    GroupInfo,
+)
+
+
+__version__ = "1.9.2"
+
+
+__all__ = [
+    "FlickrPhotosApi",
+    "FlickrApiException",
+    "ResourceNotFound",
+    "InvalidApiKey",
+    "InvalidXmlException",
+    "LicenseNotFound",
+    "License",
+    "LocationInfo",
+    "User",
+    "UserInfo",
+    "TakenGranularity",
+    "DateTaken",
+    "Size",
+    "SafetyLevel",
+    "SinglePhoto",
+    "CollectionOfPhotos",
+    "PhotosFromUrl",
+    "PhotosInAlbum",
+    "PhotosInGallery",
+    "PhotosInGroup",
+    "AlbumInfo",
+    "GalleryInfo",
+    "GroupInfo",
+    "__version__",
+]
