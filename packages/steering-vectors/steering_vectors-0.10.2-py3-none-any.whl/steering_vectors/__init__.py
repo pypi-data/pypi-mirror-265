d2e9376fb1a18536f8cff69436742e1c0d78d152
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-03 03:04:22 +0000

    Add package steering-vectors/0.10.2/steering_vectors-0.10.2-py3-none-any.whl

---
+++ packages/steering-vectors/steering_vectors-0.10.2-py3-none-any.whl/steering_vectors/__init__.py
@@ -0,0 +1,43 @@
+__version__ = "0.10.2"
+
+from .aggregators import (
+    Aggregator,
+    logistic_aggregator,
+    mean_aggregator,
+    pca_aggregator,
+)
+from .layer_matching import (
+    LayerMatcher,
+    LayerType,
+    ModelLayerConfig,
+    get_num_matching_layers,
+    guess_and_enhance_layer_config,
+)
+from .record_activations import record_activations
+from .steering_vector import PatchDeltaOperator, SteeringPatchHandle, SteeringVector
+from .train_steering_vector import (
+    SteeringVectorTrainingSample,
+    aggregate_activations,
+    extract_activations,
+    train_steering_vector,
+)
+
+__all__ = [
+    "Aggregator",
+    "mean_aggregator",
+    "pca_aggregator",
+    "logistic_aggregator",
+    "LayerType",
+    "LayerMatcher",
+    "ModelLayerConfig",
+    "get_num_matching_layers",
+    "guess_and_enhance_layer_config",
+    "PatchDeltaOperator",
+    "record_activations",
+    "SteeringVector",
+    "SteeringPatchHandle",
+    "train_steering_vector",
+    "SteeringVectorTrainingSample",
+    "aggregate_activations",
+    "extract_activations",
+]
