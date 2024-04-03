07e68894c83534c1f82258c188c2d54863ea3fd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-03 05:05:42 +0000

    Add package icoder-math/0.0.1/icoder_math-0.0.1-py3-none-any.whl

---
+++ packages/icoder-math/icoder_math-0.0.1-py3-none-any.whl/icoder_math/calculate.py
@@ -0,0 +1,27 @@
+def pow(x: float | int, y: float | int):
+    """
+    Support negative and positive float and int (Tested with 16 modes) \n
+    return x ** y
+
+    """
+    if x < 0 and y > 0:
+        return -(abs(x) ** y)
+
+    if x < 0 and y < 0:
+        return -(abs(x) ** y)
+
+    if x > 0 and y < 0 or x > 0 and y > 0:
+        return x**y
+
+
+def abs(x):
+    """
+    Return the absolute value of the argument (return positive number)
+    """
+    if x > 0:
+        return x
+    else:
+        return x * -1
+
+
+
