38a020154588f4a8ccf57da39011d7a23567cd67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-03 06:05:51 +0000

    Add package torch-model-manager/0.0.3/torch-model-manager-0.0.3.tar.gz

---
+++ packages/torch-model-manager/torch-model-manager-0.0.3.tar.gz/torch-model-manager-0.0.3/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='torch-model-manager',
+    version='0.0.3',
+    description='A package for managing PyTorch models',
+    author='Billal MOKHTARI',
+    author_email='mokhtaribillal1@gmail.com',
+    packages=find_packages(exclude=['tests', 'docs', 'utils']),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url = 'https://github.com/Billal-MOKHTARI/torch-model-manager',
+    install_requires=[
+        'torch',
+        'numpy',
+        'torchvision',
+        'torch'
+        # Add any other dependencies here
+    ],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+
+)
