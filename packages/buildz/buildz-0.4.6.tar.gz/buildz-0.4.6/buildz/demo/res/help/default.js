2d5f4d9a37fa3b3df1add3dcdd06e842263ce8ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-03 05:04:19 +0000

    Add package buildz/0.4.6/buildz-0.4.6.tar.gz

---
+++ packages/buildz/buildz-0.4.6.tar.gz/buildz-0.4.6/buildz/demo/res/help/default.js
@@ -0,0 +1,20 @@
+{
+text=
+r"""run 
+    python -m buildz xf +h [+en]
+or
+    python -m buildz ioc +h [+en]
+or
+    python -m buildz search +h [+en]
+to see help
+"""
+text.en=
+r"""run 
+    python -m buildz xf +h [+en]
+or
+    python -m buildz ioc +h [+en]
+or
+    python -m buildz search +h [+en]
+to see help
+"""
+}
