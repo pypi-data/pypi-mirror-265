18973a5458b71f64c463c5aaa1df9d95e010b624
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-03 15:05:08 +0000

    Add package fishhook/0.3/fishhook-0.3.tar.gz

---
+++ packages/fishhook/fishhook-0.3.tar.gz/fishhook-0.3/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="fishhook",
+    version="0.3",
+    author="chilaxan",
+    author_email="chilaxan@gmail.com",
+    description="Allows for runtime hooking of static class functions",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/chilaxan/fishhook",
+    packages=['fishhook'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+    ext_modules=[setuptools.Extension('fishhook._asm', sources=['fishhook/_asmmodule.c'])],
+    install_requires=['capstone', 'keystone-engine']
+)
