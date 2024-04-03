e498ff3a9e9a7d54a5f12ce5861e55d5cc0c1b6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-03 05:04:25 +0000

    Add package spotify-web-downloader/1.6/spotify_web_downloader-1.6.tar.gz

---
+++ packages/spotify-web-downloader/spotify_web_downloader-1.6.tar.gz/spotify_web_downloader-1.6/spotify_web_downloader/constants.py
@@ -0,0 +1,23 @@
+EXCLUDED_CONFIG_FILE_PARAMS = (
+    "urls",
+    "config_path",
+    "read_urls_as_txt",
+    "no_config_file",
+    "version",
+    "help",
+)
+
+MP4_TAGS_MAP = {
+    "album": "\xa9alb",
+    "album_artist": "aART",
+    "artist": "\xa9ART",
+    "comment": "\xa9cmt",
+    "copyright": "cprt",
+    "lyrics": "\xa9lyr",
+    "media_type": "stik",
+    "rating": "rtng",
+    "release_date": "\xa9day",
+    "title": "\xa9nam",
+}
+
+X_NOT_FOUND_STRING = "{} not found at {}"
