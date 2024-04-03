cf0bec547459743b522c779b91788219151e6116
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-03 14:04:38 +0000

    Add package lmwrapper/0.3.3/lmwrapper-0.3.3-py3-none-any.whl

---
+++ packages/lmwrapper/lmwrapper-0.3.3-py3-none-any.whl/lmwrapper/caching.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+from joblib import Memory
+import diskcache
+
+cur_file = Path(__file__).parent.absolute()
+
+
+def _cache_dir():
+    return cur_file / '../lm_model_cache'
+
+
+def _get_disk_cache_joblib() -> Memory:
+    return Memory(_cache_dir(), verbose=0)
+
+
+def _get_disk_cache_diskcache() -> diskcache.FanoutCache:
+    return diskcache.FanoutCache(
+        str(_cache_dir()), timeout=int(9e9), size_limit=50e9, shards=4, eviction_policy='none')
+
+
+def get_disk_cache():
+    return _get_disk_cache_diskcache()
+
+
+def clear_cache_dir():
+    import shutil
+    shutil.rmtree(_cache_dir())
