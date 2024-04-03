c7b0a33a6dbf08a1df5c161bd7f7cb892ec44eb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-03 15:04:53 +0000

    Add package mypandasikko030/0.0.0/mypandasikko030-0.0.0-py3-none-any.whl

---
+++ packages/mypandasikko030/mypandasikko030-0.0.0-py3-none-any.whl/mypandas/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+  include_package_data = True,
+
+    name = 'mypandasikko030',
+    version='0.0.1',
+    description = 'omission',
+    author = 'ikko030',
+    url= "https://github.com/ikko030/oss",
+    packages = 'mypandas',
+    download_url = "https://github.com/ikko030/oss/archive/refs/tags/v0.0.1.zip",
+    install_requires = ['pytest'],
+    classifers = [
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
