9a4a9882c9fed070f768475602bf85b431e8c547
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-03 03:05:02 +0000

    Add package scisuit/1.2.4/scisuit-1.2.4-py3-none-any.whl

---
+++ packages/scisuit/scisuit-1.2.4-py3-none-any.whl/scisuit/stats/__init__.py
@@ -0,0 +1,43 @@
+import ctypes as _ct
+
+from dataclasses import dataclass as _dataclass
+
+
+
+
+
+from .core import kurt, mode, moveavg, rolling, sample, skew
+
+from .aov import aov, aov2, aov2_results
+from .linregress import linregress
+
+from .test_basic import \
+	test_norm_ad, TestNormRes, \
+	test_f, test_f_Result, \
+	test_sign, test_sign_Result,\
+	test_t, test_t1_result, test_t2_result, test_tpaired_result, \
+	test_z, test_z_Result
+
+
+from .distributions import \
+	dbeta, pbeta, qbeta, rbeta, \
+	dbinom, pbinom, qbinom, rbinom, \
+	dchisq, pchisq, qchisq, rchisq, \
+	dexp, pexp, qexp, rexp, \
+	df, pf, qf, rf, \
+	dgamma, pgamma, qgamma,rgamma, \
+	dgeom, pgeom, qgeom, rgeom, \
+	dhyper, phyper, qhyper, rhyper, \
+	dmultinom, rmultinom, \
+	dnbinom, pnbinom, qnbinom,rnbinom, \
+	dnorm, pnorm, qnorm, rnorm, \
+	dlnorm, plnorm, qlnorm, rlnorm, \
+	dpois, ppois, qpois, rpois, \
+	dsignrank, qsignrank, psignrank, \
+	dt, pt, qt, rt, \
+	dunif, punif, qunif, runif, \
+	dweibull, pweibull, qweibull, rweibull
+
+
+
+
