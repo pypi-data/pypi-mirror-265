1e692502c2f2f1cb6d699494db823d4648af8726
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-03 15:05:08 +0000

    Add package albumentations/1.4.3/albumentations-1.4.3.tar.gz

---
+++ packages/albumentations/albumentations-1.4.3.tar.gz/albumentations-1.4.3/tests/test_random.py
@@ -0,0 +1,37 @@
+import numpy as np
+import pytest
+
+from albumentations import random_utils
+from .utils import set_seed
+
+
+def _calc(args):
+    return args[0](*args[1])
+
+@pytest.mark.parametrize(
+    ["func", "args"],
+    [
+        [random_utils.uniform, [-(1 << 15), 1 << 15, 100]],
+        [random_utils.rand, [10, 10]],
+        [random_utils.randn, [10, 10]],
+        [random_utils.normal, [0, 1, 100]],
+        [random_utils.poisson, [1 << 15, 100]],
+        [random_utils.permutation, [np.arange(1000)]],
+        [random_utils.randint, [-(1 << 15), 1 << 15, 100]],
+        [random_utils.random, [100]],
+        [random_utils.choice, [np.arange(1000), 100]],
+        [random_utils.beta, [0.3, 0.4]],
+        [random_utils.shuffle, [np.arange(1000)]]
+    ],
+)
+def test_multiprocessing(func, args, mp_pool):
+    set_seed(0)
+
+    n = 10
+    status = False
+    for _ in range(n):
+        res = mp_pool.map(_calc, [(func, args), (func, args)])
+        status = not np.allclose(res[0], res[1])
+        if status:
+            break
+    assert status
