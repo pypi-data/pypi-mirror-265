5bb7ba36e6211b770c34b28ed1c1fbbada151281
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-03 03:04:21 +0000

    Add package vdk-core/0.3.1229719287/vdk-core-0.3.1229719287.tar.gz

---
+++ packages/vdk-core/vdk-core-0.3.1229719287.tar.gz/vdk-core-0.3.1229719287/src/vdk/api/plugin/connection_hook_spec.py
@@ -0,0 +1,201 @@
+# Copyright 2023-2024 Broadcom
+# SPDX-License-Identifier: Apache-2.0
+from typing import Container
+from typing import Optional
+
+from vdk.api.plugin.hook_markers import hookspec
+from vdk.internal.builtin_plugins.connection.decoration_cursor import DecorationCursor
+from vdk.internal.builtin_plugins.connection.decoration_cursor import ManagedOperation
+from vdk.internal.builtin_plugins.connection.execution_cursor import (
+    ExecuteOperationResult,
+)
+from vdk.internal.builtin_plugins.connection.execution_cursor import ExecutionCursor
+from vdk.internal.builtin_plugins.connection.recovery_cursor import RecoveryCursor
+
+
+class ConnectionHookSpec:
+    """
+    These are hook specifications that enable plugins to hook
+        at PEP249Connection and PEP249Cursor events during execution.
+    Sequence of evaluation:
+        1. db_connection_validate_operation
+        2. db_connection_decorate_operation
+        * cursor is executed
+            ** in case of recovery needed -> db_connection_recover_operation
+
+    Use Cases
+        db_connection_validate_operation: if valid return  else raise exception
+        db_connection_decorate_operation: ..set_operation( prefix + operation)
+        db_connection_recover_operation: ..get_retries()..retry_operation()..
+    """
+
+    @hookspec
+    def db_connection_validate_operation(
+        self, operation: str, parameters: Optional[Container]
+    ) -> None:
+        """
+        Validates the query and parameters.
+
+        For example:
+        @hookimpl
+        db_connection_validate_operation(operation, parameters):
+            if(_max_query_length_limit_exceeded(operation, parameters)):
+                raise Exception("max query length limit exceeded")
+
+        :param operation: str
+            Database operation - a SQL query or command to be executed.
+        :param parameters: Optional[Container]
+            Parameters may be provided as sequence or mapping and will be bound to variables in the operation.
+            Variables are specified in a database-specific notation. See chosen database documentation for details.
+        :return:
+        """
+        pass
+
+    @hookspec
+    def db_connection_decorate_operation(
+        self, decoration_cursor: DecorationCursor
+    ) -> None:
+        """
+        Curates the operation and parameters.
+        If an exception is raised, the cursor execution will be suspended.
+
+        For example:
+        @hookimpl
+        db_connection_decorate_operation(decoration_cursor: DecorationCursor):
+            managed_operation = decoration_cursor.get_managed_operation()
+            managed_operation.set_operation("prefix" +
+                                             managed_operation.get_operation())
+
+        :param decoration_cursor: DecorationCursor
+            A PEP249Cursor implementation purposed for query and parameters decoration.
+            Provides operation details and tooling.
+        :return:
+        """
+        pass
+
+    @hookspec
+    def db_connection_before_operation(self, operation: ManagedOperation) -> None:
+        """
+        Curates the operation and parameters.
+        If an exception is raised, the cursor execution will be suspended.
+
+        For example:
+        @hookimpl
+        db_connection_decorate_operation_restrictive(managed_operation: ManagedOperation):
+            managed_operation.set_operation("prefix" +
+                                             managed_operation.get_operation())
+
+        :param operation: ManagedOperation
+            ManagedOperation object. Provides the operation (usually an SQL expression and some metadata)
+        :return:
+        """
+        pass
+
+    @hookspec(firstresult=True)
+    def db_connection_execute_operation(
+        self, execution_cursor: ExecutionCursor
+    ) -> Optional[ExecuteOperationResult]:
+        """
+        The method that executes the actual SQL query using execution cursor.
+
+        When the hook is called the call loop will only invoke up to
+        the first hookimpl which returns a result other then None.
+        It is then taken as result of the overall hook call.
+
+        You can see the default implementation at DefaultConnectionHookImpl.
+
+        If you want to override the default implementation return a non-None result using tryfirst=True or no decorator.
+        But in most cases you would not need to overwrite the default implementation (hence return None).
+        If some hookimpl function raises an exception, the behaviour is as outlined in hook_markers module
+
+        Often it may be needed to wrap around it so that you can track the execution of the query.
+
+        For example:
+
+        .. code-block::
+
+            @hookimpl(hookwrapper=True)
+            db_connection_execute_operation(execution_cursor: ExecutionCursor) -> Optional[int]:
+                # let's track duration of the query
+                start = time.time()
+                log.info(f"Starting query: {execution_cursor.get_managed_operation().get_operation()}")
+                outcome: HookCallResult
+                outcome = yield # we yield the execution to other implementations (including default one)
+                is_success: bool = outcome.excinfo is None
+                log.info(f"Query finished. duration: {time.time() - start}. is_success: {is_success}")
+                # no return!
+
+        Another example - we want to change the result
+
+        .. code-block::
+
+            @hookimpl(hookwrapper=True)
+            db_connection_execute_operation(execution_cursor: ExecutionCursor) -> Optional[int]:
+                outcome: HookCallResult
+                outcome = yield #
+                outcome.force_result(new_result)  # set new return result
+
+        Another example - let's say we are writing vdk-impala plugin and want to print more debug info
+        which is available from the Impala native cursor (provided by impyla library)
+
+        .. code-block::
+
+            @hookimpl(hookwrapper=True)
+            db_connection_execute_operation(execution_cursor: ExecutionCursor) -> Optional[int]:
+                yield # let the query execute first
+                c = cast(impala.interface.Cursor, execution_cursor)
+                log.info(f"Query {execution_cursor.get_managed_operation().get_operation()} debug info:"
+                         f"summary: {c.get_summary()}, profile: {c.get_profile()}")
+
+
+        :param execution_cursor: ExecutionCursor
+            A PEP249Cursor implementation purposed for actual query execution.
+        """
+        pass
+
+    @hookspec
+    def db_connection_recover_operation(self, recovery_cursor: RecoveryCursor) -> None:
+        """
+        Recovers the operation initiated. Retries made number is auto-incremented.
+        If no exception is raised, cursor is considered successfully executed.
+
+        For example:
+        @hookimpl
+        db_connection_recover_operation(recovery_cursor: RecoveryCursor):
+            while recovery_cursor.get_retries() < MAX_RETRIES:
+                try:
+                    recovery_cursor.execute("helper query")
+                    recovery_cursor.retry_operation()
+                    return
+                except:
+                    time.sleep(3 * recovery_cursor.get_retries()) # backoff
+            raise recovery_cursor.get_exception()
+
+        :param recovery_cursor: RecoveryCursor
+            A PEP249Cursor implementation purposed for query and parameters recovery.
+            Provides operation details and tooling.
+        :return:
+        """
+        pass
+
+    @hookspec
+    def db_connection_on_operation_failure(
+        self, operation: ManagedOperation, exception: Exception
+    ) -> None:
+        """
+        Executes if a database operation fails. Gives the caller a chance to log the error,
+        send metrics or otherwise take action on failure.
+
+        For example:
+        @hookimpl
+        db_connection_on_operation_failure(operation: ManagedOperation):
+            log.error(f"Query {operation.get_operation()} failed. Reporting the incident...")
+            metrics_api.report(operation.get_operation(), operation.get_parameters())
+
+        :param operation: ManagedOperation
+            ManagedOperation object. Provides the operation (usually an SQL expression and some metadata)
+        :param exception: Exception
+            the exception that was thrown
+        :return:
+        """
+        pass
---
+++ packages/vdk-core/vdk-core-0.3.1229719287.tar.gz/vdk-core-0.3.1229719287/src/vdk/internal/builtin_plugins/connection/managed_cursor.py
@@ -0,0 +1,403 @@
+# Copyright 2023-2024 Broadcom
+# SPDX-License-Identifier: Apache-2.0
+import logging
+import types
+from datetime import timedelta
+from timeit import default_timer as timer
+from typing import Any
+from typing import cast
+from typing import Collection
+from typing import Container
+from typing import Optional
+
+from vdk.internal.builtin_plugins.connection.connection_hooks import (
+    ConnectionHookSpecFactory,
+)
+from vdk.internal.builtin_plugins.connection.connection_hooks import (
+    DefaultConnectionHookImpl,
+)
+from vdk.internal.builtin_plugins.connection.database_managed_connection import (
+    IDatabaseManagedConnection,
+)
+from vdk.internal.builtin_plugins.connection.decoration_cursor import DecorationCursor
+from vdk.internal.builtin_plugins.connection.decoration_cursor import ManagedOperation
+from vdk.internal.builtin_plugins.connection.execution_cursor import ExecutionCursor
+from vdk.internal.builtin_plugins.connection.proxy_cursor import ProxyCursor
+from vdk.internal.builtin_plugins.connection.recovery_cursor import RecoveryCursor
+from vdk.internal.core import errors
+
+
+class ManagedCursor(ProxyCursor):
+    """
+    PEP249 Managed Cursor. It takes and sits on the control and data path of SQL queries and client.
+    """
+
+    def __init__(
+        self,
+        cursor: Any,
+        log: logging.Logger = None,
+        connection_hook_spec_factory: ConnectionHookSpecFactory = None,
+        managed_database_connection: IDatabaseManagedConnection = None,
+    ):
+        if not log:
+            log = logging.getLogger(__name__)
+        super().__init__(cursor, log)
+        self.__connection_hook_spec = None
+        if connection_hook_spec_factory:
+            self.__connection_hook_spec = (
+                connection_hook_spec_factory.get_connection_hook_spec()
+            )
+
+        self.__managed_database_connection = managed_database_connection
+
+    def __getattr__(self, attr):
+        """
+        Dynamic interception and il of any (non-overridden) attribute access.
+        In case an attribute is not explicitly managed (customized by overriding e.g. execute()) -
+        this attribute is looked up then the call is delegated, ensuring default behaviour success path.
+
+        First, the non-managed attribute call is redirected to the wrapped native cursor if attribute available,
+        otherwise to the superclass if attribute is present.
+        If the attribute is not specified by both the native cursor nor the superclass, an AttributeError is raised.
+
+        Default behaviour availability unblocks various ManagedCursor usages that rely on
+        currently not explicitly defined in the scope of ManagedCursor attributes.
+        E.g. SQLAlchemy dependency that uses the ManagedCursor, does require some specific attributes available.
+
+        For more details on customizing attributes access, see PEP562.
+        """
+        # native cursor
+        if hasattr(self._cursor, attr):
+            if isinstance(getattr(self._cursor, attr), types.MethodType):
+
+                def method(*args, **kwargs):
+                    return getattr(self._cursor, attr)(*args, **kwargs)
+
+                return method
+            return getattr(self._cursor, attr)
+        # superclass
+        if hasattr(super(), attr):
+            if isinstance(getattr(super(), attr), types.MethodType):
+
+                def method(*args, **kwargs):
+                    return getattr(super(), attr)(*args, **kwargs)
+
+                return method
+            return getattr(super(), attr)
+        raise AttributeError
+
+    def execute(
+        self, operation: str, parameters: Optional[Container] = None
+    ) -> None:  # @UnusedVariable
+        managed_operation, decoration_cursor = (
+            ManagedOperation(operation, parameters),
+            None,
+        )
+        if self.__connection_hook_spec or self.__managed_database_connection:
+            self._validate_operation(operation, parameters)
+            self._decorate_operation(managed_operation, operation)
+
+        query_start_time = timer()
+        try:
+            result = self._execute_operation(managed_operation)
+            self._log.info(
+                f"Executing query SUCCEEDED. Query {_get_query_duration(query_start_time)}"
+            )
+            self._after_operation(managed_operation)
+            return result
+        except Exception as e:
+            try:
+                self._recover_operation(e, managed_operation)
+                self._log.info(
+                    f"Recovered query {_get_query_duration(query_start_time)}"
+                )
+            except Exception as e:
+                blamee = errors.ResolvableBy.USER_ERROR
+                self._log.info(f"Failed query {_get_query_duration(query_start_time)}")
+                self._log.error(
+                    "\n".join(
+                        [
+                            "Executing query FAILED.",
+                            errors.MSG_WHY_FROM_EXCEPTION(e),
+                        ]
+                    )
+                )
+                errors.report(
+                    blamee,
+                    e,
+                )
+                raise e
+
+    def _decorate_operation(self, managed_operation: ManagedOperation, operation: str):
+        decorate_operation = None
+
+        # Check if database decorate operation is overridden by a plugin.
+        # Use the overridden method if available.
+        # Otherwise, verify the presence of hooks; if absent, use the default hook implementation.
+        if (
+            self.__managed_database_connection
+            and type(
+                self.__managed_database_connection
+            ).db_connection_decorate_operation
+            != IDatabaseManagedConnection.db_connection_decorate_operation
+        ):
+            decorate_operation = (
+                self.__managed_database_connection.db_connection_decorate_operation
+            )
+
+        elif (
+            self.__connection_hook_spec
+            and self.__connection_hook_spec.db_connection_decorate_operation.get_hookimpls()
+        ):
+            decorate_operation = (
+                self.__connection_hook_spec.db_connection_decorate_operation
+            )
+
+        # apply any changes @hookimpls to the managed operation
+        if (
+            self.__connection_hook_spec
+            and self.__connection_hook_spec.db_connection_before_operation.get_hookimpls()
+        ):
+            self.__connection_hook_spec.db_connection_before_operation(
+                operation=managed_operation
+            )
+
+        if decorate_operation:
+            self._log.debug("Decorating query:\n%s" % operation)
+            decoration_cursor = DecorationCursor(
+                self._cursor, self._log, managed_operation
+            )
+            try:
+                decorate_operation(decoration_cursor=decoration_cursor)
+            except Exception as e:
+                self._log.error(
+                    "\n".join(
+                        [
+                            "Decorating query FAILED.",
+                            errors.MSG_WHY_FROM_EXCEPTION(e),
+                        ]
+                    )
+                )
+                errors.report(errors.ResolvableBy.PLATFORM_ERROR, e)
+                raise e
+
+    def _validate_operation(self, operation: str, parameters: Optional[Container]):
+        validate_operation = None
+
+        # Check if database validate operation is overridden by a plugin.
+        # Use the overridden method if available.
+        # Otherwise, verify the presence of hooks; if absent, use the default hook implementation.
+        if (
+            self.__managed_database_connection
+            and type(
+                self.__managed_database_connection
+            ).db_connection_validate_operation
+            != IDatabaseManagedConnection.db_connection_validate_operation
+        ):
+            validate_operation = (
+                self.__managed_database_connection.db_connection_validate_operation
+            )
+
+        elif (
+            self.__connection_hook_spec
+            and self.__connection_hook_spec.db_connection_validate_operation.get_hookimpls()
+        ):
+            validate_operation = (
+                self.__connection_hook_spec.db_connection_validate_operation
+            )
+
+        if validate_operation:
+            self._log.debug("Validating query:\n%s" % operation)
+            try:
+                validate_operation(operation=operation, parameters=parameters)
+            except Exception as e:
+                self._log.error(
+                    "\n".join(
+                        [
+                            "Validating query FAILED.",
+                            errors.MSG_WHY_FROM_EXCEPTION(e),
+                        ]
+                    )
+                )
+                errors.report(
+                    errors.ResolvableBy.USER_ERROR,
+                    exception=e,
+                )
+                raise e
+
+    def _execute_operation(self, managed_operation: ManagedOperation):
+        self._log.info("Executing query:\n%s" % managed_operation.get_operation())
+        execution_cursor = ExecutionCursor(self._cursor, managed_operation, self._log)
+
+        # Check if database execute operation is overridden by a plugin.
+        # Use the overridden method if available.
+        # Otherwise, verify the presence of hooks; if absent, use the default hook implementation.
+        if (
+            self.__managed_database_connection
+            and type(self.__managed_database_connection).db_connection_execute_operation
+            != IDatabaseManagedConnection.db_connection_execute_operation
+        ):
+            result = self.__managed_database_connection.db_connection_execute_operation(
+                execution_cursor=execution_cursor
+            )
+
+        elif self.__connection_hook_spec:
+            result = self.__connection_hook_spec.db_connection_execute_operation(
+                execution_cursor=execution_cursor
+            )
+        else:
+            self._log.debug(
+                "No connection hook spec defined. "
+                "Will invoke standard cursor execute implementation."
+            )
+            result = DefaultConnectionHookImpl().db_connection_execute_operation(
+                execution_cursor
+            )
+        return result
+
+    def _after_operation(self, managed_operation: ManagedOperation):
+        execution_cursor = ExecutionCursor(self._cursor, managed_operation, self._log)
+
+        if (
+            self.__managed_database_connection
+            and self.__managed_database_connection.db_connection_after_operation
+        ):
+            try:
+                self._log.debug(
+                    "Executing after operation:\n%s" % managed_operation.get_operation()
+                )
+                self.__managed_database_connection.db_connection_after_operation(
+                    execution_cursor=execution_cursor
+                )
+            except Exception as e:
+                self._log.error(
+                    "\n".join(
+                        [
+                            "After query FAILED.",
+                            errors.MSG_WHY_FROM_EXCEPTION(e),
+                        ]
+                    )
+                )
+                errors.report(
+                    errors.ResolvableBy.USER_ERROR,
+                    exception=e,
+                )
+                raise e
+
+    def fetchall(self) -> Collection[Collection[Any]]:
+        self._log.info("Fetching all results from query ...")
+        try:
+            res = self._cursor.fetchall()
+            self._log.info("Fetching all results from query SUCCEEDED.")
+            return cast(Collection[Collection[Any]], res)
+        except:
+            self._log.info("Fetching all results from query FAILED.")
+            raise
+
+    def close(self) -> None:
+        self._log.info("Closing DB cursor ...")
+        self._cursor.close()
+        self._log.info("Closing DB cursor SUCCEEDED.")
+
+    def _recover_operation(self, exception, managed_operation):
+        # TODO: configurable generic re-try.
+
+        # Check if database recover and decorate operations are overridden by a plugin.
+        # Use the overridden methods if available.
+        # Otherwise, verify the presence of hooks; if absent, use the default hook implementation.
+
+        available_recovery_plugin_implementation = (
+            self.__managed_database_connection
+            and (
+                type(self.__managed_database_connection).db_connection_recover_operation
+                != IDatabaseManagedConnection.db_connection_recover_operation
+            )
+        )
+
+        available_decoration_plugin_implementation = (
+            self.__managed_database_connection
+            and type(
+                self.__managed_database_connection
+            ).db_connection_decorate_operation
+            != IDatabaseManagedConnection.db_connection_decorate_operation
+        )
+
+        if (
+            not self.__connection_hook_spec
+            or not self.__connection_hook_spec.db_connection_recover_operation.get_hookimpls()
+        ) and not available_recovery_plugin_implementation:
+            raise exception
+
+        if available_decoration_plugin_implementation:
+            decorate_operation = (
+                self.__managed_database_connection.db_connection_decorate_operation
+            )
+        elif (
+            self.__connection_hook_spec
+            and self.__connection_hook_spec.db_connection_decorate_operation.get_hookimpls()
+        ):
+            decorate_operation = (
+                self.__connection_hook_spec.db_connection_decorate_operation
+            )
+        else:
+            decorate_operation = None
+
+        if available_recovery_plugin_implementation:
+            recover_operation = (
+                self.__managed_database_connection.db_connection_recover_operation
+            )
+        else:
+            recover_operation = (
+                self.__connection_hook_spec.db_connection_recover_operation
+            )
+
+        # apply any changes from @hookimpls to the managed operation
+        if (
+            self.__connection_hook_spec
+            and self.__connection_hook_spec.db_connection_before_operation.get_hookimpls()
+        ):
+            self.__connection_hook_spec.db_connection_before_operation(
+                operation=managed_operation
+            )
+
+        # apply any changes from @hookimpls to the managed operation
+        if (
+            self.__connection_hook_spec
+            and self.__connection_hook_spec.db_connection_on_operation_failure.get_hookimpls()
+        ):
+            self.__connection_hook_spec.db_connection_on_operation_failure(
+                operation=managed_operation, exception=exception
+            )
+
+        recovery_cursor = RecoveryCursor(
+            self._cursor,
+            self._log,
+            exception,
+            managed_operation,
+            decorate_operation,
+        )
+        self._log.debug(f"Recovery of query {managed_operation.get_operation()}")
+        try:
+            recover_operation(recovery_cursor=recovery_cursor)
+            self._log.debug(
+                f"Recovery of query SUCCEEDED "
+                f"after {(recovery_cursor.get_retries())} retries."
+            )
+            self._after_operation(managed_operation)
+        except Exception as e:
+            self._log.debug(
+                f"Recovery of query FAILED "
+                f"after {(recovery_cursor.get_retries())} retries."
+            )
+            if type(e) is type(exception) and e.args == exception.args:  # re-raised
+                raise exception
+            raise e from exception  # keep track of originating one
+
+
+def _get_query_duration(query_start_time: float):
+    query_end_time = timer()
+    seconds = timedelta(seconds=query_end_time - query_start_time).total_seconds()
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    difference = f"{int(hours):02}h:{int(minutes):02}m:{int(seconds):02}s"
+    return f"duration {difference}"
