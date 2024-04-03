7a7934b81186cd406181fba84113a222451f86e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-03 15:05:04 +0000

    Add package evalplus/0.2.2/evalplus-0.2.2.tar.gz

---
+++ packages/evalplus/evalplus-0.2.2.tar.gz/evalplus-0.2.2/tools/sanitize.py
@@ -0,0 +1,8 @@
+"""This script is kept for compatibility. Use evalplus.sanitize if possible."""
+
+from evalplus.sanitize import main
+
+if __name__ == "__main__":
+    from fire import Fire
+
+    Fire(main)
