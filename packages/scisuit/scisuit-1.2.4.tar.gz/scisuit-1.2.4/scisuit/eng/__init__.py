0055bd61aed3ac161ec52152578cffa607cd9beb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-03 03:05:02 +0000

    Add package scisuit/1.2.4/scisuit-1.2.4.tar.gz

---
+++ packages/scisuit/scisuit-1.2.4.tar.gz/scisuit-1.2.4/scisuit/eng/__init__.py
@@ -0,0 +1,5 @@
+from .fpe import *
+from .humidair import psychrometry
+from .dryair import Air
+from .refrigerants import Refrigerant
+from .water import Water
---
+++ packages/scisuit/scisuit-1.2.4.tar.gz/scisuit-1.2.4/scisuit/plot/__init__.py
@@ -0,0 +1,11 @@
+from .charts import layout, subplot, figure, legend, show, title, \
+	 				xlabel, ylabel, xlim, ylim, set_xticks, set_yticks, set_xposition, set_yposition
+
+from .charts import canvas, boxplot, hist, psychrometry, scatter 
+
+from .quiverchart import quiver, dirfield
+from .qqcharts import qqnorm, qqplot
+from .bubblechart import bubble
+from .barcharts import bar, barh
+
+from .chartelems import Marker
