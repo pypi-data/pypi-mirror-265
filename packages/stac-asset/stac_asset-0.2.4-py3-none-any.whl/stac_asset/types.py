c973facf05fa42f1a4b4b812d2e8f91e4f81c305
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-03 05:05:39 +0000

    Add package stac-asset/0.2.4/stac_asset-0.2.4-py3-none-any.whl

---
+++ packages/stac-asset/stac_asset-0.2.4-py3-none-any.whl/stac_asset/types.py
@@ -0,0 +1,28 @@
+from asyncio import Queue
+from os import PathLike
+from typing import TYPE_CHECKING, Union
+
+from .messages import Message
+
+if TYPE_CHECKING:
+    from typing import Any
+
+    _PathLike = PathLike[Any]
+    # Needed until we drop Python 3.8
+    MessageQueue = Queue[Message]
+else:
+    _PathLike = PathLike
+    MessageQueue = Queue
+
+PathLikeObject = Union[_PathLike, str]
+"""An object representing a file system path, except we exclude `bytes` because
+`Path()` doesn't accept `bytes`.
+
+A path-like object is either a str or bytes object representing a path, or an
+object implementing the os.PathLike protocol. An object that supports the
+os.PathLike protocol can be converted to a str or bytes file system path by
+calling the os.fspath() function; os.fsdecode() and os.fsencode() can be used to
+guarantee a str or bytes result instead, respectively. Introduced by PEP 519.
+
+https://docs.python.org/3/glossary.html#term-path-like-object
+"""
