ae47c6ea5f20230c289ee25275f651110f62f642
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-03 14:05:04 +0000

    Add package types-aiobotocore-codeartifact/2.12.2/types_aiobotocore_codeartifact-2.12.2-py3-none-any.whl

---
+++ packages/types-aiobotocore-codeartifact/types_aiobotocore_codeartifact-2.12.2-py3-none-any.whl/types_aiobotocore_codeartifact/__init__.pyi
@@ -0,0 +1,55 @@
+"""
+Main interface for codeartifact service.
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_codeartifact import (
+        Client,
+        CodeArtifactClient,
+        ListDomainsPaginator,
+        ListPackageVersionAssetsPaginator,
+        ListPackageVersionsPaginator,
+        ListPackagesPaginator,
+        ListRepositoriesInDomainPaginator,
+        ListRepositoriesPaginator,
+    )
+
+    session = get_session()
+    async with session.create_client("codeartifact") as client:
+        client: CodeArtifactClient
+        ...
+
+
+    list_domains_paginator: ListDomainsPaginator = client.get_paginator("list_domains")
+    list_package_version_assets_paginator: ListPackageVersionAssetsPaginator = client.get_paginator("list_package_version_assets")
+    list_package_versions_paginator: ListPackageVersionsPaginator = client.get_paginator("list_package_versions")
+    list_packages_paginator: ListPackagesPaginator = client.get_paginator("list_packages")
+    list_repositories_paginator: ListRepositoriesPaginator = client.get_paginator("list_repositories")
+    list_repositories_in_domain_paginator: ListRepositoriesInDomainPaginator = client.get_paginator("list_repositories_in_domain")
+    ```
+"""
+
+from .client import CodeArtifactClient
+from .paginator import (
+    ListDomainsPaginator,
+    ListPackagesPaginator,
+    ListPackageVersionAssetsPaginator,
+    ListPackageVersionsPaginator,
+    ListRepositoriesInDomainPaginator,
+    ListRepositoriesPaginator,
+)
+
+Client = CodeArtifactClient
+
+__all__ = (
+    "Client",
+    "CodeArtifactClient",
+    "ListDomainsPaginator",
+    "ListPackageVersionAssetsPaginator",
+    "ListPackageVersionsPaginator",
+    "ListPackagesPaginator",
+    "ListRepositoriesInDomainPaginator",
+    "ListRepositoriesPaginator",
+)
---
+++ packages/types-aiobotocore-codeartifact/types_aiobotocore_codeartifact-2.12.2-py3-none-any.whl/types_aiobotocore_codeartifact/paginator.pyi
@@ -0,0 +1,197 @@
+"""
+Type annotations for codeartifact service client paginators.
+
+[Open documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_codeartifact/paginators/)
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+
+    from types_aiobotocore_codeartifact.client import CodeArtifactClient
+    from types_aiobotocore_codeartifact.paginator import (
+        ListDomainsPaginator,
+        ListPackageVersionAssetsPaginator,
+        ListPackageVersionsPaginator,
+        ListPackagesPaginator,
+        ListRepositoriesPaginator,
+        ListRepositoriesInDomainPaginator,
+    )
+
+    session = get_session()
+    with session.create_client("codeartifact") as client:
+        client: CodeArtifactClient
+
+        list_domains_paginator: ListDomainsPaginator = client.get_paginator("list_domains")
+        list_package_version_assets_paginator: ListPackageVersionAssetsPaginator = client.get_paginator("list_package_version_assets")
+        list_package_versions_paginator: ListPackageVersionsPaginator = client.get_paginator("list_package_versions")
+        list_packages_paginator: ListPackagesPaginator = client.get_paginator("list_packages")
+        list_repositories_paginator: ListRepositoriesPaginator = client.get_paginator("list_repositories")
+        list_repositories_in_domain_paginator: ListRepositoriesInDomainPaginator = client.get_paginator("list_repositories_in_domain")
+    ```
+"""
+
+import sys
+from typing import AsyncIterator, Generic, Iterator, TypeVar
+
+from aiobotocore.paginate import AioPaginator
+from botocore.paginate import PageIterator
+
+from .literals import (
+    AllowPublishType,
+    AllowUpstreamType,
+    PackageFormatType,
+    PackageVersionOriginTypeType,
+    PackageVersionStatusType,
+)
+from .type_defs import (
+    ListDomainsResultTypeDef,
+    ListPackagesResultTypeDef,
+    ListPackageVersionAssetsResultTypeDef,
+    ListPackageVersionsResultTypeDef,
+    ListRepositoriesInDomainResultTypeDef,
+    ListRepositoriesResultTypeDef,
+    PaginatorConfigTypeDef,
+)
+
+if sys.version_info >= (3, 12):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+
+__all__ = (
+    "ListDomainsPaginator",
+    "ListPackageVersionAssetsPaginator",
+    "ListPackageVersionsPaginator",
+    "ListPackagesPaginator",
+    "ListRepositoriesPaginator",
+    "ListRepositoriesInDomainPaginator",
+)
+
+_ItemTypeDef = TypeVar("_ItemTypeDef")
+
+class _PageIterator(Generic[_ItemTypeDef], PageIterator):
+    def __iter__(self) -> Iterator[_ItemTypeDef]:
+        """
+        Proxy method to specify iterator item type.
+        """
+
+class ListDomainsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/codeartifact.html#CodeArtifact.Paginator.ListDomains)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_codeartifact/paginators/#listdomainspaginator)
+    """
+
+    def paginate(
+        self, *, PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> AsyncIterator[ListDomainsResultTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/codeartifact.html#CodeArtifact.Paginator.ListDomains.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_codeartifact/paginators/#listdomainspaginator)
+        """
+
+class ListPackageVersionAssetsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/codeartifact.html#CodeArtifact.Paginator.ListPackageVersionAssets)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_codeartifact/paginators/#listpackageversionassetspaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        domain: str,
+        repository: str,
+        format: PackageFormatType,
+        package: str,
+        packageVersion: str,
+        domainOwner: str = ...,
+        namespace: str = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> AsyncIterator[ListPackageVersionAssetsResultTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/codeartifact.html#CodeArtifact.Paginator.ListPackageVersionAssets.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_codeartifact/paginators/#listpackageversionassetspaginator)
+        """
+
+class ListPackageVersionsPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/codeartifact.html#CodeArtifact.Paginator.ListPackageVersions)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_codeartifact/paginators/#listpackageversionspaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        domain: str,
+        repository: str,
+        format: PackageFormatType,
+        package: str,
+        domainOwner: str = ...,
+        namespace: str = ...,
+        status: PackageVersionStatusType = ...,
+        sortBy: Literal["PUBLISHED_TIME"] = ...,
+        originType: PackageVersionOriginTypeType = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> AsyncIterator[ListPackageVersionsResultTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/codeartifact.html#CodeArtifact.Paginator.ListPackageVersions.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_codeartifact/paginators/#listpackageversionspaginator)
+        """
+
+class ListPackagesPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/codeartifact.html#CodeArtifact.Paginator.ListPackages)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_codeartifact/paginators/#listpackagespaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        domain: str,
+        repository: str,
+        domainOwner: str = ...,
+        format: PackageFormatType = ...,
+        namespace: str = ...,
+        packagePrefix: str = ...,
+        publish: AllowPublishType = ...,
+        upstream: AllowUpstreamType = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> AsyncIterator[ListPackagesResultTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/codeartifact.html#CodeArtifact.Paginator.ListPackages.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_codeartifact/paginators/#listpackagespaginator)
+        """
+
+class ListRepositoriesPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/codeartifact.html#CodeArtifact.Paginator.ListRepositories)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_codeartifact/paginators/#listrepositoriespaginator)
+    """
+
+    def paginate(
+        self, *, repositoryPrefix: str = ..., PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> AsyncIterator[ListRepositoriesResultTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/codeartifact.html#CodeArtifact.Paginator.ListRepositories.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_codeartifact/paginators/#listrepositoriespaginator)
+        """
+
+class ListRepositoriesInDomainPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/codeartifact.html#CodeArtifact.Paginator.ListRepositoriesInDomain)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_codeartifact/paginators/#listrepositoriesindomainpaginator)
+    """
+
+    def paginate(
+        self,
+        *,
+        domain: str,
+        domainOwner: str = ...,
+        administratorAccount: str = ...,
+        repositoryPrefix: str = ...,
+        PaginationConfig: PaginatorConfigTypeDef = ...,
+    ) -> AsyncIterator[ListRepositoriesInDomainResultTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/codeartifact.html#CodeArtifact.Paginator.ListRepositoriesInDomain.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_codeartifact/paginators/#listrepositoriesindomainpaginator)
+        """
