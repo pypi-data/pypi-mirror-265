716134d0cf18e4ae3d3d892a1c7296d87e1ebf22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-04 06:06:04 +0000

    Add package art-training/1.0.0/art_training-1.0.0-py3-none-any.whl

---
+++ packages/art-training/art_training-1.0.0-py3-none-any.whl/art/utils/exceptions.py
@@ -0,0 +1,13 @@
+"""All predefined exceptions."""
+
+
+class CheckFailedException(Exception):
+    """Exception raised when a check fails."""
+
+    pass
+
+
+class SavingArtResultsException(Exception):
+    """Exception raised when saving ART results fails."""
+
+    pass
