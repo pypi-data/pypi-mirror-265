bf06d5063f7044ab3e4d257ea379e4646ff50f90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-04 06:05:59 +0000

    Add package Dynamojo/0.2.2/dynamojo-0.2.2-py3-none-any.whl

---
+++ packages/Dynamojo/dynamojo-0.2.2-py3-none-any.whl/dynamojo/__init__.py
@@ -0,0 +1,30 @@
+from .base import DynamojoBase
+from .config import DynamojoConfig, JoinedAttribute
+from .index import (
+    get_indexes,
+    Gsi,
+    Index,
+    IndexList,
+    IndexMap,
+    Lsi,
+    Mutator,
+    TableIndex,
+)
+from .utils import Delta
+
+
+__all__ = [
+    "Delta",
+    "DynamojoBase",
+    "DynamojoConfig",
+    "get_indexes",
+    "Gsi",
+    "Index",
+    "IndexList",
+    "IndexMap",
+    "JoinedAttribute",
+    "Lsi",
+    "Mutator",
+    "TableIndex"
+]
+
