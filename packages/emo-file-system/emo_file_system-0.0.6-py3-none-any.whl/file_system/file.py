9b7e5dffa2aae05a582a6c6a904938cb01b053ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-04 03:05:22 +0000

    Add package emo-file-system/0.0.6/emo_file_system-0.0.6-py3-none-any.whl

---
+++ packages/emo-file-system/emo_file_system-0.0.6-py3-none-any.whl/file_system/file.py
@@ -0,0 +1,77 @@
+import os
+import json
+import shutil
+from typing import Dict, List, Union
+
+
+class File:
+
+    @classmethod
+    def create_file_directory(cls, file_path: str):
+        directory = os.path.dirname(file_path)
+        cls.create_directory(directory)
+
+    @staticmethod
+    def create_directory(path: str):
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+    @staticmethod
+    def exists(path: str) -> bool:
+        return os.path.exists(path)
+
+    @classmethod
+    def remove(cls, path: str):
+        if cls.exists(path):
+            if os.path.isdir(path):
+                shutil.rmtree(path)
+            else:
+                os.remove(path)
+
+    @staticmethod
+    def read_lines(
+        filepath: str,
+        mode: str = "r",
+        encoding: str = "utf-8"
+    ) -> Union[List, Dict]:
+        with open(filepath, mode, encoding=encoding) as f:
+            return f.readlines()
+
+    @staticmethod
+    def read(filepath: str, mode: str = "r", encoding: str = "utf-8"):
+        with open(filepath, mode, encoding=encoding if mode == "r" else None) as f:
+            return f.read()
+
+    @staticmethod
+    def read_json(filepath: str, mode: str = "r", encoding: str = "utf-8"):
+        with open(filepath, mode, encoding=encoding if mode == "r" else None) as f:
+            return json.load(f)
+
+    @staticmethod
+    def write_lines(
+        filepath: str,
+        lines: list,
+        mode: str = "w",
+        encoding: str = "utf-8"
+    ):
+        with open(filepath, mode, encoding=encoding) as f:
+            f.writelines(lines)
+
+    @staticmethod
+    def write_json(
+        filepath: str,
+        lines: list,
+        mode: str = "w",
+        encoding: str = "utf-8"
+    ):
+        with open(filepath, mode, encoding=encoding) as f:
+            json.dump(lines, f, ensure_ascii=False)
+
+    @staticmethod
+    def get_filename(path, without_extentsion: bool = False) -> str:
+        filename = os.path.basename(path)
+
+        if not without_extentsion:
+            return filename
+
+        return os.path.splitext(filename)[0]
