95084e78b60069b194313276878fe6317586b540
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-04 06:05:20 +0000

    Add package financefunctions/0.0.1/financefunctions-0.0.1-py3-none-any.whl

---
+++ packages/financefunctions/financefunctions-0.0.1-py3-none-any.whl/financefunctions/__init__.py
@@ -0,0 +1,26 @@
+"""
+# =============================================================================
+#
+#  Licensed Materials, Property of Ralph Vogl, Munich
+#
+#  Project : stocksdatabase
+#
+#  Copyright (c) by Ralph Vogl
+#
+#  All rights reserved.
+#
+#  Description:
+#
+#  a simple library to handle ohlcv data in a database environment
+#
+# =============================================================================
+"""
+
+# -------------------------------------------------------------
+# IMPORTS
+# -------------------------------------------------------------
+from financefunctions.dataframes import FinanceFunctionsDataFrame
+
+__all__ = [
+    "FinanceFunctionsDataFrame",
+]
