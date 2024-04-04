c288aedc1ec5167a3b1a43edcf6c3dd12bf48411
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-04 03:05:54 +0000

    Add package setaset/0.0.2/setaset-0.0.2.tar.gz

---
+++ packages/setaset/setaset-0.0.2.tar.gz/setaset-0.0.2/README.md
@@ -0,0 +1,8 @@
+## Background
+This project is used for testing and research on set theory.
+## Install
+Simply use pip.
+`$ pip install setaset`
+## Change Log
+0.0.1 add a subset function.
+0.0.2 fix a install-bug.
---
+++ packages/setaset/setaset-0.0.2.tar.gz/setaset-0.0.2/src/setaset/setop.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""
+@Project ：setaset 
+@File    ：setop.py
+@IDE     ：PyCharm 
+@Author  ：Richard_GUA
+@Date    ：2024/3/3 21:03
+"""
+
+
+def issubsetof(seta: set, setb: set):
+    return seta.issubset(setb)
