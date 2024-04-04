4a6b3c3e20773b2c962fa1b6919aa99351a008dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-04 06:05:15 +0000

    Add package codeforces-scraper/0.3.2/codeforces-scraper-0.3.2.tar.gz

---
+++ packages/codeforces-scraper/codeforces-scraper-0.3.2.tar.gz/codeforces-scraper-0.3.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+setuptools.setup(
+        name='codeforces-scraper',
+        version='0.3.2',
+        author='thematdev',
+        author_email='thematdev@thematdev.org',
+        description='Utility to do actions on codeforces',
+        packages=['codeforces_scraper', 'codeforces_scraper.assets'],
+        install_requires=[
+            'bs4',
+            'lxml',
+            'pydantic',
+            'requests',
+        ],
+        python_requires='>=3.8',
+        zip_safe=True,
+        package_data={
+            'codeforces_scraper.assets': ['*']
+        }
+)
