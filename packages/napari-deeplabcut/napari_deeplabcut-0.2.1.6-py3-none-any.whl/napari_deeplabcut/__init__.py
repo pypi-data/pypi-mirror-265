28af82b6456d6e493d2c8b1132aa4e1eeb2fcf63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-04 04:05:34 +0000

    Add package napari-deeplabcut/0.2.1.6/napari_deeplabcut-0.2.1.6-py3-none-any.whl

---
+++ packages/napari-deeplabcut/napari_deeplabcut-0.2.1.6-py3-none-any.whl/napari_deeplabcut/__init__.py
@@ -0,0 +1,19 @@
+import warnings
+
+# FIXME: Circumvent the need to access window.qt_viewer
+warnings.filterwarnings("ignore", category=FutureWarning)
+
+try:
+    from ._version import version as __version__
+except ImportError:  # pragma: no cover
+    __version__ = "unknown"
+
+
+from ._reader import (
+    get_hdf_reader,
+    get_image_reader,
+    get_video_reader,
+    get_folder_parser,
+    get_config_reader,
+)
+from ._writer import write_hdf, write_masks
