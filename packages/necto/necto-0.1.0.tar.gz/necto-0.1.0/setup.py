367737eaef286b04f3445aa16a4fccef4caf8fa6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-04 05:05:50 +0000

    Add package necto/0.1.0/necto-0.1.0.tar.gz

---
+++ packages/necto/necto-0.1.0.tar.gz/necto-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='necto',
+    version='0.1.0',
+    author='The Necto Team',
+    author_email='email@example.com',
+    description='Reserved package',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
