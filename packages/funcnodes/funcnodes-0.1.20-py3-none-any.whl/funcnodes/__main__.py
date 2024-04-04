8f0f448912d27a93872e7752ac116023cfc49ef4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-04 03:05:30 +0000

    Add package funcnodes/0.1.20/funcnodes-0.1.20-py3-none-any.whl

---
+++ packages/funcnodes/funcnodes-0.1.20-py3-none-any.whl/funcnodes/__main__.py
@@ -0,0 +1,83 @@
+from typing import Type
+from funcnodes.frontends.funcnodes_react import run_server
+import funcnodes as fn
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Funcnodes Cli.")
+    parser.add_argument(
+        "task",
+        help="The task to perform",
+        choices=["runserver", "startworker", "startworkermanager"],
+    )
+    parser.add_argument(
+        "--port",
+        help="Port to run the server on",
+        default=fn.config.CONFIG["frontend"]["port"],
+    )
+    parser.add_argument(
+        "--no-browser",
+        help="Open the browser after starting the server",
+        action="store_false",
+    )
+
+    parser.add_argument(
+        "--workertype",
+        help="The type of worker to start",
+        default="WSWorker",
+    )
+    parser.add_argument(
+        "--new",
+        help="Create a new instance",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--uuid",
+        help="The uuid of the worker to start",
+        default=None,
+        required=False,
+    )
+
+    # Add version argument
+    parser.add_argument(
+        "--version", action="version", version=f"%(prog)s {fn.__version__}"
+    )
+
+    args = parser.parse_args()
+    if hasattr(args, "task") and args.task is not None:
+        try:
+            # Example of handling tasks
+            if args.task == "runserver":
+                run_server(port=args.port, open_browser=args.no_browser)
+
+            elif args.task == "startworker":
+                worker_class: Type[fn.worker.Worker] = getattr(
+                    fn.worker, args.workertype
+                )
+                if args.new:
+                    fn.FUNCNODES_LOGGER.info(
+                        f"Starting new worker of type {args.workertype}"
+                    )
+                    worker = worker_class()
+                    worker.run_forever()
+                else:
+                    fn.FUNCNODES_LOGGER.info(
+                        f"Starting existing worker of type {args.workertype} with uuid {args.uuid}"
+                    )
+                    if args.uuid is None:
+                        raise Exception("uuid is required to start an existing worker")
+                    worker = worker_class(uuid=args.uuid)
+                    worker.run_forever()
+
+            elif args.task == "startworkermanager":
+                fn.worker.worker_manager.start_worker_manager()
+            else:
+                print(f"Unknown task: {args.task}")
+        except Exception as e:
+            fn.FUNCNODES_LOGGER.exception(e)
+            raise
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/funcnodes/funcnodes-0.1.20-py3-none-any.whl/funcnodes/lib/__init__.py
@@ -0,0 +1,24 @@
+from .lib import (
+    Shelf,
+    serialize_shelfe,
+    FullLibJSON,
+    Library,
+    NodeClassNotFoundError,
+    get_node_in_shelf,
+)
+
+from .libparser import module_to_shelf
+
+from .libfinder import find_shelf
+
+
+__all__ = [
+    "Shelf",
+    "module_to_shelf",
+    "serialize_shelfe",
+    "FullLibJSON",
+    "Library",
+    "find_shelf",
+    "NodeClassNotFoundError",
+    "get_node_in_shelf",
+]
