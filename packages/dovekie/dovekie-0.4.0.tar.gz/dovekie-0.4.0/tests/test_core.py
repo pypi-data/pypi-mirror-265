b0ae1a2cb921960538988ac5762819320f381793
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-04 14:05:49 +0000

    Add package dovekie/0.4.0/dovekie-0.4.0.tar.gz

---
+++ packages/dovekie/dovekie-0.4.0.tar.gz/dovekie-0.4.0/tests/test_core.py
@@ -0,0 +1,20 @@
+import operator as op
+from itertools import accumulate
+
+from dovekie.core import _add, _b, _l_, _phi1_, _r_
+
+
+def test_l():
+    assert _l_(1, None) == 1
+
+def test_r():
+    assert _r_(None, 2) == 2
+
+def mco(xs: list[int]) -> int:
+    return max(accumulate(xs, _phi1_(op.add, op.mul, _r_)))
+
+def test_phi1_mco():
+    assert mco([1, 0, 1, 1, 1, 0, 0, 1, 1, 0]) == 3
+
+def test_b():
+    assert _b(op.neg, _add(1))(1) == -2
