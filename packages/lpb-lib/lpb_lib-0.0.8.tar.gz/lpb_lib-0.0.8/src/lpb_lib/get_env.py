9a25f738e0b2ec335b2305db3182e78bfb4b82a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-04 04:05:42 +0000

    Add package lpb-lib/0.0.8/lpb_lib-0.0.8.tar.gz

---
+++ packages/lpb-lib/lpb_lib-0.0.8.tar.gz/lpb_lib-0.0.8/src/lpb_lib/get_env.py
@@ -0,0 +1,19 @@
+import os
+import lpb_lib
+
+def find_code_in_env_file(path, code_to_find):    
+    """
+    Find a code in the .env file and return its value.
+    Args:
+    :path: The path to the .env file.
+    :code_to_find: The code to find in the .env file.
+    Returns:
+        :value: The value of the code in the .env file.
+    """
+    if os.path.exists(".env"):
+        with open(".env", 'r') as file:
+            for line in file:
+                if code_to_find in line:
+                    value = line.split("=")[1]
+                    return value
+    return None
