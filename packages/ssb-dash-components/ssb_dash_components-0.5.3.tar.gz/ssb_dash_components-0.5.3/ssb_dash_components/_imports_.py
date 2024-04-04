b60896a91975758c08985aef1b1fdd078fec329c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-04 03:05:38 +0000

    Add package ssb-dash-components/0.5.3/ssb_dash_components-0.5.3.tar.gz

---
+++ packages/ssb-dash-components/ssb_dash_components-0.5.3.tar.gz/ssb_dash_components-0.5.3/ssb_dash_components/_imports_.py
@@ -0,0 +1,21 @@
+from .Accordion import Accordion
+from .Button import Button
+from .Dropdown import Dropdown
+from .Glossary import Glossary
+from .Header import Header
+from .Input import Input
+from .Paragraph import Paragraph
+from .Tabs import Tabs
+from .Title import Title
+
+__all__ = [
+    "Accordion",
+    "Button",
+    "Dropdown",
+    "Glossary",
+    "Header",
+    "Input",
+    "Paragraph",
+    "Tabs",
+    "Title"
+]
