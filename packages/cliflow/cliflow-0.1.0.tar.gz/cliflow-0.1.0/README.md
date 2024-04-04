19f95fec9c10f50828ad766ef506031d3b68f739
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-04 06:05:50 +0000

    Add package cliflow/0.1.0/cliflow-0.1.0.tar.gz

---
+++ packages/cliflow/cliflow-0.1.0.tar.gz/cliflow-0.1.0/README.md
@@ -0,0 +1,13 @@
+<p align="center">
+  <a>
+    <picture>
+        <source media="(prefers-color-scheme: dark)" srcset="assets/logo-python.png">
+      <img src="assets/logo-python.png">
+    </picture>
+    <h1 align="center">CLI Flow - Python</h1>
+  </a>
+</p>
+
+<p align="center">
+  <a href="https://docs.cliflow.org">View documentation on website</a>
+</p>
---
+++ packages/cliflow/cliflow-0.1.0.tar.gz/cliflow-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+    name='cliflow',
+    version='0.1.0',
+    author='The Cliflow Team',
+    author_email='email@example.com',
+    description='Reserved Package',
+    packages=find_packages(),
+    classifiers=[
+    'Programming Language :: Python :: 3',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
