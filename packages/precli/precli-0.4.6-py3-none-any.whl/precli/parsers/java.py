bd3270309c2f53df0bdc3a7311ff40c83b14bf95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-04 06:06:08 +0000

    Add package precli/0.4.6/precli-0.4.6-py3-none-any.whl

---
+++ packages/precli/precli-0.4.6-py3-none-any.whl/precli/parsers/java.py
@@ -0,0 +1,10 @@
+# Copyright 2024 Secure Saurce LLC
+from precli.parsers import Parser
+
+
+class Java(Parser):
+    def __init__(self, enabled: list = None, disabled: list = None):
+        super().__init__("java", enabled, disabled)
+
+    def file_extensions(self) -> list[str]:
+        return [".java"]
