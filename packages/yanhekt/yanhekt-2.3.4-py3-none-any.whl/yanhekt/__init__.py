3678d36cd65f729a1201dc1036ad1266a3672499
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-04 04:04:59 +0000

    Add package yanhekt/2.3.4/yanhekt-2.3.4-py3-none-any.whl

---
+++ packages/yanhekt/yanhekt-2.3.4-py3-none-any.whl/yanhekt/__init__.py
@@ -0,0 +1,10 @@
+'''
+Project      : 
+FilePath     : \PypiPkg\src\yanhekt\__init__.py
+Descripttion : 
+Author       : GDDG08
+Date         : 2024-04-02 17:54:53
+LastEditors  : GDDG08
+LastEditTime : 2024-04-02 17:54:59
+'''
+from .yanhekt import YanHeKT
