6cfb6c602d82507935f10b7581752f75a3f8c4a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-04 04:05:45 +0000

    Add package oak-cli/0.2.8/oak_cli-0.2.8-py3-none-any.whl

---
+++ packages/oak-cli/oak_cli-0.2.8-py3-none-any.whl/oak_cli/commands/plugins/flops/main.py
@@ -0,0 +1,29 @@
+import json
+import os
+import pathlib
+
+import oak_cli.utils.api.custom_requests as custom_requests
+from oak_cli.utils.api.custom_http import HttpMethod
+
+ROOT_FL_MANAGER_URL = f"http://{os.environ.get('SYSTEM_MANAGER_URL')}:5072"
+
+
+def create_new_fl_service() -> None:
+    sla_file_name = "flops.SLA.json"
+    current_file_path = pathlib.Path(__file__).resolve()
+    sla_file_path = current_file_path.parent / sla_file_name
+    with open(sla_file_path, "r") as f:
+        SLA = json.load(f)
+
+    custom_requests.CustomRequest(
+        custom_requests.RequestCore(
+            http_method=HttpMethod.POST,
+            base_url=ROOT_FL_MANAGER_URL,
+            api_endpoint="/api/flops",
+            data=SLA,
+        ),
+        custom_requests.RequestAuxiliaries(
+            what_should_happen="Init new FLOps processes",
+            show_msg_on_success=True,
+        ),
+    ).execute()
