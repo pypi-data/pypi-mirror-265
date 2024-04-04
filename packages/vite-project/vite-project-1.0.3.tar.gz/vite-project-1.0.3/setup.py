b1ab806e96f1fee9a10d4d67cfbd77ef26226d8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-04 03:05:22 +0000

    Add package vite-project/1.0.3/vite-project-1.0.3.tar.gz

---
+++ packages/vite-project/vite-project-1.0.3.tar.gz/vite-project-1.0.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+from pathlib import Path
+
+cwd = Path(__file__).parent
+long_description = (cwd / "README.md").read_text()
+setup(
+  name="vite-project",
+  version="1.0.3",
+  package_dir={"react_app": "dist"},
+  package_data={"react_app": ["**/*.*"]},
+  long_description=long_description,
+  long_description_content_type="text/markdown"
+)
