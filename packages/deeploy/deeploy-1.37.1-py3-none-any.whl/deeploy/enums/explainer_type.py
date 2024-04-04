dc1041641a0c2852f2e76c59335a891d30befceb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-04 04:05:42 +0000

    Add package deeploy/1.37.1/deeploy-1.37.1-py3-none-any.whl

---
+++ packages/deeploy/deeploy-1.37.1-py3-none-any.whl/deeploy/enums/explainer_type.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+
+class ExplainerType(Enum):
+    """Class that contains explainer types"""
+
+    NO_EXPLAINER = 0
+    ANCHOR_TABULAR = 1
+    ANCHOR_IMAGES = 2
+    ANCHOR_TEXT = 3
+    SHAP_KERNEL = 4
+    PDP_TABULAR = 5
+    MACE_TABULAR = 6
+    CUSTOM = 7
