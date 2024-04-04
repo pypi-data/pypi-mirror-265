735287dc3494ffa10dad5b81d3c6a05e0ff0fc4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-04 06:06:08 +0000

    Add package plonecli/1.1.1/plonecli-1.1.1.tar.gz

---
+++ packages/plonecli/plonecli-1.1.1.tar.gz/plonecli-1.1.1/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+from __future__ import absolute_import
+from setuptools import find_packages
+from setuptools import setup
+
+
+test_requirements = [
+    "pytest",
+]
+
+setup(
+    # metadata see setup.cfg
+    packages=find_packages(include=["plonecli"]),
+    entry_points={
+        "console_scripts": [
+            "plonecli=plonecli.cli:cli",
+        ],
+    },
+    include_package_data=True,
+    install_requires=[
+        "setuptools",
+        "virtualenv",
+        "Click>=7.0",
+        "mr.bob<1.0.0",
+        "regex==2020.11.13",
+        "zest.releaser",
+        "bobtemplates.plone>=4.0.3",
+    ],
+    extras_require={
+        "test": test_requirements,
+        "dev": [
+            "tox",
+            "zest.releaser[recommended]",
+        ],
+    },
+    zip_safe=False,
+    keywords="plonecli",
+    scripts=["plonecli_autocomplete.sh"],
+    test_suite="tests",
+    tests_require=test_requirements,
+    setup_requires=["pytest-runner"],
+)
