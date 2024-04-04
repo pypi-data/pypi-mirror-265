7a3084ebbc45500ff2b17aef6775b73dfd788580
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-04 05:05:52 +0000

    Add package utils-nuuuwan/2.0.9/utils_nuuuwan-2.0.9-py3-none-any.whl

---
+++ packages/utils-nuuuwan/utils_nuuuwan-2.0.9-py3-none-any.whl/utils/__init__.py
@@ -0,0 +1,11 @@
+
+from utils_ai import AI
+from utils_base import (COLOR_BACKGROUND, COLOR_FOREGROUND, COLOR_FORMAT, TimeZoneOffset, Console, CSVFile,
+                        Dict, Directory, File, FiledVariable, Image, JSONFile,
+                        LatLng, LatLngLK, List, Log, PDFFile, String, Time,
+                        TimeDelta, TimeFormat, TimeUnit, TSVFile, XSVFile, Zip,
+                        _, Hash, Parallel, XMLElement, XMLUtils)
+from utils_git import Git
+from utils_lang import TTS, Language, Translator
+from utils_twitter import Tweet, Twitter
+from utils_www import WWW
