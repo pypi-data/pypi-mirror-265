939eb681c16afaf333cc5846c11924858363b5b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-04 06:05:45 +0000

    Add package quicklooks/0.0.5/quicklooks-0.0.5.tar.gz

---
+++ packages/quicklooks/quicklooks-0.0.5.tar.gz/quicklooks-0.0.5/quicklooks/__init__.py
@@ -0,0 +1,28 @@
+'''
+PLACEHOLDER FOR INSTRUCTIONS
+'''
+
+# ---- chart skeleton
+from .src.chart_skeleton import chart_skeleton
+from .src.cs_attributes import chart_size
+from .src.cs_attributes import chart_xlabel
+from .src.cs_attributes import chart_ylabel
+from .src.cs_attributes import color_libraries
+from .src.cs_attributes import fonts
+
+# ---- plots
+from .src.bar_plot import bar_plot
+from .src.line_plot import line_plot
+from .src.dist_plot import distribution_plot
+from .src.scatter_plot import scatter_plot
+
+# ---- reference items
+from .src.reference_line import reference_line
+from .src.chart_legend import legend
+from .src.text import text
+
+# ---- templates
+from .src.templates import templates
+
+# ---- save function
+from .src.save_chart import save_chart
