b3f1cd72a0f8b007b6212fa9e91b7fe63087b6b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-04 14:05:06 +0000

    Add package ctkchart/2.1.1/ctkchart-2.1.1.tar.gz

---
+++ packages/ctkchart/ctkchart-2.1.1.tar.gz/ctkchart-2.1.1/src/ctkchart/__init__.py
@@ -0,0 +1,16 @@
+from .CTkLineChart import CTkLineChart
+from .CTkLine import CTkLine
+
+ENABLED: str = "enabled"
+DISABLED: str  = "disabled"
+
+NORMAL: str  = "normal"
+DASHED: str  = "dashed"
+DOTTED: str  = "dotted"
+
+TOP: str = "top"
+SIDE: str = "side"
+
+AUTO: str = "auto"
+
+__version__ = "2.1.1"
