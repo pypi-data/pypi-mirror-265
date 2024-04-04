aaf3c91cbfc523ff5bb423da7f98a1059ac805c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-04 05:05:38 +0000

    Add package tstools-ieva/0.1/tstools_ieva-0.1-py3-none-any.whl

---
+++ packages/tstools-ieva/tstools_ieva-0.1-py3-none-any.whl/tstools/vis.py
@@ -0,0 +1,64 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.special import erf
+
+from .moments import get_mean_and_var
+
+
+def plot_trajectory_subset(timeseries, tmin, tmax):
+    """
+    Plot timeseries TIMESERIES between TMIN and TMAX.
+
+    Parameters
+    ----------
+    timeseries: numpy.array
+        the timeseries to plot.
+    tmin: float
+        lower bound of the interval to plot.
+    tmax: float
+        upper bound of the interval to plot.
+
+    Returns
+    -------
+    fig, ax, max_value: matplotlib.figure.Figure, matplotlib.axes.Axes, float
+        Figure and axes objects for the plot, and the maximum value over the trajectory subset.
+    """
+
+    t = timeseries[:, 0]  # Assuming the time values are in the first column of timeseries
+    if tmin < np.amin(t) or tmax > np.amax(t):
+        raise ValueError("Interval is out of bounds")
+
+    values = timeseries[:, 1]
+    indices_within_time_interval = (t >= tmin) & (t <= tmax)
+    subset_values = values[indices_within_time_interval]
+    max_value = np.amax(subset_values)
+
+    fig, ax = plt.subplots()
+    ax.plot(t[indices_within_time_interval], subset_values)
+
+    return fig, ax, max_value
+
+
+def plot_histogram(timeseries, nbins=10):
+    """
+    Plot the histogram for the timeseries TIMESERIES.
+
+    Parameters
+    ----------
+    timeseries: numpy.array
+        the timeseries to plot.
+    nbins: int, optional
+        The number of bins for the histogram.
+    mean: float, optional
+        The empirical mean computed over the timeseries
+    std: float, optional
+        The empirical standard deviation computed over the timeseries
+    """
+
+    hist, bin_edges = np.histogram(timeseries[:, 1], bins=100)
+    bin_centers = 0.5 * (bin_edges[1:] + bin_edges[0:-1])
+
+    fig, ax = plt.subplots()
+    ax.plot("######")
+
+    return fig, ax
