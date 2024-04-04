9872ad383a09fbb2125cdf3df56bd7b99d5ce300
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-04 05:05:38 +0000

    Add package skdir/0.0.4/skdir-0.0.4.tar.gz

---
+++ packages/skdir/skdir-0.0.4.tar.gz/skdir-0.0.4/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.4'
+DESCRIPTION = 'skdir'
+LONG_DESCRIPTION = 'A package to perform arithmetic operations'
+
+# Setting up
+setup(
+    name="skdir",
+    version=VERSION,
+    author="Sk",
+    author_email="karnamsharath3@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['arithmetic', 'math', 'mathematics', 'python tutorial', 'avi upadhyay'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
