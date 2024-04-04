115641be70baaa9c0a1ef4b0c4376025b678cca1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-04 05:06:14 +0000

    Add package communex/0.1.7/communex-0.1.7.tar.gz

---
+++ packages/communex/communex-0.1.7.tar.gz/communex-0.1.7/src/communex/cli/root.py
@@ -0,0 +1,42 @@
+import typer
+from typing import Optional, Annotated
+
+from communex import __version__
+
+from .balance import balance_app
+from .key import key_app
+from .misc import misc_app
+from .module import module_app
+from .network import network_app
+from .subnet import subnet_app
+
+app = typer.Typer()
+
+app.add_typer(key_app, name="key", help="Key operations")
+app.add_typer(balance_app, name="balance", help="Balance operations")
+app.add_typer(misc_app, name="misc", help="Other operations")
+app.add_typer(module_app, name="module", help="Module operations")
+app.add_typer(network_app, name="network", help="Network operations")
+app.add_typer(subnet_app, name="subnet", help="Subnet operations")
+
+
+def _version_callback(value: bool):
+    if value:
+        print(f"CommuneX {__version__}")
+        raise typer.Exit()
+
+
+@app.callback()
+def main(
+    json: Optional[bool] = False,
+    version: Annotated[Optional[bool], typer.Option("--version", callback=_version_callback)] = None,
+):
+    """
+    CommuneX CLI {version}
+
+    This command line interface is under development and subject to change.
+    """
+
+
+if main.__doc__ is not None:
+    main.__doc__ = main.__doc__.format(version=__version__)
