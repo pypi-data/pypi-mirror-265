b2c4f24d037d138b5067d956f8d943f9b70d168f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-04 14:05:27 +0000

    Add package deephaven-plugin-ui/0.11.0/deephaven_plugin_ui-0.11.0-py3-none-any.whl

---
+++ packages/deephaven-plugin-ui/deephaven_plugin_ui-0.11.0-py3-none-any.whl/deephaven/ui/components/spectrum/flex.py
@@ -0,0 +1,42 @@
+from __future__ import annotations
+from typing import Any
+from .basic import spectrum_element
+from .layout import (
+    Direction,
+    Wrap,
+    JustifyContent,
+    AlignContent,
+    AlignItems,
+    DimensionValue,
+)
+
+
+def flex(
+    *children: Any,
+    direction: Direction | None = None,
+    wrap: Wrap | None = None,
+    justify_content: JustifyContent | None = None,
+    align_content: AlignContent | None = None,
+    align_items: AlignItems | None = None,
+    gap: DimensionValue | None = "size-100",
+    column_gap: DimensionValue | None = None,
+    row_gap: DimensionValue | None = None,
+    **props: Any,
+):
+    """
+    Python implementation for the Adobe React Spectrum Flex component.
+    https://react-spectrum.adobe.com/react-spectrum/Flex.html
+    """
+    return spectrum_element(
+        "Flex",
+        *children,
+        direction=direction,
+        wrap=wrap,
+        justify_content=justify_content,
+        align_content=align_content,
+        align_items=align_items,
+        gap=gap,
+        column_gap=column_gap,
+        row_gap=row_gap,
+        **props,
+    )
