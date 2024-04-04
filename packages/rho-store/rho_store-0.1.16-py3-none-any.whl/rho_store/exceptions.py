ce19fab96635f5e7aefa915ee6530ed0ba2bbff4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-04 05:06:04 +0000

    Add package rho-store/0.1.16/rho_store-0.1.16-py3-none-any.whl

---
+++ packages/rho-store/rho_store-0.1.16-py3-none-any.whl/rho_store/exceptions.py
@@ -0,0 +1,20 @@
+
+class RhoStoreError(Exception):
+    """ Base exception """
+    pass
+
+
+class NotFound(Exception):
+    pass
+
+
+class RhoApiError(RhoStoreError):
+    pass
+
+
+class InvalidApiKey(RhoStoreError):
+    pass
+
+
+class FailedToGetData(RhoStoreError):
+    pass
