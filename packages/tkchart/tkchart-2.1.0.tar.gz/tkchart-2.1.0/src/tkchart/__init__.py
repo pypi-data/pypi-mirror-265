88b1b797c5a2cd0cfa733db2e03554054b98be4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-04 14:04:57 +0000

    Add package tkchart/2.1.0/tkchart-2.1.0.tar.gz

---
+++ packages/tkchart/tkchart-2.1.0.tar.gz/tkchart-2.1.0/src/tkchart/__init__.py
@@ -0,0 +1,16 @@
+from .LineChart import LineChart
+from .Line import Line
+
+ENABLED: str = "enabled"
+DISABLED: str  = "disabled"
+
+NORMAL: str  = "normal"
+DASHED: str  = "dashed"
+DOTTED: str  = "dotted"
+
+TOP: str = "top"
+SIDE: str = "side"
+
+AUTO: str = "auto"
+
+__version__ = "2.1.0"
