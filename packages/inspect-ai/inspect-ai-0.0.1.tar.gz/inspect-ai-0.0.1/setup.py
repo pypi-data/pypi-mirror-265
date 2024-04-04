84d105b71d0b8ab90ca8e1a1e158ccd35ff7fdf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-04 05:05:57 +0000

    Add package inspect-ai/0.0.1/inspect-ai-0.0.1.tar.gz

---
+++ packages/inspect-ai/inspect-ai-0.0.1.tar.gz/inspect-ai-0.0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="inspect-ai",
+    version="0.0.1",
+    packages=find_packages(),
+    description="Project under active development. Placeholder description, will fill out soon.",
+)
