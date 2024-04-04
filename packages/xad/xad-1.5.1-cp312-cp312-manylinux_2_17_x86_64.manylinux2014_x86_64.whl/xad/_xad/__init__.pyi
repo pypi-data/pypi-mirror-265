b520ceef35866f6d4fcdecce484975f017c916e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-04 04:05:36 +0000

    Add package xad/1.5.1/xad-1.5.1-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/xad/xad-1.5.1-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/xad/_xad/__init__.pyi
@@ -0,0 +1,7 @@
+from __future__ import annotations
+from . import adj_1st
+from . import exceptions
+from . import fwd_1st
+from . import math
+__all__ = ['adj_1st', 'exceptions', 'fwd_1st', 'math']
+__version__: str = '1.5.1'
