9e302745055fc80915e0edc44154010396f4014e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-04 05:06:57 +0000

    Add package easyted/0.0.2/easyted-0.0.2.tar.gz

---
+++ packages/easyted/easyted-0.0.2.tar.gz/easyted-0.0.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="easyted",  
+    version="0.0.2",
+    author="Lasal Jayawardena",  
+    author_email="lasalcjl@gmail.com", 
+    description="A Python library for easy calculation of tree edit distances with visualization capabilities.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/LasalJayawardena/easyted", 
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'nltk>=3.5',
+        'stanza>=1.2',
+        'apted>=1.0',
+    ],
+)
