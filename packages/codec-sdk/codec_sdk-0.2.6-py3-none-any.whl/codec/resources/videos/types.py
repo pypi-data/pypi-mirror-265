922751e97c1f70e1ca27a8000ea53e7abeb24d76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-04 05:06:09 +0000

    Add package codec-sdk/0.2.6/codec_sdk-0.2.6-py3-none-any.whl

---
+++ packages/codec-sdk/codec_sdk-0.2.6-py3-none-any.whl/codec/resources/videos/types.py
@@ -0,0 +1,26 @@
+from typing import Union, Optional, Any
+from pydantic import BaseModel
+
+
+class Video(BaseModel):
+    uid: str
+    collection: Optional[Union[str, Any]] = None
+    created_at: str
+    video_url: Optional[str] = None
+    duration: int
+    dimensions: list[int]
+    aspect_ratio: str
+    codec: str
+
+
+class VideoStatus(BaseModel):
+    status: str
+    video: str
+
+
+class PreUploadedVideo(BaseModel):
+    video: str
+    signed_upload_url: str
+    token: str
+    path: str
+
